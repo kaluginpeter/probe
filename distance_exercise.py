d0c889f06982f0f76efd01adb4f62a5291a31b08
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-08 12:57:32 +0300

    distance Moscow is done

--- distance_exercise.py
+++ distance_exercise.py
@@ -12,9 +12,17 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке(формула) - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
-distances = {}
+distances = dict()
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
 
+moscow_london = ((moscow[0] - london[0]) ** 2 + (moscow[1] - london[1]) ** 2) ** 0.5
+moscow_paris = ((moscow[0] - paris[0]) ** 2 + (moscow[1] - paris[1]) ** 2) ** 0.5
 
+distances['Moscow'] = {}
+distances['Moscow']['London'] = moscow_london
+distances['Moscow']['Paris'] = moscow_paris
 
 print(distances)
 
