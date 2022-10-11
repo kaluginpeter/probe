d700042e509223eb7f8a112e1d53db74041d84a5
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-11 20:17:14 +0300

    method break

--- learning_circles_for.py
+++ learning_circles_for.py
@@ -18,3 +18,12 @@ for animal in zoo_pets:
     letters_count += len(animal)
     pass
 print('Всего букв', letters_count)
+
+# Способ принудительный останов цикла - break
+zoo_pets = ['lion', 'elephant', 'monkey', 'skunk', 'horse']
+for animal in zoo_pets:
+    print('Сейчас переменная animal указывает на', animal)
+    if animal == 'elephant':
+        print('Нашли слона! :)')
+        break
+    print('Это не слон....')
