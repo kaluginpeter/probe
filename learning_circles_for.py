b29d2ed244a2313d312a48a8e7508ef2d47acfc2
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-11 20:23:40 +0300

    added example with dictionaries

--- learning_circles_for.py
+++ learning_circles_for.py
@@ -87,3 +87,28 @@ for a, b in pair_list:
 triple_list = [(1, 2, 3), (4, 5, 6)]
 for a, b, c in triple_list:
     print(a, b, c)
+
+# Так же разберем пример цикла по словарям
+zoo_pet_mass = {
+    'lion': 300,
+    'skunk': 5,
+    'elephant': 5000,
+    'horse': 400,
+}
+total_mass = 0
+for animal in zoo_pet_mass:
+    print(animal, zoo_pet_mass[animal])
+    total_mass += zoo_pet_mass[animal]
+print('Общая масса животных', total_mass)
+
+total_mass = 0
+for animal, mass in zoo_pet_mass.items():
+    print(animal, mass)
+    total_mass += mass
+print('Общая масса животных', total_mass)
+
+total_mass = 0
+for mass in zoo_pet_mass.values():
+    print(mass)
+    total_mass += mass
+print('Общая масса животных', total_mass)
