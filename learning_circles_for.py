2d596bce04515417470197557a244af41ba684ea
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-11 20:20:37 +0300

    added full example

--- learning_circles_for.py
+++ learning_circles_for.py
@@ -47,3 +47,22 @@ for animal in zoo_pets:
         continue
     print('У нас в руках', animal)
 print('Выход из цикла')
+
+# Для подкрепления информации прикрепляю полный пример
+
+zoo_pets = [
+    'lion', 'monkey', 'skunk',
+    'elephant', 'horse',
+]
+for animal in zoo_pets:
+    if animal == 'skunk':
+        print('Фууу...')
+        continue
+    print('Сейчас переменная animal указывает на', animal)
+    if animal == 'elephant':
+        print('Нашли слона! :)')
+        break
+    print('Это не слон....')
+else:
+    print('Тут слона нет...')
+print('Выход из цикла')
