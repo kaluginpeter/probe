136b02a2e70df0025da19609c80ac224b5cde31a
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-11 20:22:24 +0300

    added example with tuple

--- learning_circles_for.py
+++ learning_circles_for.py
@@ -66,3 +66,24 @@ for animal in zoo_pets:
 else:
     print('Тут слона нет...')
 print('Выход из цикла')
+
+# автоматическая распаковка содержимого списка/тьюпла(он же кортеж) происходит так
+
+a, b = 1, 2
+(a, b) = (1, 2)
+
+for element in [(1, 2), (3, 4)]:
+    a, b = element[0], element[1]
+    print(a + b)
+
+for (a, b) in [(1, 2), (3, 4)]:
+    print(a+b)
+
+pair_list = [(1, 2), (3, 4), (5, 6)]
+
+for a, b in pair_list:
+    print(a+b)
+
+triple_list = [(1, 2, 3), (4, 5, 6)]
+for a, b, c in triple_list:
+    print(a, b, c)
