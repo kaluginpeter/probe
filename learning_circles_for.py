1cb218ab3249cb4005b3cb759dfea74ed77b826d
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-11 20:14:42 +0300

    Simple example circle for

--- learning_circles_for.py
+++ learning_circles_for.py
@@ -4,3 +4,8 @@
 # for <переменная цикла> in <список>:
 #     <блок кода>
 
+# Цикл for ("для каждого элемента")
+zoo_pets = ['lion', 'elephant', 'monkey', 'skunk', 'horse']
+for animal in zoo_pets:
+    print('Сейчас переменная animal указывает на ', animal)
+print('Выход из цикла')
