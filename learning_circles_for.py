15105129dae4601c1524056523cbf2edc93cdc8d
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-11 20:15:52 +0300

    circle for count

--- learning_circles_for.py
+++ learning_circles_for.py
@@ -9,3 +9,12 @@ zoo_pets = ['lion', 'elephant', 'monkey', 'skunk', 'horse']
 for animal in zoo_pets:
     print('Сейчас переменная animal указывает на ', animal)
 print('Выход из цикла')
+
+# Можно рассчитать количество букв
+zoo_pets = ['lion', 'elephant', 'monkey', 'skunk', 'horse']
+letters_count = 0
+for animal in zoo_pets:
+    print('Сейчас переменная animal указывает на', animal)
+    letters_count += len(animal)
+    pass
+print('Всего букв', letters_count)
