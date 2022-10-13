0b94b38f70c505273cddff21ef3d4c3f3d0946b0
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-13 18:29:33 +0300

    added 30 bubbles

--- draw_bubbles.py
+++ draw_bubbles.py
@@ -14,13 +14,14 @@ def bubble(point, step):
         radius += step
         sd.circle(center_position=point, radius=radius)
 # Нарисовать 10 пузырьков в ряд
-# TODO здесь ваш код
 for x in range(100, 1001, 100):
     point = sd.get_point(x, 100)
     bubble(point=point, step=5)
 # Нарисовать три ряда по 10 пузырьков
-# TODO здесь ваш код
-
+for y in range(100, 301, 100):
+    for x in range(100, 1001, 100):
+        point = sd.get_point(x, y)
+        bubble(point=point, step=5)
 # Нарисовать 100 пузырьков в произвольных местах экрана случайными цветами
 # TODO здесь ваш код
 
