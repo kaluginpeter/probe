fd5eed2fb3d8dd9df0cbb201f1ec209e4adcb54b
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-17 18:21:39 +0300

    Added task 5

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -45,3 +45,9 @@ def summation(num):
     for i in range(num+1):
         count += i
     return count
+
+# Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+# Examples (Input -> Output):
+# * "String"      -> "SSttrriinngg"
+# * "Hello World" -> "HHeelllloo  WWoorrlldd"
+# * "1234!_ "     -> "11223344!!__  "
