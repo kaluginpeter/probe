232f5352b6314df00113f1c795d6ba2ffc9a3862
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-17 18:03:13 +0300

    Complete task 3

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -27,3 +27,8 @@ def hero(bullets, dragons):
         return False
 
 # Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+def even_or_odd(number):
+    if number % 2 == 0:
+        return 'Even'
+    else:
+        return 'Odd'
