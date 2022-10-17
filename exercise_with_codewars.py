363ced32c9fbd22efe58ddd45beea647a38e4c46
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-17 17:42:51 +0300

    start make codewars tasks 1

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1,2 +1,8 @@
 # -*- coding: utf-8 -*-
 # Привет это kaluginpeter, сегодня порешаем задачи с Codewars. Let's do it
+
+# Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+# Use conditionals to return the proper message:
+# case	             return
+# name equals owner	 'Hello boss'
+# otherwise	         'Hello guest'
