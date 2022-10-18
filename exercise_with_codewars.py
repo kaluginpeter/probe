c026d9623c79416aba6b0cd86d8ae83c152460c4
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-18 18:08:08 +0300

    Complete task 6

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -59,3 +59,6 @@ def double_char(s):
 # Given an array of integers, return a new array with each value doubled.
 # For example:
 # [1, 2, 3] --> [2, 4, 6]
+def maps(a):
+    double_a = [i*2 for i in a]
+    return double_a
