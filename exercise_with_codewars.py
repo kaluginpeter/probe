084cd20680c051b2fa62dff47760db4b942cd2eb
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-18 18:38:54 +0300

    Added task 7

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -62,3 +62,11 @@ def double_char(s):
 def maps(a):
     double_a = [i*2 for i in a]
     return double_a
+
+# Write a function that takes an array of numbers and returns the sum of the numbers.
+# The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+# Examples
+# Input: [1, 5.2, 4, 0, -1]
+# Output: 9.2
+# Input: []
+# Output: 0
