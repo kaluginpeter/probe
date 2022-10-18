26ea50da275f4ca2d7170439371488e115a4b0f8
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-18 19:19:19 +0300

    Complete task 10

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -93,3 +93,6 @@ def make_upper_case(s):
 
 # Build a function that returns an array of integers from n to 1 where n>0.
 # Example : n=5 --> [5,4,3,2,1]
+def reverse_seq(n):
+    numbers = list(range(n, 0, -1))
+    return numbers
