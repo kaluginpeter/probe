a60110cadcef771f3eb287cdafe57593e92baf59
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-19 21:54:43 +0300

    Complete task 14

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -121,3 +121,9 @@ def make_negative( number ):
 
 # Clock shows h hours, m minutes and s seconds after midnight.
 # Your task is to write a function which returns the time since midnight in milliseconds.
+def past(h, m, s):
+    hour = h * 60 *60 *1000
+    min = m * 60 * 1000
+    sec = s * 1000
+    all = hour + min + sec
+    return all
