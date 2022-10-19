a7cd3c25aa163625f2bb287127b58d1966cd0bc9
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-19 21:16:36 +0300

    Complete task 13

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -116,3 +116,5 @@ def string_to_number(s):
 # make_negative(1);  # return -1
 # make_negative(-5); # return -5
 # make_negative(0);  # return 0
+def make_negative( number ):
+    return -abs(number)
