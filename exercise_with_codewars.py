6c06b2298c4bea5ce129e159b6f6b2c3a960f49f
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-19 21:16:07 +0300

    Added task 13

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -109,3 +109,10 @@ def reverse_words(text):
 # We need a function that can transform a string into a number. What ways of achieving this do you know?
 def string_to_number(s):
     return (int(s))
+
+# In this simple assignment you are given a number and have to make it negative.
+# But maybe the number is already negative?
+# Examples
+# make_negative(1);  # return -1
+# make_negative(-5); # return -5
+# make_negative(0);  # return 0
