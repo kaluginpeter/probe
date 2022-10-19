5083d29a79934dff90eee264e17490b9e91a1a4b
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-19 22:24:31 +0300

    Complete task 15

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -133,4 +133,14 @@ def past(h, m, s):
 # if bmi <= 25.0 return "Normal"
 # if bmi <= 30.0 return "Overweight"
 # if bmi > 30 return "Obese"
+def bmi(weight, height):
+    bmi_m = weight / height ** 2
+    if bmi_m <= 18.5:
+        return "Underweight"
+    elif bmi_m <= 25.0:
+        return "Normal"
+    elif bmi_m <= 30.0:
+        return "Overweight"
+    elif bmi_m > 30:
+        return "Obese"
 
