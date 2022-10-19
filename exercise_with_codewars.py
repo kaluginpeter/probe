0abe8b1e1851d0405c71a0994e46568ee03cca28
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-19 22:23:52 +0300

    Added task 15

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -127,3 +127,10 @@ def past(h, m, s):
     sec = s * 1000
     all = hour + min + sec
     return all
+
+# Write function bmi that calculates body mass index.
+# if bmi <= 18.5 return "Underweight"
+# if bmi <= 25.0 return "Normal"
+# if bmi <= 30.0 return "Overweight"
+# if bmi > 30 return "Obese"
+
