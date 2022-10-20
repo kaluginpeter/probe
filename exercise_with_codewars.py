6186034ddb1a45dc9d2c77e6b5b130e6654a7b66
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-20 16:52:33 +0300

    Complete task 19

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -175,3 +175,15 @@ def get_count(sentence):
 # Return an array, where the first element is the count of positives numbers
 # and the second element is sum of negative numbers. 0 is neither positive nor negative.
 # If the input is an empty array or is null, return an empty array.
+def count_positives_sum_negatives(arr):
+    count = 0
+    count1 = 0
+    if not arr:
+        return arr
+    else:
+        for i in arr:
+            if i > 0:
+                count += 1
+            elif i < 0:
+                count1+= i
+        return [count, count1]
