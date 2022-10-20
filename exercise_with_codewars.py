33337eec6864d4d77f6249d737938d1e2b6b057f
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-20 16:29:43 +0300

    Added task 18

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -159,3 +159,7 @@ def simple_multiplication(number) :
         return number * 8
     else:
         return number * 9
+
+# Return the number (count) of vowels in the given string.
+# We will consider a, e, i, o, u as vowels for this Kata (but not y).
+# The input string will only consist of lower case letters and/or spaces.
