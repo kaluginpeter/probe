ba9683e6a17b33c4f4ef3b6acc9ee4f698914b6e
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-20 16:07:39 +0300

    Complete task 16

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -145,3 +145,10 @@ def bmi(weight, height):
         return "Obese"
 
 # You get an array of numbers, return the sum of all of the positives ones.
+def positive_sum(arr):
+    # Your code here
+    count = 0
+    for i in arr:
+        if i > 0:
+            count += i
+    return count
