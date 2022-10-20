f21b61abf24abded36d536bfef3147ba5f2b80bb
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-20 17:15:31 +0300

    Added task 20

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -187,3 +187,8 @@ def count_positives_sum_negatives(arr):
             elif i < 0:
                 count1+= i
         return [count, count1]
+
+# Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+# The output should be two capital letters with a dot separating them.
+# It should look like this:
+# Sam Harris => S.H
