d83e6d334cd450cdc45a7e32ef90ddaf8fd4f650
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-20 16:30:16 +0300

    Complete task 18

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -163,3 +163,10 @@ def simple_multiplication(number) :
 # Return the number (count) of vowels in the given string.
 # We will consider a, e, i, o, u as vowels for this Kata (but not y).
 # The input string will only consist of lower case letters and/or spaces.
+def get_count(sentence):
+    gl = ['a', 'e', 'i', 'o', 'u']
+    count = 0
+    for x in sentence:
+        if x in gl:
+             count += 1
+    return count
