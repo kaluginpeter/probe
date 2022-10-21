cfb96cd45b1bcd6c6f9d2312e693f3fb91578803
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-21 20:18:51 +0300

    Added task 25

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -217,3 +217,7 @@ import math
 def litres(time):
     count = time / 2
     return math.floor(count)
+
+# Given a list of integers, determine whether the sum of its elements is odd or even.
+# Give your answer as a string matching "odd" or "even".
+# If the input array is empty consider it as: [0] (array with a zero).
