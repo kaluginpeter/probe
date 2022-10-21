ff19f9d1bc2b2d9e666ee9e9588372dfcc82db6e
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-21 20:14:17 +0300

    Complete task 24

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -213,3 +213,7 @@ def repeat_str(repeat, string):
 # Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
 # You get given the time in hours and you need to return the number of litres Nathan will drink,
 # rounded to the smallest value.
+import math
+def litres(time):
+    count = time / 2
+    return math.floor(count)
