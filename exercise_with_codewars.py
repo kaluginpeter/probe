0b2082e59fbd1ce406e58fc6587b52d9b135376a
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-21 20:00:05 +0300

    Complete task 22

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -200,3 +200,6 @@ def to_jaden_case(string):
     return " ".join(w.capitalize() for w in string.split())
 
 # Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+def digitize(n):
+    sec_a = [int(a) for a in str(n)]
+    return list(reversed(sec_a))
