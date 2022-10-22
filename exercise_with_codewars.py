9c78a4eb1cfb4fdcf4ecd69117036fd7d9bcb2de
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-22 22:50:27 +0300

    Added task 27

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -233,3 +233,12 @@ def odd_or_even(arr):
 # В этой задаче у меня получилось прибегнуть к хитрости
 def find_smallest_int(arr):
     return min(arr)
+
+There is a bus moving in the city, and it takes and drop some people in each bus stop.
+
+# You are provided with a list (or array) of integer pairs.
+# Elements of each pair represent number of people get into bus (The first item)
+# and number of people get off the bus (The second item) in a bus stop.
+# Your task is to return number of people who are still in the bus after the last bus station (after the last array).
+# Even though it is the last bus stop,
+# the bus is not empty and some people are still in the bus, and they are probably sleeping there
