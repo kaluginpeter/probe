ccaba746bfd67137d564acaafd1086cd2d62289a
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-23 18:42:04 +0300

    Complete task 33

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -287,3 +287,8 @@ def first_non_consecutive(arr):
             return j
 
 # Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+def bool_to_word(boolean):
+    if boolean:
+        return 'Yes'
+    else:
+        return 'No'
