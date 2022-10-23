ad6e255e4ad4a57f6e6da629b712b9f640c23c40
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-23 19:26:57 +0300

    Complete task 34

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -294,3 +294,5 @@ def bool_to_word(boolean):
         return 'No'
 
 # Complete the solution so that the function will break up camel casing, using a space between words.
+def solution(s):
+    return ''.join(' ' + char if char.isupper() else char.strip() for char in s).strip()
