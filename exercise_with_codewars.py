f90a19e4e8c037f590d314ae6a061f27bef37f8f
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-23 18:29:27 +0300

    Complete task 31

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -273,3 +273,8 @@ def sum_two_smallest_numbers(numbers):
 
 # You are given an odd-length array of integers, in which all of them are the same, except for one single number.
 # Complete the method which accepts such an array, and returns that single different number.
+def stray(arr):
+    for i in arr:
+        if arr.count(i)==1:
+            elem = i
+    return elem
