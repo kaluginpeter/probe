bcd2d10c47c7e6f5b6090eb059b1056fd1b08c4d
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-24 20:34:04 +0300

    Added task 39

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -326,3 +326,7 @@ def duplicate_count(text):
 # Write a function that returns both the minimum and maximum number of the given list/array.
 def min_max(lst):
     return [min(lst), max(lst)]
+
+# Write a function findNeedle() that takes an array full of junk but containing one "needle"
+# After your function finds the needle it should return a message (as a string) that says:
+# "found the needle at position " plus the index it found the needle
