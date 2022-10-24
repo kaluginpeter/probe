6cc613b14b4e90491d5b926dd9aa5809be935db1
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-24 20:40:58 +0300

    Complete task 40

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -340,3 +340,11 @@ def find_needle(haystack):
 # Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total.
 # Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
 # Write a code that gives out the total amount for different days(d).
+def rental_car_cost(d):
+    cost = 40
+    if d <4:
+        return d * cost
+    elif 4 <= d < 7:
+        return d * cost - 20
+    elif d >= 7:
+        return d * cost - 50
