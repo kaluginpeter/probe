8749480a5420bd1d3ac9d431eaa34c8dad10abab
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-24 20:08:06 +0300

    Complete task 37

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -315,3 +315,10 @@ def xo(s):
 # Write a function that will return the count of distinct case-insensitive alphabetic characters
 # and numeric digits that occur more than once in the input string.
 # The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+def duplicate_count(text):
+    text = text.lower()
+    count = 0
+    for x in set(text):
+        if text.count(x) > 1:
+            count += 1
+    return count
