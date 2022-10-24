c7dae2682c2918e344e93c9cd2adbfa37ee2b7e9
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-24 20:17:22 +0300

    Complete task 38

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -324,3 +324,5 @@ def duplicate_count(text):
     return count
 
 # Write a function that returns both the minimum and maximum number of the given list/array.
+def min_max(lst):
+    return [min(lst), max(lst)]
