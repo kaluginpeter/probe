da193846b2723ddd9ad52a7f5bad7e6ffb6ebbb0
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-24 20:34:28 +0300

    Complete task 39

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -330,3 +330,5 @@ def min_max(lst):
 # Write a function findNeedle() that takes an array full of junk but containing one "needle"
 # After your function finds the needle it should return a message (as a string) that says:
 # "found the needle at position " plus the index it found the needle
+def find_needle(haystack):
+    if 'needle' in haystack: return(f"found the needle at position {haystack.index('needle')}")
