9977cb7182599e0ef8278f9e8feb122514e3ee3a
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-25 18:08:53 +0300

    Complete task 45

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -385,3 +385,11 @@ def hoop_count(n):
 # But you're an ambitious person. You want to know if you're better than the average student in your class.
 # You receive an array with your peers' test scores. Now calculate the average and compare your score!
 # Return True if you're better, else False!
+from statistics import mean
+def better_than_average(class_points, your_points):
+    class_points.insert(0, your_points)
+    avr = mean(class_points)
+    if your_points > avr:
+        return True
+    else:
+        return False
