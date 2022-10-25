edaf1048f1bf611683cc3785f134dcc1479a4bc9
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-25 18:00:05 +0300

    Added task 44

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -370,3 +370,8 @@ def validate_pin(pin):
 def remove_every_other(my_list):
     del my_list[1::2]
     return my_list
+
+# Write a program where Alex can input (n) how many times the hoop goes round
+# and it will return him an encouraging message :)
+# If Alex gets 10 or more hoops, return the string "Great, now move on to tricks".
+# If he doesn't get 10 hoops, return the string "Keep at it until you get it".
