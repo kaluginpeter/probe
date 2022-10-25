c567ce6be24aafb3cc22b814efc3ddfc688af8a8
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-25 17:55:37 +0300

    Complete task 43

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -367,3 +367,6 @@ def validate_pin(pin):
 
 # Take an array and remove every second element from the array.
 # Always keep the first element and start removing with the next element.
+def remove_every_other(my_list):
+    del my_list[1::2]
+    return my_list
