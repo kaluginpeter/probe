4ebe0c032df801233960884758e4d845b013b0d1
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-25 17:49:06 +0300

    Complete task 42

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -359,3 +359,8 @@ def cockroach_speed(s):
 # ATM machines allow 4 or 6 digit PIN codes
 # and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 # If the function is passed a valid PIN string, return true, else return false.
+def validate_pin(pin):
+    if (len(pin) == 4 or len(pin) == 6) and pin.isdigit():
+        return True
+    else:
+        return False
