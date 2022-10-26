0146f26ff7894139fbe084cbe15bcad6d922cc67
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-26 21:11:51 +0300

    Complete task 50

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -439,4 +439,6 @@ def remove_char(s):
     return s[1:-1]
 
 # Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+def remove_exclamation_marks(s):
+    return ''.join( word for word in s if word not in '!')
 
