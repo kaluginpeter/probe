38cde6eb589a5dc5668a21909dbdcb1f31248cb9
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-26 21:08:32 +0300

    Complete task 46

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -398,3 +398,10 @@ def better_than_average(class_points, your_points):
 # You need a function to handle each change from green, to yellow, to red, and then to green again.
 # Complete the function that takes a string as an argument representing the current
 # state of the light and returns a string representing the state the light should change to.
+def update_light(current):
+    if current == 'green':
+        return 'yellow'
+    elif current == 'yellow':
+        return 'red'
+    elif current == 'red':
+        return 'green'
