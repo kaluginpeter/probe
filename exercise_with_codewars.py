b90a40595ed26aadbd5ffa8ab6aed2e45bd541d9
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-26 21:11:08 +0300

    Complete task 49

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -435,3 +435,6 @@ def quarter_of(month):
 #  Your goal is to create a function that removes the first and last characters of a string.
 #  You're given one parameter, the original string.
 #  You don't have to worry with strings with less than two characters.
+def remove_char(s):
+    return s[1:-1]
+
