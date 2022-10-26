f4f79a180a63e3ba335c6180688fddb36a0b8943
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-26 21:08:11 +0300

    Added task 46

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -393,3 +393,8 @@ def better_than_average(class_points, your_points):
         return True
     else:
         return False
+
+# You're writing code to control your town's traffic lights.
+# You need a function to handle each change from green, to yellow, to red, and then to green again.
+# Complete the function that takes a string as an argument representing the current
+# state of the light and returns a string representing the state the light should change to.
