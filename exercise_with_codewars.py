8d563448b1348f087c213dc47a85867f6792953d
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-26 21:09:55 +0300

    Added task 48

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -415,3 +415,5 @@ def set_alarm(employed, vacation):
         return True
     else:
         return False
+
+# Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
