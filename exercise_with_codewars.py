2de6934ddbe7df40cc3876c921e39d414448a0f9
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-27 18:02:50 +0300

    Added task 52

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -446,3 +446,8 @@ def remove_exclamation_marks(s):
 def opposite(number):
     return number * -1
 
+# You will be given an array a and a value x.
+# All you need to do is check whether the provided array contains the value
+# Array can contain numbers or strings. X can be either.
+# Return true if the array contains the value, false if not.
+
