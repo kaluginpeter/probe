dc8a0acafa22d42b953278a324dfe38f7be84ab7
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-27 18:41:31 +0300

    Added task 55

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -469,3 +469,6 @@ def divisors(n):
         if n % i == 0:
             count+=1
     return count
+
+# Create a function that accepts 2 string arguments
+# and returns an integer of the count of occurrences the 2nd argument is found in the first one.
