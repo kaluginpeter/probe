6337d7553d8302cb44675ca89693788ececaf9be
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-27 18:03:34 +0300

    Complete task 52

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -450,4 +450,6 @@ def opposite(number):
 # All you need to do is check whether the provided array contains the value
 # Array can contain numbers or strings. X can be either.
 # Return true if the array contains the value, false if not.
+def check(seq, elem):
+    return elem in seq
 
