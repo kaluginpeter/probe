ffd1f9a119649e1058dce1db9ce71be363aac83f
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-28 18:44:55 +0300

    Complete making a burger

--- making_a_burger.py
+++ making_a_burger.py
@@ -2,3 +2,22 @@
 # Привет это снова kaluginpeter, сегодня я изучал модули и пакеты
 # и этим примером опробуем знания на практике
 # Сделаем бургер:
+import burger_ingredients
+
+burger_ingredients.buns_for_burger()
+
+burger_ingredients.bbq_sauce()
+
+burger_ingredients.salad_leaves()
+
+burger_ingredients.cucumber()
+
+burger_ingredients.tomato()
+
+burger_ingredients.red_onion()
+
+burger_ingredients.beef_patty()
+
+burger_ingredients.cheese()
+
+burger_ingredients.buns_for_burger_final()
