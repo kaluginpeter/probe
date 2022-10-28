9ef2815e8342ba6ae6d27f4a342b083bbea50b0f
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-28 19:19:09 +0300

    Added task 56

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -474,3 +474,9 @@ def divisors(n):
 # and returns an integer of the count of occurrences the 2nd argument is found in the first one.
 def str_count(strng, letter):
     return strng.count(letter)
+
+# A pangram is a sentence that contains every single letter of the alphabet at least once.
+# For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram,
+# because it uses the letters A-Z at least once (case is irrelevant).
+# Given a string, detect whether or not it is a pangram.
+# Return True if it is, False if not. Ignore numbers and punctuation.
