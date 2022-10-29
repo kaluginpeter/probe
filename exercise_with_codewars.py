ce64ab1d6a178f67e15b75f7fc1d6ae2fa9f6a62
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-29 13:43:40 +0300

    Added task 64

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -587,3 +587,7 @@ def find_uniq(arr):
             found.add(a)
     res = list(found)
     return (res[0])
+
+# In this simple exercise, you will create a program that will take two lists of integers, a and b.
+# Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b.
+# You must find the difference of the cuboids' volumes regardless of which is bigger.
