a7edd023fda19cf85444fe454b52dbcc65a70dbd
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-29 12:07:02 +0300

    Complete task 60

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -529,3 +529,25 @@ def paperwork(n, m):
 # Write a 'welcome' function that takes a parameter 'language' (always a string),
 # and returns a greeting - if you have it in your database.
 # It should default to English if the language is not in the database, or in the event of an invalid input.
+def greet(language):
+    base = {'english': 'Welcome',
+    'czech': 'Vitejte',
+    'danish': 'Velkomst',
+    'dutch': 'Welkom',
+    'estonian': 'Tere tulemast',
+    'finnish': 'Tervetuloa',
+    'flemish': 'Welgekomen',
+    'french': 'Bienvenue',
+    'german': 'Willkommen',
+    'irish': 'Failte',
+    'italian': 'Benvenuto',
+    'latvian': 'Gaidits',
+    'lithuanian': 'Laukiamas',
+    'polish': 'Witamy',
+    'spanish': 'Bienvenido',
+    'swedish': 'Valkommen',
+    'welsh': 'Croeso'}
+    if language in base:
+        return base[language]
+    else:
+        return 'Welcome'
