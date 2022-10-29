af0228da89b7a06b003589403376f3083db47c32
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-29 12:06:34 +0300

    Added task 60

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -523,3 +523,9 @@ def paperwork(n, m):
         return 0
     else:
         return n * m
+
+# Think of a way to store the languages as a database (eg an object).
+# The languages are listed below so you can copy and paste!
+# Write a 'welcome' function that takes a parameter 'language' (always a string),
+# and returns a greeting - if you have it in your database.
+# It should default to English if the language is not in the database, or in the event of an invalid input.
