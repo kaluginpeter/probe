aff5ae725731f5589b5fc831a001cb51c3817c4a
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-30 13:33:51 +0300

    Complete task 66

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -602,3 +602,5 @@ def century(year):
     return (math.ceil(year / 100))
 
 # Complete the square sum function so that it squares each number passed into it and then sums the results together.
+def square_sum(numbers):
+    return sum([integer*integer for integer in numbers])
