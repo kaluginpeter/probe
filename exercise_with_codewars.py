102ad89ba5bb8c3283938149bbe7f5206f875175
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-31 18:23:06 +0300

    Added task 73

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -668,3 +668,8 @@ def unique_in_order(iterable):
             new.append(iterable[i])
     new.append(iterable[-1])
     return new
+
+# Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter.
+# Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+# If the parameter is itself not a perfect square then -1 should be returned.
+# You may assume the parameter is non-negative.
