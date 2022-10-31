aab56acaf4f74830734e583857e9baf0716fa4c3
Peter Kalugin <streetworkout200412@gmail.com>
2022-10-31 18:29:13 +0300

    Added task 74

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -679,3 +679,6 @@ def find_next_square(sq):
         return (perfect_sq + 1) ** 2
     else:
         return -1
+
+# Complete the function that takes a non-negative integer n as input,
+# and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
