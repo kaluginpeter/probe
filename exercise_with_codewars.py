e048f1e68e27001bfc1e702bc171ec0692132be7
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-01 20:23:16 +0300

    Complete task 79

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -726,3 +726,6 @@ def to_alternating_case(string):
 
 # Write a function that takes an array of strings as an argument
 # and returns a sorted array containing the same strings, ordered from shortest to longest.
+def sort_by_length(arr):
+    arr.sort(key=len)
+    return sorted(arr, key=len)
