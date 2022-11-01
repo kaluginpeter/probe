65914e6d12c0c3ed13384b81f8debd0c5e948adf
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-01 20:20:57 +0300

    Complete task 77

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -711,3 +711,8 @@ def array_diff(a, b):
 
 # Complete the function that takes two integers (a, b, where a < b)
 # and return an array of all integers between the input parameters, including them.
+def between(a,b):
+    list = []
+    for i in range(a, b+1):
+        list.append(i)
+    return list
