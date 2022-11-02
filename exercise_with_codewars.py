71088b465a62fe811350a670e52521058bbb56f8
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-02 21:57:40 +0300

    Complete task 82

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -757,3 +757,6 @@ def sum_str(a, b):
 # Сalculate how many years ago the father was twice as old as his son
 # (or in how many years he will be twice as old). The answer is always greater or equal to 0,
 # no matter if it was in the past or it is in the future.
+def twice_as_old(dad_years_old, son_years_old):
+    ages = son_years_old * 2
+    return abs(ages - dad_years_old)
