b8cace7d371457cf9900c92765ae62aefae5c1aa
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-02 22:55:19 +0300

    Complete task 84

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -778,3 +778,5 @@ def tower_builder(n_floors):
 # You are given two interior angles (in degrees) of a triangle.
 # Write a function to return the 3rd.
 # Note: only positive integers will be tested.
+def other_angle(a,b):
+    return 180 - a - b
