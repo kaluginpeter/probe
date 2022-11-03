484660176c4d30b97e7c3399bb1ee89454d2c540
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-03 19:22:12 +0300

    Added task 87

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -802,3 +802,7 @@ def accum(s):
         char = i.lower() * count
         word = word + (i.upper() + char + '-')
     return word[:-1]
+
+# You are given an array with positive numbers and a non-negative number N.
+# You should find the N-th power of the element in the array with the index N.
+# If N is outside of the array, then return -1.
