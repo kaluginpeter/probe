c60fb71cac25258857ebbee3c479177c44b9599d
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-03 20:03:14 +0300

    Added task 89

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -819,3 +819,8 @@ def square_digits(num):
         square = int(i) ** 2
         elem += str(square)
     return int(elem)
+
+# The goal of this exercise is to convert a string to a new string where each character
+# in the new string is "(" if that character appears only once in the original string, or ")"
+# if that character appears more than once in the original string.
+# Ignore capitalization when determining if a character is a duplicate.
