d890c70d744d82ced8cda7b0d39407b88228061d
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-04 15:49:36 +0300

    Complete task 91

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -842,3 +842,13 @@ def area_or_perimeter(l , w):
 # Your job is to write a function which increments a string, to create a new string.
 # If the string already ends with a number, the number should be incremented by 1.
 # If the string does not end with a number. the number 1 should be appended to the new string.
+def increment_string(strng):
+    stripped = strng.rstrip('1234567890')
+    ints = strng[len(stripped):]
+    if len(ints) == 0:
+        return strng + '1'
+    else:
+        length_word = len(ints)
+        new_ints = int(ints) + 1
+        new_ints = str(new_ints).zfill(length_word)
+        return stripped + new_ints
