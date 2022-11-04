53166abcf61dbffb9b5e313d1f2a0aaf752beb8a
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-04 14:31:33 +0300

    Added task 90

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -833,3 +833,6 @@ def duplicate_encode(word):
         else:
             new_word += '('
     return new_word
+
+# You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+# If it is a square, return its area. If it is a rectangle, return its perimeter.
