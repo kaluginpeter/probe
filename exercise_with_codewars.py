8a47986668e85ddc266f043c32a2f218d74a441c
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-04 16:38:05 +0300

    Complete task 94

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -869,3 +869,6 @@ def correct(s):
     return s
 
 # Write a function that when given a URL as a string, parses out just the domain name and returns it as a string.
+import re
+def domain_name(url):
+    return url.split("www.")[-1].split("//")[-1].split(".")[0]
