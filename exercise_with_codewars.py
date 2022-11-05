5be5484e6a284b5b3544189613158a1f946ef0e3
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-05 16:14:04 +0300

    Added task 97

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -917,3 +917,6 @@ def is_isogram(string):
         return True
     else:
         return False
+
+# You will be given a number and you will need to return it as a string in Expanded Form. For example:
+# expanded_form(12) # Should return '10 + 2'
