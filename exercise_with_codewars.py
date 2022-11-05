d381a2017ba95f562f56c5072b6c21a1ac17f06c
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-05 16:33:23 +0300

    Added task 99

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -936,3 +936,8 @@ def disemvowel(string_):
         if char in list:
             string_ = string_.replace(char, '')
     return string_
+
+# In DNA strings, symbols "A" and "T" are complements of each other,
+# as "C" and "G". Your function receives one side of the DNA (string, except for Haskell);
+# you need to return the other complementary side.
+# DNA strand is never empty or there is no DNA at all (again, except for Haskell).
