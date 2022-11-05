7f796b09a4a4bce5a42113ac8bf9160c3b37f1b1
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-05 16:33:46 +0300

    Complete task 99

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -941,3 +941,15 @@ def disemvowel(string_):
 # as "C" and "G". Your function receives one side of the DNA (string, except for Haskell);
 # you need to return the other complementary side.
 # DNA strand is never empty or there is no DNA at all (again, except for Haskell).
+def DNA_strand(dna):
+    dnk =''
+    for char in dna:
+        if char == 'A':
+            dnk += 'T'
+        elif char == 'T':
+            dnk += 'A'
+        elif char == 'G':
+            dnk += 'C'
+        elif char == 'C':
+            dnk += 'G'
+    return dnk
