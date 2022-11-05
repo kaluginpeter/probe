c549fd038230bba3ea7896eef065d31e57083018
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-05 16:07:34 +0300

    Complete task 96

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -907,3 +907,13 @@ def divided_by(second_integer):
 # An isogram is a word that has no repeating letters, consecutive or non-consecutive.
 # Implement a function that determines whether a string that contains only letters is an isogram.
 # Assume the empty string is an isogram. Ignore letter case.
+def is_isogram(string):
+    some_string = ''
+    for word in string.lower():
+        if word not in some_string:
+            some_string += word
+    print(some_string)
+    if len(some_string) == len(string):
+        return True
+    else:
+        return False
