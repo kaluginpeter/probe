9b47842e1ce6273a4cee30b526c7e515619c355a
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-06 12:24:42 +0300

    Added task 102

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -966,3 +966,7 @@ def remove_url_anchor(url):
         index = url.index('#')
         return url[:index]
     return url
+
+# You will be given a list of strings. You must sort it alphabetically (case-sensitive,
+# and based on the ASCII values of the chars) and then return the first value.
+# The returned value must be a string, and have "***" between each of its letters.
