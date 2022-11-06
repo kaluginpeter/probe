f636658af69b53ee835b0c38921499627db2302b
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-06 12:25:04 +0300

    Complete task 102

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -970,3 +970,6 @@ def remove_url_anchor(url):
 # You will be given a list of strings. You must sort it alphabetically (case-sensitive,
 # and based on the ASCII values of the chars) and then return the first value.
 # The returned value must be a string, and have "***" between each of its letters.
+def two_sort(array):
+    array.sort()
+    return '***'.join(array[0])
