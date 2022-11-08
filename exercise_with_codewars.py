bf84654431c4472312bcd6599cc53268315368a1
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-08 18:25:59 +0300

    Complete task 113

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1071,3 +1071,8 @@ def solution(string):
 
 # Finish the solution so that it sorts the passed in array of numbers.
 # If the function passes in an empty array or null/nil value then it should return an empty array.
+def solution(nums):
+    if not nums:
+        return []
+    nums.sort()
+    return nums
