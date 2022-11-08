c372ef4d9b2cb7ef32ce44d3101848ce7051e4fb
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-08 18:32:50 +0300

    Complete task 114

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1079,3 +1079,5 @@ def solution(nums):
 
 # Given a set of numbers, return the additive inverse of each.
 # Each positive becomes negatives, and the negatives become positives.
+def invert(lst):
+    return [i * -1 for i in lst]
