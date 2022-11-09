9a0dea1ddbc666cb02a1514259a8c329add4925b
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-09 18:46:17 +0300

    Complete task 116

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1091,3 +1091,5 @@ total_goals = la_liga_goals + champions_league_goals + copa_del_rey_goals
 
 # The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard.
 # It is your job to fix the code and get the program working again!
+def say_hello(name):
+    return 'Hello, ' + name
