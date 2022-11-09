0e905ae46a24ebafbd075e8a688ebc60673a8cc0
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-09 18:52:16 +0300

    Complete task 117

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1097,3 +1097,8 @@ def say_hello(name):
 # Your task is to make function, which returns the sum of a sequence of integers.
 # The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
 # If begin value is greater than the end, function should returns 0
+def sequence_sum(begin_number, end_number, step):
+    sum = 0
+    for i in range(begin_number, end_number + 1, step):
+        sum += i
+    return sum
