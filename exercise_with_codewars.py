ee8c2cd7956ed77c0b61f4d46df38f5c079c0b07
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-10 21:23:29 +0300

    Added task 121

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1125,3 +1125,6 @@ def is_square(n):
 # Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
 def unusual_five():
     return len('     ')
+
+# This function should test if the factor is a factor of base.
+# Return true if it is a factor or false if it is not.
