42fd9d6c621cb1347191e6a55dfe569dd997370f
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-10 21:23:44 +0300

    Complete task 121

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1128,3 +1128,5 @@ def unusual_five():
 
 # This function should test if the factor is a factor of base.
 # Return true if it is a factor or false if it is not.
+def check_for_factor(base, factor):
+    return base % factor == 0
