4937aa401dba4319c020461d607660a2b4ea6a83
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-11 16:20:03 +0300

    Complete task 127

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1186,3 +1186,10 @@ def find_multiples(integer, limit):
 # The main idea is to count all the occurring characters in a string.
 # If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
 # What if the string is empty? Then the result should be empty object literal, {}.
+def count(string):
+    dict = {}
+    count = 0
+    for char in string:
+        count = string.count(char)
+        dict[f"{char}"] = count
+    return dict
