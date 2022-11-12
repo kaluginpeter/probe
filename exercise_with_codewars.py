aac8eb1b7852d6a538853a740bf5d6fef8415ad1
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-12 16:21:37 +0300

    Complete task 132

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1252,3 +1252,8 @@ def sum_array(arr):
 # You must check that all values in the array are below or equal to the limit value.
 # If they are, return true. Else, return false.
 # You can assume all values in the array are numbers.
+def small_enough(array, limit):
+    for elem in array:
+        if elem > limit:
+            return False
+    return True
