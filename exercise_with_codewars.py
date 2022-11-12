1e7ed2c48f799d4ecd4cc8d970ecaad9617331ab
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-12 16:21:14 +0300

    Added task 132

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1247,3 +1247,8 @@ def sum_array(arr):
         return 0
     integer = sum(arr)
     return integer - max(arr) - min(arr)
+
+# You will be given an array and a limit value.
+# You must check that all values in the array are below or equal to the limit value.
+# If they are, return true. Else, return false.
+# You can assume all values in the array are numbers.
