e6ba0d6360f818285f164a602de88e51ae9fee23
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-12 16:46:35 +0300

    Complete task 134

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1269,3 +1269,5 @@ def count_by(x, n):
 
 # I created this function to add five to any number that was passed in to it
 # and return the new value. It doesn't throw any errors but it returns the wrong number.
+def add_five(num):
+    return num + 5
