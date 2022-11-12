af19c13bbab20ca8a021e857b0ce3ecfc9f7a465
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-12 14:51:39 +0300

    Added task 131

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1236,3 +1236,9 @@ def get_winner(ballots):
     for char in elite:
         if list.count(char) > len(ballots) / 2 or list.count(char) == len(ballots):
             return char
+
+# Sum all the numbers of a given array ( cq. list ),
+# except the highest and the lowest element ( by value, not by index! ).
+# The highest or lowest element respectively is a single element at each edge,
+# even if there are more than one with the same value.
+# Mind the input validation.
