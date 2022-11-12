7a5f0cfcc157eec02cf392445ce67ec690c69b1a
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-12 16:39:48 +0300

    Added task 133

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1257,3 +1257,7 @@ def small_enough(array, limit):
         if elem > limit:
             return False
     return True
+
+# Create a function with two arguments that will return an array of the first n multiples of x.
+# Assume both the given number and the number of times to count will be positive numbers greater than 0.
+# Return the results as an array or list ( depending on language ).
