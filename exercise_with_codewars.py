12aced1e98e6b4b25e18e180baac6cd2a22fa4d2
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-12 16:40:08 +0300

    Complete task 133

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1261,3 +1261,8 @@ def small_enough(array, limit):
 # Create a function with two arguments that will return an array of the first n multiples of x.
 # Assume both the given number and the number of times to count will be positive numbers greater than 0.
 # Return the results as an array or list ( depending on language ).
+def count_by(x, n):
+    list = []
+    for i in range(x, x * n + 1, x):
+        list.append(i)
+    return list
