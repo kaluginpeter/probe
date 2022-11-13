d095f0211914271e1e2189610e21fc770a76617d
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-13 18:59:11 +0300

    Complete task 137

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1301,3 +1301,6 @@ def queue_time(customers, n):
 # Find max(abs(length(x) − length(y)))
 # If a1 and/or a2 are empty return -1 in each language
 # except in Haskell (F#) where you will return Nothing (None).
+def mxdiflg(a1, a2):
+    if a1 and a2: return max(abs(len(x) - len(y)) for x in a1 for y in a2)
+    return -1
