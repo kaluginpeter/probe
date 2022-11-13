efc29a1b1565a5ffad3447edd1261b790a62e2c8
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-13 19:40:20 +0300

    Complete task 139

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1320,3 +1320,5 @@ def warn_the_sheep(queue):
 # Note: anagrams are case insensitive
 # Complete the function to return true if the two arguments given are anagrams of each other;
 # return false otherwise.
+def is_anagram(test, original):
+    return set(test.lower()) == set(original.lower()) and len(test) == len(original)
