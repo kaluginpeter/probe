f16b5476f9e2677042d76b02ae549c223dae9b21
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-13 18:24:08 +0300

    Added task 136

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1287,3 +1287,6 @@ def order(sentence):
                 if char == str(i):
                     new_list.append(elem)
     return ' '.join(new_list)
+
+# There is a queue for the self-checkout tills at the supermarket.
+# Your task is write a function to calculate the total time required for all the customers to check out!
