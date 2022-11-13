4844ac1dcf575ada6c0897f4ddbc8d2bae568f1d
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-13 17:59:13 +0300

    Added task 135

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1271,3 +1271,10 @@ def count_by(x, n):
 # and return the new value. It doesn't throw any errors but it returns the wrong number.
 def add_five(num):
     return num + 5
+
+# Your task is to sort a given string.
+# Each word in the string will contain a single number.
+# This number is the position the word should have in the result.
+# Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+# If the input string is empty, return an empty string.
+# The words in the input String will only contain valid consecutive numbers.
