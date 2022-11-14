23c3f1b7486fe0b6a661a46f2aa30d08bb68777e
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-14 19:43:46 +0300

    Added task 142

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1345,3 +1345,10 @@ def rps(p1, p2):
 # Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
 def hello(name=None):
     return ('Hello, World!' if not name else f"Hello, {name.title()}!")
+
+# Timmy & Sarah think they are in love, but around where they live,
+# they will only know once they pick a flower each.
+# If one of the flowers has an even number of petals
+# and the other has an odd number of petals it means they are in love.
+# Write a function that will take the number of petals of each flower
+# and return true if they are in love and false if they aren't.
