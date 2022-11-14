3bb7db3e86ce796161aae4dc7c66e0d323e9ad94
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-14 19:44:26 +0300

    Complete task 142

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1352,3 +1352,5 @@ def hello(name=None):
 # and the other has an odd number of petals it means they are in love.
 # Write a function that will take the number of petals of each flower
 # and return true if they are in love and false if they aren't.
+def lovefunc( flower1, flower2 ):
+    return (flower1 % 2 != 0 and flower2 % 2 == 0) or (flower2 % 2 != 0 and flower1 % 2 == 0)
