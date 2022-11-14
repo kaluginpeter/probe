685f9ec0fd170f718706b22637e8d13f111a3f0b
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-14 20:40:56 +0300

    Complete task 144

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1366,3 +1366,8 @@ def isPerfect(n):
 # Teens under 18 old.
 # Young under 21 old.
 # Adults have 21 or more.
+def people_with_age_drink(age):
+    if age < 14: return 'drink toddy'
+    elif age < 18: return 'drink coke'
+    elif age < 21: return 'drink beer'
+    elif age >= 21: return 'drink whisky'
