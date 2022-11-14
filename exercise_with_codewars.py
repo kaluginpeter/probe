053939b672a9a85e76656c3c5eb2884bad67207e
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-14 19:26:42 +0300

    Complete task 141

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1343,3 +1343,5 @@ def rps(p1, p2):
 # Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World!
 # if name is not given (or passed as an empty String).
 # Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+def hello(name=None):
+    return ('Hello, World!' if not name else f"Hello, {name.title()}!")
