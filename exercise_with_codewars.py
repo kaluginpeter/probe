7c6c96b7e7c31dec986eaee5dbd467d6b0f66103
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-15 18:37:48 +0300

    Added task 147

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1392,3 +1392,7 @@ def bouncing_ball(h, bounce, window):
                 count += 1
         return count
     return -1
+
+# Given a two-dimensional array of integers,
+# return the flattened version of the array with all the integers in the sorted (ascending) order.
+# Example:
