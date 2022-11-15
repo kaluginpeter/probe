dab6fea2d693ebb1f958ee082538d95d1cb83fd5
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-15 21:49:14 +0300

    Complete task 149

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1435,3 +1435,5 @@ def decrypt(encrypted_text, n):
 # You are given two sorted arrays that both only contain integers.
 # Your task is to find a way to merge them into a single one, sorted in asc order.
 # Complete the function mergeArrays(arr1, arr2), where arr1 and arr2 are the original sorted arrays.
+def merge_arrays(arr1, arr2):
+    return sorted(set(arr1 + arr2))
