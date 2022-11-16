afefb2e44afdceba0f83f39e1c7ed1eccaea44e2
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-16 22:11:14 +0300

    Added task 152

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1474,3 +1474,6 @@ def number(lines):
         list.append(f"{i}: {elem}")
         i += 1
     return list
+
+# Simple, given a string of words, return the length of the shortest word(s).
+# String will never be empty and you do not need to account for different data types.
