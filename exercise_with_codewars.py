458e028891e0213cf868b03fea6aa9ae84098b1f
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-16 17:28:01 +0300

    Complete task 151

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1467,3 +1467,10 @@ def title_case(title, minor_words=''):
 # Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
 # Write a function which takes a list of strings and returns each line prepended by the correct number.
 # The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+def number(lines):
+    list = []
+    i = 1
+    for elem in lines:
+        list.append(f"{i}: {elem}")
+        i += 1
+    return list
