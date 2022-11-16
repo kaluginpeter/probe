e7f099b32a20b7e2045139719e31a267a45a20b9
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-16 17:27:42 +0300

    Added task 151

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1463,3 +1463,7 @@ def title_case(title, minor_words=''):
         if elem not in exam:
             list2.append(elem.title())
     return ' '.join(list2)
+
+# Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+# Write a function which takes a list of strings and returns each line prepended by the correct number.
+# The numbering starts at 1. The format is n: string. Notice the colon and space in between.
