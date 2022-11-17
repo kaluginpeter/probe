e530e5533d793bb25b0a856e9f0ceee81a9ed10a
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-17 17:02:57 +0300

    Added task 155

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1489,3 +1489,8 @@ def reverse_letter(string):
 # There will always be only one integer that appears an odd number of times.
 def find_it(seq):
     return min([i for i in seq if seq.count(i) % 2 != 0])
+
+# Define a function that takes an integer argument and returns a logical value true
+# or false depending on if the integer is a prime.
+# Per Wikipedia, a prime number ( or a prime ) is a natural number greater than 1
+# that has no positive divisors other than 1 and itself.
