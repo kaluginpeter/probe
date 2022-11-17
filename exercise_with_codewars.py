75738e8ea2bdb34039aee19854cbdf10b733e97a
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-17 21:08:14 +0300

    Complete task 159

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1566,3 +1566,9 @@ def sum_of_minimums(numbers):
 # "yes, ascending" - if the numbers in the array are sorted in an ascending order
 # "yes, descending" - if the numbers in the array are sorted in a descending order
 # "no" - otherwise
+def is_sorted_and_how(arr):
+    if sorted(arr, key = int) == arr:
+        return 'yes, ascending'
+    elif sorted(arr, key = int, reverse = True) == arr:
+        return 'yes, descending'
+    return 'no'
