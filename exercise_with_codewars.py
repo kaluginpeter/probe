11c0c6252454d5d6e1ac2e8aca9ea38035a7b3df
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-18 18:16:27 +0300

    Added task 163

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1608,3 +1608,11 @@ def choose_best_sum(t, k, ls):
         largest_distance = max([distance for distance in condition if distance <= t])
     return largest_distance
 
+# ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters
+# after it in the alphabet. ROT13 is an example of the Caesar cipher.
+# Create a function that takes a string and returns the string ciphered with Rot13.
+# If there are numbers or special characters included in the string,
+# they should be returned as they are. Only letters from the latin/english alphabet should be shifted,
+# like in the original Rot13 "implementation".
+# Please note that using encode is considered cheating.
+
