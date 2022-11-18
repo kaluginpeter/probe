2ff2dec05567a3202bf464bc08a624c587ffab41
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-18 17:07:55 +0300

    Complete task 161

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1585,3 +1585,6 @@ def min_value(digits):
 # Implement a function that adds two numbers together and returns their sum in binary.
 # The conversion can be done before, or after the addition.
 # The binary number returned should be a string.
+def add_binary(a,b):
+    sum = bin(int(a) + int(b))
+    return ''.join(list(sum)[2:])
