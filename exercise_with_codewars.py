949e8618937666c8f36b1fc6ad8d3b4fb1aaf589
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-19 14:38:53 +0300

    Added task 165

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1631,3 +1631,7 @@ def round_to_next5(n):
         n =  n - n%5 + 5
     return n
 
+# You need to write a function that reverses the words in a given string.
+# A word can also fit an empty string. If this is not clear enough, here are some examples:
+# As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
