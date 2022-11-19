46594f6582d8004ee8d91433aeb71daa8fbaa8ec
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-19 17:09:29 +0300

    Added task 169

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1656,3 +1656,12 @@ def gimme(input_array):
 def open_or_senior(data):
     return ['Senior' if elem[0] >= 55 and elem[1] > 7 else 'Open' for elem in data]
 
+# Alice and Bob were on a holiday. Both of them took many pictures of the places they've been,
+# and now they want to show Charlie their entire collection.
+# However, Charlie doesn't like these sessions, since the motif usually repeats.
+# He isn't fond of seeing the Eiffel tower 40 times.
+# He tells them that he will only sit for the session if they show the same motif at most N times.
+# Luckily, Alice and Bob are able to encode the motif as a number.
+# Can you help them to remove numbers such that their list contains each number only up to N times,
+# without changing the order?
+
