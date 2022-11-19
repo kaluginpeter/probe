9ff1e2d2bdb5e4cbdb3e4edf129ebb168f64a088
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-19 14:48:16 +0300

    Added task 166

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1637,3 +1637,6 @@ def round_to_next5(n):
 def reverse(st):
     return ' '.join(list(st.split())[::-1])
 
+# Write a function named sumDigits which takes a number as input
+# and returns the sum of the absolute value of each of the number's decimal digits.
+
