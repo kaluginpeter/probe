2e89a5086887de82a77b7de18c3592aad0579323
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-19 15:41:49 +0300

    Added task 167

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1642,3 +1642,7 @@ def reverse(st):
 def sumDigits(number):
     return sum(int(d) for d in str(abs(number)))
 
+# As a part of this Kata, you need to create a function that when provided with a triplet,
+# returns the index of the numerical element that lies between the other two elements.
+# The input to the function will be an array of three distinct numbers (Haskell: a tuple).
+
