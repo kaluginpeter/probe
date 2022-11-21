85124d8f7f3ae112ad820d722f0e201b484c2b8e
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-21 20:05:03 +0300

    Complete task 178

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1741,3 +1741,5 @@ def data_reverse(data):
 # Implement a function that accepts 3 integer values a, b, c.
 # The function should return true if a triangle can be built with
 # the sides of given length and false in any other case.
+def is_triangle(a, b, c):
+    return max(a, b ,c) < a + b + c - max(a, b ,c)
