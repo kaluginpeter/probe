0d5aa0442f7e1fd46e230f73ecf6844b2d542b9e
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-22 18:27:33 +0300

    Added task 184

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1795,3 +1795,10 @@ def alphabet_war(fight):
     elif right_count > left_count:
         return 'Right side wins!'
     return "Let's fight again!"
+
+# Several people are standing in a row divided into two teams.
+# The first person goes into team 1, the second goes into team 2, the third goes into team 1, and so on.
+# Task
+# Given an array of positive integers (the weights of the people),
+# return a new array/tuple of two integers, where the first one is the total weight of team 1,
+# and the second one is the total weight of team 2.
