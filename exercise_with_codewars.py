6383f683c4954a66d0ca46119c7e20292fee77ad
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-22 18:21:32 +0300

    Added task 183

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1771,3 +1771,12 @@ def predict_age(age_1, age_2, age_3, age_4, age_5, age_6, age_7, age_8):
 # So given a string "super", we should return a list of [2, 4].
 def vowel_indices(word):
     return [index + 1 for index, elem in enumerate(word) if elem.lower() in 'aeoiuy']
+
+# Introduction
+# There is a war and nobody knows - the alphabet war!
+# There are two groups of hostile letters.
+# The tension between left side letters and right side letters was too high and the war began.
+# Task
+# Write a function that accepts fight string consists of only small letters and return who wins the fight.
+# When the left side wins return Left side wins!, when the right side wins return Right side wins!,
+# in other case return Let's fight again!.
