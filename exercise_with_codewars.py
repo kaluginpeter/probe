17577846997220055330575c5f7d33f3f72c0790
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-23 22:02:04 +0300

    Added task 189

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1840,3 +1840,7 @@ def angle(n):
 # Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
 def binary_array_to_number(arr):
     return int("".join([str(i) for i in arr]), 2)
+
+# Your task is to return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
+# Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.)
+# obtained by continued summation of the natural numbers 1, 2, 3, 4, 5, etc."
