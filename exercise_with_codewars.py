d761b444de2ee4bb744ae53f771d121054c1decb
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-23 21:42:06 +0300

    Complete task 186

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1825,3 +1825,9 @@ def solve(s):
 # The population regularly increases by 2 percent per year and moreover 50 new inhabitants
 # per year come to live in the town. How many years does the town need
 # to see its population greater or equal to p = 1200 inhabitants?
+def nb_year(p0, percent, aug, p):
+    count = 0
+    while p0 < p:
+        p0 += int(p0 * (percent / 100) + aug)
+        count += 1
+    return count
