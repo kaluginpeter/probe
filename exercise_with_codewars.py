1b05a1a3231027fdb241b017b644bc87995bf9f8
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-23 22:02:25 +0300

    Complete task 189

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1844,3 +1844,8 @@ def binary_array_to_number(arr):
 # Your task is to return the sum of Triangular Numbers up-to-and-including the nth Triangular Number.
 # Triangular Number: "any of the series of numbers (1, 3, 6, 10, 15, etc.)
 # obtained by continued summation of the natural numbers 1, 2, 3, 4, 5, etc."
+def sum_triangular_numbers(n):
+    if n < 0:
+        return 0
+    sum = sum_triangular_numbers(n-1)
+    return sum + (n * (n + 1)) / 2
