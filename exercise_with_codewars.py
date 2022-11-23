cb276b8947cca9b908d7a19611237c24dfb6a956
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-23 21:32:32 +0300

    Complete task 185

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1809,3 +1809,14 @@ def row_weights(array):
 # and your task is to convert that string to either lowercase only or uppercase only based on:
 # make as few changes as possible.
 # if the string contains equal number of uppercase and lowercase letters, convert the string to lowercase.
+def solve(s):
+    count_lower: int = 0
+    count_upper: int = 0
+    for i in s:
+        if i.islower():
+            count_lower += 1
+            continue
+        count_upper += 1
+    if count_lower >= count_upper:
+        return s.lower()
+    return s.upper()
