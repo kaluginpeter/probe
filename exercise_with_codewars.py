802aaa8dbb7f1ebb1a5dcaa9534edea2a51f1ee8
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-24 19:37:54 +0300

    Added task 193

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1885,3 +1885,8 @@ def nbMonths(start_price_old, start_price_new, saving_per_month, percent_loss_by
         start_price_old *= ((100 - percent_loss_by_month) / 100)
         start_price_new *= ((100 - percent_loss_by_month) / 100)
     return [months, round(start_price_old + savings - start_price_new)]
+
+# Your car is old, it breaks easily. The shock absorbers are gone
+# and you think it can handle about 15 more bumps before it dies totally.
+# Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n).
+# If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
