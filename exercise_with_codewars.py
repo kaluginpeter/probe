30044ead35610cfc27160917972c7b3d171e8e81
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-24 19:38:12 +0300

    Complete task 193

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1890,3 +1890,5 @@ def nbMonths(start_price_old, start_price_new, saving_per_month, percent_loss_by
 # and you think it can handle about 15 more bumps before it dies totally.
 # Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n).
 # If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
+def bumps(road):
+    return "Woohoo!" if road.count("n") <= 15 else "Car Dead"
