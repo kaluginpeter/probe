87d309838c3251877ce86ed939b2c73a1f9f0062
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-24 18:09:07 +0300

    Added task 190

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1849,3 +1849,7 @@ def sum_triangular_numbers(n):
         return 0
     sum = sum_triangular_numbers(n-1)
     return sum + (n * (n + 1)) / 2
+
+# Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+# we want to find a positive integer k, if it exists, such that the sum of the digits of n taken
+# to the successive powers of p is equal to k * n.
