a10ee6aeb57d4976be7e1d31c6994d05aa049df9
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-24 19:54:09 +0300

    Complete task 194

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1895,3 +1895,6 @@ def bumps(road):
 
 # Find the number with the most digits.
 # If two numbers in the argument array have the same number of digits, return the first one in the array.
+def find_longest(arr):
+    arr_len = [len(str(n)) for n in arr]
+    return arr[arr_len.index(max(arr_len))]
