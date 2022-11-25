c4e9bac6f1f07dd445b53e54a8051c5823093415
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-25 16:43:33 +0300

    Added task 195

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1898,3 +1898,11 @@ def bumps(road):
 def find_longest(arr):
     arr_len = [len(str(n)) for n in arr]
     return arr[arr_len.index(max(arr_len))]
+
+# There are pillars near the road. The distance between the pillars is the same
+# and the width of the pillars is the same. Your function accepts three arguments:
+# number of pillars (≥ 1);
+# distance between pillars (10 - 30 meters);
+# width of the pillar (10 - 50 centimeters).
+# Calculate the distance between the first and the last pillar in centimeters
+# (without the width of the first and last pillar).
