b80429c7f46bcc0f137b53afce9af597b9433050
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-25 17:11:22 +0300

    Added task 200

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1934,3 +1934,7 @@ def main(verb, noun):
 # Code as fast as you can! You need to double the integer and return it.
 def doubleInteger(i):
     return i * 2
+
+# Complete function saleHotdogs/SaleHotDogs/sale_hotdogs, function accept 1 parameters:
+# n, n is the number of customers to buy hotdogs, different numbers have different
+# prices (refer to the following table), return a number that the customer need to pay how much money.
