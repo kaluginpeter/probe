bba5a07ce33232769f26d92038d081d6245440bb
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-25 16:59:24 +0300

    Complete task 197

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1923,3 +1923,5 @@ def add_length(str_):
 # in its original string format, and returns the shortened format.
 # Assume shortenToDate's input will always be a string, e.g. "Friday May 2, 7pm".
 # Assume shortenToDate's output will be the shortened string, e.g., "Friday May 2".
+def shorten_to_date(long_date):
+    return long_date.split(',')[0]
