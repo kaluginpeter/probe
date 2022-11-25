32f5c04e911f38f0356cdb0f5e2d6cf63b56af9a
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-25 17:36:41 +0300

    Complete task 203

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1958,3 +1958,5 @@ def generate_shape(n: int) -> str:
 # Create a function called _if which takes 3 arguments:
 # a boolean value bool and 2 functions (which do not take any parameters): func1 and func2
 # When bool is truth-ish, func1 should be called, otherwise call the func2.
+def _if(bool, func1, func2):
+    return func1() if bool == True else func2()
