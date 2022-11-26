7a1e072e07e83f3738936d31a96aa61880b76a9e
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-26 22:57:42 +0300

    Added task 212

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2022,3 +2022,8 @@ def remove(s):
     while s.endswith('!'):
         s = s[:-1]
     return s
+
+# Write a function that will check if two given characters are the same case.
+# If either of the characters is not a letter, return -1
+# If both characters are the same case, return 1
+# If both characters are letters, but not the same case, return 0
