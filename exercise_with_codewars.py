3c4847166164291a9fb74d968673c4a4915a1368
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-26 23:01:28 +0300

    Added task 213

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2031,3 +2031,5 @@ def same_case(a: str, b: str) -> int:
     if not a.isalpha() or not b.isalpha():
         return -1
     return 1 if a.islower() and b.islower() or a.isupper() and b.isupper() else 0
+
+# Write a function that returns a string in which firstname is swapped with last name.
