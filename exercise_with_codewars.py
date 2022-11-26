c99cefd11cf9270816b291eae4c6a7756fa6143f
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-26 21:59:58 +0300

    Complete task 207

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -1984,3 +1984,5 @@ def is_vow(s):
 
 # In Python, there is a built-in filter function that operates similarly to JS's filter.
 # For more information on how to use this function, visit https://docs.python.org/3/library/functions.html#filter
+def get_even_numbers(arr):
+    return list(filter(lambda x: x % 2 == 0, arr))
