141ce041a714303a09bffbe715de0dae57d72b1d
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-27 13:41:31 +0300

    Complete task 216

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2054,3 +2054,7 @@ def over_the_road(address, n):
 # that checks whether the two arrays have the "same" elements, with the same multiplicities
 # (the multiplicity of a member is the number of times it appears). "Same" means,
 # here, that the elements in b are the elements in a squared, regardless of the order.
+def comp(array1, array2):
+    if array1 == None or array2 == None:
+        return False
+    return sorted(number ** 2 for number in array1) == sorted(array2)
