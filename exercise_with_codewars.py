9d2a580fa63bb504ac581cb830735cb9db1c9f31
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-27 14:11:29 +0300

    Complete task 219

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2073,3 +2073,5 @@ def contamination(text, char):
 
 # Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
 # Assume that the input n will always be a positive integer.
+def sum_cubes(n):
+    return sum(i**3 for i in range(n + 1))
