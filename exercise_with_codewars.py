19a0e490aa3ce0c79219ddfcac7b54746c2ae968
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-27 12:16:57 +0300

    Complete task 215

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2047,3 +2047,5 @@ def cube_checker(volume, side):
 # You've just moved into a perfectly straight street with exactly n identical houses
 # on either side of the road. Naturally, you would like to find out the house number
 # of the people on the other side of the street.
+def over_the_road(address, n):
+    return (n * 2) - address + 1
