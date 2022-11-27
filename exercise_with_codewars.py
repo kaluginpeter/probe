d843355e83025d2a7e2abe300c309a718cb9ba68
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-27 14:05:42 +0300

    Complete task 218

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2068,3 +2068,5 @@ def print_array(arr):
 # If the text or the character are empty, return an empty string.
 # There will never be a case when both are empty as nothing is going on!!
 # Note: The character is a string of length 1 or an empty string.
+def contamination(text, char):
+    return char * len(text) if text and char != '' else ''
