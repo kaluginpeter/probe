6f8a3230906bd85c82581f43a2f79774484aa7d4
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-27 14:19:28 +0300

    Added task 220

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2075,3 +2075,10 @@ def contamination(text, char):
 # Assume that the input n will always be a positive integer.
 def sum_cubes(n):
     return sum(i**3 for i in range(n + 1))
+
+# We need a simple function that determines if a plural is needed or not.
+# It should take a number, and return true if a plural should
+# be used with that number or false if not. This would be useful when printing
+# out a string such as 5 minutes, 14 apples, or 1 sun.
+# You only need to worry about english grammar rules for this kata,
+# where anything that isn't singular (one of something), it is plural (not one of something).
