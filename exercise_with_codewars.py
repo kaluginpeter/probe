6920ff9a957f0ad503e13b041b2439ff19f72d62
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-27 14:05:19 +0300

    Added task 218

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2062,3 +2062,9 @@ def comp(array1, array2):
 # Output: String with comma delimited elements of the array in th same order.
 def print_array(arr):
     return ','.join(str(i) for i in arr)
+
+# An AI has infected a text with a character!!
+# This text is now fully mutated to this character.
+# If the text or the character are empty, return an empty string.
+# There will never be a case when both are empty as nothing is going on!!
+# Note: The character is a string of length 1 or an empty string.
