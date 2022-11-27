19a34ad218fc7162172a9803feadea2aee880f05
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-27 15:44:37 +0300

    Complete task 224

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2108,3 +2108,5 @@ a, b, name = 'dev', 'Lab', 'devLab'
 # Determine the total number of digits in the integer (n>=0) given
 # as input to the function. For example, 9 is a single digit,
 # 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+def digits(n):
+    return len(str(n))
