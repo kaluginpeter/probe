4f9faa2757f78c1236a2a02e80aa4f73807cb2bb
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-27 14:30:51 +0300

    Complete task 222

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2096,3 +2096,8 @@ def check(a, x):
 # If n is an integer, Return a string with dash'-'marks before and after each odd integer,
 # but do not begin or end the string with a dash mark. If n is negative, then the negative sign should be removed.
 # If n is not an integer, return the string "None".
+def dashatize(num):
+    num_str = str(num)
+    for i in ['1','3','5','7','9']:
+        num_str = num_str.replace(i,'-' + i + '-')
+    return num_str.strip('-').replace('--','-')
