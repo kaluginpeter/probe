608c891a6151e01fd313c4386190ed22f83d4750
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-28 18:04:46 +0300

    Complete task 226

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2127,3 +2127,19 @@ class Sleigh(object):
 # Write a function called checkCoupon which verifies that a coupon code is valid and not expired.
 # A coupon is no more valid on the day AFTER the expiration date.
 # All dates will be passed as strings in this format: "MONTH DATE, YEAR".
+from datetime import date
+def check_coupon(entered_code, correct_code, current_date, expiration_date):
+    month = {'January': 1, 'February': 2, 'March': 3, 'April': 4,
+            'May': 5, 'June': 6, 'July': 7, 'August': 8, 'September': 9, 'October': 10,
+            'November': 11, 'December': 12}
+    if entered_code is correct_code:
+        current_date = current_date.replace(',', '')
+        expiration_date = expiration_date.replace(',', '')
+        formatted_current = [month.get(i) if i in month.keys() else int(i) for i in current_date.split()]
+        formatted_expiration = [month.get(i) if i in month.keys() else int(i) for i in expiration_date.split()]
+        to_date_current = date(formatted_current[2], formatted_current[0], formatted_current[1])
+        to_date_formatted_expiration = date(formatted_expiration[2], formatted_expiration[0], formatted_expiration[1])
+        if to_date_current <= to_date_formatted_expiration:
+            return True
+        return False
+    return False
