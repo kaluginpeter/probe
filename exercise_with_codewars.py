c31aafb984de1b5e952b38bcb12e3ba480e174c6
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-28 18:32:40 +0300

    Complete task 230

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2170,3 +2170,8 @@ def include(arr,item):
 # if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
 # if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
 # if it was over 88 you should return "Leo got one already!"
+def leo(oscar):
+    if oscar == 88: return 'Leo finally won the oscar! Leo is happy'
+    elif oscar == 86: return 'Not even for Wolf of wallstreet?!'
+    elif oscar != 88 and oscar != 86 and oscar < 88: return 'When will you give Leo an Oscar?'
+    elif oscar > 88: return 'Leo got one already!'
