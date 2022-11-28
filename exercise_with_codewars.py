4da588f99f01091cea1e1c6abd643f05a88bbc26
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-28 18:39:12 +0300

    Complete task 231

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2178,3 +2178,9 @@ def leo(oscar):
 
 # Given a string s, write a method (function) that will return true if
 # its a valid single integer or floating number or false if its not.
+def isDigit(string):
+    try:
+        float(string)
+        return True
+    except:
+        return False
