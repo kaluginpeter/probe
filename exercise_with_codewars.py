37dc1fc494bd8de82586bd9f6b9d619a8b47a62d
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-28 19:05:37 +0300

    Complete task 234

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2197,3 +2197,5 @@ def greet(name):
     return f'Hello, {name} how are you doing today?'
 
 # Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+def get_size(w,h,d):
+    return [((w * h) + (w * d) + (h * d)) * 2, w * h * d]
