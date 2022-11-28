f94a01e3773b476bd266e1101be4ccf30bebf7ea
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-28 18:32:22 +0300

    Added task 230

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2162,3 +2162,11 @@ def define_suit(card):
 # and returns true if the item belongs to the list, otherwise false.
 def include(arr,item):
     return item in arr
+
+# You have to write a function that describe Leo:
+# def leo(oscar):
+#   pass
+# if oscar was (integer) 88, you have to return "Leo finally won the oscar! Leo is happy".
+# if oscar was 86, you have to return "Not even for Wolf of wallstreet?!"
+# if it was not 88 or 86 (and below 88) you should return "When will you give Leo an Oscar?"
+# if it was over 88 you should return "Leo got one already!"
