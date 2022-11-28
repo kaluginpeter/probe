e6821969879e05bd6f4627ca3cf05bf8c6ea9886
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-28 17:27:18 +0300

    Added task 225

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2110,3 +2110,12 @@ a, b, name = 'dev', 'Lab', 'devLab'
 # 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
 def digits(n):
     return len(str(n))
+
+# Christmas is coming and many people dreamed of having a ride with Santa's sleigh.
+# But, of course, only Santa himself is allowed to use this wonderful transportation.
+# And in order to make sure, that only he can board the sleigh, there's an authentication mechanism.
+# Your task is to implement the authenticate() method of the sleigh, which takes the name of the person,
+# who wants to board the sleigh and a secret password. If, and only if,
+# the name equals "Santa Claus" and the password is "Ho Ho Ho!"
+# (yes, even Santa has a secret password with uppercase and lowercase letters
+# and special characters :D), the return value must be true. Otherwise it should return false.
