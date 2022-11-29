c61cefcdfb3d73f4d04d3cfb5b40d9a0d457e18d
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-29 21:50:43 +0300

    Added task 241

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2260,3 +2260,7 @@ def up_array(arr):
 # For example, the following code will result in an array containing the numbers 0 to 4
 def arr(n=0):
     return list(range(n))
+
+# Time to test your basic knowledge in functions! Return the odds from a list:
+# [1, 2, 3, 4, 5]  -->  [1, 3, 5]
+# [2, 4, 6]        -->  []
