08521fa2cabd5e704ba194a89e2e9c1cac80fdd0
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-29 21:51:06 +0300

    Complete task 241

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2264,3 +2264,4 @@ def arr(n=0):
 # Time to test your basic knowledge in functions! Return the odds from a list:
 # [1, 2, 3, 4, 5]  -->  [1, 3, 5]
 # [2, 4, 6]        -->  []
+odds = lambda list: [i for i in list if i % 2 !=0]
