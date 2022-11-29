5e0f06dab20e1948077cb8dc457c409f7270e45d
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-29 19:49:57 +0300

    Complete task 235

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2202,3 +2202,5 @@ def get_size(w,h,d):
 
 # Write a function which removes from string all non-digit characters
 # and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+def get_number_from_string(string):
+    return int(''.join(list(i for i in string if i in '0123456789')))
