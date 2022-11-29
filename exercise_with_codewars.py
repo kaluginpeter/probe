65b9e75c9d0f21dd637a210c6227b4a098d6b26d
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-29 21:45:45 +0300

    Added task 240

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2254,3 +2254,7 @@ def up_array(arr):
     for char in integer:
         list.append(int(char))
     return list
+
+# We want an array, but not just any old array, an array with contents!
+# Write a function that produces an array with the numbers 0 to N-1 in it.
+# For example, the following code will result in an array containing the numbers 0 to 4
