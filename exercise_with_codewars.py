474f215fbc0006844795634473b4e407123eb2cf
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-30 18:26:20 +0300

    Complete task 251

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2339,3 +2339,5 @@ def sum_mul(n, m):
 # Your job is simple, if x squared is more than 1000,
 # return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
 # Note: Input will either be a positive integer (or a string for untyped languages).
+def apple(x):
+  return "It's hotter than the sun!!" if int(x)**2 > 1000 else "Help yourself to a honeycomb Yorkie for the glovebox."
