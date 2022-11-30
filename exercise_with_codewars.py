b7d8ca0b71f434164ff40d3e4a88f84edd678f6f
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-30 18:19:24 +0300

    Complete task 249

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2326,3 +2326,5 @@ def hex_to_dec(s):
 
 # Create a combat function that takes the player's current health
 # and the amount of damage recieved, and returns the player's new health. Health can't be less than 0.
+def combat(health, damage):
+    return health - damage if health > damage else 0
