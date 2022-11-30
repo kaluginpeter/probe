174122acb234e77de016d6d01599f85d1922f07f
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-30 18:14:45 +0300

    Added task 247

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2314,3 +2314,6 @@ def shark(pontoon_distance, shark_distance, you_speed, shark_speed, dolphin):
 # Get ASCII value of a character.
 def get_ascii(c):
    return ord(c)
+
+# Write a function that takes a single string (word) as argument.
+# The function must return an ordered list containing the indexes of all capital letters in the string.
