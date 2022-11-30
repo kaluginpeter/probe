a7ca011434bb08c97a5db331b9c94b2d99a7fb98
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-30 22:16:47 +0300

    Complete task 254

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2371,3 +2371,8 @@ def dna_to_rna(dna):
 # Find the sum of the odd numbers within an array, after cubing the initial integers.
 # The function should return undefined/None/nil/NULL if any of the values aren't numbers.
 # Note: Booleans should not be considered as numbers.
+def cube_odd(arr):
+    for elem in arr:
+        if type(elem) != int:
+            return None
+    return sum([i**3 for i in arr if i % 2 != 0])
