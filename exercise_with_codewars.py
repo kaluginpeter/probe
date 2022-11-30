97ec269b67504b2df4a3db34845114ab526ef523
Peter Kalugin <streetworkout200412@gmail.com>
2022-11-30 17:43:02 +0300

    Added task 245

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2291,3 +2291,12 @@ def nb_dig(n, d):
 # assigned him the problem of adding up all the whole numbers from 1 through a given number n.
 def f(n):
     return sum(range(1, n + 1)) if type(n) is int and n > 0 else None
+
+# Your friend invites you out to a cool floating pontoon around 1km off the beach.
+# Among other things, the pontoon has a huge slide that drops you out right into the ocean,
+# a small way from a set of stairs used to climb out.
+# As you plunge out of the slide into the water, you see a shark hovering
+# in the darkness under the pontoon... Crap!
+# You need to work out if the shark will get to you before you can get to the pontoon.
+# To make it easier... as you do the mental calculations in the water you either freeze
+# when you realise you are dead, or swim when you realise you can make it!
