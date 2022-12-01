ee87421cc121ba01649d3a8624ac73602d1c5cad
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-01 19:03:00 +0300

    Added task 258

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2397,3 +2397,12 @@ def lowercase_count(strng):
 def men_from_boys(arr):
     return sorted(list(set(list(i for i in arr if i % 2 == 0)))) +\
            sorted(list(set(list(i for i in arr if i % 2 != 0))), reverse = True)
+
+# Step 1: Create a function called encode() to replace all
+# the lowercase vowels in a given string with numbers according to the following pattern:
+# For example, encode("hello") would return "h2ll4".
+# There is no need to worry about uppercase vowels in this kata.
+# Step 2: Now create a function called decode() to turn the numbers
+# back into vowels according to the same pattern shown above.
+# For example, decode("h3 th2r2") would return "hi there".
+# For the sake of simplicity, you can assume that any numbers passed into the function will correspond to vowels.
