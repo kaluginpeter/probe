815fd9e79e562aca1db9ed41cb7c53e8bec809ec
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-01 19:24:47 +0300

    Added task 261

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2425,3 +2425,9 @@ def billboard(name, price=30):
 # Complete the function which converts a binary number (given as a string) to a decimal number.
 def bin_to_decimal(inp):
     return int(inp, 2)
+
+# Given an array/list of integers, find the Nth smallest element in the array.
+# Notes
+# Array/list size is at least 3.
+# Array/list's numbers could be a mixture of positives , negatives and zeros.
+# Repetition in array/list's numbers could occur, so don't remove duplications.
