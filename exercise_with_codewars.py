058a504b67ad096140e89aab67816de33012a2f9
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-01 19:29:45 +0300

    Complete task 262

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2436,3 +2436,5 @@ def nth_smallest(arr, pos):
 
 # Implement String#digit? (in Java StringUtils.isDigit(String)),
 # which should return true if given object is a digit (0-9), false otherwise.
+def is_digit(n):
+    return n in '0123456789' and n != ''
