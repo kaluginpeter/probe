0d313f3bd223f30ffc6eb593d2e5541aaaeab251
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-01 19:49:46 +0300

    Added task 264

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2444,3 +2444,14 @@ def is_digit(n):
 # Return as a number.
 def div_con(x):
     return sum(list(i if type(i) == int else 0 for i in x)) - sum(list(int(i) if type(i) == str else 0 for i in x ))
+
+# A balanced number is a number where the sum of digits to the left of the middle digit(s)
+# and the sum of digits to the right of the middle digit(s) are equal.
+# If the number has an odd number of digits, then there is only one middle digit.
+# (For example, 92645 has one middle digit, 6.) Otherwise, there are two middle digits.
+# (For example, the middle digits of 1301 are 3 and 0.)
+# The middle digit(s) should not be considered when determining whether a number is balanced or not,
+# e.g. 413023 is a balanced number because the left sum and right sum are both 5.
+# The task
+# Given a number, find if it is balanced, and return the string "Balanced" or "Not Balanced" accordingly.
+# The passed number will always be positive.
