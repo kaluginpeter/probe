5b3cb999a108903f07df665334e194c3f55e18b2
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-01 18:53:48 +0300

    Added task 257

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2388,3 +2388,9 @@ def max_diff(lst):
 # Your task is simply to count the total number of lowercase letters in a string.
 def lowercase_count(strng):
     return len(list(i for i in strng if i.islower()))
+
+# Return an array/list where Even numbers come first then odds
+# Since , Men are stronger than Boys , Then Even numbers in ascending order While odds in descending .
+# Array/list size is at least 4 .
+# Array/list numbers could be a mixture of positives , negatives .
+# Have no fear , It is guaranteed that no Zeroes will exists .
