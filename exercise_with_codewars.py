7647139fb96b449be3e66aa6cb2e346b137d0bfc
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-02 21:46:19 +0300

    Added task 272

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2515,3 +2515,7 @@ def multiplication_table(size):
 # Your task is to return the correct string using the Template String Feature.
 def temple_strings(obj, feature):
     return f"{obj} are {feature}"
+
+# Count the number of occurrences of each character
+# and return it as a (list of tuples) in order of appearance. For empty output return (an empty list).
+# Consult the solution set-up for the exact data structure implementation depending on your language.
