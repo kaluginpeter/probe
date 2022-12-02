0ba1fd8b029be293b9f7dfd5d08a58a06c024d74
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-02 21:46:46 +0300

    Complete task 272

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2519,3 +2519,6 @@ def temple_strings(obj, feature):
 # Count the number of occurrences of each character
 # and return it as a (list of tuples) in order of appearance. For empty output return (an empty list).
 # Consult the solution set-up for the exact data structure implementation depending on your language.
+import collections
+def ordered_count(inp):
+    return list(collections.Counter(inp).items())
