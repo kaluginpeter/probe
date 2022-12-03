a314609fd4ce496ab88755f3dac8690178a4e8f4
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-03 23:02:03 +0300

    Complete task 280

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2614,3 +2614,5 @@ def reverse_alternate(s):
 # n is provided, you have NO CONTROL over its value.
 # Your expected output is an array of positive integers from 1 to n (inclusive).
 # Your job is to write an algorithm that gets you from the input to the output.
+def pre_fizz(n):
+    return [i for i in range(1, n + 1)]
