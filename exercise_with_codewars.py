d88f67f25e86f598c79667cb5839478ad2dcbf54
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-03 21:31:58 +0300

    Added task 276

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2542,3 +2542,14 @@ def string_clean(s):
 def max_tri_sum(numbers):
     return sum(list(sorted(set(numbers), reverse = True))[:3])
 
+# The word i18n is a common abbreviation of internationalization
+# in the developer community, used instead of typing the whole word
+# and trying to spell it correctly. Similarly, a11y is an abbreviation of accessibility.
+# Write a function that takes a string and turns any and all "words"
+# (see below) within that string of length 4 or greater into an abbreviation, following these rules:
+# A "word" is a sequence of alphabetical characters. By this definition,
+# any other character like a space or hyphen (eg. "elephant-ride")
+# will split up a series of letters into two words (eg. "elephant" and "ride").
+# The abbreviated version of the word should have the first letter,
+# then the number of removed characters, then the last letter (eg. "elephant ride" => "e6t r2e").
+
