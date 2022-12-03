b64a44b5cc97af4e95e2497b355330ee96655682
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-03 21:00:46 +0300

    Added task 275

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2533,3 +2533,10 @@ def spacify(string):
 # in a lot of numbers at random places in the text.
 def string_clean(s):
     return ''.join('' if ch.isdigit() else ch for ch in s)
+
+# iven an array/list [] of n integers , find maximum triplet sum in the array Without duplications .
+# Notes :
+# Array/list size is at least 3 .
+# Array/list numbers could be a mixture of positives , negatives and zeros .
+# Repetition of numbers in the array/list could occur , So (duplications are not included when summing).
+
