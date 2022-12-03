8313ca97294ddf8b2646b0ea251ccd98f16aa573
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-03 23:24:41 +0300

    Complete task 282

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2624,3 +2624,7 @@ def uni_total(s):
 
 # Complete the function that calculates the area of the red square,
 # when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+from math import pi
+
+def square_area(A):
+    return round((2 * A / pi) ** 2, 2)
