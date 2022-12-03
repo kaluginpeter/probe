dca65da461e1cfaaff69ac7936c7a478bb4b1807
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-03 23:27:43 +0300

    Complete task 283

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2632,3 +2632,5 @@ def square_area(A):
 # Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts
 # a string containing up to 26 unique alphabetical characters, and returns
 # a string containing the same characters in alphabetical order.
+def sort_gift_code(code):
+    return "".join(sorted(code))
