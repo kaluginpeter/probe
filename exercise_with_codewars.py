fdb255685dc963be5a26af97f5298f18e1f109af
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-03 22:59:33 +0300

    Complete task 279

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2601,3 +2601,10 @@ def calculate_age(year_of_birth, current_year):
 # Throw away any leading or trailing whitespace, while ensuring there
 # is exactly one space between each word. Punctuation marks should be
 # treated as if they are a part of the word in this kata.
+def reverse_alternate(s):
+    list = s.split()
+    for elem in list[1::2]:
+        word = elem[::-1]
+        list.insert(list.index(elem), word)
+        list.remove(elem)
+    return ' '.join(list)
