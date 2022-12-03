2b8b8a299e82fb9584791a7d0ace4ebc1e9d8984
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-03 23:33:24 +0300

    Added task 284

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2634,3 +2634,7 @@ def square_area(A):
 # a string containing the same characters in alphabetical order.
 def sort_gift_code(code):
     return "".join(sorted(code))
+
+# Your task is to complete this Class, the Person class has been created.
+# You must fill in the Constructor method to accept a name as string and an age as number,
+# complete the get Info property and getInfo method/Info getter which should return johns age is 34
