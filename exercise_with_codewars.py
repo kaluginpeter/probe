c238b9a01cb96b70f063f91d2be3a48506dfc5a6
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-05 19:15:08 +0300

    Complete task 301

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2805,3 +2805,7 @@ class Ball(object):
 # (The reason you return Zach is because the input is a string)
 # Note: Make sure you return the correct message with correct spaces and punctuation.
 # Please leave feedback for this kata. Cheers!
+def cookie(x):
+    if type(x) is str: return "Who ate the last cookie? It was Zach!"
+    if type(x) is int or type(x) is float: return "Who ate the last cookie? It was Monica!"
+    return "Who ate the last cookie? It was the dog!"
