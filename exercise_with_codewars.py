9d4eb8996e0c827469eaa7a3291e5e498fb31139
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-05 18:46:05 +0300

    Complete task 297

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2754,3 +2754,26 @@ def integrate(coefficient, exponent):
 # Given a lowercase string that has alphabetic characters only and no spaces,
 # return the highest value of consonant substrings. Consonants are any letters of the alphabet except "aeiou".
 # We shall assign the following values: a = 1, b = 2, c = 3, .... z = 26.
+import string
+def solve(s):
+    streakCounter = 0
+    tempCounter = 0
+    streakList = []
+    alphDict = dict(zip(string.ascii_lowercase, [num for num in range(1, 27, 1)]))
+    for i in list(alphDict):
+        if i in list('aeiou'):
+            alphDict.pop(i)
+        else:
+            pass
+    for sym in s:
+        if sym in list(alphDict):
+            streakCounter += 1
+            tempCounter += alphDict[sym]
+        else:
+            if streakCounter > 0:
+                streakList.append(tempCounter)
+                tempCounter = 0
+                streakCounter = 0
+            else:
+                pass
+    return max(streakList)
