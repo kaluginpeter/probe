27718a47a3e335fe3804a4f2cf9bec1c59ee9413
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-05 19:19:58 +0300

    Complete task 302

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2823,3 +2823,5 @@ def cookie(x):
 # In this kata, the function will take a string as its argument,
 # and return a string with every word replaced by the explanation to everything,
 # according to Freud. Note that an empty string, or no arguments, should return an empty string.
+def to_freud(sentence):
+    return ' '.join(['sex'] * len(sentence.split()))
