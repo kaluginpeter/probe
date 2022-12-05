a9ac3c0e15d5554af0f39a805b8b5c8cc925da50
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-05 18:49:20 +0300

    Complete task 298

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2779,3 +2779,5 @@ def solve(s):
     return max(streakList)
 
 # Given a non-negative integer n, write a function to_binary/ToBinary which returns that number in a binary format.
+def to_binary(n):
+    return int(bin(n)[2:])
