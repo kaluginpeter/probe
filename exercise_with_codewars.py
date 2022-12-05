e2a132d3549b2d4a194df1602fb784bd0bfc4d8c
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-05 18:25:22 +0300

    Added task 296

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2742,3 +2742,9 @@ def pairs(ar):
         elif abs(ar[i] - ar[i+1]) == 1:
             counter += 1
     return counte
+
+# Create a function that finds the integral of the expression passed.
+# In order to find the integral all you need to do is add one to the exponent (the second argument),
+# and divide the coefficient (the first argument) by that new number.
+# For example for 3x^2, the integral would be 1x^3: we added 1 to the exponent,
+# and divided the coefficient by that new number).
