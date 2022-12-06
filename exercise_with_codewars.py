507a689a98e40bb08e648cd1974046826e49e07b
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-06 20:47:37 +0300

    Complete task 310

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2880,3 +2880,7 @@ class Ghost(object):
 # You are creating a text-based terminal version of your favorite board game.
 # In the board game, each turn has six steps that must happen in this order:
 # roll the dice, move, combat, get coins, buy more health, and print status.
+from preloaded import *
+health, position, coins = 100, 0, 0
+def main(): pass
+log = 'roll_dice move combat get_coins buy_health print_status'.split()
