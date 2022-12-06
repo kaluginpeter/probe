53e1e46b0cfa11fca3f0cc3b99fd2ec4465856ab
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-06 20:09:21 +0300

    Added task 307

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2856,3 +2856,7 @@ def multiply_all(arr):
     def multiply(num):
         return [num * elem for elem in arr]
     return multiply
+
+# This series of katas will introduce you to basics of doing geometry with computers.
+# Point objects have x and y attributes (X and Y in C#) attributes.
+# Write a function calculating distance between Point a and Point b.
