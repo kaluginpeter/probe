a9d67f37a75fc209aaaffff216addea806d7c479
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-06 19:12:35 +0300

    Added task 305

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2836,3 +2836,10 @@ def solve(arr):
 # Your task is to return a number from a string.
 def filter_string(string):
     return int(''.join(i for i in string if i.isdigit()))
+
+# Given an array/list [] of integers , Find the product of the k maximal numbers.
+# Notes
+# Array/list size is at least 3 .
+# Array/list's numbers Will be mixture of positives , negatives and zeros
+# Repetition of numbers in the array/list could occur.
+
