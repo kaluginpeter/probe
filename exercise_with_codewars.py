a5b1de667b718c7c67fe012ced7d5766ba23bbec
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-06 20:47:16 +0300

    Added task 310

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2876,3 +2876,7 @@ import random
 class Ghost(object):
   def __init__(self):
     self.color = random.choice(["white", "yellow", "purple", "red"])
+
+# You are creating a text-based terminal version of your favorite board game.
+# In the board game, each turn has six steps that must happen in this order:
+# roll the dice, move, combat, get coins, buy more health, and print status.
