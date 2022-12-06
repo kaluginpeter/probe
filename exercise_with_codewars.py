d935078b82d827081985f9d87651590655e82ffb
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-06 20:17:32 +0300

    Complete task 309

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2872,3 +2872,7 @@ def nearest_sq(n):
 # Create a class Ghost
 # Ghost objects are instantiated without any arguments.
 # Ghost objects are given a random color attribute of "white" or "yellow" or "purple" or "red" when instantiated
+import random
+class Ghost(object):
+  def __init__(self):
+    self.color = random.choice(["white", "yellow", "purple", "red"])
