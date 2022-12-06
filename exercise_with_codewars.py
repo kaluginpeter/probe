2742995db8e04b37e49442eee3b78fc7a9f712d4
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-06 21:12:52 +0300

    Added task 312

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2902,3 +2902,13 @@ def contain_all_rots(strng, arr):
         rotate = rotate[1:]
         rotate += first_letter
     return True
+
+# Given a string, turn each character into its ASCII character code
+# and join them together to create a number - let's call this number total1:
+# 'ABC' --> 'A' = 65, 'B' = 66, 'C' = 67 --> 656667
+# Then replace any incidence of the number 7 with the number 1, and call this number 'total2':
+# total1 = 656667
+#               ^
+# total2 = 656661
+#               ^
+# Then return the difference between the sum of the digits in total1 and total2:
