7ce1865e68dc01a5482203eea21ef358818833d2
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-06 20:09:39 +0300

    Complete task 307

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -2860,3 +2860,5 @@ def multiply_all(arr):
 # This series of katas will introduce you to basics of doing geometry with computers.
 # Point objects have x and y attributes (X and Y in C#) attributes.
 # Write a function calculating distance between Point a and Point b.
+def distance_between_points(a, b):
+    return ((b.x - a.x) ** 2 + (b.y - a.y) ** 2) ** 0.5
