75d18a1855d0398a1e83060f502cadcee322d782
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-07 22:57:17 +0300

    Complete task 322

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3039,3 +3039,10 @@ def boredom(staff):
 # Each crew member adds 1.5 units to the ship draft. If after removing the weight
 # of the crew, the draft is still more than 20, then the ship is worth looting.
 # Any ship weighing that much must have a lot of booty!
+class Ship:
+    def __init__(self, draft, crew):
+        self.draft = draft
+        self.crew = crew
+    # Your code here
+    def is_worth_it(self):
+        return self.draft - self.crew * 1.5 > 20
