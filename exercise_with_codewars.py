36d2f3d3b8d3b23d3611d1bad7c9a5f50f08c75f
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-07 22:54:15 +0300

    Complete task 321

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3015,3 +3015,21 @@ def evil(n):
 # You will be provided with an object(staff) containing the staff names as keys,
 # and the department they work in as values.
 # Each department has a different boredom assessment score, as follows:
+def boredom(staff):
+    dict_score = {
+        'accounts': 1,
+        'finance': 2,
+        'canteen': 10,
+        'regulation': 3,
+        'trading': 6,
+        'change': 6,
+        'IS': 8,
+        'retail': 5,
+        'cleaning': 4,
+        'pissing about': 25
+    }
+    boredom_score = 0
+    for v in staff.values():
+        if v in dict_score:
+            boredom_score += dict_score.get(v)
+    return 'kill me now' if boredom_score <= 80 else 'i can handle this' if 80 < boredom_score < 100 else 'party time!!'
