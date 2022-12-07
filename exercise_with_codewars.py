53e88508fee6fbd76f4118a7a4be646d81209787
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-07 22:59:03 +0300

    Added task 323

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3046,3 +3046,12 @@ class Ship:
     # Your code here
     def is_worth_it(self):
         return self.draft - self.crew * 1.5 > 20
+
+# In this kata you will have to write a function that takes litres and price_per_litre (in dollar) as arguments.
+# Purchases of 2 or more litres get a discount of 5 cents per litre, purchases of 4 or more litres get a discount
+# of 10 cents per litre, and so on every two litres,
+# up to a maximum discount of 25 cents per litre.
+# But total discount per litre cannot be more than 25 cents.
+# Return the total cost rounded to 2 decimal places.
+# Also you can guess that there will not be negative or non-numeric inputs.
+# Good Luck!
