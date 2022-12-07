793b7e06ae3485c9a6d62f3a6c66308998c02e90
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-07 22:48:34 +0300

    Complete task 320

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3006,3 +3006,5 @@ def outed(meet, boss):
 # The first few Odious numbers: 1, 2, 4, 7, 8, 11, 13, 14, 16, 19
 # You have to write a function that determine if a number is Evil of Odious,
 # function should return "It's Evil!" in case of evil number and "It's Odious!" in case of odious number.
+def evil(n):
+    return "It's Evil!" if  bin(n).count('1') % 2 == 0 else "It's Odious!"
