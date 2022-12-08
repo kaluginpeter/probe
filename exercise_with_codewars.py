a239ce6e3b333e0a026d40849437fd301b3c9d56
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-08 19:47:46 +0300

    Added task 329

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3092,3 +3092,13 @@ def excluding_vat_price(price):
 # a2 + b2 = c2
 def pythagorean_triple(integers):
     return sorted(integers)[0]**2 + sorted(integers)[1]**2 == sorted(integers)[2]**2
+
+# Some new cashiers started to work at your restaurant.
+# They are good at taking orders, but they don't know how to capitalize words, or use a space bar!
+# All the orders they create look something like this:
+# "milkshakepizzachickenfriescokeburgerpizzasandwichmilkshakepizza"
+# The kitchen staff are threatening to quit, because of how difficult it is to read the orders.
+# Their preference is to get the orders as a nice clean string with spaces and capitals like so:
+# "Burger Fries Chicken Pizza Pizza Pizza Sandwich Milkshake Milkshake Coke"
+# The kitchen staff expect the items to be in the same order as they appear in the menu.
+# The menu items are fairly simple, there is no overlap in the names of the items:
