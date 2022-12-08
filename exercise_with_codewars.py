c6cafb23ae5f160fb6df2c0cf64bffe04c5f07d4
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-08 18:48:29 +0300

    Added task 326

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3072,3 +3072,10 @@ def switcheroo(string):
 # Note that spaces should be ignored in this case.
 def vaporcode(s):
     return '  '.join(i.upper() for i in s.replace(' ', ''))
+
+# Create a method all which takes two params:
+# a sequence
+# a function (function pointer in C)
+# and returns true if the function in the params returns true for every element in the sequence.
+# Otherwise, it should return false. If the sequence is empty,
+# it should return true, since technically nothing failed the test.
