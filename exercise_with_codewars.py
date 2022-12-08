bd7650eb0bee34e25cd4faeff0125bd90973668f
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-08 21:43:52 +0300

    Added task 333

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3144,3 +3144,10 @@ def exponent(a,b):
   return a ** b
 def subt(a,b):
   return a - b
+
+# Some really funny web dev gave you a sequence of numbers from his API response as an sequence of strings!
+# You need to cast the whole array to the correct type.
+# Create the function that takes as a parameter a sequence of numbers represented
+# as strings and outputs a sequence of numbers.
+# ie:["1", "2", "3"] to [1, 2, 3]
+# Note that you can receive floats as well.
