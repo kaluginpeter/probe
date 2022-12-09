a0461d6cf5a806b34147fc6a8b55e4dbc1212a69
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-09 19:36:39 +0300

    Added task 340

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3206,3 +3206,8 @@ def capitalize(s,ind):
 # (CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
 def meeting(s):
     return ''.join(sorted('({1}, {0})'.format(*(x.split(':'))) for x in s.upper().split(';')))
+
+# You probably know the "like" system from Facebook and other pages. People can "like" blog posts,
+# pictures or other items. We want to create the text that should be displayed next to such an item.
+# Implement the function which takes an array containing the names of people that like an item.
+# It must return the display text as shown in the examples:
