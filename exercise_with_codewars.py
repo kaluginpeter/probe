537e8751d58e774fe75b7deb5bf124a7b11f3891
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-09 17:47:05 +0300

    Added task 335

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3158,3 +3158,8 @@ def to_float_array(arr):
 # each digit is repeated a number of times equals to its value.
 def explode(s):
     return ''.join(c * int(c) for c in s)
+
+# Disarium number is the number that The sum of its digits powered
+# with their respective positions is equal to the number itself.
+# Task
+# Given a number, Find if it is Disarium or not .
