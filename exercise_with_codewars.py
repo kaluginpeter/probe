72ade5d8623ea0eb45706ea6a03e208903a13de8
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-09 19:54:08 +0300

    Complete task 342

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3233,3 +3233,5 @@ def calculate(num1, operation, num2):
 
 # Write a method that takes one argument as name
 # and then greets that name, capitalized and ends with an exclamation point.
+def greet(name):
+    return f'Hello {name.title()}!'
