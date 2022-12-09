b49eb596ae67c59d0dd262b68556806122eab16c
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-09 20:02:36 +0300

    Complete task 344

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3249,3 +3249,4 @@ class Hero(object):
 # Add the value "codewars" to the websites array.
 # After your code executes the websites array should == ["codewars"]
 # The websites array has already been defined for you using the following code:
+websites.append("codewars")
