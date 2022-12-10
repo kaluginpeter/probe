b5871ec11cd5c233f51139b61c4ef5883148ba02
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-10 22:28:47 +0300

    Added task 353

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3312,3 +3312,8 @@ def name_value(my_list):
 # from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
 def alternateCase(s):
     return s.swapcase()
+
+# Write a function that accepts two arguments and generates a sequence
+# containing the integers from the first argument to the second inclusive.
+# Input
+# Pair of integers greater than or equal to 0. The second argument will always be greater than or equal to the first.
