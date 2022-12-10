5e9388c8a183bef551c5550367b38f51e240cd44
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-10 22:02:02 +0300

    Complete task 348

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3275,3 +3275,5 @@ def to_leet_speak(str):
 # Given a string, write a function that returns the string
 # with a question mark ("?") appends to the end, unless the original
 # string ends with a question mark, in which case, returns the original string.
+def ensure_question(s):
+    return s if s.endswith('?') else s + '?'
