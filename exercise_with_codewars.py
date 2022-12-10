8ba940c0f521924a94041bed98f7b9e39653db94
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-10 22:25:08 +0300

    Added task 351

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3293,3 +3293,15 @@ def sort_my_string(s):
 # arrays will only contain numbers so don't worry about checking that
 def incrementer(nums):
     return [i+j+1 if i+j+1<10 else int(str(i+j+1)[-1]) for i,j in enumerate(nums)]
+
+# Given a string "abc" and assuming that each letter in the string
+# has a value equal to its position in the alphabet, our string
+# will have a value of 1 + 2 + 3 = 6. This means that: a = 1, b = 2, c = 3 ....z = 26.
+# You will be given a list of strings and your task will be to return the values of the
+# strings as explained above multiplied by the position of that string in the list.
+# For our purpose, position begins with 1.
+# nameValue ["abc","abc abc"] should return [6,24] because of [ 6 * 1, 12 * 2 ]. Note how spaces are ignored.
+# "abc" has a value of 6, while "abc abc" has a value of 12. Now, the value at position 1 is multiplied
+# by 1 while the value at position 2 is multiplied by 2.
+# Input will only contain lowercase characters and spaces.
+# Good luck!
