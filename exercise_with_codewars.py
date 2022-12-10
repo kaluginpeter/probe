58c8502630a7c4e7d939299df598ae0b32fbf425
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-10 22:05:59 +0300

    Added task 349

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3277,3 +3277,6 @@ def to_leet_speak(str):
 # string ends with a question mark, in which case, returns the original string.
 def ensure_question(s):
     return s if s.endswith('?') else s + '?'
+
+# Given a string s. You have to return another string such that
+# even-indexed and odd-indexed characters of s are grouped and groups are space-separated (see sample below)
