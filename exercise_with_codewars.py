3256410096a1cc7f58248cd2d0dff7fc6c6cf32b
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-10 22:27:02 +0300

    Complete task 352

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3310,3 +3310,5 @@ def name_value(my_list):
 
 # Write function alternateCase which switch every letter in string
 # from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+def alternateCase(s):
+    return s.swapcase()
