77cb87f126fca5bbad61c7994102af6e90b4234e
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-10 21:49:42 +0300

    Complete task 346

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3258,3 +3258,6 @@ def unlucky_days(year):
 	return sum(calendar.weekday(year, m, 13) == 4 for m in range(1, 13))
 
 # There are 32 letters in the Polish alphabet: 9 vowels and 23 consonants.
+def correct_polish_letters(st):
+    dict = {'ą':'a', 'ć':'c', 'ę':'e', 'ł':'l', 'ń':'n', 'ó':'o', 'ś':'s', 'ź':'z', 'ż':'z'}
+    return ''.join(dict[i] if i in dict else i for i in st)
