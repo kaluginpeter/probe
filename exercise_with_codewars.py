b54a716329e2f302fec9a77d52ffd36d4af91c97
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-10 21:58:54 +0300

    Complete task 347

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3265,3 +3265,9 @@ def correct_polish_letters(st):
 # Your task is to write a function toLeetSpeak that converts a regular english sentence to Leetspeak.
 #More about LeetSpeak You can read at wiki -> https://en.wikipedia.org/wiki/Leet
 # Consider only uppercase letters (no lowercase letters, no numbers) and spaces.
+def to_leet_speak(str):
+    dict = {'A' : '@',  'B' : '8',  'C' : '(',  'D' : 'D',  'E' : '3',  'F' : 'F',  'G' : '6',
+            'H' : '#',  'I' : '!',  'J' : 'J', 'K' : 'K',  'L' : '1',  'M' : 'M',
+            'N' : 'N',  'O': '0',  'P' : 'P',  'Q' : 'Q',  'R' : 'R',  'S' : '$',
+            'T' : '7',  'U' : 'U',  'V' : 'V',  'W' : 'W',  'X' : 'X',  'Y' : 'Y',  'Z' : '2', ' ': ' '}
+    return ''.join(dict[i] for i in str)
