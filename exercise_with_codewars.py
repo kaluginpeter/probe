955632ff662699bf5887e430bb7817b1350e7592
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-11 13:48:32 +0300

    Added task 362

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3400,3 +3400,8 @@ def quadratic(x1, x2):
 # 12 + 22 + 32 + 42 + 52 + 62 + 72 + 82 + 92 + 102 = 385
 def difference_of_squares(n):
     return sum([i for i in range(1,n+1)])**2 - sum([i**2 for i in range(1, n+1)])
+
+# You are creating an "Escape the room" game.
+# The first step is to create a hash table called rooms that contains
+# all of the rooms of the game. There should be at least 3 rooms inside it,
+# each being a hash table with at least three properties (e.g. name, description, completed).
