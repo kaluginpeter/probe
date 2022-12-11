1af398e4d7a4be65548f15f721d409d1b262d006
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-11 13:56:34 +0300

    Added task 363

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3413,3 +3413,11 @@ rooms = {
     'room3': {
     'name': 'Pasha', 'description': 'golang', 'completed': True }
         }
+
+# This Kata is intended as a small challenge for my students
+# All Star Code Challenge #22
+# Create a function that takes an integer argument of seconds
+# and converts the value into a string describing how many hours and minutes comprise that many seconds.
+# Any remaining seconds left over are ignored.
+# Note:
+# The string output needs to be in the specific form - "X hour(s) and X minute(s)"
