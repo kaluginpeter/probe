7ba5691779376c799b67ec402c4be294ffdae87c
Peter Kalugin <streetworkout200412@gmail.com>
2022-12-11 13:49:11 +0300

    Complete task 362

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3405,3 +3405,11 @@ def difference_of_squares(n):
 # The first step is to create a hash table called rooms that contains
 # all of the rooms of the game. There should be at least 3 rooms inside it,
 # each being a hash table with at least three properties (e.g. name, description, completed).
+rooms = {
+    'room1': {
+    'name': 'Peter', 'description': 'python', 'completed': False },
+    'room2': {
+    'name': 'Ivan', 'description': 'js', 'completed': True },
+    'room3': {
+    'name': 'Pasha', 'description': 'golang', 'completed': True }
+        }
