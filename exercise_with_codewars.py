ad6072057d4a0f7e59f1ef13c083d0d158f48ebc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-12 19:11:13 +0300

    Complete task 368

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3474,3 +3474,5 @@ def compare(s1,s2):
 # being the coefficient, and the second number being the exponent.
 # Your function should multiply the two numbers, and then subtract 1 from the exponent.
 # Then, it has to print out an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+def derive(coefficient, exponent):
+    return f"{coefficient*exponent}x^{exponent-1 if exponent >2 else exponent}"
