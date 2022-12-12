41688640cbca2a6a934d68aecff83935d97f5f07
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-12 19:46:25 +0300

    Added task 371

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3515,3 +3515,8 @@ def longest_palindrome (s):
         return 1
     else:
         return len(lst[-1])
+
+# Given a string, return a new string that has transformed based on the input:
+# Change case of every character, ie. lower case to upper case, upper case to lower case.
+# Reverse the order of words from the input.
+# Note: You will have to handle multiple spaces, and leading/trailing spaces.
