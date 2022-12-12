0c514bece836d7683b89326939ed42ba622135f5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-12 19:10:33 +0300

    Added task 368

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3469,3 +3469,8 @@ def compare(s1,s2):
     if any(x for x in s2 if not x.isalpha()):
         s2 = ''
     return sum(ord(x.upper()) for x in s1) == sum(ord(x.upper()) for x in s2)
+
+# This function takes two numbers as parameters, the first number
+# being the coefficient, and the second number being the exponent.
+# Your function should multiply the two numbers, and then subtract 1 from the exponent.
+# Then, it has to print out an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
