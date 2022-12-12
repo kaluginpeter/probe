a04705d289e46901d0988c9b992ce6bc140000d1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-12 20:01:54 +0300

    Added task 373

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3528,3 +3528,8 @@ def string_transformer(s):
 def solve(a,b):
     return ''.join(i if i not in b else '' for i in a) + ''.join(i if i not in a else '' for i in b)
 #set(a)^(set(b))
+
+# Create a method sayHello/say_hello/SayHello that takes as input a name, city,
+# and state to welcome a person. Note that name will be an array consisting of one
+# or more values that should be joined together with one space between each,
+# and the length of the name array in test cases will vary.
