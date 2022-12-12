920d35469483ac873ca23e0cfbd361f82dcb4c2a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-12 20:13:08 +0300

    Added task 374

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3535,3 +3535,9 @@ def solve(a,b):
 # and the length of the name array in test cases will vary.
 def say_hello(name, city, state):
     return f"Hello, {' '.join(i for i in name)}! Welcome to {city}, {state}!"
+
+# A sequence or a series, in mathematics, is a string of objects, like numbers,
+# that follow a particular pattern. The individual elements in a sequence are called terms.
+# A simple example is 3, 6, 9, 12, 15, 18, 21, ..., where the pattern is: "add 3 to the previous term".
+# In this kata, we will be using a more complicated sequence: 0, 1, 3, 6, 10, 15, 21, 28,
+# ... This sequence is generated with the pattern: "the nth term is the sum of numbers from 0 to n, inclusive".
