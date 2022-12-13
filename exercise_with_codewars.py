462c246ff1e3965a393fb1bd94bb670fa0b7e9e6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-13 15:47:51 +0300

    Complete task 375

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3551,3 +3551,7 @@ def sum_of_n(n):
 # ((P-1)! + 1) / (P * P)
 # should give a whole number.
 # Your task is to create a function that returns true if the given number is a Wilson prime.
+def am_i_wilson(n):
+    list = [5, 13, 563, 5971, 558771, 1964215, 8121909,
+            12326713, 23025711, 26921605, 341569806, 399292158]
+    return n in list
