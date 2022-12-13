bf8d33c51402ebf5f9776ba4a03d69cf0425e5f1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-13 16:37:07 +0300

    Complete task 381

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3604,3 +3604,5 @@ def xor(a,b):
     return a != b
 
 # Create a function args_count, that returns the count of passed arguments
+def args_count(*args, **kwargs):
+    return len(args) + len(kwargs)
