84a84a531dc568b2c5623425504112a6608f0aed
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-13 16:20:27 +0300

    Added task 379

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3582,3 +3582,11 @@ def max_gap(numbers):
 def hydrate(drink_string):
     s = sum([int(i) if i.isdigit() else 0 for i in drink_string])
     return f"{s} {'glass' if s == 1 else 'glasses'} of water"
+
+# Every Friday and Saturday night, farmer counts amount of sheep returned back to his farm
+# (sheep returned on Friday stay and don't leave for a weekend).
+# Sheep return in groups each of the days -> you will be given two arrays
+# with these numbers (one for Friday and one for Saturday night).
+# Entries are always positive ints, higher than zero.
+# Farmer knows the total amount of sheep, this is a third parameter.
+# You need to return the amount of sheep lost (not returned to the farm) after final sheep counting on Saturday.
