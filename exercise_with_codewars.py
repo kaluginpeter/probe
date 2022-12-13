275bb9d78597a01777571e6373c2dfe82f9cf640
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-13 15:47:12 +0300

    Added task 375

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3545,3 +3545,9 @@ def sum_of_n(n):
     if n < 0:
         return sorted([sum(x for x in range(i,1)) for i in range(n, 1)])[::-1]
     return [sum([x for x in range(i+1)]) for i in range(n+1)]
+
+# Wilson primes satisfy the following condition. Let P represent a prime number.
+# Then,
+# ((P-1)! + 1) / (P * P)
+# should give a whole number.
+# Your task is to create a function that returns true if the given number is a Wilson prime.
