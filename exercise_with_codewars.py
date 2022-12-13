959a5c3d19f02bb43a4e3b1c0b34d71531f34484
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-13 15:55:19 +0300

    Complete task 376

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3560,3 +3560,5 @@ def am_i_wilson(n):
 # If a number is even, convert it to binary.
 # If a number is odd, convert it to hex.
 # Numbers will be positive. The hexadecimal string should be lowercased.
+def evens_and_odds(n):
+    return bin(n)[2:] if n%2 == 0 else hex(n)[2:]
