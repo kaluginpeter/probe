97aefdb08cfc80d69c55aa325f344195a6f9c6a0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-13 16:56:38 +0300

    Added task 384

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3617,3 +3617,9 @@ def remove(s):
 # There are N blue beads. After looking at the arrangement below work out the number of red beads.
 def count_red_beads(n):
     return 0 if n < 2 else n*2-2
+
+# A trick I learned in elementary school to determine whether
+# or not a number was divisible by three is to add all of the integers
+# in the number together and to divide the resulting sum by three.
+# If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
+# Given a series of digits as a string, determine if the number represented by the string is divisible by three.
