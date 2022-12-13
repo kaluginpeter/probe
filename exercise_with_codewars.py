c4067ebd93352115df480ad0f5dde146ba24249c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-13 16:40:02 +0300

    Complete task 382

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3610,3 +3610,5 @@ def args_count(*args, **kwargs):
 # Remove all exclamation marks from sentence but ensure a exclamation
 # mark at the end of string. For a beginner kata, you can assume that the
 # input data is always a non empty string, no need to verify it.
+def remove(s):
+    return s.replace("!", "") + "!"
