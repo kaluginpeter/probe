147693eb65c5d5d9edb9212e54a421bb10c8c437
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-13 16:13:01 +0300

    Added task 378

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3574,3 +3574,8 @@ import numpy as np
 def max_gap(numbers):
     numbers = np.array(sorted(numbers))
     return np.max(numbers[1:]-numbers[:-1])
+
+# Welcome to the Codewars Bar!
+# Codewars Bar recommends you drink 1 glass of water per standard drink so you're not hungover tomorrow morning.
+# Your fellow coders have bought you several drinks tonight in the form of a string.
+# Return a string suggesting how many glasses of water you should drink to not be hungover.
