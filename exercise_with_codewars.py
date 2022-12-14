879f90b30e1c474f7abc529f2bdd901adf69f281
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-15 00:05:52 +0300

    Added task 395

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3759,3 +3759,8 @@ def seven_ate9(str_):
             strung_out = strung_out.replace('797','77')
             continue
     return strung_out
+
+# The vowel substrings in the word codewarriors are o,e,a,io. The longest of these has a length of 2. Given a lowercase string that has alphabetic characters only (both vowels and consonants) and no spaces, return the length of the longest vowel substring.
+# Vowels are any of aeiou.
+# Good luck!
+# If you like substring Katas, please try:
