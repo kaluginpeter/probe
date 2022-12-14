926d6f790e978f4afb036ac6a028602e22125d53
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-14 16:59:55 +0300

    Added task 390

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3683,3 +3683,12 @@ def last(lst):
 # Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
 def meeting(rooms):
     return rooms.index("O") if "O" in rooms else "None available!"
+
+# You and a group of friends are earning some extra money in the school holidays
+# by re-painting the numbers on people's letterboxes for a small fee.
+# Since there are 10 of you in the group each person just concentrates on painting one digit!
+# For example, somebody will paint only the 1's, somebody else will paint only the 2's and so on...
+# But at the end of the day you realise not everybody did the same amount of work.
+# To avoid any fights you need to distribute the money fairly. That's where this Kata comes in.
+# Kata Task
+# Given the start and end letterbox numbers, write a method to return the frequency of all 10 digits painted.
