76cf9dbf5ee3d5b0c0f0d7ed181e8d4788d0efbc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-14 12:36:07 +0300

    Complete task 387

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3652,3 +3652,5 @@ class Solution:
 # int parameter w0 - initial weight -
 # int parameter p1 - final percent of water -
 # potatoesshould return the final weight coming out of the oven w1 truncated as an int.
+def potatoes(p0, w0, p1):
+    return w0 * (100 - p0) // (100 - p1)
