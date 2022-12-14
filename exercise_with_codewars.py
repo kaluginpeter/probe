df4cb4c44be840e9770ad53e0ba23f487e1790fe
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-14 17:10:39 +0300

    Added task 391

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3699,3 +3699,10 @@ def paint_letterboxes(start, finish):
             squad[i % 10]+=1
             i //= 10
     return squad
+
+# Imagine you are creating a game where the user has to guess the correct number.
+# But there is a limit of how many guesses the user can do.
+# If the user tries to guess more than the limit, the function should throw an error.
+# If the user guess is right it should return true.
+# If the user guess is wrong it should return false and lose a life.
+# Can you finish the game so all the rules are met?
