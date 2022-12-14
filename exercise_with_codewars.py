d3f8dde4a71f13bfa961cca2a54561cd19bb2741
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-14 17:23:00 +0300

    Added task 393

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3744,3 +3744,7 @@ def new_avg(arr, newavg):
         return ceil(ndon)
     else:
         raise ValueError('Negative number found')
+
+# In this kata, your job is to return the two distinct highest values in a list.
+# If there're less than 2 unique values, return as many of them, as possible.
+# The result should also be ordered from highest to lowest.
