440c6d87470c64768ebccb00b655b10ee3739893
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-15 00:11:43 +0300

    Complete task 400

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3806,5 +3806,21 @@ def calculate(s):
 
 # Write a class Block that creates a block (Duh..)
 ##Requirements
-# The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height] from which the Block should be created.
+# The constructor should take an array as an argument, this will contain 3 integers
+# of the form [width, length, height] from which the Block should be created.
+class Block:
+    def __init__(self, measurements):
+        self.width, self.length, self.height = measurements
+    def get_height(self):
+        return self.height
+    def get_length(self):
+        return self.length
+    def get_surface_area(self):
+        return 2 * (self.height * self.length +
+                    self.length * self.width +
+                    self.height * self.width)
+    def get_volume(self):
+        return self.height * self.length * self.width
+    def get_width(self):
+        return self.width
 
