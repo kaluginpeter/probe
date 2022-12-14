8b81bd31be1635d0f83d7f138213c6bb401f92b6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-14 16:37:17 +0300

    Added task 388

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3654,3 +3654,17 @@ class Solution:
 # potatoesshould return the final weight coming out of the oven w1 truncated as an int.
 def potatoes(p0, w0, p1):
     return w0 * (100 - p0) // (100 - p1)
+
+# Haskell has some useful functions for dealing with lists:
+# $ ghci
+# GHCi, version 7.6.3: http://www.haskell.org/ghc/  :? for help
+# λ head [1,2,3,4,5]
+# 1
+# λ tail [1,2,3,4,5]
+# [2,3,4,5]
+# λ init [1,2,3,4,5]
+# [1,2,3,4]
+# λ last [1,2,3,4,5]
+# 5
+# Your job is to implement these functions in your given language.
+# Make sure it doesn't edit the array; that would cause problems! Here's a cheat sheet:
