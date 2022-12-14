7465d6f00ac195972484a061abd3530c3c5e0c6c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-14 16:41:49 +0300

    Complete task 389

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3681,3 +3681,5 @@ def last(lst):
 # that everyone constantly wants to have a meeting with you, and that the meeting rooms are always taken!
 # In this kata, you will be given an array. Each value represents a meeting room. Your job?
 # Find the first empty one and return its index (N.B. There may be more than one empty room in some test cases).
+def meeting(rooms):
+    return rooms.index("O") if "O" in rooms else "None available!"
