4a4ced133838b2772935997e3508b8b16a5868b6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-15 00:10:35 +0300

    Complete task 399

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3798,4 +3798,9 @@ def logical_calc(array, op):
 
 # In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
 # Note: the input will not be empty.
+import re
+def calculate(s):
+    s = re.sub('plus', '+', s)
+    s = re.sub('minus','-', s)
+    return str(eval(s))
 
