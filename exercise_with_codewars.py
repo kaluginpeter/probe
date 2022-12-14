0dc6279b085f5528b4c1abe16769463aca385d7f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-15 00:16:32 +0300

    Added task 403

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3866,3 +3866,9 @@ from datetime import *
 def period_is_late(last, today, cycle_length):
     return last + timedelta(days = cycle_length) < today
 
+# You will be given a 2D array of the maze and an array of directions.
+# Your task is to follow the directions given.
+# If you reach the end point before all your moves have gone, you should return Finish.
+# If you hit any walls or go outside the maze border, you should return Dead.
+# If you find yourself still in the maze after using all the moves, you should return Lost.
+
