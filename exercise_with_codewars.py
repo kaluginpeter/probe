4c92426f8659c5ec0ec2961c07cfb40d8275bcc0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-14 17:23:53 +0300

    Complete task 393

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3748,3 +3748,5 @@ def new_avg(arr, newavg):
 # In this kata, your job is to return the two distinct highest values in a list.
 # If there're less than 2 unique values, return as many of them, as possible.
 # The result should also be ordered from highest to lowest.
+def two_highest(arg1):
+    return sorted(set(arg1), reverse=True)[:2]
