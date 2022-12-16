9a556ea43a753dc047141100856c458b7475de86
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-16 22:30:13 +0300

    Complete task 409

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3921,3 +3921,5 @@ def shorter_reverse_longer(a, b):
 
 # Complete the function to find the count of the most frequent item of an array.
 # You can assume that input is an array of integers. For an empty array return 0
+def most_frequent_item_count(collection):
+    return [max(collection.count(i) for i in collection)][0] if collection else 0
