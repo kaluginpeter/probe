1566aa7984f124a32ee71899adfb03d1b97c214c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-16 21:59:46 +0300

    Complete task 406

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3902,3 +3902,6 @@ def solution(pairs):
 
 # Complete the solution so that it returns a formatted string.
 # The return value should equal "Value is VALUE" where value is a 5 digit padded number.
+def solution(value):
+    value = str(value)
+    return 'Value is ' + '0'*(5-len(value)) + value
