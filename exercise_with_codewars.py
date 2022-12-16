5eb467b1655a2a34c081dafb3fbd9fdeb2d66cf7
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-16 21:56:53 +0300

    Complete task 405

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3897,3 +3897,5 @@ class List(object):
 # Complete the solution so that it takes the object (JavaScript/CoffeeScript)
 # or hash (ruby) passed in and generates a human readable string from its key/value pairs.
 # The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+def solution(pairs):
+    return ",".join(sorted(["{} = {}".format(k,v) for k,v in pairs.items()]))
