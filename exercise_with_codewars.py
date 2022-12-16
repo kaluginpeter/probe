88153fb0a186d35e5ad173100fff8733d0c4be92
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-16 22:22:38 +0300

    Complete task 408

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3914,3 +3914,7 @@ def number_of_occurrences(element, sample):
 # In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
 # Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
 # If a and b have the same length treat a as the longer producing b+reverse(a)+b
+def shorter_reverse_longer(a, b):
+    if len(a) < len(b):
+        return f'{a}{b[::-1]}{a}'
+    return f'{b}{a[::-1]}{b}'
