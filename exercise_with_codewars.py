9adee46a1d59c90b7086aef52a0b398fe1ad4c75
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-16 21:55:42 +0300

    Added task 405

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3893,3 +3893,7 @@ def maze_runner(maze, directions):
 class List(object):
     def remove_(self, integer_list, values_list):
         return [i for i in integer_list if i not in values_list]
+
+# Complete the solution so that it takes the object (JavaScript/CoffeeScript)
+# or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+# The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
