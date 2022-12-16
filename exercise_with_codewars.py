408e8ce1aadc62753f73f806a4a404bd8bcc299f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-16 22:38:28 +0300

    Added task 412

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3949,3 +3949,8 @@ def validate_hello(greetings):
 # (including for, for/in, while, and do/while loops).
 def average(array):
     return round(sum(array) / len(array))
+
+# Write a function that reverses the bits in an integer.
+# For example, the number 417 is 110100001 in binary.
+# Reversing the binary is 100001011 which is 267.
+# You can assume that the number is not negative.
