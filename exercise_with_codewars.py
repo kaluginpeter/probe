343a12ba0baeb8eca35707f79352a5e14a928212
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-16 22:37:04 +0300

    Complete task 411

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3947,3 +3947,5 @@ def validate_hello(greetings):
 # Create a function that returns the average of an array of numbers ("scores"),
 # rounded to the nearest whole number. You are not allowed to use any loops
 # (including for, for/in, while, and do/while loops).
+def average(array):
+    return round(sum(array) / len(array))
