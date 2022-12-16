8451461c9e4dbcde8d3fe3857f0f0c452897417b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-16 22:33:22 +0300

    Added task 410

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3923,3 +3923,20 @@ def shorter_reverse_longer(a, b):
 # You can assume that input is an array of integers. For an empty array return 0
 def most_frequent_item_count(collection):
     return [max(collection.count(i) for i in collection)][0] if collection else 0
+
+# You received a whatsup message from an unknown number. Could it be from that girl/boy
+# with a foreign accent you met yesterday evening?
+# Write a simple function to check if the string contains the word hallo in different languages.
+# These are the languages of the possible people you met the night before:
+# hello - english
+# ciao - italian
+# salut - french
+# hallo - german
+# hola - spanish
+# ahoj - czech republic
+# czesc - polish
+# Notes
+# you can assume the input is a string.
+# to keep this a beginner exercise you don't need to check
+# if the greeting is a subset of word (Hallowen can pass the test)
+# function should be case insensitive to pass the tests
