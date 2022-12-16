bbf7fa75229dbb49abccf75c865d2940c27752a4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-16 22:22:09 +0300

    Added task 408

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3909,3 +3909,8 @@ def solution(value):
 # Write a function that returns the number of occurrences of an element in an array.
 def number_of_occurrences(element, sample):
     return sample.count(element)
+
+# Given 2 strings, a and b, return a string of the form: shorter+reverse(longer)+shorter.
+# In other words, the shortest string has to be put as prefix and as suffix of the reverse of the longest.
+# Strings a and b may be empty, but not null (In C# strings may also be null. Treat them as if they are empty.).
+# If a and b have the same length treat a as the longer producing b+reverse(a)+b
