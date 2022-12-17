c59fc93df5cb7ee166a00ff5014fbcd669358e5a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-17 21:58:03 +0300

    Complete task 423

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4080,3 +4080,10 @@ def is_nice(arr):
 # multiplied by the integer at the last index.
 # Indices in sequence start from 0.
 # If the sequence is empty, you should return 0.
+def even_last(numbers):
+    total = 0
+    if numbers:
+        last = numbers[-1]
+        numbers = [num for i, num in enumerate(numbers) if i % 2 ==0]
+        total = sum(numbers) * last
+    return total
