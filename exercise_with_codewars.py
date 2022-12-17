48d7cd38f514be410498b683efb5a04ce4bd6466
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-17 21:04:16 +0300

    Complete task 416

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -3994,3 +3994,5 @@ def partlist(arr):
 
 # Implement the function which should return true if given object
 # is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.
+def is_vowel(s):
+    return s.lower() in ['a', 'e', 'i', 'o', 'u']
