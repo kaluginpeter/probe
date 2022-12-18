95be0a5d848cf102128846e353b7342f5a12c120
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-18 11:40:49 +0300

    Added task 426

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4103,3 +4103,10 @@ def swap(st):
 # Valid data will always be passed in and n will always be >= 1.
 def fillable(stock, merch, n):
     return merch in stock and stock[merch] >= n
+
+# Your task is to write a function called valid_spacing()
+# or validSpacing() which checks if a string has valid spacing.
+# The function should return either true or false (or the corresponding value in each language).
+# For this kata, the definition of valid spacing is one space between words,
+# and no leading or trailing spaces. Words can be any consecutive sequence
+# of non space characters. Below are some examples of what the function should return:
