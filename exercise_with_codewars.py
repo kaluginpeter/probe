de73851dfd58b39827ff8583501181c212267881
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-18 12:13:58 +0300

    Complete task 429

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4133,3 +4133,5 @@ def squares(x, n):
 
 # Write a function filterLucky/filter_lucky() that accepts
 # a list of integers and filters the list to only include the elements that contain the digit 7.
+def filter_lucky(lst):
+    return list(filter(lambda x: '7' in str(x), lst))
