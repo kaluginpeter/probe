ce853f107e6096f4ecbe374a01ed46a4899e6a8d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-18 13:08:01 +0300

    Complete task 435

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4186,3 +4186,6 @@ def make_string(s):
 # the sum of the integers inside a string. For example,
 # in the string "The30quick20brown10f0x1203jumps914ov3r1349the102l4zy dog", the sum of the integers is 3635.
 # Note: only positive integers will be tested.
+import re
+def sum_of_integers_in_string(s):
+    return sum([int(i) for i in re.findall(r'\d+', s)])
