61312c49bb65c834e5d431a843bf96425b4acdaf
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-18 12:24:49 +0300

    Added task 430

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4135,3 +4135,20 @@ def squares(x, n):
 # a list of integers and filters the list to only include the elements that contain the digit 7.
 def filter_lucky(lst):
     return list(filter(lambda x: '7' in str(x), lst))
+
+# You and a friend have decided to play a game to drill your statistical
+# intuitions. The game works like this:
+# You have a bunch of red and blue marbles.
+# To start the game you grab a handful of marbles of each color
+# and put them into the bag, keeping track of how many of each color go in.
+# You take turns reaching into the bag, guessing a color, and then pulling one marble out.
+# You get a point if you guessed correctly. The trick is you only have three seconds to make your guess,
+# so you have to think quickly.
+# You've decided to write a function, guessBlue() to help automatically calculate whether you should guess
+# "blue" or "red". The function should take four arguments:
+# the number of blue marbles you put in the bag to start
+# the number of red marbles you put in the bag to start
+# the number of blue marbles pulled out so far (always lower than the starting number of blue marbles)
+# the number of red marbles pulled out so far (always lower than the starting number of red marbles)
+# guessBlue() should return the probability of drawing a blue marble, expressed as a float.
+# For example, guessBlue(5, 5, 2, 3) should return 0.6.
