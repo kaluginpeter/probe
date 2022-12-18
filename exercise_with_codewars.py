e6ff3d4f3d35ececad7f85af64dc13e8a7825a76
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-18 11:57:25 +0300

    Added task 428

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4117,3 +4117,7 @@ def valid_spacing(s):
 # the elements of the input-array / list sorted in lexicographical order.
 def sortme(names):
     return sorted(names)
+
+# Complete the function that returns an array of length n,
+# starting with the given number x and the squares of the previous number.
+# If n is negative or zero, return an empty array/list.
