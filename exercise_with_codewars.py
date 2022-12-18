79363929f3dffff634baab8ad12eab2cb76e646f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-18 11:57:54 +0300

    Complete task 428

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4121,3 +4121,12 @@ def sortme(names):
 # Complete the function that returns an array of length n,
 # starting with the given number x and the squares of the previous number.
 # If n is negative or zero, return an empty array/list.
+def squares(x, n):
+    if (n<1): return []
+    result = [x]
+    i=0
+    while i < n-1:
+        i+=1
+        x*=x
+        result.append(x)
+    return result
