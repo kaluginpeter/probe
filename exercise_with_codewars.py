f418f33e6a243fcc5a04799301f9a112d285101b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-19 20:36:06 +0300

    Complete task 437

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4202,3 +4202,12 @@ def greet(name):
 # Also we got a problem with negative values. Correct the code so negative
 # values will be switched to positive ones!
 # The constructor taking no arguments should assign 0 to Cube's Side property.
+class Cube(object):
+    def __init__(self, side=0):
+        self._side = abs(side)
+
+    def get_side(self):\
+        return self._side
+
+    def set_side(self, new_side):
+        self._side = abs(new_side)
