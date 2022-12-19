41f03130ef9d24b97f283d3fb22622b9f5cf06d0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-19 20:35:36 +0300

    Added task 437

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4194,3 +4194,11 @@ def sum_of_integers_in_string(s):
 # and greet the person by saying hello. Return null/nil/None if input is empty string or null/nil/None.
 def greet(name):
     return f"hello {name}!" if name else None
+
+# Hey Codewarrior!
+# You already implemented a Cube class, but now we need your help again!
+# I'm talking about constructors. We don't have one.
+# Let's code two: One taking an integer and one handling no given arguments!
+# Also we got a problem with negative values. Correct the code so negative
+# values will be switched to positive ones!
+# The constructor taking no arguments should assign 0 to Cube's Side property.
