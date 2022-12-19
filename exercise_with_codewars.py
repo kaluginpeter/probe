c53facfc1b4a79a716933529cda6a8450f8b2851
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-19 21:05:54 +0300

    Added task 441

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4233,3 +4233,14 @@ def next_happy_year(year):
         if len(set(list(str(year)))) == 4:
             happy_year = True
     return year
+
+# The medians of a triangle are the segments that unit the vertices with the midpoint of their opposite sides.
+# The three medians of a triangle intersect at the same point, called the barycenter or the centroid.
+# Given a triangle, defined by the cartesian coordinates of its vertices we need
+# to localize its barycenter or centroid.
+# The function bar_triang() or barTriang or bar-triang, receives the coordinates
+# of the three vertices A, B and C  as three different arguments and outputs the
+# coordinates of the barycenter O in an array [xO, yO]
+# This is how our asked function should work: the result of the coordinates should
+# be expressed up to four decimals, (rounded result).
+# You know that the coordinates of the barycenter are given by the following formulas.
