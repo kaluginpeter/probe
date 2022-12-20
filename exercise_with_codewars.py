7114f1abf023c5ddbd4c0a5602e615c3a15cd672
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-20 14:12:09 +0300

    Added task 446

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4287,3 +4287,10 @@ def minimum_steps(numbers, value):
 # shorter than the original. Return the same string if nothing was truncated.
 def solution(st, limit):
     return st[:limit] + '...' if limit < len(st) else st
+
+# Fellow code warrior, we need your help!
+# We seem to have lost one of our sequence elements, and we need your help to retrieve it!
+# Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order),
+# but one of them seems to be missing.
+# Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive),
+# and returns the missing element.
