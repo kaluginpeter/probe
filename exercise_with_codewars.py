81ea15ad0894750d979454aba364a75b6c82a41d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-20 14:39:27 +0300

    Added task 448

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4302,3 +4302,9 @@ def get_missing_element(seq):
 # Given a positive integer N , Return the extra perfect numbers in range from 1 to N .
 def extra_perfect(n):
     return [i for i in range(1, n+1, 2)]
+
+# Simple enough this one - you will be given an array. The values in the array will either
+# be numbers or strings, or a mix of both. You will not get an empty array, nor a sparse one.
+# Your job is to return a single array that has first the numbers sorted in ascending order,
+# followed by the strings sorted in alphabetic order. The values must maintain their original type.
+# Note that numbers written as strings are strings and must be sorted with the other strings.
