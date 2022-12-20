8b1be3b5059fa691cf3f39d70cbb0e9bd8ca1563
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-20 14:22:02 +0300

    Added task 447

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4296,3 +4296,7 @@ def solution(st, limit):
 # and returns the missing element.
 def get_missing_element(seq):
     return [i for i in range(10) if i not in seq][0]
+
+# Extra perfect number is the number that first and last bits are set bits.
+# Task
+# Given a positive integer N , Return the extra perfect numbers in range from 1 to N .
