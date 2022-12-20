6f24263cae3f4b400b6d44de0f138c7af210ebaf
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-20 17:10:36 +0300

    Added task 453

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4346,3 +4346,11 @@ def triple_x(s):
 # Return the resulting lamps string. See example tests for more clarity.
 def fly_by(lamps, drone):
     return 'o'*len(drone) + lamps[len(drone):] if len(drone) <= len(lamps) else 'o'*len(lamps)
+
+# You are given a string of n lines, each substring being n characters long. For example:
+# s = "abcd\nefgh\nijkl\nmnop"
+# We will study the "horizontal" and the "vertical" scaling of this square of strings.
+# A k-horizontal scaling of a string consists of replicating k times each character of the string (except '\n').
+# Example: 2-horizontal scaling of s: => "aabbccdd\neeffgghh\niijjkkll\nmmnnoopp"
+# A v-vertical scaling of a string consists of replicating v times each part of the squared string.
+# Example: 2-vertical scaling of s: => "abcd\nabcd\nefgh\nefgh\nijkl\nijkl\nmnop\nmnop"
