bccab90002d42b861cc83018469fe225852f89c9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-21 23:35:38 +0300

    Added task 465

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4478,3 +4478,12 @@ def find_all(array, n):
 def switcher(arr):
     letters = ' ?!abcdefghijklmnopqrstuvwxyz'
     return ''.join(letters[::-1][int(idx) - 1] for idx in arr)
+
+# My friend wants a new band name for her band. She like bands that use the formula:
+# "The" + a noun with the first letter capitalized, for example:
+# "dolphin" -> "The Dolphin"
+# However, when a noun STARTS and ENDS with the same letter,
+# she likes to repeat the noun twice and connect them together
+# with the first and last letter, combined into one word (WITHOUT "The" in front), like this:
+# "alaska" -> "Alaskalaska"
+# Complete the function that takes a noun as a string, and returns her preferred band name written as a string.
