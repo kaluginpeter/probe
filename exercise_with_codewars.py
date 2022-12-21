0703ea35ed7374d9699dccae1347a2ac92fdaaa9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-21 22:55:06 +0300

    Added task 459

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4423,3 +4423,8 @@ def movie(card, ticket, perc):
         total_tickets += ticket
         i += 1
     return i - 1
+
+# You will be given an array of objects representing data
+# about developers who have signed up to attend the next coding meetup that you are organising.
+# Your task is to return an object which includes the count
+# of food options selected by the developers on the meetup sign-up form..
