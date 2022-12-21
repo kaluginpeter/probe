e90615800984a5e7dd5538094c94bb3eae64dcab
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-21 23:19:35 +0300

    Complete task 461

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4452,3 +4452,7 @@ def oper(fct, s):
 # Each floating-point number should be formatted that only the first two decimal places are returned.
 # You don't need to check whether the input is a valid number because only valid numbers are used in the tests.
 # Don't round the numbers! Just cut them after two decimal places!
+from math import trunc
+def two_decimal_places(number):
+    factor = float(10 ** 2)
+    return trunc(number * factor) / factor
