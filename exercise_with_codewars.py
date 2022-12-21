3b3f1f38cb49b8f8c86c6d3cd2ffbd3eace63b28
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-21 23:33:13 +0300

    Added task 464

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4469,3 +4469,9 @@ def special_number(n):
 # Assume that n and all values in the given array will always be integers.
 def find_all(array, n):
     return [e for e,i in enumerate(array) if i == n]
+
+# Given an array of numbers (in string format), you must return a string.
+# The numbers correspond to the letters of the alphabet in reverse order:
+# a=26, z=1 etc. You should also account for '!', '?' and ' ' that are represented by
+# '27', '28' and '29' respectively.
+# All inputs will be valid.
