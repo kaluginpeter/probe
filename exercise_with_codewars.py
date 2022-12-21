7a7cda2f482d94afa4d9b22b88c88e0d035d4635
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-21 21:58:36 +0300

    Added task 457

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4390,3 +4390,9 @@ def crap(garden, bags, cap):
 # return an integer representing the total distance travelled for visiting each floor in the array in order.
 def elevator_distance(array):
     return sum([abs(array[i] - array[i+1]) for i in range(len(array)-1)])
+
+# Complete the function to create backronyms. Transform the given string (without spaces)
+# to a backronym, using the preloaded dictionary and return a string of words,
+# separated with a single space (but no trailing spaces).
+# The keys of the preloaded dictionary are uppercase letters A-Z
+# and the values are predetermined words, for example:
