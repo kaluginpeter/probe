f0ae9060577d9cce939f324946cc18347827fa35
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-21 21:49:32 +0300

    Added task 456

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4383,3 +4383,8 @@ def crap(garden, bags, cap):
             if e == "D":
                 return "Dog!!"
     return "Clean" if c <= bags * cap else "Cr@p"
+
+# Imagine you start on the 5th floor of a building, then travel down to the 2nd floor,
+# then back up to the 8th floor. You have travelled a total of 3 + 6 = 9 floors of distance.
+# Given an array representing a series of floors you must reach by elevator,
+# return an integer representing the total distance travelled for visiting each floor in the array in order.
