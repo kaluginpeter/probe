0a8fb98659566293e24dd23ca05f9b93673585dd
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-22 16:25:28 +0300

    Added task 473

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4592,3 +4592,13 @@ def find_screen_height(width, ratio):
     lhs, rhs = list(map(int, ratio.split(':')))
     height = int(width * rhs/lhs)
     return str(width) + 'x' + str(height)
+
+# Create the function prefill that returns an array of n elements
+# that all have the same value v. See if you can do this without using a loop.
+# You have to validate input:
+# v can be anything (primitive or otherwise)
+# if v is ommited, fill the array with undefined
+# if n is 0, return an empty array
+# if n is anything other than an integer or integer-formatted string (e.g. '123') that is >=0, throw a TypeError
+# When throwing a TypeError, the message should be n is invalid, where
+# you replace n for the actual value passed to the function.
