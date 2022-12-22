894d3845363231224d5c03163671b32003a4f7f5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-22 13:59:11 +0300

    Added task 467

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4513,3 +4513,10 @@ def num_primorial(n):
                 sieve[bottom::si] = [0] * -((bottom - top) // si)
         return [2] + [el for el in sieve if el]
     return functools.reduce(lambda a, b: a*b, sieveOfEratosthenes(10000)[:n])
+
+# Well met with Fibonacci bigger brother, AKA Tribonacci.
+# As the name may already reveal, it works basically like a Fibonacci,
+# but summing the last 3 (instead of 2) numbers of the sequence to generate the next.
+# And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+# So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting
+# input (AKA signature), we have this sequence:
