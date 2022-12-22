eb2131dc4326a356987d358be4f7f56577acb8a3
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-22 15:20:40 +0300

    Added task 469

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4549,3 +4549,8 @@ def find_missing_number(sequence):
         if b not in sequence:
             return b
     return 0
+
+# The bloody photocopier is broken... Just as you were sneaking around
+# the office to print off your favourite binary code!
+# Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+# Given a string of binary, return the version the photocopier gives you as a string.
