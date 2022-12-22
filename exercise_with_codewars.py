e86dec0cf18ea1094646207bd420e1e5bbde0e65
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-22 15:37:22 +0300

    Added task 472

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4576,3 +4576,15 @@ def sum_args(*args):
 NATO
 def to_nato(words):
     return ' '.join(NATO[i.upper()] if i not in ',.!?' else i for i in words.replace(' ', ''))
+
+# Cheesy Cheeseman just got a new monitor! He is happy with it,
+# but he just discovered that his old desktop wallpaper no longer fits.
+# He wants to find a new wallpaper, but does not know which size wallpaper
+# he should be looking for, and alas, he just threw out the new monitor's box.
+# Luckily he remembers the width and the aspect ratio of the monitor from
+# when Bob Mortimer sold it to him. Can you help Cheesy out?
+# The Challenge
+# Given an integer width and a string ratio written as WIDTH:HEIGHT,
+# output the screen dimensions as a string written as WIDTHxHEIGHT.
+# Note: The calculated height should be represented as an integer.
+# If the height is fractional, truncate it.
