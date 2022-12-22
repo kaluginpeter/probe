563288517c247a32344c71eb5c4d5bf0ec430445
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-22 17:13:13 +0300

    Complete task 475

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4646,3 +4646,7 @@ def stock_list(listOfArt, listofCat):
 # If the sum of even numbers is greater than the odd numbers return: "Even is greater than Odd"
 # If the sum of odd numbers is greater than the sum of even numbers return: "Odd is greater than Even"
 # If the total of both even and odd numbers are identical return: "Even and Odd are the same"
+def even_or_odd(s):
+    a = sum(int(i) for i in s if int(i)%2 != 0)
+    b = sum(int(i) for i in s if int(i)%2 == 0)
+    return 'Even is greater than Odd' if b > a else 'Odd is greater than Even' if a>b else 'Even and Odd are the same'
