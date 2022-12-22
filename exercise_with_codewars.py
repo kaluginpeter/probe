7b8f5f151b5783bf83443da0aa9bd7473dfbc662
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-22 15:14:24 +0300

    Added task 468

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4529,3 +4529,12 @@ def tribonacci(signature, n):
             yield a
             a, b, c = b, c, a+b+c
     return list(fib(n))
+
+# You receive some random elements as a space-delimited string. Check if the
+# elements are part of an ascending sequence of integers
+# starting with 1, with an increment of 1 (e.g. 1, 2, 3, 4).
+# Return:
+# 0 if the elements can form such a sequence, and no number is missing ("not broken", e.g. "1 2 4 3")
+# 1 if there are any non-numeric elements in the input ("invalid", e.g. "1 2 a")
+# n if the elements are part of such a sequence, but some numbers are missing,
+# and n is the lowest of them ("broken", e.g. "1 2 4" or "1 5")
