2b99c7367abf154eb30aa2b0c57f7aefb1395282
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-22 17:03:45 +0300

    Added task 474

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4607,3 +4607,20 @@ def prefill(n,v='undefined'):
         return [v for _ in range(int(n))]
     else:
         raise TypeError('%s is invalid'%(n))
+
+# A bookseller has lots of books classified in 26 categories labeled A, B, ... Z. Each book has a code
+# c of 3, 4, 5 or more characters. The 1st character of a code is a capital letter which defines the book category.
+# In the bookseller's stocklist each code c is followed
+# by a space and by a positive integer n (int n >= 0) which indicates the quantity of books of this code in stock.
+# For example an extract of a stocklist could be:
+# L = {"ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"}.
+# or
+# L = ["ABART 20", "CDXEF 50", "BKWRK 25", "BTSQZ 89", "DRTYM 60"] or ....
+# You will be given a stocklist (e.g. : L) and a list of categories in capital letters e.g :
+# M = {"A", "B", "C", "W"}
+# or
+# M = ["A", "B", "C", "W"] or ...
+# and your task is to find all the books of L with codes
+# belonging to each category of M and to sum their quantity according to each category.
+# For the lists L and M of example you have to return the
+# string (in Haskell/Clojure/Racket/Prolog a list of pairs):
