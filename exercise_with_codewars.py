59609ee17c6b2bd91757952dfa38e18a5e7a9310
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-23 19:38:53 +0300

    Added task 480

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4721,3 +4721,14 @@ def selfie_and_rot(strng):
     return '\n'.join(i+'.'*len(i) for i in strng.split('\n')) + '\n' +'\n'.join('.'*len(i)+i[::-1] for i in strng.split('\n')[::-1])
 def oper(fct, s):
     return fct(s)
+
+# Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume
+# of n3 n^3n  3
+#  , the cube above will have volume of (n−1)3 (n-1)^3(n−1) 3
+#   and so on until the top which will have a volume of 13 1^31 3 .
+# You are given the total volume m of the building. Being given m can you find the
+# number n of cubes you will have to build?
+# The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an
+# integer m and you have to return the integer n such
+# as n3+(n−1)3+(n−2)3+...+13=m n^3 + (n-1)^3 + (n-2)^3 + ... + 1^3 = mn
+# 3 +(n−1) 3 +(n−2) 3 +...+1 3 =m if such a n exists or -1 if there is no such n.
