a7c47db1f6d37f8bcdd1c092bd5fd3c7b0c4c104
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-23 20:15:03 +0300

    Added task 484

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4788,3 +4788,14 @@ def scrolling_text(text):
         lst.append(text.upper())
         text = text[1:] + text[0]
     return lst
+
+# John wants to decorate the walls of a room with wallpaper. He wants a fool-proof method for getting it right.
+# John knows that the rectangular room has a length of l meters, a width of w meters,
+# a height of h meters. The standard width of the rolls he wants to buy is 52 centimeters.
+# The length of a roll is 10 meters. He bears in mind however, that it’s best to have an
+# extra length of wallpaper handy in case of mistakes or miscalculations so he wants to
+# buy a length 15% greater than the one he needs.
+# Last time he did these calculations he got a headache, so could you help John?
+# Task
+# Your function wallpaper(l, w, h) should return as a plain English word in lower
+# case the number of rolls he must buy.
