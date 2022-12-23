7155a739482493e4173a4e108a96aac08cc41a7b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-23 19:58:43 +0300

    Added task 482

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4765,3 +4765,10 @@ group = {
 def split_the_bill(x):
     total_each_owed = sum(x.values())/float(len(x))
     return {key:round(value - total_each_owed, 2) for key, value in x.items()}
+
+# For this game of BINGO, you will receive a single array of 10 numbers
+# from 1 to 26 as an input. Duplicate numbers within the array are possible.
+# Each number corresponds to their alphabetical order letter (e.g. 1 = A. 2 = B, etc).
+# Write a function where you will win the game if your numbers can spell "BINGO".
+# They do not need to be in the right order in the input array. Otherwise you will lose.
+# Your outputs should be "WIN" or "LOSE" respectively.
