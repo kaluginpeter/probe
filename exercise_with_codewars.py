128528d5626640633fd5a5c779b5b8df239d5632
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-23 20:08:51 +0300

    Complete task 483

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4782,3 +4782,9 @@ def bingo(array):
 # Let's create some scrolling text!
 # Your task is to complete the function which takes a string,
 # and returns an array with all possible rotations of the given string, in uppercase.
+def scrolling_text(text):
+    lst = []
+    for i in range(len(text)):
+        lst.append(text.upper())
+        text = text[1:] + text[0]
+    return lst
