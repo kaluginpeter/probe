488b21d28cb978131d0471c07b46570191200a13
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-24 15:58:45 +0300

    Added task 490

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4859,3 +4859,8 @@ def consecutive(arr):
 # All non-vowels including non alpha characters (spaces,commas etc.) should be included.
 def vowel_one(s):
     return ''.join('1' if i.lower() in 'aeoiu' else '0' for i in s)
+
+# Write a function to find if a number is lucky or not.
+# If the sum of all digits is 0 or multiple of 9 then the number is lucky.
+# 1892376 => 1+8+9+2+3+7+6 = 36. 36 is divisible by 9, hence number is lucky.
+# Function will return true for lucky numbers and false for others.
