e2bee977a8b7e6d06ec6f93f0f0ca9a82246b1c0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-24 16:39:54 +0300

    Added task 493

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4889,3 +4889,14 @@ def tankvol(h, d, vt):
 # you can only count it once in your sum.
 def unique_sum(lst):
     return sum(set(lst)) if lst else None
+
+# Welcome young Jedi! In this Kata you must create a function that takes an amount of US currency
+# in cents, and returns a dictionary/hash which shows the least amount of coins used to make up
+# that amount. The only coin denominations considered in this exercise are: Pennies (1¢),
+# Nickels (5¢), Dimes (10¢) and Quarters (25¢). Therefor the dictionary returned should
+# contain exactly 4 key/value pairs.
+# Notes:
+# If the function is passed either 0 or a negative number, the function should
+# return the dictionary with all values equal to 0.
+# If a float is passed into the function, its value should be rounded down,
+# and the resulting dictionary should never contain fractions of a coin.
