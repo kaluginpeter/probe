661e014cad803406545e54d586636092bb15e6be
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-24 15:55:45 +0300

    Complete task 489

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4857,3 +4857,5 @@ def consecutive(arr):
 # Write a function that takes a string
 # and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
 # All non-vowels including non alpha characters (spaces,commas etc.) should be included.
+def vowel_one(s):
+    return ''.join('1' if i.lower() in 'aeoiu' else '0' for i in s)
