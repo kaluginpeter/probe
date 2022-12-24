811f150ddfe4b2448a4ea19cd6bd9517cfbc902c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-24 15:55:17 +0300

    Added task 489

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4853,3 +4853,7 @@ def consecutive(arr):
         return len(range(min(arr), max(arr)+1)) - len(arr)
     except:
         return 0
+
+# Write a function that takes a string
+# and outputs a strings of 1's and 0's where vowels become 1's and non-vowels become 0's.
+# All non-vowels including non alpha characters (spaces,commas etc.) should be included.
