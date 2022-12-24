1949e1d1f9c5e78645b47e5effe35ddfcc1a7c91
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-24 16:53:19 +0300

    Complete task 494

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4915,3 +4915,6 @@ def loose_change(cents):
 # Move every letter in the provided string forward 10 letters through the alphabet.
 # If it goes past 'z', start again at 'a'.
 # Input will be a string with length > 0.
+def move_ten(st):
+    d = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz'
+    return ''.join(d[d.index(i)+10] for i in st)
