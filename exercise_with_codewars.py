54c0e7072c6f9c16af072dc970f89ca7b7bae0ab
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-25 21:32:54 +0300

    Added task 498

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4961,3 +4961,8 @@ def bingo(ticket,win):
                 count += 1
                 break
     return 'Winner!' if count >= win else 'Loser!'
+
+# Given a positive number n > 1 find the prime factor
+# decomposition of n. The result will be a string with the following form :
+#  "(p1**n1)(p2**n2)...(pk**nk)"
+# with the p(i) in increasing order and n(i) empty if n(i) is 1.
