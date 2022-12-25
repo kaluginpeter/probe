d18b1eb895d2c37f422f2150b42b6f539d24786d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-25 21:41:32 +0300

    Added task 499

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4990,3 +4990,8 @@ def prime_factors(n):
         else:
             t = t + '(' +str(key[0]) + '**' + str(key[1]) + ')'
     return t
+
+# Write a function insert_dash(num) / insertDash(num) / InsertDash(int num) that will insert dashes
+# ('-') between each two odd digits in num. For example:
+# if num is 454793 the output should be 4547-9-3. Don't count zero as an odd digit.
+# Note that the number will always be non-negative (>= 0).
