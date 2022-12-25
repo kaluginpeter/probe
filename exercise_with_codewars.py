c1e55c99ba075878dabe51b7c737843f55b9d2fb
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-25 22:27:08 +0300

    Complete task 504

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5073,3 +5073,5 @@ def get_ages(sum_, diff):
 
 # Create a function add(n)/Add(n) which returns a function that always adds n to any number
 # Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+def add(n):
+    return lambda x: x + n
