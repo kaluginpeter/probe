2abf47af062fe1173584a6ad062ff10860207294
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-25 21:27:55 +0300

    Added task 497

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -4939,3 +4939,17 @@ def compose(f, g):
 # If D <= 0, return an empty list.
 def solution(n,d):
     return [int(i) for i in str(n)[-d:]] if d > 0 else []
+
+# Time to win the lottery!
+# Given a lottery ticket (ticket), represented by an array of 2-value arrays,
+# you must find out if you've won the jackpot.
+# Example ticket:
+# [ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+# To do this, you must first count the 'mini-wins' on your ticket.
+# Each subarray has both a string and a number within it.
+# If the character code of any of the characters in the string matches the number,
+# you get a mini win. Note you can only have one mini win per sub array.
+# Once you have counted all of your mini wins, compare that number to the
+# other input provided (win). If your total is more than or equal to (win),
+# return 'Winner!'. Else return 'Loser!'.
+# All inputs will be in the correct format. Strings on tickets are not always the same length.
