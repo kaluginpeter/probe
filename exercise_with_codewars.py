5add3583aa020bdaed4561dcaf6945fc1f1da703
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-26 16:48:11 +0300

    Added task 515

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5192,3 +5192,8 @@ def chained(functions):
             result = f(result)
         return result
     return apply
+
+Unscramble the eggs.
+
+# The string given to your function has had an "egg"
+# inserted directly after each consonant. You need to return the string before it became eggcoded.
