4b814480806e9e2c541c7be8ab87a331510e62fa
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-26 16:48:35 +0300

    Complete task 515

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5197,3 +5197,5 @@ Unscramble the eggs.
 
 # The string given to your function has had an "egg"
 # inserted directly after each consonant. You need to return the string before it became eggcoded.
+def unscramble_eggs(word):
+    return word.replace('egg','')
