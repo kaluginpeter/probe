20f74604fdf29b876e5e2f7bc7622d7945711bb4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-26 16:30:11 +0300

    Added task 510

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5121,3 +5121,11 @@ def nickname_generator(name):
 # the number of times that character occurs in s/$s and separating each number with the character(s) sep/$sep.
 def freq_seq(s, sep):
     return sep.join(str(s.count(i)) for i in s)
+
+# You are to write a function that takes a string as its first parameter.
+# This string will be a string of words.
+# You are expected to then use the second parameter, which will be an integer,
+# to find the corresponding word in the given string. The first word would be represented by 0.
+# Once you have the located string you are finally going to multiply by it the
+# third provided parameter, which will also be an integer.
+# You are additionally required to add a hyphen in between each word.
