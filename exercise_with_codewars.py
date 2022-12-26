13657c4145bac7ae8a6287f3a747e5e2b96f8b1c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-26 16:40:08 +0300

    Complete task 513

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5157,3 +5157,28 @@ def createDict(keys, values):
 # for the city town and the strng data or data1 (In R and Julia this function is called avg).
 # function: variance(town, strng) should return the variance of rainfall
 # for the city town and the strng data or data1.
+import re
+def mean(town, strng):
+    data_split = re.findall(r'.+(?:\n|$)', strng)
+    for counter, town_info in enumerate(data_split):
+        if town in town_info:
+            town_name = re.match(r'\w+', town_info)
+            if town != town_name.group():
+                continue
+            numbers = re.findall(r'\d+\.?\d+', town_info)
+            float_numbers = [float(x) for x in numbers]
+            return sum(float_numbers)/len(float_numbers)
+    return -1
+def variance(town, strng):
+    data_split = re.findall(r'.+(?:\n|$)', strng)
+    for counter, town_info in enumerate(data_split):
+        if town in town_info:
+            town_name = re.match(r'\w+', town_info)
+            if town != town_name.group():
+                continue
+            numbers = re.findall(r'\d+\.?\d+', town_info)
+            float_numbers = [float(x) for x in numbers]
+            mean = sum(float_numbers)/len(float_numbers)
+            squared_nums = [(x-mean)**2 for x in float_numbers]
+            return sum(squared_nums)/(len(squared_nums))
+    return -1
