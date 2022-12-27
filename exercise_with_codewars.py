4ab577a15df6b7457e1277bfb5e57bc19f7eb8bd
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-27 20:39:04 +0300

    Added task 521

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5255,3 +5255,8 @@ def caffeine_buzz(n):
     if n % 2 == 0:
         i += 'Script'
     return i
+
+# A new task for you!
+# You have to create a method, that corrects a given time string.
+# There was a problem in addition, so many of the time strings are broken.
+# Time is formatted using the 24-hour clock, so from 00:00:00 to 23:59:59.
