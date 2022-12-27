7baae541e90da6f7499997b39d5626f4110cbbf0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-27 20:19:16 +0300

    Added task 519

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5227,3 +5227,9 @@ def last_survivor(letters, coords):
     for i in coords:
         letters = letters[:i] + letters[i+1:]
     return letters
+
+# Complete the function power_of_two/powerOfTwo (or equivalent, depending on your language)
+# that determines if a given non-negative integer is a power of two. From the corresponding Wikipedia entry:
+# a power of two is a number of the form 2n where n is an integer,
+# i.e. the result of exponentiation with number two as the base and integer n as the exponent.
+# You may assume the input is always valid.
