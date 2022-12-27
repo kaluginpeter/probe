85ebe3b565e98869ae9e3b2ada27dbe1039c7d63
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-27 20:33:52 +0300

    Added task 520

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5238,3 +5238,9 @@ def power_of_two(n: int) -> list:
     while i < n:
         i=i*2
     return i == n if n!=1 else True
+
+# Complete the function which takes a non-zero integer as its argument.
+# If the integer is divisible by 3, return the string "Java".
+# If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+# If one of the condition above is true and the integer is even, add "Script" to the end of the string.
+# If none of the condition is true, return the string "mocha_missing!"
