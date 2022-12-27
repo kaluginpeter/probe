904dfb01e5e736dd034e435cc6050593f937fe18
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-27 20:08:19 +0300

    Added task 517

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5206,3 +5206,7 @@ def fibonacci(n: int) -> int:
     for i in range(n):
         a,b = b, a+b
     return a
+
+# In this Kata, you will be given an array of unique elements, and your task
+# is to rearrange the values so that the first max value is followed by the first minimum,
+# followed by second max value then second min value, etc.
