234023692969d1304246fac8291bf89502eebf31
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-27 20:14:01 +0300

    Added task 518

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5217,3 +5217,9 @@ def solve(arr):
         ls.append(l[i])
         ls.append(l[::-1][i])
     return ls[:-2] if len(arr)%2 == 0 else ls[:-1]
+
+# You are given a string of letters and an array of numbers.
+# The numbers indicate positions of letters that must be removed, in order,
+# starting from the beginning of the array.
+# After each removal the size of the string decreases (there is no empty space).
+# Return the only letter left.
