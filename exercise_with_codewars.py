c881db42dd9235bd3f0b6d644ae88d7ecf4754e7
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-28 21:13:23 +0300

    Added task 527

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5353,3 +5353,13 @@ def max_rot(n):
         if current > maximum:
             maximum = current
     return maximum
+
+# Scenario
+# the rhythm of beautiful musical notes is drawing a Pendulum
+# Beautiful musical notes are the Numbers
+# Task
+# Given an array/list [] of n integers , Arrange them in a way similar to the to-and-fro movement of a Pendulum
+# The Smallest element of the list of integers , must come in center position of array/list.
+# The Higher than smallest , goes to the right .
+# The Next higher number goes to the left of minimum number and So on ,
+# in a to-and-fro manner similar to that of a Pendulum.
