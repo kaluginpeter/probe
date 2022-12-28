c188b258fc8a3f78fdca3f108d5ae0f0ec6234cb
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-28 21:40:18 +0300

    Complete task 530

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5389,3 +5389,5 @@ def even_chars(st):
 # in the array and 2 occurs 3 times.
 # The Stanton measure of [1, 4, 1, 2, 11, 2, 3, 1] is 1, because 1 occurs 3 times
 # in the array and 3 occurs 1 time.
+def stanton_measure(arr):
+    return arr.count(arr.count(1))
