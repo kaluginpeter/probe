5be47a79268700777724624ac7db38a2473c7688
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-28 23:11:49 +0300

    Complete task 534

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5417,3 +5417,28 @@ def pattern(n):
 
 # The UK driving number is made up from the personal details of the driver. The individual letters
 # and digits can be code using the below information
+from dateutil.parser import parse
+def driver(data):
+    lic = ""
+    if len(data[2]) >= 5:
+          lic += data[2][:5]
+    else:
+            lic += data[2]
+            while (len(lic) < 5): lic += "9"
+    lic += (str(parse(data[3]).year))[2]
+    month = parse(data[3]).month
+    if data[4] == "F": month += 50
+    month = str(month)
+    if len(month) == 1: month = "0" + month
+    lic += month
+    day = str(parse(data[3]).day)
+    if len(day) == 1: day = "0" + day
+    lic += day
+    lic += (str(parse(data[3]).year))[3]
+    lic +=  data[0][:1]
+    if (data[1]) != "":
+        lic += data[1][:1]
+    else:
+        lic += "9"
+    lic += "9AA"
+    return lic.upper()
