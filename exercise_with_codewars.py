9e2e4342f7d4b54448d623efa75a6571b991ad02
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-29 16:21:25 +0300

    Added task 542

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5548,3 +5548,9 @@ def remove(s):
         j = j[:-1]
     r = (len(s)-len(j))
     return j.replace('!', '') + '!' * r
+
+# You have to create a function calcType, which receives 3 arguments: 2 numbers,
+# and the result of an unknown operation performed on them (also a number).
+# Based on those 3 values you have to return a string, that describes which operation
+# was used to get the given result.
+# The possible return strings are: "addition", "subtraction", "multiplication", "division".
