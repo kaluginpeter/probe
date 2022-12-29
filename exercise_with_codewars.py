db318c15f0b686118f6847ae00c18cf3ef7a5517
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-29 16:59:21 +0300

    Added task 545

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5574,3 +5574,9 @@ def mean(lst):
 # Write reverseList function that simply reverses lists.
 def reverse_list(lst):
     return lst[::-1]
+
+
+# Write function heron which calculates the area of a
+# triangle with sides a, b, and c (x, y, z in COBOL). Heron 's formula:
+# s∗(s−a)∗(s−b)∗(s−c)\sqrt {s * (s - a) * (s - b) * (s - c)} s∗(s−a)∗(s−b)∗(s−c)
+# Output should have 2 digits precision.
