0f61d117203602fd5b1c77750b96609f36289f72
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-29 13:59:19 +0300

    Complete task 538

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5472,3 +5472,13 @@ def solve(arr):
 # Some people just have a first name; some people have first
 # and last names and some people have first, middle and last names.
 # You task is to initialize the middle names (if there is any).
+def initialize_names(name):
+    arr = name.split()
+    l = []
+    if len(arr)>2:
+        l.append(arr[0])
+        for i in arr[1:-1]:
+            l.append(i[0].upper()+'.')
+        l.append(arr[-1])
+        return ' '.join(l)
+    return name
