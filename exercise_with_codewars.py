bfeea9fd442d4bd0caab4bc139732d0d9b8cf541
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-29 13:35:35 +0300

    Complete task 536

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5457,3 +5457,5 @@ def solve(s,g):
 # n/$n and generates an array containing the integers from 0 to n/$n inclusive.
 # For example, generateIntegers(3)/generate_integers(3) should return [0, 1, 2, 3].
 # n/$n can be any integer greater than or equal to 0.
+def generate_integers(n):
+    return list(range(n + 1))
