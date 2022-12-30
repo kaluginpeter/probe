0e8dff7f0ae3c34613e9f1570910a59542c1171a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-30 16:13:52 +0300

    Added task 553

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5657,3 +5657,9 @@ def encode(message, key):
 # The values of the nested arrays don't have to be arrays.
 def arr_check(arr):
     return all(type(i) == list for i in arr)
+
+# Your task is very simple. Just write a function takes an input string
+# of lowercase letters and returns true/false depending on whether the string is in alphabetical order or not.
+# Examples (input -> output)
+# "kata" -> false ('a' comes after 'k')
+# "ant" -> true (all characters are in alphabetical order)
