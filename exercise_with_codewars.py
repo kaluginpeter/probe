0931491b8f84de8502f01c6e6f22392abc8016b8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-30 16:10:52 +0300

    Complete task 552

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5655,3 +5655,5 @@ def encode(message, key):
 # Is every value in the array an array?
 # This should only test the second array dimension of the array.
 # The values of the nested arrays don't have to be arrays.
+def arr_check(arr):
+    return all(type(i) == list for i in arr)
