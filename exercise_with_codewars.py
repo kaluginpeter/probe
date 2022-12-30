34e71f10933520497a25df14369067dbe231f8dd
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-30 17:06:32 +0300

    Complete task 554

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5669,3 +5669,7 @@ def alphabetic(s):
 # Write function replaceAll (Python: replace_all) that will replace all occurrences of an item with another.
 # Python / JavaScript: The function has to work for strings and lists.
 # Example: replaceAll [1,2,2] 1 2 -> in list [1,2,2] we replace 1 with 2 to get new list [2,2,2]
+def replace_all(obj, find, replace):
+    if type(obj) == list:
+        return [replace if i == find else i for i in obj]
+    return obj.replace(find, replace)
