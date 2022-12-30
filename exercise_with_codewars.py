1aef8457cd78abf81c741bfe29f1ac46625fa8e3
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-30 16:07:39 +0300

    Added task 551

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5626,3 +5626,13 @@ def how_many_times(annual_price, individual_price):
 def remove(s):
     count = s.count('!')
     return s.replace('!', '') + '!'*count
+
+# Digital Cypher assigns to each letter of the alphabet unique number. For example:
+# a  b  c  d  e  f  g  h  i  j  k  l  m
+# 1  2  3  4  5  6  7  8  9 10 11 12 13
+# n  o  p  q  r  s  t  u  v  w  x  y  z
+# 14 15 16 17 18 19 20 21 22 23 24 25 26
+# Instead of letters in encrypted word we write the corresponding number, eg. The word scout:
+#  s  c  o  u  t
+# 19  3 15 21 20
+# Then we add to each obtained digit consecutive digits from the key. For example. In case of key equal to 1939 :
