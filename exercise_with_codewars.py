6593a1529c994561b26edad5f2051eb522eda19f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-30 15:56:02 +0300

    Added task 549

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5613,3 +5613,8 @@ def odd_one(arr):
         return arr.index(max([i for i in arr if i%2!=0]))
     except:
         return -1
+
+# Lot of museum allow you to be a member, for a certain
+# amount amount_by_year you can have unlimitted acces to the museum.
+# In this kata you should complete a function in order to know after how many visit it
+# will be better to take an annual pass. The function take 2 arguments annual_price and individual_price.
