38c51664b566b457b4d3fcd0d07ad93fc1091ed8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-30 17:05:59 +0300

    Added task 554

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5665,3 +5665,7 @@ def arr_check(arr):
 # "ant" -> true (all characters are in alphabetical order)
 def alphabetic(s):
     return s == "".join(sorted(s))
+
+# Write function replaceAll (Python: replace_all) that will replace all occurrences of an item with another.
+# Python / JavaScript: The function has to work for strings and lists.
+# Example: replaceAll [1,2,2] 1 2 -> in list [1,2,2] we replace 1 with 2 to get new list [2,2,2]
