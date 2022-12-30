fad71edfe9ba2b7612edc815a77262536b69c54c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-30 15:45:33 +0300

    Added task 547

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5594,3 +5594,10 @@ def heron(a, b, c):
 def dominator(arr):
     w = [i for i in arr if arr.count(i) > len(arr)//2]
     return w[0] if w else -1
+
+# Christmas is coming, and Santa has a long list to go through, to find who deserves presents for the
+# big day. Go through a list of children, and return
+# a list containing every child who appeared on Santa's list.
+# Do not add any child more than once. Output should be sorted.
+# Comparison should be case sensitive and the returned list should contain only one copy of
+# each name: "Sam" and "sam" are different, but "sAm" and "sAm" are not.
