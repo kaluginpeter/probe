1e578dca576f13cc399901d7a05a9d98f3942585
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-30 15:52:13 +0300

    Added task 548

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5603,3 +5603,8 @@ def dominator(arr):
 # each name: "Sam" and "sam" are different, but "sAm" and "sAm" are not.
 def find_children(santas_list, children):
     return sorted(set([child for child in children if child in santas_list]))
+
+# Create a method that takes an array/list as an input,
+# and outputs the index at which the sole odd number is located.
+# This method should work with arrays with negative numbers. If there are
+# no odd numbers in the array, then the method should output -1.
