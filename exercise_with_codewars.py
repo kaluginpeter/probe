762954bde67b39b1aa649f5d4f0b24bda87db6aa
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-31 14:29:47 +0300

    Complete task 563

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5823,3 +5823,14 @@ def remove(s):
 # The function called 'game' with parameter n (integer >= 0) returns as result an
 # irreducible fraction written as an array of integers: [numerator, denominator].
 # If the denominator is 1 return [numerator].
+def game(n):
+    if n == 0: return [0]
+    if n == 1: return [1, 2]
+    if n == 2: return [3, 2]
+    if n == 3: return [9, 2]
+    s, step = 4.5, 3.5
+    for _ in range(n-3):
+        s = s + step
+        step += 1.0
+    if int(s) == s: return [s]
+    else: return [int(str(2*int(s)+1)), 2]
