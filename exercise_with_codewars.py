f9754c2ff23c60c7fc8f2242986b19f776401f00
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2022-12-31 14:40:37 +0300

    Added task 564

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5834,3 +5834,10 @@ def game(n):
         step += 1.0
     if int(s) == s: return [s]
     else: return [int(str(2*int(s)+1)), 2]
+
+# You will be given an array of numbers.
+# For each number in the array you will need to create an object.
+# The object key will be the number, as a string. The value will be the corresponding character code, as a string.
+# Return an array of the resulting objects.
+# All inputs will be arrays of numbers.
+# All character codes are valid lower case letters. The input array will not be empty.
