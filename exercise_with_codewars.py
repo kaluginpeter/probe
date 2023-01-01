3544a7ddcb4099648935432c37ecef9ff72ae162
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-01 14:38:26 +0300

    Complete task 573

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5974,3 +5974,10 @@ def spread(func, args):
 # Complete the function that takes an array of words.
 # You must concatenate the nth letter from each word to construct a new
 # word which should be returned as a string, where n is the position of the word in the list.
+def nth_char(words):
+    word = ''
+    i = 0
+    while i < len(words):
+        word += words[i][i]
+        i+=1
+    return word
