5544bcd7bc1e4a21c2db811316f7fafc725d8bf2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-01 14:34:04 +0300

    Added task 572

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5964,3 +5964,7 @@ def killer(suspect_info, dead):
     for k,v in suspect_info.items():
         if all(i in v for i in dead):
             return k
+
+# You must create a function, spread, that takes a function and
+# a list of arguments to be applied to that function. You must make this function return
+# the result of calling the given function/lambda with the given arguments.
