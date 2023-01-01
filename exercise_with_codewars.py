e93c69ef4a587bf9a9b3f43bfba51bf6d663291d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-01 14:32:01 +0300

    Added task 571

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5953,3 +5953,10 @@ def compute_depth(n):
                 l.append(i)
         count+=1
     return count-1
+
+# Some people have been killed!
+# You have managed to narrow the suspects down to just a few. Luckily, you know every
+# person who those suspects have seen on the day of the murders.
+# Task.
+# Given a dictionary with all the names of the suspects and everyone that they have
+# seen on that day which may look like this:
