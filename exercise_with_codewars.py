0400e81709ef35588abfaea6022bcb91d99f363c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-01 14:34:24 +0300

    Complete task 572

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -5968,3 +5968,5 @@ def killer(suspect_info, dead):
 # You must create a function, spread, that takes a function and
 # a list of arguments to be applied to that function. You must make this function return
 # the result of calling the given function/lambda with the given arguments.
+def spread(func, args):
+    return func(*args)
