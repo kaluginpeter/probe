06c7b614510146be9babaf9e00be484b52d0d831
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-02 15:25:13 +0300

    Complete task 581

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6111,3 +6111,6 @@ def filter_even_length_words(words):
 # Note that you may have troubles if you do not return an actual copy,
 # item by item, just a pointer or an alias for an existing list or array.
 # If not a list or array is given as a parameter in interpreted languages, the function should raise an error.
+def copy_list(l):
+    copy = [i for i in l]
+    return copy
