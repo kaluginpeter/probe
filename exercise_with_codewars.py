5e35f8820e9c7f8f0a71d068583c617d407694b1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-02 15:31:19 +0300

    Complete task 582

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6122,3 +6122,7 @@ def copy_list(l):
 # instead of including i or I put the number 1 in the password;
 # instead of including o or O put the number 0 in the password;
 # instead of including s or S put the number 5 in the password.
+def make_password(phrase):
+    d = {'i': '1', 'o': '0', 's':'5'}
+    word = [i[0] for i in phrase.split()]
+    return ''.join(d[i.lower()] if i.lower() in d else i for i in word)
