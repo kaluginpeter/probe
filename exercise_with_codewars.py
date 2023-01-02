a9529498fe4d4dd673651fed12bf3fc99a76a7f8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-02 15:13:47 +0300

    Added task 578

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6068,3 +6068,14 @@ def fruit(reels, spins):
                 if l.count(elem) == 2:
                     return double[elem] * 2
     return 0
+
+# If you have completed the Tribonacci sequence kata, you would know by now that
+# mister Fibonacci has at least a bigger brother. If not, give it a quick look to get how things work.
+# Well, time to expand the family a little more: think of
+# a Quadribonacci starting with a signature of 4 elements and each following element is
+# the sum of the 4 previous, a Pentabonacci (well Cinquebonacci would probably sound a bit more italian,
+# but it would also sound really awful) with a signature of 5 elements
+# and each following element is the sum of the 5 previous, and so on.
+# Well, guess what? You have to build a Xbonacci function that takes a signature of X elements -
+# and remember each next element is the sum of the last X elements -
+# and returns the first n elements of the so seeded sequence.
