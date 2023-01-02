59569a7070610fa630ef41132b5f46276bd1c3f4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-02 15:22:44 +0300

    Complete task 580

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6103,3 +6103,5 @@ def search(budget, prices):
 # containing only the elements of the given array whose length is an even number.
 # var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
 # console.log(output); // --> ['word', 'word']
+def filter_even_length_words(words):
+    return [word for word in words if len(word) % 2 == 0]
