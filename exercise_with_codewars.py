5ef396d532bd2d3983531195a97794a68b4e9cc7
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-02 16:28:53 +0300

    Added task 585

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6154,3 +6154,12 @@ def dative(word):
             return word + "nek"
         elif letter in "aáoóuú":
             return word + "nak"
+
+# A Cartesian coordinate system is a coordinate system that specifies each point uniquely
+# in a plane by a pair of numerical coordinates, which are the signed distances to
+# the point from two fixed perpendicular directed lines, measured in the same unit of length.
+# The сoordinates of a point in the grid are written as (x,y). Each point in a
+# coordinate system has eight neighboring points. Provided that the grid step = 1.
+# It is necessary to write a function that takes a coordinate on the x-axis and y-axis
+# and returns a list of all the neighboring points. Points inside your returned list
+# need not be sorted (any order is valid).
