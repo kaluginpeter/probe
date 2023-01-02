16df319522a3f404b724e6f329903de50da71465
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-02 16:03:52 +0300

    Added task 584

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6136,3 +6136,15 @@ def sort_vowels(s):
         return '\n'.join(i+'|' if i.lower() not in 'aeoiu' else '|'+i for i in s)
     except:
         return ''
+
+# Vowel harmony is a phenomenon in some languages. It means that "A
+# vowel or vowels in a word are changed to sound the same (thus "in harmony.")" (wikipedia).
+# This kata is based on vowel harmony in Hungarian.
+# Task:
+# Your goal is to create a function dative() (Dative() in C#) which returns
+# the valid form of a valid Hungarian word w in dative case i. e.
+# append the correct suffix nek or nak to the word w based on vowel harmony rules.
+# Vowel Harmony Rules (simplified)
+# When the last vowel in the word is
+# a front vowel (e, é, i, í, ö, ő, ü, ű) the suffix is -nek
+# a back vowel (a, á, o, ó, u, ú) the suffix is -nak
