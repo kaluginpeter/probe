8147db6409d077128036e9aad259dd5b5f36ac46
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-02 14:06:07 +0300

    Added task 576

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6009,3 +6009,8 @@ def chain(init_val, functions):
     for func in functions:
         init_val = func(init_val)
     return init_val
+
+# You'll be given a list of two strings, and each will contain exactly one colon (":") in the middle
+# (but not at beginning or end). The length of the strings, before and after the colon, are random.
+# Your job is to return a list of two strings
+# (in the same order as the original list), but with the characters after each colon swapped.
