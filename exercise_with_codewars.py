2c18184f99fc1544a7c4dd0d4b73eba815b1c840
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-02 15:22:25 +0300

    Added task 580

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6097,3 +6097,9 @@ def Xbonacci(signature,n):
 # beans you can afford. The prices in this string are to be sorted in ascending order.
 def search(budget, prices):
     return ','.join(str(i) for i in sorted(list(filter(lambda x: x<= budget, prices))))
+
+# Write a function called "filterEvenLengthWords".
+# Given an array of strings, "filterEvenLengthWords" returns an array
+# containing only the elements of the given array whose length is an even number.
+# var output = filterEvenLengthWords(['word', 'words', 'word', 'words']);
+# console.log(output); // --> ['word', 'word']
