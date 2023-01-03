618723dadbf2c879aa513c6ad5becfe73f62cd2d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-03 21:57:59 +0300

    Added task 593

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6283,3 +6283,13 @@ def fizz_buzz_cuckoo_clock(time):
     elif mm %  3 == 0:return "Fizz"
     elif mm %  5 == 0:return "Buzz"
     else:return "tick"
+
+# Can Santa save Christmas?
+# Oh no! Santa's little elves are sick this year. He has to distribute the presents on his own.
+# But he has only 24 hours left. Can he do it?
+# Your Task:
+# You will get an array as input with time durations as string
+# in the following format: HH:MM:SS. Each duration represents the
+# time taken by Santa to deliver a present. Determine whether he can do it
+# in 24 hours or not. In case the time required to deliver all of the presents is exactly 24 hours,
+# Santa can complete the delivery ;-) .
