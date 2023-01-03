86e30c3588368449ba58d633ff14714405298824
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-03 21:58:23 +0300

    Complete task 593

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6292,4 +6292,7 @@ def fizz_buzz_cuckoo_clock(time):
 # in the following format: HH:MM:SS. Each duration represents the
 # time taken by Santa to deliver a present. Determine whether he can do it
 # in 24 hours or not. In case the time required to deliver all of the presents is exactly 24 hours,
-# Santa can complete the delivery ;-) .
+# Santa can complete the delivery ;-).
+def determineTime(arr):
+    total_sec = sum(h * 3600 + m * 60 + s for h, m, s in [list(map(int, elem.split(':'))) for elem in arr])
+    return total_sec <= 86400
