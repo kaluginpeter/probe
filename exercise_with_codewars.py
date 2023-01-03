1ccecc2cf908b4fb3ddcd0a23f0bb7da6918a120
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-03 21:39:06 +0300

    Complete task 589

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6197,3 +6197,10 @@ def pass_hash(str):
 # The formats are primarily used in HTML and CSS.
 # Your task is to implement a function which takes a color as a string and returns
 # the parsed color as a map (see Examples).
+def parse_html_color(color):
+    color = PRESET_COLORS.get(color.lower(), color)
+    if len(color) == 7:
+        r, g, b = (int(color[i:i+2], 16) for i in range(1, 7, 2))
+    else:
+        r, g, b = (int(color[i+1]*2, 16) for i in range(3))
+    return dict(zip("rgb", (r, g, b)))
