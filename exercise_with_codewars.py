127d613f66217e812e7cb3944fc2710804235600
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-03 21:24:54 +0300

    Added task 588

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6180,3 +6180,12 @@ def pofi(n):
 # number from 0 to ... . Empty array returns 0
 def catch_sign_change(lst):
     return sum((x>=0)!=(y>=0) for x, y in zip(lst,lst[1:]))
+
+# When you sign up for an account somewhere, some websites do not actually store
+# your password in their databases. Instead, they will transform your password into something
+# else using a cryptographic hashing algorithm.
+# After the password is transformed, it is then called a password hash. Whenever you try to login,
+# the website will transform the password you tried
+# using the same hashing algorithm and simply see if the password hashes are the same.
+# Create the function that converts a given string into an md5 hash. The return value
+# should be encoded in hexadecimal.
