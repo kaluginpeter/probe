55a6fb596d2ea323aff0cfeb9872e6ad3b6d5a13
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-03 21:14:45 +0300

    Added task 586

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6166,3 +6166,9 @@ def dative(word):
 def cartesian_neighbor(x, y):
     return [(x - 1, y - 1), (x - 1, y), (x - 1, y + 1), (x, y - 1), (x, y + 1), (x + 1, y - 1), (x + 1, y),
             (x + 1, y + 1)]
+
+# i is the imaginary unit, it is defined by i²=−1i² = -1i²=−1,
+# therefore it is a solution to x²+1=0x² + 1 = 0x²+1=0.
+# Your Task
+# Complete the function pofi that returns iii to the power of a given non-negative integer
+# in its simplest form, as a string (answer may contain iii).
