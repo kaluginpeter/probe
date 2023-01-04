03f1b13a4fb07c8f2f2c9526682ab5224c69baf0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-04 14:08:58 +0300

    Complete task 596

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6332,3 +6332,19 @@ def get_nth(node, index):
 # Implement String#ipv4_address?, which should return true if given object is an IPv4 address
 # - four numbers (0-255) separated by dots.
 # It should only accept addresses in canonical representation, so no leading 0s, spaces etc.
+import re
+def ipv4_address(address):
+    regex = re.compile(r'''
+    #      200-249   | 250-255 | 100-109| 0-99 110-199
+    \b([2]([0-4][0-9]|[5][0-5])|(10[0-9]|[1]?[1-9]?[0-9]))\.
+    ([2]([0-4][0-9]|[5][0-5])|(10[0-9]|[1]?[1-9]?[0-9]))\.
+    ([2]([0-4][0-9]|[5][0-5])|(10[0-9]|[1]?[1-9]?[0-9]))\.
+    ([2]([0-4][0-9]|[5][0-5])|(10[0-9]|[1]?[1-9]?[0-9]))\b    
+    ''', re.VERBOSE)
+    mo = regex.match(address)
+    if mo is None:
+        return False
+    elif  mo.group() == address:
+        return (bool(mo))
+    else:
+        return False
