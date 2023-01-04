4568d03182164697127aba293c0f03b8a40481d1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-04 14:30:58 +0300

    Added task 599

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6374,3 +6374,8 @@ def binary_to_string(binary):
         return ASCII_value
     except:
         return '' if binary == '' else None
+
+# Each exclamation mark's weight is 2; each question mark's weight is 3. Putting two
+# strings left and right on the balance - are they balanced?
+# If the left side is more heavy, return "Left"; if the right side
+# is more heavy, return "Right"; if they are balanced, return "Balance".
