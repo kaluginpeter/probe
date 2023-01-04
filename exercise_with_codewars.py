e02ee5ed2475670dde72743c642219b575c2be54
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-04 14:31:20 +0300

    Complete task 599

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6379,3 +6379,12 @@ def binary_to_string(binary):
 # strings left and right on the balance - are they balanced?
 # If the left side is more heavy, return "Left"; if the right side
 # is more heavy, return "Right"; if they are balanced, return "Balance".
+def balance(left, right):
+    count_l = 0
+    count_r = 0
+    d = {'!':2, '?':3}
+    for i in left:
+        count_l += d[i]
+    for i in right:
+        count_r += d[i]
+    return 'Balance' if count_l == count_r else 'Left' if count_l > count_r else 'Right'
