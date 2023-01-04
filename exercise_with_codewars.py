6647917f260edfd58aa13fa3431889ae05f3bed9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-04 14:26:31 +0300

    Added task 598

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6357,3 +6357,10 @@ def ipv4_address(address):
 # You're given a list of compareable elements:
 def is_monotone(heights):
     return heights == sorted(heights) if heights else True
+
+# Write a function that takes in a binary string and returns the equivalent
+# decoded text (the text is ASCII encoded).
+# Each 8 bits on the binary string represent 1 character on the ASCII table.
+# The input string will always be a valid binary string.
+# Characters can be in the range from "00000000" to "11111111" (inclusive)
+# Note: In the case of an empty binary string your function should return an empty string.
