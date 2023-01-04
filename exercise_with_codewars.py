f755d83f938f237049b971dba7b00037b973a999
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-04 15:03:06 +0300

    Added task 603

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6452,3 +6452,6 @@ def calculator(txt):
 # objects into a flat Array. Your function must only do one level of flattening.
 def flatten(lst):
     return sum(([i] if not isinstance(i, list) else i for i in lst), [])
+
+# Given a square matrix (i.e. an array of subarrays), find the sum of values from the first value of
+# the first array, the second value of the second array, the third value of the third array, and so on...
