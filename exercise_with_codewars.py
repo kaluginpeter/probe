7810622b76175347a843bf0a242070e9e4c86369
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-05 15:05:40 +0300

    Complete task 607

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6510,3 +6510,5 @@ def cake(candles, debris):
 # Complete the method that takes a sequence of objects with two keys each: country or state, and
 # capital. Keys may be symbols or strings.
 # The method should return an array of sentences declaring the state or country and its capital.
+def capital(capitals):
+    return [f"The capital of {c.get('state') or c['country']} is {c['capital']}" for c in capitals]
