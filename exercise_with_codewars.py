953d44a5ed0e8f88f671f2079b4d49f863417bc2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-05 15:24:38 +0300

    Added task 611

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6592,3 +6592,9 @@ def black_or_white_key(key_press_count):
     count = (key_press_count - 1) % 88 % 12
     l = [0, 2, 3, 5, 7, 8, 10]
     return 'white' if count in l else 'black'
+
+# Find the longest substring in alphabetical order.
+# Example: the longest alphabetical substring in "asdfaaaabbbbcttavvfffffdf" is "aaaabbbbctt".
+# There are tests with strings up to 10 000 characters long so your code will need to be efficient.
+# The input will only consist of lowercase characters and will be at least one letter long.
+# If there are multiple solutions, return the one that appears first.
