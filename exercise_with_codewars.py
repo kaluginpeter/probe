23a526fc1689fcfc5283ef7905983e2bd68eb23e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-05 15:30:52 +0300

    Added task 613

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6635,3 +6635,14 @@ def selfie_and_diag1(strng):
     return '\n'.join([x+'|'+y for x,y in zip(op1,op2)])
 def oper(fct, s):
     return fct(s)
+
+# Input : an array of integers.
+# Output : this array, but sorted in such a way that there are two wings:
+# the left wing with numbers decreasing,
+# the right wing with numbers increasing.
+# the two wings have the same length. If the length of the array is odd the wings are around the
+# bottom, if the length is even the bottom is considered to be part of the right wing.
+# each integer l of the left wing must be greater or equal to its counterpart r in the right wing, the
+# difference l - r being as small as possible. In other words the
+# right wing must be nearly as steep as the left wing.
+# The function is make_valley or makeValley or make-valley.
