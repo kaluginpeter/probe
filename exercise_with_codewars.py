0eae9b84e6efac0e622071c4783511f41394bfcd
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-05 15:24:57 +0300

    Complete task 611

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6598,3 +6598,11 @@ def black_or_white_key(key_press_count):
 # There are tests with strings up to 10 000 characters long so your code will need to be efficient.
 # The input will only consist of lowercase characters and will be at least one letter long.
 # If there are multiple solutions, return the one that appears first.
+import re
+def longest(s):
+    matches = re.findall('a*b*c*d*e*f*g*h*i*j*k*l*m*n*o*p*q*r*s*t*u*v*w*x*y*z*', s)
+    current_longest = matches[0]
+    for match in matches:
+        if len(match) > len(current_longest):
+            current_longest = match
+    return current_longest
