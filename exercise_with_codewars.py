165201579a0fc813dc6f97a230e84a8f75cfba7b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-06 14:41:59 +0300

    Added task 623

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6731,3 +6731,10 @@ def mirror(data: list) -> list:
 # Write a function, called sumPPG, that takes two NBA player objects/struct/Hash/Dict/Class and sums their PPG
 def sum_ppg(player_one, player_two):
     return player_one['ppg'] + player_two['ppg']
+
+# This kata requires you to convert minutes (int) to hours and minutes in the format hh:mm (string).
+# If the input is 0 or negative value, then you should return "00:00"
+# Hint: use the modulo operation to solve this challenge. The modulo operation simply returns the remainder
+# after a division. For example the remainder of 5 / 2 is 1, so 5 modulo 2 is 1.
+# Example
+# If the input is 78, then you should return "01:18", because 78 minutes converts to 1 hour and 18 minutes.
