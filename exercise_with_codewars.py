a55ba2e9b76a7366d95da93c48e2969a9e1588f5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-06 14:26:34 +0300

    Added task 620

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6703,3 +6703,11 @@ def reverse(n, count=0):
 # format of date: YYYY/MM/DD. Your job is: Take the years and calculate the difference between them.
 def how_many_years (date1,date2):
     return abs(int(date1.split('/')[0]) - int(date2.split('/')[0]))
+
+# Your task is to find all the elements of an array that are non consecutive.
+# A number is non consecutive if it is not exactly one larger
+# than the previous element in the array. The first element gets a pass and is never considered non consecutive.
+# Create a function name all_non_consecutive
+# E.g., if we have an array [1,2,3,4,6,7,8,15,16] then 6 and 15 are non-consecutive.
+# You should return the results as an array of objects with two values i:
+# <the index of the non-consecutive number> and n: <the non-consecutive number>.
