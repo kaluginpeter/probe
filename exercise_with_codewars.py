20e165ec091b3325a9fb58e3b6f5d42b0a225d71
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-06 14:04:54 +0300

    Added task 617

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6681,3 +6681,12 @@ def to_acronym(inp):
 # returns a single letter string of the first not repeated character in the entire string.
 def no_repeat(string):
     return [i for i in string if string.count(i) == 1][0]
+
+# Create a function that takes a number and returns an array of strings containing the number cut off at each digit.
+# Examples
+# 420 should return ["4", "42", "420"]
+# 2017 should return ["2", "20", "201", "2017"]
+# 2010 should return ["2", "20", "201", "2010"]
+# 4020 should return ["4", "40", "402", "4020"]
+# 80200 should return ["8", "80", "802", "8020", "80200"]
+# PS: The input is guaranteed to be an integer in the range [0, 1000000]
