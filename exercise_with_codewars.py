aaa38685d3687142f288baf48bf9ef2382cd1c01
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-07 17:47:01 +0300

    Added task 634

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6904,3 +6904,10 @@ def solve(a,b):
 # Result should be separated by comma and space.
 def geometric_sequence_elements(a, r, n):
     return ", ".join(str(a * r ** i) for i in range(n))
+
+# You are working at a lower league football stadium and you've been tasked with automating the scoreboard.
+# The referee will shout out the score, you have already set up the voice recognition module
+# which turns the ref's voice into a string, but the spoken score needs to be converted into
+# a pair for the scoreboard!
+# e.g. "The score is four nil" should return [4,0]
+# Either teams score has a range of 0-9, and the ref won't say the same string every time e.g.
