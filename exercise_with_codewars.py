32cfdb026e42cefd66a35df9013f612b627b4138
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-07 13:07:25 +0300

    Added task 629

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6813,3 +6813,11 @@ def paul(x):
     d = {'kata':5, 'Petes kata':10, 'life':0, 'eating':1}
     count = sum(d[i] for i in x)
     return 'Super happy!' if count < 40 else 'Happy!' if 40 <= count < 70 else 'Sad!' if 70 <= count < 100 else 'Miserable!'
+
+# It's important day today: the class has just had a math test. You will be given a list of marks.
+# Complete the function that will:
+# Calculate the average mark of the whole class and round it to 3 decimal places.
+# Make a dictionary/hash with keys "h", "a", "l" to make clear how many high, average and
+# low marks they got. High marks are 9 & 10, average marks are 7 & 8, and low marks are 1 to 6.
+# Return list [class_average, dictionary] if there are different type of marks, or [class_average,
+# dictionary, "They did well"] if there are only high marks.
