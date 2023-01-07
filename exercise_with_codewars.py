1a9c9ca72f32bc05165e99de2a38de331de1cf72
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-07 12:56:07 +0300

    Added task 628

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6805,3 +6805,7 @@ def ghostbusters(building):
 # In languages that do not have an empty option, just return None
 def last(lst):
     return lst[-1] if lst else None
+
+# Paul is an excellent coder and sits high on the CW leaderboard. He solves kata like a banshee but would
+# also like to lead a normal life, with other activities. But he just can't stop solving all the kata!!
+# Given an array (x) you need to calculate the Paul Misery Score. The values are worth the following points:
