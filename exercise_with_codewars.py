d4d5f094cd75555ce1b15f3c3d0cb07e2bcfc278
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-07 17:49:31 +0300

    Added task 635

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6918,3 +6918,6 @@ def scoreboard(string):
         if elem in d:
             score.append(d[elem])
     return score
+
+# In this Kata the aim is to compare each pair of integers from 2 arrays, and return a new array of large numbers.
+# Note: Both arrays have the same dimensions.
