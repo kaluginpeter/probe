ca4bab58df2376ae9c5eaef40fd145c6ba349604
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-07 12:27:13 +0300

    Added task 627

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6798,3 +6798,8 @@ def list_to_array(lst):
 # So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
 def ghostbusters(building):
     return building.replace(' ', '') if ' ' in building else "You just wanted my autograph didn't you?"
+
+# Write a function last that accepts a list and returns the last element in the list.
+# If the list is empty:
+# In languages that have a built-in option or result type (like OCaml or Haskell), return an empty option
+# In languages that do not have an empty option, just return None
