ea42aca4ac51c70120de1187405550e56a6ccf9d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-07 12:56:30 +0300

    Complete task 628

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6809,3 +6809,7 @@ def last(lst):
 # Paul is an excellent coder and sits high on the CW leaderboard. He solves kata like a banshee but would
 # also like to lead a normal life, with other activities. But he just can't stop solving all the kata!!
 # Given an array (x) you need to calculate the Paul Misery Score. The values are worth the following points:
+def paul(x):
+    d = {'kata':5, 'Petes kata':10, 'life':0, 'eating':1}
+    count = sum(d[i] for i in x)
+    return 'Super happy!' if count < 40 else 'Happy!' if 40 <= count < 70 else 'Sad!' if 70 <= count < 100 else 'Miserable!'
