73e86d53a9c2658cba9a060683f97b3b2deb8c16
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-07 12:25:46 +0300

    Complete task 626

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6796,3 +6796,5 @@ def list_to_array(lst):
 # Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
 # In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
 # So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+def ghostbusters(building):
+    return building.replace(' ', '') if ' ' in building else "You just wanted my autograph didn't you?"
