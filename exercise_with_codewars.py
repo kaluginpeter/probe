8cf49ef80f4f4e52e14993e5134089a932091f4d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-08 15:23:43 +0300

    Added task 642

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7000,3 +7000,9 @@ class create_message(str):
 def bald(s):
     c = 'Clean!' if s.count('/') == 0 else 'Unicorn!' if s.count('/') == 1 else 'Homer!' if s.count('/') == 2 else 'Careless!' if 3 <= s.count('/') <= 5 else 'Hobo!'
     return [s.replace('/', '-'), c]
+
+# Complete the function that takes a list of numbers (nums), as the only argument to the function.
+# Take each number in the list and square it if it is even, or square root the
+# number if it is odd. Take this new list and return the sum of it, rounded to two decimal places.
+# The list will never be empty and will only contain values that are greater than or equal to zero.
+# Good luck!
