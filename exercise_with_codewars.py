c3345647a98cf7de5e91c3d7fea3314bf2b70b74
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-08 15:11:02 +0300

    Complete task 640

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6980,3 +6980,6 @@ def arithmetic_sequence_sum(a, r, n):
 # Implement a function that receives a string, and lets you extend it with repeated calls. When no argument
 # is passed you should return a string consisting of space-separated words you've received earlier.
 # Note: there will always be at least 1 string; all inputs will be non-empty.
+class create_message(str):
+    def  __call__(self, s=""):
+        return create_message((self+" "+s).rstrip())
