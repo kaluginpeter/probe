c23717ccc1e17de8c49db21bccab390abcd01db8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-08 15:03:33 +0300

    Added task 639

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -6966,3 +6966,11 @@ def last(*args):
         return [args][-1][-1][-1]
     except:
         return args[-1]
+
+# In your class, you have started lessons about "arithmetic progression". Because you are also a programmer,
+# you have decided to write a function.
+# This function, arithmetic_sequence_sum(a, r, n), should return the sum of the
+# first (n) elements of a sequence in which each element is the sum of the given integer
+# (a), and a number of occurences of the given integer (r), based on the element's position within the sequence.
+# For example:
+# arithmetic_sequence_sum(2, 3, 5) should return 40:
