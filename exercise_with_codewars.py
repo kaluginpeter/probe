b5adc176988d77249123d782c089dd9f399f5943
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-08 15:30:12 +0300

    Added task 643

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7008,3 +7008,13 @@ def bald(s):
 # Good luck!
 def sum_square_even_root_odd(nums):
     return round(sum(i**2 if i%2==0 else i **.5 for i in nums), 2)
+
+# In this Kata, you will be given an array of arrays and your task will be to return the
+# number of unique arrays that can be formed by picking exactly one element from each subarray.
+# For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites.
+# They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+# Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4,
+# since the extra outcomes are just duplicates.
+# See test cases for more examples.
+# Good luck!
+# If you like this Kata, please try:
