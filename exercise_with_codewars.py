15c10a44a5bc58981161a2ecc43959e1a1d3a4d5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-09 12:25:48 +0300

    Added task 646

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7047,3 +7047,11 @@ def solution(stones):
         for match in matches:
             res += len(match)-1
     return res
+
+# For this Kata you will be given an array of numbers and another number n.
+# You have to find the sum of the n largest numbers of the array
+# and the product of the n smallest numbers of the array, and compare the two.
+# If the sum of the n largest numbers is higher, return "sum"
+# If the product of the n smallest numbers is higher, return "product"
+# If the 2 values are equal, return "same"
+# Note The array will never be empty and n will always be smaller than the length of the array.
