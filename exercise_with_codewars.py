e9f7d99223069328336243d917916004b2817b6b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-09 21:22:13 +0300

    Added task 648

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7085,3 +7085,9 @@ def bin_rota(arr):
         else:
             l += arr[i][::-1]
     return l
+
+# You are given an array of non-negative integers, your task is to complete the series from 0 to
+# the highest number in the array.
+# If the numbers in the sequence provided are not in order you should order them,
+# but if a value repeats, then you must return a sequence with only one item,
+# and the value of that item must be 0. like this:
