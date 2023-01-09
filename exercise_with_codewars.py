9d7c875c5ce7ed7a980759ee81924cda9e9a5f62
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-09 21:50:02 +0300

    Complete task 653

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7151,3 +7151,5 @@ def int_diff(lst, n):
 # "Boom" if it is divisible by 5, "BangBoom" if it divisible by 3 and 5,
 # and "Miss" if it isn't divisible by any of them. Note: Your program should only return one value
 # Ex: Input: 105 --> Output: "BangBoom" Ex: Input: 9 --> Output: "Bang" Ex:Input: 25 --> Output: "Boom"
+def multiple(x):
+    return 'Bang' if x%3==0 and x%5!=0 else 'Boom' if x%3!=0 and x%5==0 else 'BangBoom' if x%3==0 and x%5==0 else 'Miss'
