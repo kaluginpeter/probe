f37619a82bc4fd41a740d02f81fe584df76bc210
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-09 20:35:08 +0300

    Added task 647

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7060,3 +7060,19 @@ def sum_or_product(array, n):
     big = sum(sorted(array)[-n:])
     small = functools.reduce(lambda a, b : a * b, sorted(array)[:n])
     return 'sum' if big > small else 'product' if small > big else 'same'
+
+# A startup office has an ongoing problem with its bin.
+# Due to low budgets, they don't hire cleaners. As a result, the staff are left to voluntarily empty the bin.
+# It has emerged that a voluntary system is not working and the bin is often overflowing.
+# One staff member has suggested creating a rota system based upon the staff seating plan.
+# Create a function binRota that accepts a 2D array of names.
+# The function will return a single array containing staff names in the order that they should empty the bin.
+# Adding to the problem, the office has some temporary staff.
+# This means that the seating plan changes every month. Both staff members'
+# names and the number of rows of seats may change. Ensure that the function
+# binRota works when tested with these changes.
+# Notes:
+# All the rows will always be the same length as each other.
+# There will be no empty spaces in the seating plan.
+# There will be no empty arrays.
+# Each row will be at least one seat long.
