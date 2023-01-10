a270e17a698a65610f166c13b66b0946857a5a30
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-10 15:34:15 +0300

    Added task 664

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7298,3 +7298,8 @@ class List(object):
     def count_spec_digits(self, integers_list, digits_list):
         s = "".join(str(i) for i in integers_list)
         return [(dig, s.count(str(dig))) for dig in digits_list]
+
+# Given 2 string parameters, show a concatenation of:
+# the reverse of the 2nd string with inverted case; e.g Fish -> HSIf
+# a separator in between both strings: @@@
+# the 1st string reversed with inverted case and then mirrored; e.g Water -> RETAwwATER
