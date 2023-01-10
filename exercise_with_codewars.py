0dfe6ca7bb7e153b6d20ebb17d1487232d6bcfa1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-11 01:31:20 +0300

    Complete task 672

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7379,3 +7379,7 @@ def word_to_bin(word):
 # Good luck!
 # If you like this Kata, try:
 # Dominant primes. It takes this idea a step further.
+def is_prime(n):
+    return n >= 2 and all(n%i for i in range(2, 1+int(n**.5)))
+def total(arr):
+    return sum(n for i, n in enumerate(arr) if is_prime(i))
