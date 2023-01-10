dae22a7585e63f4315692230b181e92b7baff4be
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-11 01:05:32 +0300

    Complete task 668

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7343,3 +7343,6 @@ def days_represented(trips):
 # Let cost of all liquid be x
 # Your function should return string like this : 'It is (weekday) today, (name),
 # you have to work, you must spray (number) trees and you need (x) dollars to buy liquid'
+def task(w,n,c):
+    workers = {"Monday" : "James", "Tuesday" : "John", "Wednesday" : "Robert", "Thursday" : "Michael", "Friday" : "William"}
+    return f"It is {w} today, {workers[w]}, you have to work, you must spray {n} trees and you need {n * c} dollars to buy liquid"
