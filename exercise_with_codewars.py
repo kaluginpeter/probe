2bded80fd8326d371cf013961e70eaec056924ba
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-11 00:58:24 +0300

    Complete task 666

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7315,3 +7315,6 @@ def random_case(x):
 
 # You have to write a function pattern which creates the following pattern upto n number of rows.
 # If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
+def pattern(n):
+    l = list(range(1, n + 1))
+    return '\n'.join(''.join(map(str, l[i:])) for i in range(n))
