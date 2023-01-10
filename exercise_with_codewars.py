6f29896feecc383b82ede7beb7f4887960a6afe4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-10 15:29:47 +0300

    Added task 663

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7284,3 +7284,13 @@ def owned_cat_and_dog(cy, dy):
     cat = 0 if cy < 15 else 1 if cy < 24 else 2 + (cy - 24) // 4
     dog = 0 if dy < 15 else 1 if dy < 24 else 2 + (dy - 24) // 5
     return [cat, dog]
+
+# We need a method in the List Class that may count specific digits from a given list of integers.
+# This marked digits will be given in a second list. The method .count_spec_digits()/.countSpecDigits() will
+# accept two arguments, a list of an uncertain amount of integers integers_lists/integersLists (and of an uncertain
+# amount of digits, too) and a second list, digits_list/digitsList that has the specific digits to
+# count which length cannot be be longer than 10 (It's obvious, we've got ten digits).
+# The method will output a list of tuples, each tuple having two elements, the first
+# one will be a digit to count, and second one, its corresponding total frequency in all
+# the integers of the first list. This list of tuples should be ordered with the same order that the digits
+# have in digitsList
