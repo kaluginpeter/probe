2ac9d8d5e45f44a192f653294ddb6ec5bee0790e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-10 15:27:28 +0300

    Complete task 662

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7280,3 +7280,7 @@ def name_in_str(str, name):
 # 15 dog years for first year
 # +9 dog years for second year
 # +5 dog years for each year after that
+def owned_cat_and_dog(cy, dy):
+    cat = 0 if cy < 15 else 1 if cy < 24 else 2 + (cy - 24) // 4
+    dog = 0 if dy < 15 else 1 if dy < 24 else 2 + (dy - 24) // 5
+    return [cat, dog]
