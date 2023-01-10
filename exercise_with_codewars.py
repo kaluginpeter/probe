96dfe873737c7d699e369740d9912ebae42dec11
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-10 15:34:38 +0300

    Complete task 664

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7303,3 +7303,5 @@ class List(object):
 # the reverse of the 2nd string with inverted case; e.g Fish -> HSIf
 # a separator in between both strings: @@@
 # the 1st string reversed with inverted case and then mirrored; e.g Water -> RETAwwATER
+def reverse_and_mirror(s1, s2):
+    return f"{s2[::-1].swapcase()}@@@{s1[::-1].swapcase()}{s1.swapcase()}"
