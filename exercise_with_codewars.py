5e1a2b74d7ce1c361be5bc1bd59b94818345b83e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-11 01:01:26 +0300

    Added task 667

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7318,3 +7318,12 @@ def random_case(x):
 def pattern(n):
     l = list(range(1, n + 1))
     return '\n'.join(''.join(map(str, l[i:])) for i in range(n))
+
+# How many days are we represented in a foreign country?
+# My colleagues make business trips to a foreign country. We must find the number of days our
+# company is represented in a country. Every day that one or more colleagues are present in the
+# country is a day that the company is represented. A single day cannot count for more than one day.
+# Write a function that recieves a list of pairs and returns the number of days that
+# the company is represented in the foreign country. The first number of the pair
+# is the number of the day of arrival and the second number of the pair is the day
+# of departure of someone who travels, i.e. 1 january is number 1 and 31 of december is 365.
