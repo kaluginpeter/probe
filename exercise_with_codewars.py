b88a7bd2d784950b38290ac6ed01d3de017b0b7f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-11 01:21:25 +0300

    Complete task 671

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7370,3 +7370,5 @@ def find_e(s):
 # Write a function that takes a string and returns an array containing binary
 # numbers equivalent to the ASCII codes of the characters of the string. The binary strings should
 # be eight digits long.
+def word_to_bin(word):
+    return [bin(ord(letter))[2:].zfill(8) for letter in word]
