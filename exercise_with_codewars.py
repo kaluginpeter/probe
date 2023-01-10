5157410c3c19408a6cf88c9b78004b46efe3fda6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-11 01:10:02 +0300

    Complete task 669

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7350,3 +7350,5 @@ def task(w,n,c):
 # Write a function consonantCount, consonant_count or ConsonantCount that takes a string of English-language
 # text and returns the number of consonants in the string.
 # Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
+def consonant_count(s):
+    return sum([1 if i not in 'aeiou' and i.isalpha() else 0 for i in s.replace(' ', '').lower()])
