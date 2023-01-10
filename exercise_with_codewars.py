60d3fa715bcc11f112761260ceb0dc65f8ca611b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-10 13:53:45 +0300

    Added task 656

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7179,3 +7179,13 @@ def my_parse_int(string):
         return int(string)
     except:
         return 'NaN'
+
+# Your job is to create a class called Song.
+# A new Song will take two parameters, title and artist.
+# mount_moose = Song('Mount Moose', 'The Snazzy Moose')
+# mount_moose.title => 'Mount Moose'
+# mount_moose.artist => 'The Snazzy Moose'
+# You will also have to create an instance method named howMany() (or how_many()).
+# The method takes an array of people who have listened to the song that day.
+# The output should be how many new listeners the song gained on that day out of all listeners.
+# Names should be treated in a case-insensitive manner, i.e. "John" is the same as "john".
