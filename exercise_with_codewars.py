0b659e002b8f49ec2f38f63f77ae440a109e7e79
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-11 13:10:58 +0300

    Complete task 673

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7391,3 +7391,11 @@ def total(arr):
 # The distance that the snail crawls during the day (meters)
 # The distance that the snail slides down during the night (meters)
 # Calculate number of day when the snail will reach the top of the column.
+def snail(column, day, night):
+    count = 0
+    while column > 0:
+        count += 1
+        if column - day <= 0:
+            return count
+        column -= day - night
+    return count
