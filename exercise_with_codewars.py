d3ec75229653a06960d727cc893682831bca608f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-11 13:23:41 +0300

    Added task 675

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7403,3 +7403,13 @@ def snail(column, day, night):
 # Complete the solution so that it returns the number of times the search_text is found within the full_text.
 def solution(full_text, search_text):
     return full_text.count(search_text)
+
+# A binary gap within a positive number num is any sequence of consecutive zeros that is
+# surrounded by ones at both ends in the binary representation of num.
+# For example:
+# 9 has binary representation 1001 and contains a binary gap of length 2.
+# 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3.
+# 20 has binary representation 10100 and contains one binary gap of length 1.
+# 15 has binary representation 1111 and has 0 binary gaps.
+# Write function gap(num) that,  given a positive num,  returns the length of its longest binary gap.
+# The function should return 0 if num doesn't contain a binary gap.
