7730c71edc5f19e5e08cae6fe88de61c23837fbb
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-12 16:06:13 +0300

    Complete task 683

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7539,3 +7539,5 @@ def solve(a, b):
 # punctuation of any sort lead them to raise their voices and put them in danger.
 # The young borrowers have begged their parents to stop using caps and punctuation.
 # Change the input text s to new borrower speak. Help save the next generation!
+def borrow(s):
+    return s.replace(' ', '').replace('!', '').replace('?', '').replace('.', '').replace(',', '').replace(';', '').replace(':', '').lower()
