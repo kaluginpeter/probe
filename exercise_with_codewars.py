89be6608f49eb1ef3924cb73bab7d8046e8c41f0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-12 15:50:16 +0300

    Complete task 681

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7500,3 +7500,9 @@ def sponge_meme( s ):
 # 'middle' letter of the word where it is.
 # An example should clarify:
 # 'taxi' would become 'atix' 'taxis' would become 'atxsi'
+import re
+def inside_out(s):
+    return re.sub(r'\S+', lambda m: inside_out_word(m.group()), s)
+def inside_out_word(s):
+    i, j = len(s) // 2, (len(s) + 1) // 2
+    return s[:i][::-1] + s[i:j] + s[j:][::-1]
