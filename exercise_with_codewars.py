e7f5e5d929abc3a1d721b8211a0df86bac98bb40
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-12 16:27:19 +0300

    Complete task 684

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7548,3 +7548,5 @@ def borrow(s):
 # If there are more pairs of a certain number, count each pair only once.
 # E.g.: for [0, 0, 0, 0] the return value is 2 (= 2 pairs of 0s)
 # Random tests: maximum array length is 1000, range of values in array is between 0 and 1000
+def duplicates(arr):
+    return sum(arr.count(i) // 2 for i in set(arr))
