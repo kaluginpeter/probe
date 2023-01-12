82c2539cb58e4a4de037be76cf9dc610051b37f4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-12 16:26:51 +0300

    Added task 684

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7541,3 +7541,10 @@ def solve(a, b):
 # Change the input text s to new borrower speak. Help save the next generation!
 def borrow(s):
     return s.replace(' ', '').replace('!', '').replace('?', '').replace('.', '').replace(',', '').replace(';', '').replace(':', '').lower()
+
+# You are given array of integers, your task will be to count all pairs in that array and return their count.
+# Notes:
+# Array can be empty or contain only one value; in this case return 0
+# If there are more pairs of a certain number, count each pair only once.
+# E.g.: for [0, 0, 0, 0] the return value is 2 (= 2 pairs of 0s)
+# Random tests: maximum array length is 1000, range of values in array is between 0 and 1000
