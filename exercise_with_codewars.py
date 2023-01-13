95770ce401953a7927ef096bfcdc7f1a7808b71e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-13 19:28:50 +0300

    Complete task 695

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7646,3 +7646,5 @@ def words_to_sentence(words):
 # [["name", "Jeremy"], ["age", 24], ["role", "Software Engineer"]]
 # Note: The output array should be sorted alphabetically.
 # Good Luck!
+def convert_hash_to_array(hash):
+    return sorted([[k, v] for k,v in hash.items()])
