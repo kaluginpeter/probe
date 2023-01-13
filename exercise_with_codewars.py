856b13e202d2b7811fcf185a97e83eeb06940b56
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-13 19:10:40 +0300

    Complete task 691

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7617,3 +7617,5 @@ def search_names(logins):
 # Python dictionaries are inherently unsorted. So what do you do if you need to sort the contents of a dictionary?
 # Create a function that returns a sorted list of (key, value) tuples (Javascript: arrays of 2 items).
 # The list must be sorted by the value and be sorted largest to smallest.
+def sort_dict(d):
+  return sorted(d.items(), key=lambda x: x[1], reverse=True)
