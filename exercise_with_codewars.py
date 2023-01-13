f4af12409ce3e05345e4fe3b109289b021e3f72b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-13 19:23:24 +0300

    Added task 694

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7635,3 +7635,5 @@ def sum_from_string(string):
 # Example: the number 6 is a triangular number because all sides of a triangle has the same amount of points.
 def is_triangular(t):
     return (8 * t + 1)**.5 % 1 == 0
+
+# Write function which will create a string from a list of strings, separated by space.
