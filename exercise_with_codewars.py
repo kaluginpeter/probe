29c02d076ac44d3820707c64d67bbe49a9ddd84d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-13 19:15:01 +0300

    Added task 692

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7619,3 +7619,11 @@ def search_names(logins):
 # The list must be sorted by the value and be sorted largest to smallest.
 def sort_dict(d):
   return sorted(d.items(), key=lambda x: x[1], reverse=True)
+
+# Given a random string consisting of numbers, letters, symbols, you need to sum up the numbers in the string.
+# Note:
+# Consecutive integers should be treated as a single number. eg, 2015 should be
+# treated as a single number 2015, NOT four numbers
+# All the numbers should be treaded as positive integer. eg, 11-14
+# should be treated as two numbers 11 and 14. Same as 3.14, should be treated as two numbers 3 and 14
+# If no number was given in the string, it should return 0
