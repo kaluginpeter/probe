3a4a66dfdc7f079be2c3bb55ba04d35b930b441b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-13 18:48:45 +0300

    Added task 687

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7578,3 +7578,12 @@ def decode(r):
 # For example:
 def to_currency(price):
   return '{:,}'.format(price)
+
+# Description:
+# Given a string, you need to write a method that order every letter in this string in ascending order.
+# Also, you should validate that the given string is not empty or null. If so, the method should return:
+# "Invalid String!"
+# Notes
+# • the given string can be lowercase and uppercase.
+# • the string could include spaces or other special characters like '# ! or ,'. Sort them based on their
+# ASCII codes
