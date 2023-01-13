c926cebd67159ba7d744de3579e5827ee313b4ed
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-13 19:21:28 +0300

    Complete task 693

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7633,3 +7633,5 @@ def sum_from_string(string):
 
 # Triangular number is any amount of points that can fill an equilateral triangle.
 # Example: the number 6 is a triangular number because all sides of a triangle has the same amount of points.
+def is_triangular(t):
+    return (8 * t + 1)**.5 % 1 == 0
