99993e1e7d015d3ad11e56279c4b13613fdd7a93
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-14 22:17:34 +0300

    Complete task 704 N 7777 strings

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7771,3 +7771,7 @@ def lineup_students(s):
 # 9 -> "Laundry Basket"
 # The returned value should be a string in the form of "First Name Last Name".
 # For example, a birthday of November 18 would return "The Terrifying Teaspoon"
+def get_villain_name(birthdate):
+    first = [ "The Evil","The Vile","The Cruel", "The Trashy","The Despicable", "The Embarrassing", "The Disreputable","The Atrocious", "The Twirling",  "The Orange","The Terrifying", "The Awkward"]
+    last = ["Mustache", "Pickle", "Hood Ornament", "Raisin", "Recycling Bin", "Potato", "Tomato", "House Cat", "Teaspoon", "Laundry Basket"]
+    return first[birthdate.month - 1] + ' ' + last[int(str(birthdate.day)[-1])]
