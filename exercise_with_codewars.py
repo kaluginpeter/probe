36c23cd35e624565fb26769ce5515ca3a9e92c61
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-14 21:44:42 +0300

    Complete task 700!!! GOAT Here We Go Again

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7712,3 +7712,6 @@ def climb(n):
 
 # Given an array of strings, reverse them and their order in such way that their length
 # stays the same as the length of the original inputs.
+def reverse(a):
+    l = reversed(''.join(a))
+    return [''.join(next(l) for k in i) for i in a]
