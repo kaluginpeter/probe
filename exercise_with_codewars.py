e198a586e9a0542308f63f839c6cb586e0b5d02d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-14 21:59:52 +0300

    Added task 703

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7733,3 +7733,10 @@ def generate_pairs(n):
 import re
 def password(s):
     return bool(re.match(r'(?=.*[A-Z])(?=.*[a-z])(?=.*\d).{8}', s))
+
+# Suzuki needs help lining up his students!
+# Today Suzuki will be interviewing his students to ensure they are progressing in their training.
+# He decided to schedule the interviews based on the length of the students name in descending order.
+# The students will line up and wait for their turn.
+# You will be given a string of student names. Sort them and return a list of names in descending order.
+# Here is an example input:
