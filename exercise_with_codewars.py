cf48e005b855947580e6c360a657cf90d3d9f99f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-15 12:16:45 +0300

    Complete task 706

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7797,3 +7797,5 @@ def starts_with(st, prefix):
 # If a word starts with a number or special character, skip the word and leave it out of the result.
 # Input String will not be empty.
 # For an input String: "hey You, Sort me Already!" the function should return: "You, Sort Already! hey me"
+def capitals_first(string):
+    return ' '.join([i for i in string.split() if i[0].isupper()] + [i for i in string.split() if i[0].islower()])
