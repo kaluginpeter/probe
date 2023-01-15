4c9fed9217fe520b64bae243e8b9c7533505bf8f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-15 12:53:47 +0300

    Added task 709

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7816,3 +7816,9 @@ def baby_shark_lyrics():
     for i in l:
         res += f"{i}, doo doo doo doo doo doo\n"*3 + i+'!\n'
     return res + "Run away,…"
+
+# The hamming distance of two equal-length strings is the number of positions, in which the two string differ.
+# In other words, the number of character substitutions required to transform one string into the other.
+# For this first Kata, you will write a function hamming_distance(a, b) with two equal-length strings
+# containing only 0s and 1s as parameters. There is no need to test the parameters for validity
+# (but you can, if you want).The function's output should be the hamming distance of the two strings as an integer.
