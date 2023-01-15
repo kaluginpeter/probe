42e6174d1d0e19c7440570ef0a2426a90d1fdd3d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-15 13:07:14 +0300

    Complete task 711

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7834,3 +7834,6 @@ def is_all_possibilities(arr):
 # You are given a string representing a number in binary. Your task is to delete
 # all the unset bits in this string and return the corresponding number (after keeping only the '1's).
 # In practice, you should implement this function:
+def eliminate_unset_bits(number):
+    i = number.replace('0', '')
+    return int(i, 2) if i  else 0
