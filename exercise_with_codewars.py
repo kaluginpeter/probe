8ace4afa771e45403c9cd2887e3699310f3f09bf
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-15 13:00:03 +0300

    Complete task 710

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7828,3 +7828,5 @@ def hamming_distance(a, b):
 # A non-empty array a of length n is called an array of all possibilities if it contains all numbers
 # between [0,a.length-1].Write a method named isAllPossibilities that accepts an integer array and
 # returns true if the array is an array of all possibilities, else false.
+def is_all_possibilities(arr):
+    return sorted(arr) == list(range(0, max(arr)+1)) if arr else False
