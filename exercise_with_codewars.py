a07f6ad61bfc5cc3119cec8c4fa4b873e16c921c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-16 19:47:46 +0300

    Complete task 716

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7883,3 +7883,5 @@ def solve(st):
 # Given a string as input, move all of its vowels to the end of the string, in the same order as they were before.
 # Vowels are (in this kata): a, e, i, o, u
 # Note: all provided input strings are lowercase.
+def move_vowels(s):
+    return ''.join(sorted(s, key=lambda x: x in 'aeiou'))
