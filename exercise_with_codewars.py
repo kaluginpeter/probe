e5cba2b9cd0fa9e6697b9666aeb45a9ab45364af
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-16 20:44:57 +0300

    Complete task 719

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7905,3 +7905,5 @@ def avg_array(arrs):
 # Let's assume we need "clean" strings. Clean means a string should only contain letters a-z, A-Z and spaces.
 # We assume that there are no double spaces or line breaks.
 # Write a function that takes a string and returns a string without the unnecessary characters.
+def remove_chars(s):
+    return ''.join(i for i in s if i.isalpha() or i == ' ')
