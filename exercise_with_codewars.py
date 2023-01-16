b4f0270553668df3007d887449651f9c92e29a00
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-16 21:15:14 +0300

    Added task 725

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7952,3 +7952,16 @@ def strange_math(n, k):
 # "abcd" are ["a","ab","abc"]. The suffixes are ["bcd", "cd", "d"]. You should not overlap the prefix and suffix.
 def solve(st):
     return next((i for i in range(len(st) // 2, 0, -1) if st[:i] == st[-i:]), 0)
+
+# I've got a crazy mental illness. I dislike numbers a lot. But it's a
+# little complicated: The number I'm afraid of depends on which day of the week it is...
+# This is a concrete description of my mental illness:
+# Monday --> 12
+# Tuesday --> numbers greater than 95
+# Wednesday --> 34
+# Thursday --> 0
+# Friday --> numbers divisible by 2
+# Saturday --> 56
+# Sunday --> 666 or -666
+# Write a function which takes a string (day of the week) and an integer
+# (number to be tested) so it tells the doctor if I'm afraid or not. (return a boolean)
