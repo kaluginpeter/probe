ffd307b849ec8be7d943420dfe76f1c71d90ca6c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-16 21:01:37 +0300

    Added task 722

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7920,3 +7920,9 @@ def encode(string):
 # on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace'].
 def spacey(array):
     return [''.join(array[:i]) for i in range(1, len(array) + 1)]
+
+# Given two arrays of integers m and n, test if they contain at least one identical element.
+# Return true if they do; false if not.
+# Your code must handle any value within the range of a 32-bit integer,
+# and must be capable of handling either array being empty (which is a false result, as
+# there are no duplicated elements).
