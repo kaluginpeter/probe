3f8709a0cb504f334970b234f645f0b4bf5d7a42
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-16 20:41:55 +0300

    Complete task 718

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7899,3 +7899,5 @@ def explode(arr):
 # Create a function that takes a 2D array as an input, and outputs another array that contains
 # the average values for the numbers in the nested arrays at the corresponding indexes.
 # Note: the function should also work with negative numbers and floats.
+def avg_array(arrs):
+    return [sum(i) / len(i) for i in zip(*arrs)]
