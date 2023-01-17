191468ed9d4c2f2903f2e7e52dac7bd8dac6de21
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-17 18:20:27 +0300

    Added task 728

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7983,3 +7983,10 @@ def next_pal(val):
 # 0s for 1s and all the 1s for 0s. For example:
 def ones_complement(binary_number):
     return ''.join('1' if i == '0' else '0' for i in binary_number)
+
+# In the drawing below we have a part of the Pascal's triangle, horizontal lines are numbered from zero (top).
+# We want to calculate the sum of the squares of the binomial coefficients on
+# a given horizontal line with a function called easyline (or easyLine or easy-line).
+# Can you write a program which calculate easyline(n) where n is the horizontal line number?
+# The function will take n (with: n>= 0) as parameter and will return the sum of the squares
+# of the binomial coefficients on line n.
