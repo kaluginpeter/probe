c88cf1192655fcbeeb92acdc4fe9cdbd1e76d957
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-17 19:07:52 +0300

    Complete task 733

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8039,3 +8039,5 @@ def get_turkish_number(n):
 # Given an array containing only integers, add all the elements and return the binary equivalent of that sum.
 # If the array contains any non-integer element (e.g. an object, a float, a string and so on), return false.
 # Note: The sum of an empty array is zero.
+def arr2bin(arr):
+    return bin(sum(arr))[2:] if all(type(i) == int for i in arr) else False
