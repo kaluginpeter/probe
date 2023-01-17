fa10863318e97642fc1223cf30897223ef3f8b16
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-17 17:59:37 +0300

    Complete task 726

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7973,3 +7973,8 @@ def am_I_afraid(day,num):
 # may search about them using your favourite search engine.
 # In this kata you will be given a positive integer, val and you have to create the function next_pal
 # ()(nextPal Javascript) that will output the smallest palindrome number higher than val.
+def next_pal(val):
+    val += 1
+    while str(val) != str(val)[::-1]:
+        val += 1
+    return val
