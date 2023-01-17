3f0778d5b91560abd36909089ff3ed920e8a23dc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-17 18:28:46 +0300

    Added task 729

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -7992,3 +7992,7 @@ def ones_complement(binary_number):
 # of the binomial coefficients on line n.
 def easyline(n):
     return easyline(n - 1) * (4 * n - 2) // n if n else 1
+
+# Implement a function that receives two integers m and n and generates a sorted list
+# of pairs (a, b) such that m <= a <= b <= n.
+# The input m will always be smaller than or equal to n (e.g., m <= n)
