4709bdc5fad1af35061081eda8b4a5696c98cd36
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-18 17:34:07 +0300

    Added task 741

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8118,3 +8118,10 @@ def get_count(words=""):
     vowel = "".join([c for c in letter if c in 'aeiou'])
     conson = "".join([c for c in letter if c not in 'aeiou'])
     return {'vowels': len(vowel),'consonants': len(conson)}
+
+# We have the number 12385. We want to know the value of the closest cube but higher
+# than 12385. The answer will be 13824.
+# Now, another case. We have the number 1245678. We want to know the 5th power, closest and
+# higher than that number. The value will be 1419857.
+# We need a function find_next_power ( findNextPower in JavaScript, CoffeeScript and Haskell), that receives two
+# arguments, a value val, and the exponent of the power, pow_, and outputs the value that
