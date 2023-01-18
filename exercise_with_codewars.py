e4585a5d277b2c1f172aba9dc04e7ff49047ad99
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-18 17:34:30 +0300

    Complete task 741

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8125,3 +8125,5 @@ def get_count(words=""):
 # higher than that number. The value will be 1419857.
 # We need a function find_next_power ( findNextPower in JavaScript, CoffeeScript and Haskell), that receives two
 # arguments, a value val, and the exponent of the power, pow_, and outputs the value that
+def find_next_power(val, pow_):
+    return int(val ** (1.0 / pow_) + 1) ** pow_
