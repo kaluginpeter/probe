98989c853e2c396811bf4e234383ccc63a2c9623
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-18 17:38:43 +0300

    Complete task 742

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8133,3 +8133,5 @@ def find_next_power(val, pow_):
 # Given an array of numbers, your function should return an array of arrays, where
 # each subarray contains all the duplicates of a particular number. Subarrays should be in the same
 # order as the first occurence of the number they contain:
+def group(arr):
+    return [[v] * arr.count(v) for k, v in enumerate(arr) if arr.index(v) == k]
