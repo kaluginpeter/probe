15cdff88525586ac733739d10fba0f5a1e02e383
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-19 19:22:35 +0300

    Complete task 753

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8252,3 +8252,8 @@ def fix(paragraph):
 # returns true  / True if every element in an array is an integer or a float with no decimals.
 # returns true  / True if array is empty.
 # returns false / False for every other input.
+def is_int_array(arr):
+    try:
+        return arr == list(map(int, arr))
+    except:
+        return False
