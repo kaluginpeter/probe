5727481a665fe20958d6a3c86dfd0ef2f522ed02
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-19 18:15:58 +0300

    Added task 746

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8174,3 +8174,16 @@ def sum_no_duplicates(l):
 from collections import Counter
 def combine(*args):
     return sum((Counter(i) for i in args), Counter())
+
+# Create a function that takes an array of letters, and combines them into words in a sentence.
+# The array will be formatted as so:
+# [
+#   ['J','L','L','M'],
+#   ['u','i','i','a'],
+#   ['s','v','f','n'],
+#   ['t','e','e','']
+# ]
+# The function should combine all the 0th indexed letters to create the word 'just', all
+# the 1st indexed letters to create the word 'live', etc.
+# Shorter words will have an empty string in the place once the word has already
+# been mapped out (see the last element in the last element in the array).
