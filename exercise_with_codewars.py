03d410ac52347a80d93474ca3d81aa288f61675f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-19 19:37:45 +0300

    Complete task 755

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8269,3 +8269,7 @@ def factory(x):
 
 # Create a function that returns the lowest product of 4 consecutive digits in a number given as a string.
 # This should only work if the number has 4 digits or more. If not, return "Number is too small".
+from operator import mul
+from functools import reduce
+def lowest_product(input):
+    return min(reduce(mul, [*map(int, input)][i:i + 4]) for i in range(0, len(input) - 3)) if len(input) > 3 else "Number is too small"
