0969fd36c0282b5d089811cd1baa81f9795916f6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-19 18:28:36 +0300

    Complete task 747

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8193,3 +8193,6 @@ def arr_adder(arr):
 # A triangle is called an equable triangle if its area equals its perimeter. Return true, if
 # it is an equable triangle, else return false. You will be provided with
 # the length of sides of the triangle. Happy Coding!
+def equable_triangle(a,b,c):
+    p = (a + b + c) / 2
+    return (p*(p-a)*(p-b)*(p-c)) ** .5 == a + b + c
