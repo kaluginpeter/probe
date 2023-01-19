245291493a5ccb30519f21de711bd052faad6914
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-19 19:15:28 +0300

    Added task 752

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8231,3 +8231,15 @@ def reverse_by_center(s):
 # Well here's the twist. The inputs will gradually increase with their index as parameter to the function.
 def add(*args):
     return sum(v * (k + 1) for k,v in enumerate(args))
+
+# In English, all words at the begining of a sentence should begin with a capital letter.
+# You will be given a paragraph that does not use capital letters.
+# Your job is to capitalise the first letter of the first word of each sentence.
+# For example,
+# input:
+# "hello. my name is inigo montoya. you killed my father. prepare to die."
+# output:
+# "Hello. My name is inigo montoya. You killed my father. Prepare to die."
+# You may assume:
+# there will be no punctuation besides full stops and spaces
+# all but the last full stop will be followed by a space and at least one word
