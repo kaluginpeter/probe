5b0db66f5b63ec3f6691662f0281b2a0f6c24bca
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-19 18:55:06 +0300

    Complete task 751

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8229,3 +8229,5 @@ def reverse_by_center(s):
 # You will create a function named add. This function will return the sum
 # of all the arguments. Sounds easy, doesn't it??
 # Well here's the twist. The inputs will gradually increase with their index as parameter to the function.
+def add(*args):
+    return sum(v * (k + 1) for k,v in enumerate(args))
