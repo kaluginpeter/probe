ce1511763481747e83f48ce7eedebe3e3c803e35
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-19 18:28:13 +0300

    Added task 747

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8189,3 +8189,7 @@ def combine(*args):
 # been mapped out (see the last element in the last element in the array).
 def arr_adder(arr):
     return ' '.join(map(''.join, zip(*arr)))
+
+# A triangle is called an equable triangle if its area equals its perimeter. Return true, if
+# it is an equable triangle, else return false. You will be provided with
+# the length of sides of the triangle. Happy Coding!
