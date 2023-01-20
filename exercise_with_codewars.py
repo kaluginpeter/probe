7aad527ba485b89fc7715f7522fc9130f6d779a9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-20 19:05:01 +0300

    Complete task 763

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8327,3 +8327,5 @@ def decode(str):
 # take an array as argument and the size of the chunk.
 # Example: if an array of size 123 is given and chunk size is 10 there will be
 # 13 parts, 12 of size 10 and 1 of size 3.
+def make_parts(arr, chunkSize):
+    return [arr[i: i + chunkSize] for i in range(0, len(arr), chunkSize)]
