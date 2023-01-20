feff8c96429d360bf9d7643483317b89364db90e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-20 18:30:22 +0300

    Complete task 757

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8282,3 +8282,5 @@ def number_format(n):
 # User experience specialists believe that certain colours have certain psychological meanings for us.
 # You are given a 2D array, composed of a colour and its 'common' association in each array element. The function
 # you will write needs to return the colour as 'key' and association as its 'value'.
+def colour_association(arr):
+    return [{i[0]: i[1]} for i in arr]
