6fb888c5d6ece16fef106e719be6af153e6e1b7a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-20 18:29:57 +0300

    Added task 757

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8277,3 +8277,8 @@ def lowest_product(input):
 # Format any integer provided into a string with "," (commas) in the correct places.
 def number_format(n):
     return format(n, ',d')
+
+# Colour plays an important role in our lifes. Most of us like this colour better then another.
+# User experience specialists believe that certain colours have certain psychological meanings for us.
+# You are given a 2D array, composed of a colour and its 'common' association in each array element. The function
+# you will write needs to return the colour as 'key' and association as its 'value'.
