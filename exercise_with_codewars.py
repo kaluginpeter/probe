f8bf3834a584659a6b9181599d9c4b71332377aa
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-20 18:45:48 +0300

    Complete task 760

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8298,3 +8298,6 @@ def get_decimal(n):
 
 # Make your strings more nerdy: Replace all 'a'/'A' with 4, 'e'/'E' with 3 and
 # 'l' with 1 e.g. "Fundamentals" --> "Fund4m3nt41s"
+def nerdify(txt):
+    d = {'a': 4, 'e': 3, 'l': 1, 'A': 4, 'E': 3}
+    return ''.join(str(d[i]) if i in d else i for i in txt)
