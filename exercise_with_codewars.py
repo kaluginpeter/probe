f5f2a574b19a18dd7c79af11940d74cc45cf070d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-20 18:56:18 +0300

    Added task 761

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8301,3 +8301,20 @@ def get_decimal(n):
 def nerdify(txt):
     d = {'a': 4, 'e': 3, 'l': 1, 'A': 4, 'E': 3}
     return ''.join(str(d[i]) if i in d else i for i in txt)
+
+# The GADERYPOLUKI is a simple substitution cypher used in scouting to encrypt messages. The
+# encryption is based on short, easy to remember key.
+# The key is written as paired letters, which are in the cipher simple replacement.
+# The most frequently used key is "GA-DE-RY-PO-LU-KI".
+#  G => A
+#  g => a
+#  a => g
+#  A => G
+#  D => E
+#   etc.
+# The letters, which are not on the list of substitutes, stays in the encrypted text without changes.
+# Task
+# Your task is to help scouts to encrypt and decrypt thier messages. Write the Encode and Decode functions.
+# Input/Output
+# The input string consists of lowercase and uperrcase characters and white . The substitution has to be
+# case-sensitive.
