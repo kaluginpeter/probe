e45cfb95d1275a6b0e79e5f5b7e844b0ae72bda4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-20 19:17:13 +0300

    Added task 765

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8347,3 +8347,17 @@ def make_parts(arr, chunkSize):
 # Be careful about trailing whitespace in your solution.
 def format_poem(poem):
     return '.\n'.join(poem.split('. '))
+
+# You are a barista at a big cafeteria. Normally there are a lot of baristas,
+# but your boss runs a contest and he told you that, if you could
+# handle all the orders with only one coffee machine in such a way that the
+# sum of all the waiting times of the customers is the smallest possible, he will give you a substantial raise.
+# So you are the only barista today, and you only have one coffee machine that can brew one coffee at a time.
+# People start giving you their orders.
+# Let's not think about the time you need to write down their orders, but you need
+# 2 additional minutes to clean the coffee machine after each coffee you make.
+# Now you have a list coffees of the orders and you write down next to each of the orders the time
+# you need to brew each one of those cups of coffee.
+# Task:
+# Given a list of the times you need to brew each coffee, return the minimum total waiting time.
+# If you get it right, you will get that raise your boss promised you!
