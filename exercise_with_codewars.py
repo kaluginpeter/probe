3f9ecb2fd4235c35952572c4238c7c18ea7d3249
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-21 14:27:16 +0300

    Added task 773

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8433,3 +8433,9 @@ def mispelled(word1, word2):
 # equal to one length unit. The smallest triangle will have one length unit.
 def t_area(s):
     return (s.count('\n') - 2) ** 2 / 2
+
+# You will be given two strings a and b consisting of lower case letters, but a will
+# have at most one asterix character. The asterix (if any) can be replaced with an
+# arbitrary sequence (possibly empty) of lowercase letters. No other character of string a can be replaced.
+# If it is possible to replace the asterix in a to obtain string b, then string b matches the pattern.
+# If the string matches, return true else false.
