7a2208cc571d358851d15d72dbca6a0a51ae4359
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-21 14:12:21 +0300

    Added task 770

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8394,3 +8394,14 @@ def pattern(n):
 # is to calculate number of elements in the first array which are also present in the second array.
 def match_arrays(v, r):
     return sum(1 for i in v if i in r)
+
+# The odd and even numbers are fighting against each other!
+# You are given a list of positive integers. The odd numbers from the list will fight using their 1 bits
+# from their binary representation, while the even numbers will fight using their 0 bits.
+# If present in the list, number 0 will be neutral, hence not fight for either side.
+# You should return:
+# odds win if number of 1s from odd numbers is larger than 0s from even numbers
+# evens win if number of 1s from odd numbers is smaller than 0s from even numbers
+# tie if equal, including if list is empty
+# Please note that any prefix that might appear in the binary representation, e.g. 0b,
+# should not be counted towards the battle.
