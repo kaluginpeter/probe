d1c2b454003bf28c130b0402bb191e629ff75d70
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-21 14:29:31 +0300

    Added task 774

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8442,3 +8442,10 @@ def t_area(s):
 import re
 def solve(a, b):
     return bool(re.fullmatch(a.replace('*', '.*'), b))
+
+# Complete the function so that it returns the number of seconds that have elapsed between the start
+# and end times given.
+# Tips:
+# The start/end times are given as Date (JS/CoffeeScript), DateTime (C#), Time (Nim),
+# datetime(Python) and Time (Ruby) instances.
+# The start time will always be before the end time.
