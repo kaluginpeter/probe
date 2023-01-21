c83e556676b047d13d1373f92458f734891626bd
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-21 14:04:22 +0300

    Added task 768

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8380,3 +8380,9 @@ def roots(a,b,c):
 import math
 def factorial(n):
     return math.prod(list(range(1, n + 1))) if n >= 0 else None
+
+# You have to write a function pattern which creates the following pattern (see examples) up to the desired
+# number of rows.
+# If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
+# If any even number is passed as argument then the pattern should last upto the largest
+# odd number which is smaller than the passed even number.
