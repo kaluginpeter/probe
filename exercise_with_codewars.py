d51f09934ab99aae80f0cc81e32332826abf6435
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-21 14:16:35 +0300

    Complete task 771

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8420,3 +8420,10 @@ def bits_battle(numbers):
 # This can include an extra char at the end or the beginning of either of words.
 # In the tests that expect true, the mispelled word will always differ mostly by
 # one character. If the two words are the same, return True.
+def mispelled(word1, word2):
+    if word1 == word2[1:] or word1[1:] == word2 or word1[:-1] == word2 or word1 == word2[:-1]:
+        return True
+    if any(word1[:i] + word1[i+1:] == word2[:i] + word2[i+1:] for i in range(len(word1))):
+        return True
+    else:
+        return False
