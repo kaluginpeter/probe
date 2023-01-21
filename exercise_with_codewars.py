76996c2869f90293a859c78c982da876ff1ced4b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-21 14:19:49 +0300

    Added task 772

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8427,3 +8427,7 @@ def mispelled(word1, word2):
         return True
     else:
         return False
+
+# Calculate area of given triangle. Create a function t_area that will take a string
+# which will represent triangle, find area of the triangle, one space will be
+# equal to one length unit. The smallest triangle will have one length unit.
