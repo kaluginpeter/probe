76869727640c3811958fad9f20ec5002141bceee
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-21 14:08:25 +0300

    Complete task 769

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8392,3 +8392,5 @@ def pattern(n):
 
 # You have two arrays in this kata, every array contains unique elements only. Your task
 # is to calculate number of elements in the first array which are also present in the second array.
+def match_arrays(v, r):
+    return sum(1 for i in v if i in r)
