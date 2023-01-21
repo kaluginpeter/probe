1c281da16275d88abd2132ac79709e829666d6e4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-21 13:52:28 +0300

    Complete task 766

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8368,3 +8368,5 @@ def barista(coffees):
 # to 2 decimal places, if there are any possible roots, else return None/null/nil/nothing.
 # If you use discriminant,when discriminant = 0, x1 = x2 = root => return sum of both roots.
 # There will always be valid arguments.
+def roots(a,b,c):
+    return round(-b / a, 2) if b ** 2 >= 4 * a * c else None
