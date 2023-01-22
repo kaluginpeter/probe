d9b2dc24cadf20460a7b0af7b3f2bc98686ffa08
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-22 12:25:40 +0300

    Added task 781

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8509,3 +8509,13 @@ def last_index_of(head, search_val):
             pos = count
         head = head.next
     return pos
+
+# A squared string is a string of n lines, each substring being n characters long.
+# We are given two n-squared strings. For example:
+# s1 = "abcd\nefgh\nijkl\nmnop" s2 = "qrst\nuvwx\nyz12\n3456"
+# Let us build a new string strng of size (n + 1) x n in the following way:
+# The first line of strng has the first char of the first line of s1 plus the chars of the last line of s2.
+# The second line of strng has the first two chars of the second line of s1 plus the chars
+# of the penultimate line of s2 except the last char
+# and so on until the nth line of strng has the n chars of the nth line of s1
+# plus the first char of the first line of s2.
