96bf245834c018122e8a6933e769d4ab8755e3cb
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-22 12:50:40 +0300

    Added task 785

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8545,3 +8545,12 @@ def shades_of_grey(n):
 # If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
 def pattern(integer):
     return "\n".join("".join(str(j) for j in range(integer, integer - i, -1)) for i in range(1, integer + 1))
+
+# Complete the function that takes an array of integers as input and finds the sum of squares of
+# the elements at even positions (i.e. 2nd, 4th, etc.) plus the sum of the rest of the elements at odd position.
+# For empty arrays, result should be zero (except for Haskell).
+# Note
+# The values at even positions need to be squared. For a language with zero-based indices,
+# this will occur at oddly-indexed locations. For instance, in Python,
+# the values at indices 1, 3, 5, etc. should be squared because these are the second,
+# fourth, and sixth positions in the list.
