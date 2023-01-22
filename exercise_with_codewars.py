bf87b2a2ce8f9f74bc9c1168cbd70e2154c504dc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-22 12:21:33 +0300

    Complete task 780

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8501,3 +8501,11 @@ def is_even(n):
 # Implement the method lastIndexOf (last_index_of in PHP and Python), which accepts a linked list (head) and
 # a value, and returns the index (zero based) of the last occurrence of that value if exists, or -1 otherwise.
 # For example: Given the list: 1 -> 2 -> 3 -> 3, and the value 3, lastIndexOf / last_index_of should return 3.
+def last_index_of(head, search_val):
+    count = pos = -1
+    while head:
+        count += 1
+        if head.data == search_val:
+            pos = count
+        head = head.next
+    return pos
