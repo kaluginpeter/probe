8d53a15f8af624d91b0fd8ecd88025cf4b4ca32f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-23 19:46:56 +0300

    Added task 793

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8640,3 +8640,10 @@ def solution(items, index, default_value):
         return items[index]
     except:
         return default_value
+
+# Rick wants a faster way to get the product of the largest pair in an array.
+# Your task is to create a performant solution to find the product of the largest two integers
+# in a unique array of positive numbers.
+# All inputs will be valid.
+# Passing [2, 6, 3] should return 18, the product of [6, 3].
+# Disclaimer: only accepts solutions that are faster than his, which has a running time O(nlogn).
