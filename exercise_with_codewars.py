d50239665c7f8435a1dc329d65240e2aa8348f9f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-23 19:55:12 +0300

    Complete task 795

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8663,3 +8663,5 @@ def add_arrays(array1, array2):
 
 # Complete the method that returns true if 2 integers share at least
 # two 1 bits, otherwise return false. For simplicity assume that all numbers are non-negative.
+def shared_bits(a, b):
+    return bin(a & b).count('1') > 1
