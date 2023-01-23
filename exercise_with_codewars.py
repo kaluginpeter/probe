67968762daf6d6d61ab1061a5894c0fb9449577c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-23 19:09:24 +0300

    Added task 788

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8573,3 +8573,13 @@ class Converter():
     @staticmethod
     def to_hex(s):
         return ''.join(hex(ord(i))[2:] for i in s)
+
+# Your non-profit company has assigned you the task of calculating some simple statistics on donations. You have
+# an array of integers, representing various amounts of donations your company has been given.
+# In particular, you're interested in the median value for donations.
+# The median is the middle number of a sorted list of numbers. If the list
+# is of even length, the 2 middle values are averaged.
+# Write a function that takes an array of integers as an argument and returns the median of those integers.
+# Notes:
+# The sorting step is vital.
+# Input arrays are non-empty.
