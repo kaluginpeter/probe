d5bee6b921e488366a80adeac73cb3c68e1617f9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-23 19:18:49 +0300

    Added task 789

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8586,3 +8586,14 @@ class Converter():
 def median(array):
     l = len(array) % 2 != 0
     return sorted(array)[len(array)//2] if l else (sorted(array)[len(array)//2-1] + sorted(array)[len(array)//2])/2
+
+# A palindrome is a word, phrase, number, or other sequence of characters which
+# reads the same backward as forward. Examples of numerical palindromes are:
+# 2332
+# 110011
+# 54322345
+# You'll be given 2 numbers as arguments: (num,s). Write a function which returns an array
+# of s number of numerical palindromes that come after num. If num is a palindrome itself,
+# it should be included in the count.
+# Return "Not valid" instead if any one of the inputs is not an integer or is less than 0.
+# For this kata, single digit numbers will NOT be considered numerical palindromes.
