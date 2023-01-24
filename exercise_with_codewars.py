26d87ed2298139d8aa18439dcb26f63ad75f4e1a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-24 20:09:35 +0300

    Complete task 797

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8681,3 +8681,5 @@ def rad_ladies(name):
 
 # Given two integers a and x, return the minimum non-negative number to add
 # to / subtract from a to make it a multiple of x.
+def minimum(a, x):
+    return min(a % x, -a % x)
