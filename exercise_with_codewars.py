c755ce2546efe3117227f42cece605ae393531d1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-25 22:04:22 +0300

    Added task 814

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8858,3 +8858,14 @@ def build_one_two_three():
 # Inputs should not be modified.
 def zip_with(fn, a1, a2):
     return list(map(fn, a1, a2))
+
+# You probably know that some characters written on a piece of paper, after turning this sheet 180
+# degrees, can be read, although sometimes in a different way. So, uppercase letters
+# "H", "I", "N", "O", "S", "X", "Z" after rotation are not changed, the letter "M"
+# becomes a "W", and Vice versa, the letter "W" becomes a "M".
+# We will call a word "shifter" if it consists only of letters
+# "H", "I", "N", "O", "S", "X", "Z", "M" and "W". After turning the sheet, this word can
+# also be read, although in a different way. So, the word "WOW "turns into the word "MOM".
+# On the other hand, the word "HOME" is not a shifter.
+# Find the number of unique shifter words in the input string (without duplicates). All shifters to
+# be counted, even if they are paired (like "MOM" and "WOW"). String contains only uppercase letters.
