294ba427392e341ff928357fc4ef19487ec783f0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-25 22:07:58 +0300

    Complete task 815

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8876,3 +8876,8 @@ def shifter(st):
 # I love Fibonacci numbers in general, but I must admit I love some more than others.
 # I would like for you to write me a function that, when given a number n (n >= 1 ),
 # returns the nth number in the Fibonacci Sequence.
+def nth_fib(n):
+  a, b = 0, 1
+  for i in range(n-1):
+  	a, b = b, a + b
+  return a
