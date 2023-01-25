e99dd6c2bb8a9d7711fe1bca4290a237d38151e0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-25 22:00:46 +0300

    Added task 813

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8849,3 +8849,10 @@ def push(head, data):
     return Node(data, head)
 def build_one_two_three():
     return Node(1, Node(2, Node(3)))
+
+# zip_with takes a function and two arrays and zips the arrays together, applying the
+# function to every pair of values.
+# The function value is one new array.
+# If the arrays are of unequal length, the output will only be as long as the shorter one.
+# (Values of the longer array are simply not used.)
+# Inputs should not be modified.
