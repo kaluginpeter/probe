84aa9df8e285f9d68cd9e1c33c4a25f5aef6f3ee
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-25 21:25:12 +0300

    Added task 807

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8781,3 +8781,18 @@ def compound_array(a, b):
         if a: l.append(a.pop(0))
         if b: l.append(b.pop(0))
     return l
+
+# In this kata, your task is to implement an extended version of the famous rock-paper-scissors
+# game. The rules are as follows:
+# Scissors cuts Paper
+# Paper covers Rock
+# Rock crushes Lizard
+# Lizard poisons Spock
+# Spock smashes Scissors
+# Scissors decapitates Lizard
+# Lizard eats Paper
+# Paper disproves Spock
+# Spock vaporizes Rock
+# Rock crushes Scissors
+# Task:
+# Given two values from the above game, return the Player result as "Player 1 Won!", "Player 2 Won!", or "Draw!".
