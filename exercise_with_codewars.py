2a3e1697a36b85ce66bcddf0e45c92a6c618ccee
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-27 18:11:00 +0300

    Complete task 820

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8957,3 +8957,9 @@ def mean_vs_median(numbers):
 # Mash 2 arrays together so that the returning array has alternating elements of the
 # 2 arrays . Both arrays will always be the same length.
 # eg. [1,2,3] + ['a','b','c'] = [1, 'a', 2, 'b', 3, 'c']
+def array_mash(a, b):
+    l = []
+    while a or b:
+        l.append(a.pop(0))
+        l.append(b.pop(0))
+    return l
