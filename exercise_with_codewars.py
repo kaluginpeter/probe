77ac11fbc9bc7d7e5919932f5141a840384d9578
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-27 18:05:03 +0300

    Added task 819

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8941,3 +8941,11 @@ def trim(phrase, size):
     elif size <= 3 :
         return phrase[:size] + '...'
     return phrase[:size - 3] + '...'
+
+# Your goal is to implement the method meanVsMedian which accepts an
+# odd-length array of integers and returns one of the following:
+# 'mean' - in case mean value is larger than median value
+# 'median' - in case median value is larger than mean value
+# 'same' - in case both mean and median share the same value
+# Reminder: Median
+# Array will always be valid (odd-length >= 3)
