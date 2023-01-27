8e2ac9ee85eb0816acab01c11091cdc46466d8cc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-27 18:50:29 +0300

    Added task 825

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9013,3 +9013,9 @@ def grid_index(grid, indexes):
 # Examples:
 def change_count(s):
     return f"${sum(CHANGE[i] for i in s.split()):.2f}"
+
+# Write a regex to validate a 24 hours time string. See examples to figure out what you should check for:
+# Accepted: 01:00 - 1:00
+# Not accepted:
+# 24:00
+# You should check for correct length and no spaces.
