8c726fa565408a218b5b7f4afc8e563c6bea1e12
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-27 18:31:56 +0300

    Added task 823

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -8986,3 +8986,16 @@ class Arith():
         self.i = integer.index(int)
     def add(self, int):
          return integer[integer.index(int) + self.i]
+
+# You are given an n by n ( square ) grid of characters, for example:
+# [['m', 'y', 'e'],
+#  ['x', 'a', 'm'],
+#  ['p', 'l', 'e']]
+# You are also given a list of integers as input, for example:
+# [1, 3, 5, 8]
+# You have to find the characters in these indexes of the grid if you think of the indexes as:
+# [[1, 2, 3],
+#  [4, 5, 6],
+#  [7, 8, 9]]
+# Remember that the indexes start from one and not zero.
+# Then you output a string like this:
