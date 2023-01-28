5f86baee1e4e3db756bc07904342cd0d145dd101
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-28 21:58:12 +0300

    Added task 833

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9098,3 +9098,10 @@ def diagonal(m):
     if principal  > secondary : return 'Principal Diagonal win!'
     if secondary  > principal : return 'Secondary Diagonal win!'
     return 'Draw!'
+
+# For a given 2D vector described by cartesian coordinates of its initial point and
+# terminal point in the following format:
+# [[x1, y1], [x2, y2]]
+# Your function must return the vector's length represented as a floating point number.
+# Error must be within 1e-7.
+# Coordinates can be integers or floating point numbers.
