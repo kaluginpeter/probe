232ba4fecfbd0966c99a54da1fc6d495fdc7b947
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-28 21:55:26 +0300

    Added task 832

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9075,3 +9075,20 @@ def sort_by_area(seq):
             return x[0] * x[1]
         return 3.14 * x * x
     return sorted(seq, key=func)
+
+# Principal Diagonal -- The principal diagonal in a matrix identifies those elements of the
+# matrix running from North-West to South-East.
+# Secondary Diagonal -- the secondary diagonal of a matrix identifies those elements of the matrix
+# running from North-East to South-West.
+# For example:
+# matrix:             [1, 2, 3]
+#                     [4, 5, 6]
+#                     [7, 8, 9]
+# principal diagonal: [1, 5, 9]
+# secondary diagonal: [3, 5, 7]
+# Task
+# Your task is to find which diagonal is "larger": which diagonal has a bigger sum of their elements.
+# If the principal diagonal is larger, return "Principal Diagonal win!"
+# If the secondary diagonal is larger, return "Secondary Diagonal win!"
+# If they are equal, return "Draw!"
+# Note: You will always receive matrices of the same dimension.
