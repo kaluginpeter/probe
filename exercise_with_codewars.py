7c13943b3588ee1e5bb41363435a663dd8ee5bf6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-28 21:47:33 +0300

    Added task 830

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9054,3 +9054,10 @@ def even_and_odd(n):
     even = ''.join(str(i) for i in str(n) if int(i) % 2 == 0)
     odd = ''.join(str(i) for i in str(n) if int(i) % 2 != 0)
     return (int(even) if even else 0, int(odd) if odd else 0)
+
+# Write a function that will take a key of X and place it in the middle of Y repeated N times.
+# Extra challege (not tested): You can complete this with under 70
+# characters without using regex. Challenge yourself to do this. It wont be best practices but it will work.
+# Rules:
+# If X cannot be placed in the middle, return X.
+# N will always be > 0.
