bd78b46c410c954f13ffd13812cf185f16ad9c45
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-28 21:43:49 +0300

    Added task 829

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9041,3 +9041,12 @@ def eq_sum_powdig(hMax, exp):
 # All inputs will be integers.
 def add(a):
     return lambda x: x + a
+
+# Given a number N, can you fabricate the two numbers NE and NO such that NE is formed by even digits
+# of N and NO is formed by odd digits of N? Examples:
+# input	NE	NO
+# 126453	264	153
+# 3012	2	31
+# 4628	4628	0
+# 0 is considered as an even number.
+# In C and JavaScript you should return an array of two elements such as the first is NE and the second is NO.
