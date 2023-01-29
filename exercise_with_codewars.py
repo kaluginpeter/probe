4956749ec34e19f23078b139c72b79ef1be3a61b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-29 16:14:00 +0300

    Complete task 839

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9189,3 +9189,5 @@ def total_bill(s):
 
 # You'll be passed an array of objects (list) - you must sort them in
 # descending order based on the value of the specified property (sortBy).
+def sort_list(sort_key, l):
+    return sorted(l, key=lambda x: x[sort_key], reverse=True)
