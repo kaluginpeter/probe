a8c17824172810425a24a76e26c148e23bacff11
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-29 16:34:59 +0300

    Added task 845

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9254,3 +9254,11 @@ def abundant_number(num):
 def zombie_shootout(z, r, a):
     count = min(r * 2, a)
     return f"You shot all {z} zombies." if count >= z else f"You shot {count} zombies before being eaten: { 'overwhelmed' if count == 2 * r else 'ran out of ammo'}."
+
+# Task
+# Your challenge is to write a function named getSlope/get_slope/GetSlope that calculates the slope
+# of the line through two points.
+# Input
+# Each point that the function takes in is an array 2 elements long. The first number
+# is the x coordinate and the second number is the y coordinate. If the line through the
+# two points is vertical or if the same point is given twice, the function should return null/None.
