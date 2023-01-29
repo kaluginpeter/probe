de85c74b87c9d418777aeb3e55a7d2634bb146f3
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-29 16:29:30 +0300

    Added task 843

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9225,3 +9225,9 @@ def decode_pass(pass_list, bits):
 # Don't forget about the indexing pitfall ;)
 def seqlist(first,c,l):
     return list(range(first,first+l*c,c))
+
+# An abundant number or excessive number is a number for which the sum of its proper divisors is greater than
+# the number itself.
+# The integer 12 is the first abundant number. Its proper divisors are 1, 2, 3, 4 and 6 for a total of 16 (> 12).
+# Derive function abundantNumber(num)/abundant_number(num) which returns true/True/.true.
+# if num is abundant, false/False/.false. if not.
