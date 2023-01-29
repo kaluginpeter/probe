be740b99d3415cb1ce04e9782f60bfe828232e3b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-29 16:19:03 +0300

    Added task 840

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9191,3 +9191,12 @@ def total_bill(s):
 # descending order based on the value of the specified property (sortBy).
 def sort_list(sort_key, l):
     return sorted(l, key=lambda x: x[sort_key], reverse=True)
+
+# How sexy is your name? Write a program that calculates how sexy one's name is according to the criteria below.
+# There is a preloaded dictionary of letter scores called SCORES(Python & JavaScript) / $SCORES (Ruby).
+# Add up the letters (case-insensitive) in your name to get your sexy score. Ignore other characters.
+#     SCORES = {'A': 100, 'B': 14, 'C': 9, 'D': 28, 'E': 145, 'F': 12, 'G': 3,
+#               'H': 10, 'I': 200, 'J': 100, 'K': 114, 'L': 100, 'M': 25,
+#               'N': 450, 'O': 80, 'P': 2, 'Q': 12, 'R': 400, 'S': 113, 'T': 405,
+#               'U': 11, 'V': 10, 'W': 10, 'X': 3, 'Y': 210, 'Z': 23}
+# The program must return how sexy one's name is according to the "Sexy score ranking" chart.
