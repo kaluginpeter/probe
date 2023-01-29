8c19b245d59ffcef670561692fa6a05734ec98ef
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-29 16:26:40 +0300

    Added task 842

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9213,3 +9213,13 @@ def sexy_name(name):
 def decode_pass(pass_list, bits):
     pas = "".join(chr(int(x, 2)) for x in bits.split())
     return pas if pas in pass_list else False
+
+# In this kata, you will write an arithmetic list which is basically a list that contains consecutive
+# terms in the sequence.
+# You will be given three parameters :
+# first the first term in the sequence
+# c the constant that you are going to ADD ( since it is an arithmetic sequence...)
+# l the number of terms that should be returned
+# Useful link: Sequence
+# Be sure to check out my Arithmetic sequence Kata first ;)
+# Don't forget about the indexing pitfall ;)
