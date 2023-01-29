7cb1defa82b4a663d624ae08bb8a4dbc7131a323
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-29 16:00:54 +0300

    Added task 837

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9143,3 +9143,14 @@ def find_employees_role(name):
             if name.split()[0] == i['first_name']:
                 return i['role']
     return 'Does not work here!'
+
+# Linked Lists - Append
+# Write an Append() function which appends one linked list to another. The head of the
+# resulting list should be returned.
+# var listA = 1 -> 2 -> 3 -> null
+# var listB = 4 -> 5 -> 6 -> null
+# append(listA, listB) === 1 -> 2 -> 3 -> 4 -> 5 -> 6 -> null
+# If both listA and listB are null/NULL/None/nil, return null/NULL/None/nil. If one list is null/NULL/None/nil
+# and the other is not, simply return the non-null/NULL/None/nil list.
+# The push() and buildOneTwoThree() (build_one_two_three() in PHP and ruby) functions need not be
+# redefined. The Node class is also predefined for you in PHP.
