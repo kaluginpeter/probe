c8025e4d295c61218724de266a0f2678fbbed1ea
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-30 21:14:25 +0300

    Added task 853

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9340,3 +9340,7 @@ def remove_duplicate_words(s):
 # For example:
 def in_asc_order(arr):
     return arr == sorted(arr)
+
+# You will be given an array of objects (hashes in ruby) representing data about
+# developers who have signed up to attend the coding meetup that you are organising for the first time.
+# Your task is to return the number of JavaScript developers coming from Europe.
