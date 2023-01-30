ead70c78fc101d97efeb9a8edab2809f2b5545ff
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-30 21:34:12 +0300

    Complete task 854

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9349,3 +9349,5 @@ def count_developers(lst):
 
 # Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
 # Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+def reverseNumber(n):
+    return int(str(abs(n))[::-1]) * (-1 if n < 0 else 1)
