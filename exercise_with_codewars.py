da2b52dc676400ea003f476f96cb0ad60644f6ea
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-30 21:02:50 +0300

    Added task 850

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9303,3 +9303,12 @@ def dont_give_me_five(start, end):
 # more oldest age, then return both of them in array format.
 def two_oldest_ages(ages):
     return sorted(ages)[-2:]
+
+# Given a Divisor and a Bound , Find the largest integer N , Such That ,
+# Conditions :
+# N is divisible by divisor
+# N is less than or equal to bound
+# N is greater than 0.
+# Notes
+# The parameters (divisor, bound) passed to the function are only positive values .
+# It's guaranteed that a divisor is Found
