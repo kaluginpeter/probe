1c1883bb850904762c3b1377a7f780340ddf8356
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-30 21:36:35 +0300

    Added task 855

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9351,3 +9351,8 @@ def count_developers(lst):
 # Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
 def reverseNumber(n):
     return int(str(abs(n))[::-1]) * (-1 if n < 0 else 1)
+
+# Write a program to determine if a string contains only unique characters. Return true if it does and false
+# otherwise.
+# The string may contain any of the 128 ASCII characters. Characters are case-sensitive,
+# e.g. 'a' and 'A' are considered different characters.
