2f3cd958077eefc0e026eaa284c59c557952c926
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-30 20:52:33 +0300

    Complete task 847

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9276,3 +9276,6 @@ def maskify(cc):
 # all of the integer's divisors(except for 1 and the number itself), from smallest
 # to largest. If the number is prime return the string '(integer) is prime' (null in C#)
 # (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+def divisors(integer):
+    l =  [i for i in range(2, integer) if integer % i == 0]
+    return l if l else f"{integer} is prime"
