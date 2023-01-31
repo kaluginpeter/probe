bfb176395ba0966eb0409c50a42f6a57720a9350
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-31 19:19:49 +0300

    Added task 863

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9454,3 +9454,8 @@ def find_deleted_number(arr, mixed_arr):
 import calendar
 def isLeapYear(year):
     return calendar.isleap(year)
+
+# Complete the function that takes a sequence of numbers as
+# single parameter. Your function must return the sum of the even values of this sequence.
+# Only numbers without decimals like 4 or 4.0 can be even.
+# The input is a sequence of numbers: integers and/or floats.
