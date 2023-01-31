e7c2956db4d19991595bbbeab6cca2959369e4ed
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-31 19:21:51 +0300

    Added task 864

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9461,3 +9461,9 @@ def isLeapYear(year):
 # The input is a sequence of numbers: integers and/or floats.
 def sum_even_numbers(seq):
     return sum(i for i in seq if i % 2 == 0)
+
+# In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur
+# only twice. Your task will be to return the sum of the numbers that occur only once.
+# For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7
+# and 8 occur once, and their sum is 15. Every other number occurs twice.
+# More examples in the test cases.
