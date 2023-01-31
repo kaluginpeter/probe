8ac234e121056b1a6b8f7b70fca884d451e0238a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-31 19:02:27 +0300

    Complete task 857

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9375,3 +9375,15 @@ def break_chocolate(n, m):
 # If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
 # N will never be less than 1.
 # Method calling example:
+def fizzbuzz(n):
+    l = []
+    for i in range (1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            l.append("FizzBuzz")
+        elif i % 3 == 0:
+            l.append("Fizz")
+        elif i % 5 == 0:
+            l.append("Buzz")
+        else:
+            l.append(i)
+    return l
