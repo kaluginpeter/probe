8266217c8d138c95c6374bcf5df3d3aa402532c0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-31 19:20:10 +0300

    Complete task 863

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9459,3 +9459,5 @@ def isLeapYear(year):
 # single parameter. Your function must return the sum of the even values of this sequence.
 # Only numbers without decimals like 4 or 4.0 can be even.
 # The input is a sequence of numbers: integers and/or floats.
+def sum_even_numbers(seq):
+    return sum(i for i in seq if i % 2 == 0)
