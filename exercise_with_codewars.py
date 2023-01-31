a1bb40d77a98b7d165b6cd27caea9fc6d114df29
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-31 19:08:11 +0300

    Added task 859

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9402,3 +9402,8 @@ def palindrome_chain_length(n):
         c += 1
         n = n + int(str(n)[::-1])
     return c
+
+# Task
+# Given a positive integer n, calculate the following sum:
+# n + n/2 + n/4 + n/8 + ...
+# All elements of the sum are the results of integer division.
