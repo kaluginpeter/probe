09e1ba2a37091c018f1a795f243019860f3b5ecb
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-31 19:02:00 +0300

    Added task 857

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9367,3 +9367,11 @@ def has_unique_chars(string):
 # we do not have any chocolate to split). Input will always be a non-negative integer.
 def break_chocolate(n, m):
     return (n * m) - 1 if n or m else 0
+
+# Return an array containing the numbers from 1 to N, where N is the parametered value.
+# Replace certain values however if any of the following conditions are met:
+# If the value is a multiple of 3: use the value "Fizz" instead
+# If the value is a multiple of 5: use the value "Buzz" instead
+# If the value is a multiple of 3 & 5: use the value "FizzBuzz" instead
+# N will never be less than 1.
+# Method calling example:
