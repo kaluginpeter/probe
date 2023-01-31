acba49edd4e91290f710268f441fb16ecb0587df
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-01-31 19:08:32 +0300

    Complete task 859

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9407,3 +9407,9 @@ def palindrome_chain_length(n):
 # Given a positive integer n, calculate the following sum:
 # n + n/2 + n/4 + n/8 + ...
 # All elements of the sum are the results of integer division.
+def halving_sum(n):
+    c = 0
+    while n > 0:
+        c += n
+        n = n // 2
+    return c
