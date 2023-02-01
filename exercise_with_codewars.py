fa631b136c59f3f307f6145edb5a4a3349d716f5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-01 18:30:21 +0300

    Complete task 872

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9520,3 +9520,5 @@ def get_average(lst):
 
 # You are provided with an array of positive integers and an additional integer n (n > 1).
 # Calculate the sum of each value in the array to the nth power. Then subtract the sum of the original array.
+def modified_sum(a, n):
+    return sum(i**n for i in a) - sum(a)
