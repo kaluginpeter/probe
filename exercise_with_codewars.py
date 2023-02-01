03acce0db28e9451b3e26209a168fb7c832f5dca
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-01 18:32:49 +0300

    Added task 873

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9522,3 +9522,10 @@ def get_average(lst):
 # Calculate the sum of each value in the array to the nth power. Then subtract the sum of the original array.
 def modified_sum(a, n):
     return sum(i**n for i in a) - sum(a)
+
+# This question is a variation on the Arithmetic Progression kata
+# The following was a question that I received during a technical interview for an entry level software
+# developer position. I thought I'd post it here so that everyone could give it a go:
+# You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is
+# missing. Write a function to find and return this number. What are the
+# time and space complexities of your solution?
