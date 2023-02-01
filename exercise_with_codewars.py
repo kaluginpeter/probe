a9e080bb912df76ebdddd303f07d1bfc6b84a08e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-01 18:38:38 +0300

    Added task 874

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9531,3 +9531,9 @@ def modified_sum(a, n):
 # time and space complexities of your solution?
 def missing_no(nums):
     return sum(range(101)) - sum(nums)
+
+# You are given an array of n+1 integers 1 through n. In addition there is a single duplicate integer.
+# The array is unsorted.
+# An example valid array would be [3, 2, 5, 1, 3, 4].
+# It has the integers 1 through 5 and 3 is duplicated. [1, 2, 4, 5, 5] would not be valid as it is missing 3.
+# You should return the duplicate value as a single integer.
