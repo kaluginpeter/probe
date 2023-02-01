0c7b54a1e5ce399444d8500752d8c6e23efb7c53
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-01 18:43:41 +0300

    Complete task 875

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9547,3 +9547,7 @@ def find_dup(arr):
 # arr will contain a maximum of 50 arrays.
 # After calculating all the averages, add them all together, then round down, as shown in the example below:
 # The example given: sumAverage([[3, 4, 1, 3, 5, 1, 4], [21, 54, 33, 21, 77]]), the answer being 44.
+from statistics import mean
+from math import floor
+def sum_average(arr):
+    return floor(sum(mean(i) for i in arr))
