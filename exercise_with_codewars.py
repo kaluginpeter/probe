9b57c728e27c5559446ea49cb6af3560427fc341
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-01 18:33:22 +0300

    Complete task 873

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9529,3 +9529,5 @@ def modified_sum(a, n):
 # You are given an unsorted array containing all the integers from 0 to 100 inclusively. However, one number is
 # missing. Write a function to find and return this number. What are the
 # time and space complexities of your solution?
+def missing_no(nums):
+    return sum(range(101)) - sum(nums)
