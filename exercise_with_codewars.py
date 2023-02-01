b1a000c9662d997c7748c11d13604188fa6b7734
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-01 18:43:20 +0300

    Added task 875

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9539,3 +9539,11 @@ def missing_no(nums):
 # You should return the duplicate value as a single integer.
 def find_dup(arr):
     return sum(arr) - sum(set(arr))
+
+# Program a function sumAverage(arr) where arr is an array containing arrays full of numbers, for example:
+# sum_average([[1, 2, 2, 1], [2, 2, 2, 1]]);
+# First, determine the average of each array. Then, return the sum of all the averages.
+# All numbers will be less than 100 and greater than -100.
+# arr will contain a maximum of 50 arrays.
+# After calculating all the averages, add them all together, then round down, as shown in the example below:
+# The example given: sumAverage([[3, 4, 1, 3, 5, 1, 4], [21, 54, 33, 21, 77]]), the answer being 44.
