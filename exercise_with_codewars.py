dc6e9c1f70df6a9f427c94c1d2274fa1a915a5dd
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-02 20:14:12 +0300

    Complete task 883

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9610,3 +9610,5 @@ def tv_remote(word):
 # No Description
 # Only by Thinking and Testing
 # Look at result of testcase, guess the code!
+def testit(a, b):
+    return sorted(list(set(a)) + list(set(b)))
