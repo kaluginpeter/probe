9ffbe3338a161e024535aab0f01483a31514535b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-02 19:21:14 +0300

    Complete task 876

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9554,3 +9554,10 @@ def sum_average(arr):
 
 # Should be easy, begin by looking at the code. Debug the code and the functions should work.
 # There are three functions: Multiplication (x) Addition (+) and Reverse (!esreveR)
+import math
+def multi(l_st):
+    return math.prod(l_st)
+def add(l_st):
+    return sum(l_st)
+def reverse(string):
+    return string[::-1]
