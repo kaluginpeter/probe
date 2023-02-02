75644f6682d82b055a8f58d3934d29d6e27c2c5e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-02 20:06:45 +0300

    Complete task 881

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9586,3 +9586,5 @@ def solve(eq):
 
 # In this Kata, you will be given a string and two indexes (a and b). Your task is
 # to reverse the portion of that string between those two indices inclusive.
+def solve(s,a,b):
+    return s[:a] + s[a:b+1][::-1] + s[b+1:]
