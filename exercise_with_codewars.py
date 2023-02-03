58340f34ad3acd68dfe3797976be5488a3de85bd
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-03 23:08:00 +0300

    Added task 893

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9706,3 +9706,9 @@ def get_first_python(users):
 # the bottom corners to the midpoint of the top edge and the centre of the rectangle.
 def arrow_area(a, b):
     return a * b / 4
+
+# Debug a function called calculate that takes 3 values. The first and
+# third values are numbers. The second value is a character. If
+# the character is "+" , "-", "*", or "/", the function will return the result
+# of the corresponding mathematical function on the two numbers. If the string is
+# not one of the specified characters, the function should return null.
