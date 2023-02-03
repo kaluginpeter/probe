b39b2d3f96110d1bd1326e2cb512144959cd39a8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-03 22:59:57 +0300

    Added task 891

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9689,3 +9689,10 @@ class Dinglemouse(object):
         self.last_name = last_name
     def get_full_name(self):
         return (self.first_name + ' ' + self.last_name).strip()
+
+# You will be given an array of objects (associative arrays in PHP)
+# representing data about developers who have signed up to attend the next
+# coding meetup that you are organising. The list is ordered according to who signed up first.
+# Your task is to return one of the following strings:
+# < firstName here >, < country here > of the first Python developer who has signed up; or
+# There will be no Python developers if no Python developer has signed up.
