43b440cd6aa242d22c373fc028ed30d5fadd96cb
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-04 14:31:15 +0300

    Added task 903

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9794,3 +9794,9 @@ def mygcd(x, y):
 # in the radially opposite position to firstNumber.
 def circle_of_numbers(n, fst):
     return (fst + (n / 2)) % n
+
+# In this kata the function returns an array/list of numbers without its last element.
+# The function is already written for you and the basic tests pass, but random tests fail. Your
+# task is to figure out why and fix it.
+# Good luck!
+# Hint: watch out for side effects.
