f06c85f3540b845985af2a60e2f1be63d4a97464
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-04 13:28:19 +0300

    Added task 899

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9762,3 +9762,6 @@ def pattern(n):
 # Determine the area of the largest square that can fit inside a circle with radius r.
 def area_largest_square(r):
     return 2 * r**2
+
+# Triangular numbers are so called because of the
+# equilateral triangular shape that they occupy when laid out as dots. i.e.
