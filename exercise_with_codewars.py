21dba076b96c97fdff2f9c0c9d2b1e9ce8d607c7
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-04 14:35:05 +0300

    Complete task 904

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9806,3 +9806,5 @@ def without_last(lst):
 # In this kata you will create a function to check a non-negative input to see if it is a prime number.
 # The function will take in a number and will return True if it is a prime number and False if it is not.
 # A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
+def is_prime(n):
+    return pow(2, n - 1, n) == 1 if n > 2 else n == 2
