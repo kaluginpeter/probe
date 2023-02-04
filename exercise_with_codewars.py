10d89465ce72dcf2a64e4a8836601ffb46dfca73
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-04 13:28:41 +0300

    Complete task 899

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9765,3 +9765,5 @@ def area_largest_square(r):
 
 # Triangular numbers are so called because of the
 # equilateral triangular shape that they occupy when laid out as dots. i.e.
+def triangular(n):
+    return n * (n + 1) // 2 if n > 0 else 0
