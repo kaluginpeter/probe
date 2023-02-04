3d5dea26522c829d70602eac1c7c52075238e477
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-04 14:25:16 +0300

    Added task 901

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9778,3 +9778,8 @@ def greet_developers(lst):
     for x in lst:
         x["greeting"] = f"Hi {x['firstName']}, what do you like the most about {x['language']}?"
     return lst
+
+# Find the greatest common divisor of two positive integers. The integers
+# can be large, so you need to find a clever solution.
+# The inputs x and y are always greater or equal to 1,
+# so the greatest common divisor will always be an integer that is also greater or equal to 1.
