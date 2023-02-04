87b41d5f4e745434425a64ba146ed98a25db3420
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-04 14:34:45 +0300

    Added task 904

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9802,3 +9802,7 @@ def circle_of_numbers(n, fst):
 # Hint: watch out for side effects.
 def without_last(lst):
     return lst[:-1]
+
+# In this kata you will create a function to check a non-negative input to see if it is a prime number.
+# The function will take in a number and will return True if it is a prime number and False if it is not.
+# A prime number is a natural number greater than 1 that has no positive divisors other than 1 and itself.
