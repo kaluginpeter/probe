d3d3c594bd13d8e17251d42d534fd235284931eb
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-04 13:19:59 +0300

    Complete task 898

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9760,3 +9760,5 @@ def pattern(n):
     return('\n'.join(''.join(str(i) for i in range(n,j,-1)) for j in range(n)))
 
 # Determine the area of the largest square that can fit inside a circle with radius r.
+def area_largest_square(r):
+    return 2 * r**2
