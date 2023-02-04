51d697eaf64be8353ba6f1f48839ef48b891835d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-04 14:25:38 +0300

    Complete task 901

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9783,3 +9783,6 @@ def greet_developers(lst):
 # can be large, so you need to find a clever solution.
 # The inputs x and y are always greater or equal to 1,
 # so the greatest common divisor will always be an integer that is also greater or equal to 1.
+import math
+def mygcd(x, y):
+    return math.gcd(x, y)
