bdf31cc59727413d0f6c35a8ac1296889baf6855
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-05 19:07:59 +0300

    Added task 912

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9856,3 +9856,9 @@ def find_missing(arr1, arr2):
 # non-numeric values and multiply the rest by given multiplier.
 def multiply_and_filter(seq, multiplier):
     return [i * multiplier for i in seq if type(i) == int or type(i) == float]
+
+# Agent 47, you have a new task! Among citizens of the city
+# X are hidden 2 dangerous criminal twins. Your task is to identify them and eliminate!
+# Given an array of integers, your task is to find
+# two same numbers and return one of them, for example in array [2, 3, 6, 34, 7, 8, 2] answer is 2.
+# If there are no twins in the city - return None or the equivalent in the language that you are using.
