465895f21972fa26728a379a0c8bd1bf9e2c6c45
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-05 19:01:22 +0300

    Added task 911

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9850,3 +9850,7 @@ def doubles(s):
 # numerical value exists in one and not the other is not a valid solution.
 def find_missing(arr1, arr2):
     return sum(arr1) - sum(arr2)
+
+# Your task is to write a function, which takes two arguments and returns a sequence. First argument is
+# a sequence of values, second is multiplier. The function should filter all
+# non-numeric values and multiply the rest by given multiplier.
