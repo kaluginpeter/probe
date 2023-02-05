2d7b83cf08d6adec3a70c6fa27cc224d273ca8c1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-05 19:01:46 +0300

    Complete task 911

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9854,3 +9854,5 @@ def find_missing(arr1, arr2):
 # Your task is to write a function, which takes two arguments and returns a sequence. First argument is
 # a sequence of values, second is multiplier. The function should filter all
 # non-numeric values and multiply the rest by given multiplier.
+def multiply_and_filter(seq, multiplier):
+    return [i * multiplier for i in seq if type(i) == int or type(i) == float]
