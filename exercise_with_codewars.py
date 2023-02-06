35c589e19c669109a31f63efada62bf44de4ff79
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-06 19:54:09 +0300

    Complete task 917

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9910,3 +9910,5 @@ def battle(x, y):
 # You need to create a function, helloWorld, that will return the String Hello, World!
 # without actually using raw strings. This includes quotes, double quotes and template strings.
 # You can, however, use the String constructor and any related functions.
+def hello_world():
+  return chr(72) + chr(101) + chr(108) + chr(108) + chr(111) + chr(44) + chr(32) + chr(87) + chr(111) + chr(114) + chr(108) + chr(100) + chr(33)
