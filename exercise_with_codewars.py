3a5e373e3e98ccf714ea4e694cfbf1474f0b1d0f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-06 20:48:16 +0300

    Added task 925

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9982,3 +9982,14 @@ def per(n):
         n = reduce(mul, map(int, str(n)))
         res.append(n)
     return res
+
+# There is a narrow hallway in which people can go right and left only. When two people meet in
+# the hallway, by tradition they must salute each other. People move at the same speed left and right.
+# Your task is to write a function that, given a string representation
+# of people moving in the hallway, will count the number of salutes that will occur.
+# Note: 2 salutes occur when people meet, one to the other and vice versa.
+# Input
+# People moving right will be represented by >; people moving left will be represented
+# by <. An example input would be >--<--->->. The - character represents empty space,
+# which you need not worry about.
+# Examples
