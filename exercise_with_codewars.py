6e76bed378caaa71411faa9848e5a3734d707601
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-06 20:33:51 +0300

    Complete task 922

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9952,3 +9952,5 @@ def filter_numbers(string):
 # Find the difference between two collections. The difference means that either the character is
 # present in one collection or it is present in other, but not in both. Return a sorted list with the difference.
 # The collections can contain any character and can contain duplicates.
+def diff(a, b):
+    return sorted(list(set([i for i in a if i not in b] + [i for i in b if i not in a])))
