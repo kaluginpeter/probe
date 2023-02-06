78d9833242c23bfa08ada18004ff32981a42f239
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-06 20:24:35 +0300

    Added task 919

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9919,3 +9919,13 @@ def hello_world():
 def find_squares(n):
     i = (n - 1) // 2
     return f'{(i + 1)**2}-{i**2}'
+
+# A cyclops number is a number in binary that is made up of all 1's,
+# with one 0 in the exact middle. That means all cyclops
+# numbers must have an odd number of digits for there to be an exact middle.
+# A couple examples:
+# 101
+# 11111111011111111
+# You must take an input, n, that will be in decimal format (base 10), then return True if that number
+# wil be a cyclops number when converted to binary, or False if it won't.
+# Assume n will be a positive integer.
