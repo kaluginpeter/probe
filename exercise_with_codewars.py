46dc5c0474da2de51f7c5c6eccb5eee91d0a21f5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-06 20:39:30 +0300

    Complete task 923

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9960,3 +9960,10 @@ def diff(a, b):
 # Every time you go down below 0 level counts as an entry of a valley,
 # and as you go up to 0 level from valley counts as an exit of a valley.
 # One passed valley is equal one entry and one exit of a valley.
+def counting_valleys(s):
+    level, valleys = 0, 0
+    for step in s:
+        if step == 'U' and level == -1:
+            valleys += 1
+        level += {'U': 1, 'F': 0, 'D': -1}[step]
+    return valleys
