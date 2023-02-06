763846ba87d04d5cc9b382bc959a816dc87508a5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-06 20:30:42 +0300

    Complete task 921

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -9946,3 +9946,5 @@ def shuffled_array(l):
 
 # Oh no, Timmy's filter doesn't seem to be working? Your task is
 # to fix the FilterNumber function to remove all the numbers from the string.
+def filter_numbers(string):
+    return "".join(x for x in string if not x.isdigit())
