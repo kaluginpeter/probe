f1dd3224fa888be2b2fadb2f59749f6a93a8271e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-07 20:58:30 +0300

    Complete task 928

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10011,3 +10011,6 @@ from gmpy2 import next_prime
 # integer, B is the number of multiples of 5 (but not 3)
 # below the given integer and C is the number of multiples of 3 and 5 below the given integer.
 # For example, solution(20) should return [5, 2, 1]
+def solution(number):
+    A, B, C = (number - 1) // 3, (number - 1) // 5, (number - 1) // 15
+    return [A - C, B - C, C]
