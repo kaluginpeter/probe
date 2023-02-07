9d517eb994334df32672f11b58a60126053598a9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-07 20:55:48 +0300

    Complete task 927

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10004,3 +10004,4 @@ def divisions(n, divisor):
 # Get the next prime number!
 # You will get a numbern (>= 0) and your task is to find the next prime number.
 # Make sure to optimize your code: there will numbers tested up to about 10^12.
+from gmpy2 import next_prime
