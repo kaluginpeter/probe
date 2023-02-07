7fed776222d775324bb39e668f05cc3de297e685
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-07 20:58:10 +0300

    Added task 928

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10005,3 +10005,9 @@ def divisions(n, divisor):
 # You will get a numbern (>= 0) and your task is to find the next prime number.
 # Make sure to optimize your code: there will numbers tested up to about 10^12.
 from gmpy2 import next_prime
+
+# Write a function that takes an integer and returns an array [A, B, C],
+# where A is the number of multiples of 3 (but not 5) below the given
+# integer, B is the number of multiples of 5 (but not 3)
+# below the given integer and C is the number of multiples of 3 and 5 below the given integer.
+# For example, solution(20) should return [5, 2, 1]
