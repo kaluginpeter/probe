c6932133bbf129dfaa3690a25337ea0c9153f2a8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-08 22:53:14 +0300

    Added task 942

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10119,3 +10119,9 @@ def itemgetter(item):
     return item['name']
 def get_names(data):
     return list(map(itemgetter,data))
+
+# You will be given an array of objects (associative arrays in PHP) representing data about developers who
+# have signed up to attend the next coding meetup that you are organising.
+# Your task is to return:
+# true if at least one Ruby developer has signed up; or
+# false if there will be no Ruby developers.
