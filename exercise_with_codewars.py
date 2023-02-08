25be09a3c11121819c47c086c9818edf691bc1cf
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-08 22:46:26 +0300

    Added task 939

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10091,3 +10091,8 @@ class Person:
         self.name = name
     def greet(self, other):
         return f"Hello {other}, my name is {self.name}"
+
+# Each day a plant is growing by upSpeed meters. Each night that plant's height decreases
+# by downSpeed meters due to the lack of sun heat. Initially, plant is 0 meters tall.
+# We plant the seed at the beginning of a day. We want to know when the height of the
+# plant will reach a certain level.
