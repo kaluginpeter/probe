6c26166d1d700582e8a604f49cf5786cb2dbabb5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-08 22:44:00 +0300

    Added task 938

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10079,3 +10079,10 @@ def solution(digits):
 # If no letters are given, the function should return 'z'
 def add_letters(*letters):
     return chr((sum(ord(c) - 96 for c in letters) - 1) % 26 + 97)
+# The following code could use a bit of object-oriented artistry. While it's a simple method
+# and works just fine as it is, in a larger system it's best to organize methods
+# into classes/objects. (Or, at least, something similar depending on your language)
+# Refactor the following code so that it belongs to a Person class/object. Each Person instance
+# will have a greet method. The Person instance should be instantiated with a name
+# so that it no longer has to be passed into each greet method call.
+# Here is how the final refactored code would be used:
