a3a303e5242cbc38aa4b72ee752c03eeb7616a8c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-08 22:40:39 +0300

    Added task 937

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10070,3 +10070,10 @@ def solve(n):
 # Adapted from ProjectEuler.net
 def solution(digits):
     return max(int(digits[i:i+5]) for i in range(0,len(digits)-4))
+
+# Your task is to add up letters to one letter.
+# The function will be given a variable amount of arguments, each one being a letter to add.
+# Notes:
+# Letters will always be lowercase.
+# Letters can overflow (see second to last example of the description)
+# If no letters are given, the function should return 'z'
