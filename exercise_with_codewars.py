8e294c7f83fc1bd681785045d5938a21e1347a63
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-09 20:24:58 +0300

    Added task 955

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10231,3 +10231,10 @@ def check_concatenated_sum(n, r):
 # "Dad" passed into the function should return 1 as "d" and "D" are duplicates.
 def count_consonants(text):
     return len(set(filter(str.isalpha, text.lower())) - set("aeiou"))
+
+# Mrs. Frizzle is beginning to plan lessons for her science class next semester, and
+# wants to encourage friendship amongst her students. To accomplish her goal, Mrs.
+# Frizzle will ensure each student has a chance to partner with every other student
+# in the class in a series of science projects.
+# Mrs. Frizzle does not know who will be in her class next semester, but she does know
+# she will have n students total in her class.
