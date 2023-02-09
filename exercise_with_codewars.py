28763541e34e12720b858e2e2e586f9b426eb1a7
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-09 20:22:26 +0300

    Added task 954

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10220,3 +10220,12 @@ def sum_of_a_beach(beach):
 # that each digit is concatenated n times and then summed.
 def check_concatenated_sum(n, r):
     return abs(n) == sum(int(i * r) for i in str(abs(n)) if r)
+
+# Complete the function that counts the number of unique consonants in a string
+# (made up of printable ascii characters).
+# Consonants are letters used in English other than "a", "e", "i", "o", "u".
+# Remember, your function needs to return the number of unique consonants
+# - disregarding duplicates. For example, if the string passed into the function reads "add", the
+# function should return 1 rather than 2, since "d" is a duplicate.
+# Similarly, the function should also disregard duplicate consonants of differing cases. For example,
+# "Dad" passed into the function should return 1 as "d" and "D" are duplicates.
