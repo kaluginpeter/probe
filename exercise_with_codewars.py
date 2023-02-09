9a19d7d8e56e7de73753294c50c7730db7885ea7
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-09 19:38:25 +0300

    Complete task 947

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10167,3 +10167,9 @@ def testit (a, b):
 
 # The following code was thought to be working properly, however when the code
 # tries to access the age of the person instance it fails.
+class Person():
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.full_name = f"{first_name} {last_name}"
+        self.age = age
