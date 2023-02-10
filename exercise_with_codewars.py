2c37903b789b6211bba223a4adf4b5595ed40659
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-10 20:59:45 +0300

    Added task 960

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10275,3 +10275,11 @@ def decode(s):
 import re
 def filter_words(phrase):
     return re.sub("(bad|mean|ugly|horrible|hideous)","awesome", phrase, flags=re.IGNORECASE)
+
+# This is the simple version of Shortest Code series. If you need some
+# challenges, please try the challenge version.
+# Task:
+# Find out "B"(Bug) in a lot of "A"(Apple).
+# There will always be one bug in apple, not need to consider the situation that without bug or more than one bugs.
+# input: string Array apple
+# output: Location of "B", [x,y]
