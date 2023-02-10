e24337bc76776b6ed9465c6a82542edb0c17e073
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-10 21:00:08 +0300

    Complete task 960

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10283,3 +10283,11 @@ def filter_words(phrase):
 # There will always be one bug in apple, not need to consider the situation that without bug or more than one bugs.
 # input: string Array apple
 # output: Location of "B", [x,y]
+def sc(apple):
+    c = 0
+    c1 = 0
+    for i in apple:
+        if 'B' in i:
+            c += i.index('B')
+            c1 += apple.index(i)
+    return [c1, c]
