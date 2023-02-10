f54e1076fe08fe839120232d0cac3cbd24a4bda2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-10 20:37:55 +0300

    Added task 956

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10240,3 +10240,11 @@ def count_consonants(text):
 # she will have n students total in her class.
 def projectPartners(n):
     return n * (n - 1) / 2
+
+# You have been employed by the Japanese government to write a function that tests whether or not a
+# building is strong enough to withstand a simulated earthquake.
+# A building will fall if the magnitude of the earthquake is greater than the strength of the building.
+# An earthquake takes the form of a 2D-Array. Each element within the Outer-Array represents a shockwave,
+# and each element within the Inner-Arrays represents a tremor.
+# The magnitude of the earthquake is determined by the product of the values
+# of its shockwaves. A shockwave is equal to the sum of the values of its tremors.
