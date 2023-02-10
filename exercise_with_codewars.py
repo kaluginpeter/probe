1c20d968302b3f0e161bd9a35c32c519fd8e2740
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-10 21:08:05 +0300

    Added task 961

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10291,3 +10291,9 @@ def sc(apple):
             c += i.index('B')
             c1 += apple.index(i)
     return [c1, c]
+
+# Yet another staple for the functional programmer. You have a sequence of values and some predicate for those values.
+# You want to remove the longest prefix of elements such that the predicate is true
+# for each element. We'll call this the dropWhile function. It accepts two arguments.
+# The first is the sequence of values, and the second is the predicate function. The
+# function does not change the value of the original sequence.
