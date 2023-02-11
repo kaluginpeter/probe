bc26c54c0ba74fa50edbd180abb80ab32704666f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-11 22:16:40 +0300

    Added task 971

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10374,3 +10374,8 @@ def comfortable_word(word):
     left = [c in 'qwertasdfgzxcvb' for c in word[::2]]
     right = [c in 'yuiophjklnm' for c in word[1::2]]
     return bool((all(left) and all(right)) or (not any(left) and not any(right)))
+
+# Bob is a chicken sexer. His job is to sort baby chicks into a Male(M)
+# and Female(F) piles. When bob can't guess can throw his hands up and declare it with a '?'.
+# Bob's bosses don't trust Bob's ability just yet, so they have paired him with an expert sexer.
+# All of Bob's decisions will be checked against the expert's choices to generate a correctness score.
