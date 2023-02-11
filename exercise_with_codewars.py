b9d772e9febc2cb343e783f4ca345771a20e2c2f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-11 22:31:32 +0300

    Complete task 973

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10403,3 +10403,5 @@ def is_valid(word):
 # teams scores and return the winner but timmy has gotten confused and sometimes teams
 # don't enter their scores, total the scores out of 3! Help timmy fix his program!
 # Return true if team 1 wins or false if team 2 wins!
+def calculate_total(t1, t2):
+    return sum(t1) > sum(t2)
