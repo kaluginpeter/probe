c76fbdb989f330c76e90c8203a3e7a25c6ad8bdc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-11 22:34:16 +0300

    Added task 974

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10405,3 +10405,10 @@ def is_valid(word):
 # Return true if team 1 wins or false if team 2 wins!
 def calculate_total(t1, t2):
     return sum(t1) > sum(t2)
+
+# You will be given two ASCII strings, a and b. Your task is write a function
+# to determine which one of these strings is "worth" more, and return it.
+# A string's worth is determined by the sum of its ASCII codepoint indexes. So,
+# for example, the string HELLO has a value of 372: H is codepoint 72, E 69, L 76, and O is 79.
+# The sum of these values is 372.
+# In the event of a tie, you should return the first string, i.e. a.
