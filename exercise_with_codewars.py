8d5396a8f960c08a426d60ad3a1de5c630056146
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-12 19:44:09 +0300

    Complete task 985

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10501,3 +10501,8 @@ def greet_jedi(first, last):
 # In this kata, you will take the keys and values of a dict and swap them around.
 # You will be given a dictionary, and then you will want to return a dictionary
 # with the old values as the keys, and list the old keys as values under their original keys.
+def switch_dict(dic):
+    res = {}
+    for key, value in dic.items():
+        res.setdefault(value, []).append(key)
+    return res
