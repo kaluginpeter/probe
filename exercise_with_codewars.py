4423239f580c738c173165a544769c513acaebdf
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-12 19:33:20 +0300

    Complete task 982

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10481,3 +10481,5 @@ def solve(nums,div):
 # Fortunately, I discovered that the virus hides my censored letters inside root directory.
 # It will be very tedious to recover all these files manually, so your goal is to implement
 # uncensor function that does the hard work automatically.
+def uncensor(infected, discovered):
+	return infected.replace('*', '{}').format(*discovered)
