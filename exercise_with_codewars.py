b23df19f0d30be3b79d573ecfb2aef208e828496
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-12 19:16:52 +0300

    Added task 979

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10445,3 +10445,11 @@ def hamming_weight(x):
 import itertools
 def transpose_two_strings(arr):
     return '\n'.join(' '.join(i) for i in itertools.zip_longest(arr[0], arr[1], fillvalue=' '))
+
+# Everybody knows a little german, right? But remembering the correct articles is a tough job.
+# Write yourself a little helper, that returns the noun with the matching article:
+# each noun containing less than 2 vowels has the article "das"
+# each noun containing 2/3 vowels has the article "die"
+# each noun containing more than 3 vowels has the article "der"
+# Caution: Vowels are "a,e,i,o,u". Umlaute (ä ö ü) are also being counted!
+# (This Kata is a joke, there is no such grammar rule!)
