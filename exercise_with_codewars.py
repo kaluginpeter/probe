6e52a308b5bca923a8340f5cad6a6ecd72109dfb
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-12 19:11:58 +0300

    Complete task 978

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10442,3 +10442,6 @@ def hamming_weight(x):
 # You will be given an array that contains two strings. Your job is to create a
 # function that will take those two strings and transpose them, so that the strings go from
 # top to bottom instead of left to right.
+import itertools
+def transpose_two_strings(arr):
+    return '\n'.join(' '.join(i) for i in itertools.zip_longest(arr[0], arr[1], fillvalue=' '))
