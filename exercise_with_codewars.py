96a648be84d0018a6c7697dc2e216d805f1f8713
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-12 19:05:18 +0300

    Added task 977

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10431,3 +10431,8 @@ def first_reverse_try(arr):
 # All inputs will be arrays/nested arrays.
 def append_arrays(seq1, seq2):
     return seq1 + seq2
+
+# The Hamming weight of a string is the number of symbols that are different from the zero-symbol
+# of the alphabet used. There are several algorithms for efficient computing of the Hamming
+# weight for numbers. In this Kata, speaking technically, you have to find out the
+# number of '1' bits in a binary representation of a number. Thus,
