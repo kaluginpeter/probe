ec0b7f10546443d7811ea75eb2568e3726469ac2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-13 17:47:40 +0300

    Added task 988

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10519,3 +10519,9 @@ def consecutive_ducks(n):
 from itertools import accumulate
 def get_section_id(scroll, sizes):
     return next((k for k,v in enumerate(accumulate(sizes)) if v > scroll), -1)
+
+# DropCaps means that the first letter of the starting word of the paragraph should be in caps and
+# the remaining lowercase, just like you see in the newspaper.
+# But for a change, let"s do that for each and every word of the given String. Your
+# task is to capitalize every word that has length greater than 2, leaving smaller words as they are.
+# *should work also on Leading and Trailing Spaces and caps.
