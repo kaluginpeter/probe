4cc5b47bef1e1e4220e4f42fdb7e9ef10b3f477c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-13 17:53:53 +0300

    Added task 989

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10527,3 +10527,10 @@ def get_section_id(scroll, sizes):
 # *should work also on Leading and Trailing Spaces and caps.
 def drop_cap(str_):
     return ' '.join(i.capitalize() if len(i) > 2 else i for i in str_.split(' '))
+
+# Every month, a random number of students take the driving test at Fast & Furious (F&F)
+# Driving School. To pass the test, a student cannot accumulate more than 18 demerit points.
+# At the end of the month, F&F wants to calculate the average demerit points
+# accumulated by ONLY the students who have passed, rounded to the nearest integer.
+# Write a function which would allow them to do so. If no students passed the
+# test that month, return 'No pass scores registered.'.
