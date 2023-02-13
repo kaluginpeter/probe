70f1567ab0e2fe1200d6fd4e30775db9f18a4a32
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-13 18:20:00 +0300

    Added task 994

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10581,3 +10581,11 @@ def i(word):
     if not word or word[0].islower() or word[0].upper() == 'I' or sum(1 for i in word.lower() if i in 'aeoiu') >= sum(1 for i in word.lower() if i not in 'aeoiu'):
         return 'Invalid word'
     return 'i' + word
+
+# You have a string that consists of zeroes and ones. Now choose
+# any two adjacent positions in the string: if one of them is 0, and
+# the other one is 1, remove these two digits from the string.
+# Return the length of the resulting (smallest) string that you can get after applying
+# this operation multiple times?
+# Note: after each operation, the remaining digits are separated by spaces and thus not adjacent
+# anymore - see the examples below.
