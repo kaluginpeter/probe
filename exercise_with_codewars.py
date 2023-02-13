7edab44249fddc8a71c5116397af68e31a088d88
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-13 18:04:31 +0300

    Added task 991

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10556,3 +10556,7 @@ def user_contacts(data):
             continue
         d[i[0]] = None
     return d
+
+# Zebulan has worked hard to write all his python code in strict compliance to PEP8 rules. In
+# this kata, you are a mischievous hacker that has set out to sabotage all his good code.
+# Your job is to take PEP8 compatible function names and convert them to camelCase. For example:
