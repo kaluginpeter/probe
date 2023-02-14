39b4339b1ce169b3c0d35caa7dcf3f0669ec3007
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-14 20:58:41 +0300

    Complete task 1000 Amazing!!! Here we go again

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10639,3 +10639,5 @@ def solution(number):
 
 # Write a function that accepts an array of 10 integers (between 0 and 9), that
 # returns a string of those numbers in the form of a phone number.
+def create_phone_number(n):
+    return f"({''.join(str(i) for i in n[:3])}) {''.join(str(i) for i in n[3:6])}-{''.join(str(i) for i in n[6:])}"
