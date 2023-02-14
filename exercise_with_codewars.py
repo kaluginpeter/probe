e4b2e5ae764668f3394d154f0f1e63d596c4eb69
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-14 20:52:07 +0300

    Added task 999

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10627,3 +10627,10 @@ def histogram(results):
 def sum_arrays(arrays, shift):
     sh = [[0]*i*shift + a + [0]*(len(arrays)-i-1)*shift  for i, a in enumerate(arrays)]
     return list(map(sum, zip(*sh)))
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
+# 3, 5, 6 and 9. The sum of these multiples is 23.
+# Finish the solution so that it returns the sum of all the multiples of
+# 3 or 5 below the number passed in. Additionally, if the number is negative, return 0
+# (for languages that do have them).
+# Note: If the number is a multiple of both 3 and 5, only count it once.
