98f7d3bc97138f68367249ae60b7295efeeaa1c0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-14 21:01:02 +0300

    Complete task 1001

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10642,8 +10642,11 @@ def solution(number):
 def create_phone_number(n):
     return f"({''.join(str(i) for i in n[:3])}) {''.join(str(i) for i in n[3:6])}-{''.join(str(i) for i in n[6:])}"
 
-Digital root is the recursive sum of all the digits in a number.
-
+# Digital root is the recursive sum of all the digits in a number.
 # Given n, take the sum of the digits of n. If that value
 # has more than one digit, continue reducing in this way until a single-digit number is
 # produced. The input will be a non-negative integer.
+def digital_root(n):
+    while len(str(n)) > 1:
+        n = sum(int(i) for i in str(n))
+    return n
