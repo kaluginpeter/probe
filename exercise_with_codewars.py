fc75fec2a0ffa506c94b99efe8d32159b44e8225
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-14 21:14:57 +0300

    Complete task 1005

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10672,3 +10672,5 @@ def narcissistic( value ):
 # Complete the solution so that it splits the string into pairs of two characters.
 # If the string contains an odd number of characters then it should replace the
 # missing second character of the final pair with an underscore ('_').
+def solution(s):
+    return [s[i:i+2] if len(s[i:i+2]) == 2 else s[i:i+2] + '_' for i in range(0, len(s), 2)]
