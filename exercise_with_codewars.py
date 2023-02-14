7f51bdb2c498c888dab59fe72a817309ef577e9e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-14 20:14:27 +0300

    Added task 996

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10599,3 +10599,10 @@ def zero_and_one(s):
 # when representations are of different lengths, the unmatched left-most bits are ignored
 def interlockable(a, b):
     return not a & b
+
+# Create a function that takes two arguments:
+# An array of objects which feature the season, the team and the country of the Champions League winner.
+# Country (as a string, for example, 'Portugal')
+# You function should then return the number which represents the number of times a team from
+# a given country has won. Return 0 if there have been no wins.
+# For example if the input array is as follows:
