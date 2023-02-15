b83da820426dc5a7636e5f7d77f5c637e110de28
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-15 17:31:42 +0300

    Complete task 1007

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10687,3 +10687,5 @@ def in_array(array1, array2):
 
 # Write simple .camelCase method (camel_case function in PHP, CamelCase in C# or
 # camelCase in Java) for strings. All words must have their first letter capitalized without spaces.
+def camel_case(string):
+    return string.title().replace(" ", "")
