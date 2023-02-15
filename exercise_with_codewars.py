b1c0ec385dcb31c100dc02d4552d4a24fb46cb96
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-15 18:14:39 +0300

    Complete task 1011

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10721,3 +10721,9 @@ def valid_phone_number(phone_number):
 
 # Assume "#" is like a backspace in string. This means that string "a#bc#d" actually is "bd"
 # Your task is to process a string with "#" symbols.
+def clean_string(s):
+    l = []
+    for i in s:
+        if i == '#' and l: l.pop()
+        elif i != '#': l.append(i)
+    return ''.join(l)
