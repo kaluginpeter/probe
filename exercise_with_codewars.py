9cbfdcd3b58d1eac0b3a8902e8f2d6fae9cffc2b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-15 18:03:24 +0300

    Added task 1010

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10708,3 +10708,8 @@ def good_vs_evil(good, evil):
     evil = sum([int(x) * y for x, y in zip(list(evil.split(' ')), [1, 2, 2, 2, 3, 5, 10])])
     res = ['Battle Result: No victor on this battle field', 'Battle Result: Good triumphs over Evil', 'Battle Result: Evil eradicates all trace of Good']
     return res[0] if good == evil else res[1] if good > evil else res[2]
+
+# Write a function that accepts a string, and returns true if it is in the form of a phone number.
+# Assume that any integer from 0-9 in any of the spots will produce a valid phone number.
+# Only worry about the following format:
+# (123) 456-7890 (don't forget the space after the close parentheses)
