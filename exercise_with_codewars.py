aba129394b4febb8b4a49abf3850723b8b0f195e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-15 17:23:23 +0300

    Complete task 1006

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10677,3 +10677,10 @@ def solution(s):
 
 # Given two arrays of strings a1 and a2 return a sorted array r
 # in lexicographical order of the strings of a1 which are substrings of strings of a2
+def in_array(array1, array2):
+    r = []
+    for i in array1:
+        for j in array2:
+            if i in j:
+                r.append(i)
+    return sorted(set(r))
