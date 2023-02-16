a69b9a1a1761e69a548ca48610733f87b840c73a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-16 21:32:09 +0300

    Added task 1017

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10774,3 +10774,14 @@ def number_of_pairs(gloves):
 # Write a function that when given a number >= 0, returns an Array of ascending length subarrays.
 def pyramid(n):
     return [[1] * i for i in range(1, n+1)]
+
+# In this kata you have to write a simple Morse code decoder. While the Morse code
+# is now mostly superseded by voice and digital data communication channels,
+# it still has its use in some applications around the world.
+# The Morse code encodes every character as a sequence of "dots" and "dashes". For
+# example, the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−. The
+# Morse code is case-insensitive, traditionally capital letters are used. When the message
+# is written in Morse code, a single space is used to separate the character
+# codes and 3 spaces are used to separate words. For example, the message
+# HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+# NOTE: Extra spaces before or after the code have no meaning and should be ignored.
