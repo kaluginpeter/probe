041de99bda89667c6b337e1352d4dcc7ce52a034
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-16 22:29:34 +0300

    Added task 1025

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10869,3 +10869,10 @@ def calc(expr):
         elif token:
             s.append(float(token))
     return s.pop()
+
+# Write a function that accepts two square matrices (N x N two dimensional arrays), and return the
+# sum of the two. Both matrices being passed into the function will be of size N x
+# N (square), containing only integers.
+# How to sum two matrices:
+# Take each cell [n][m] from the first matrix, and
+# add it with the same [n][m] cell from the second matrix. This will be cell [n][m] of the solution matrix.
