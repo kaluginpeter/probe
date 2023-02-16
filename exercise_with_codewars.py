7ff8df5c318b764abbb3a405bf967efb140501a7
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-16 22:22:27 +0300

    Added task 1022

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10840,3 +10840,7 @@ def sqInRect(lng, wdth):
             wdth, lng = lng, wdth
     res.append(wdth)
     return res
+
+# which takes numbers num1 and num2 and returns 1 if there is
+# a straight triple of a number at any place in num1 and also a straight double of the same number in num2.
+# If this isn't the case, return 0
