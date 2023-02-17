4c95fe40326b23f9307c455d52fb0c413813d7cc
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-17 23:01:40 +0300

    Added task 1029

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10898,3 +10898,8 @@ def longest_repetition(chars):
 # returns a string that is the decimal representation of the number grouped by commas after every 3 digits.
 def group_by_commas(n):
     return '{:,}'.format(n)
+
+# Your task is to write such a run-length encoding. For a given string, return
+# a list (or array) of pairs (or arrays) [ (i1, s1), (i2, s2), …, (in, sn) ], such that one can reconstruct
+# the original string by replicating the character sx ix times and concatening all those strings.
+# Your run-length encoding should be minimal, ie. for all i the values si and si+1 should differ.
