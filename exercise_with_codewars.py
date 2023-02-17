aaece2b8f0c425356e17e97357d7d0b5584dfeee
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-17 23:16:45 +0300

    Added task 1033

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10926,3 +10926,10 @@ def dup(arry):
 # of the integers in the input array should not matter.
 def twos_difference(lst):
     return [(i, i+2) for i in sorted(lst) if i + 2 in lst]
+
+# Write a function that receives two strings and returns n, where n is equal to the
+# number of characters we should shift the first string forward to match the second. The check should
+# be case sensitive.
+# For instance, take the strings "fatigue" and "tiguefa". In this case, the
+# first string has been rotated 5 characters forward to produce the second string, so 5 would be returned.
+# If the second string isn't a valid rotation of the first string, the method returns -1.
