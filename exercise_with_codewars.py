23d0b33adfa9882575adf7b1c50c53adbdbfed6b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-17 22:56:07 +0300

    Complete task 1027

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10888,3 +10888,8 @@ def fold_array(array, runs):
     return nums
 
 # For a given string s find the character c (or C) with longest consecutive repetition and return:
+import re
+def longest_repetition(chars):
+    if not chars: return ("", 0)
+    l = max(re.findall(r"((.)\2*)", chars), key=lambda x: len(x[0]))
+    return (l[1], len(l[0]))
