8019d3b687605933ca38c12f2217e0047888f3fa
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-18 22:56:11 +0300

    Complete task 1042

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10992,3 +10992,12 @@ def delete_digit(n):
 
 # You have to sort the inner content of every word of a string in descending order.
 # The inner content is the content of a word without first and the last char.
+def sort_the_inner_content(str):
+    w = str.split()
+    f = []
+    for i in w:
+        if len(i) > 2:
+            f.append(i[0] + ''.join(sorted(i[1:-1], reverse=True)) + i[-1])
+            continue
+        f.append(i)
+    return ' '.join(f)
