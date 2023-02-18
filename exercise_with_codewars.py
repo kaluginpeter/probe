69587923f2a6b8994e20a50c066078688b6afa04
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-18 22:34:43 +0300

    Added task 1038

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10965,3 +10965,9 @@ def format_words(words):
 # the columns and rows of the original are swapped.
 def transpose(matrix):
     return list(map(list, zip(*matrix)))
+
+# You will be given a sequence of objects representing data about developers who have signed up
+# to attend the next coding meetup that you are organising.
+# Your task is to return a sequence which includes the developer who is the oldest. In
+# case of a tie, include all same-age senior developers listed in the same order
+# as they appeared in the original input array.
