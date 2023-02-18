2e4bc084970b13add55e5ae954fc2e56ce51072c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-18 22:35:06 +0300

    Complete task 1038

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -10971,3 +10971,5 @@ def transpose(matrix):
 # Your task is to return a sequence which includes the developer who is the oldest. In
 # case of a tie, include all same-age senior developers listed in the same order
 # as they appeared in the original input array.
+def find_senior(lst):
+    return [i for i in lst if i['age'] == max(i['age'] for i in lst)]
