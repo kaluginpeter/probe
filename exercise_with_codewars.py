5b6a31eb09fa14439a47e3074c22b0d7956e2aac
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-19 19:41:51 +0300

    Complete task 1052

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11095,3 +11095,5 @@ def oper_array(fct, arr, init):
     return res
 
 # Given u0 = 1, u1 = 2 and the relation 6unun+1-5unun+2+un+1un+2 = 0 calculate un for any integer n >= 0.
+def fcn (n):
+    return 2**n
