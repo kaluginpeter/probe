dfa2a707a60a202321f50b9a2d1fe1ae458f93cb
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-20 18:40:57 +0300

    Added task 1060

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11159,3 +11159,7 @@ def numericals(s):
         d[i] = d.get(i, 0) + 1
         w += str(d[i])
     return w
+
+# Complete the solution so that it returns true if it contains any duplicate argument values.
+# Any number of arguments may be passed into the function.
+# The array values passed in will only be strings or numbers. The only valid return values are true and false.
