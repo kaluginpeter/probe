145a9677169b855c9cff8960fa02b5c1f13ab9c3
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-20 18:29:58 +0300

    Complete task 1057

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11131,3 +11131,8 @@ def grabscrab(said, possible_words):
 # it as a floored whole number (ie: no decimal places) written out as a string. Eg:
 # "zero nine five two" -> "four"
 #If the string is empty or includes a number greater than 9, return "n/a"
+def average_string(s):
+    d = {'zero':0, 'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9}
+    if s and all(i in d for i in s.split()):
+        return list(d.keys())[list(d.values()).index(int(sum(d[i] for i in s.split()) / len(s.split())))]
+    return 'n/a'
