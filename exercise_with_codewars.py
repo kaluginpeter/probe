47528bdd64aa5dd241c62ea116bd953ba2043106
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-20 18:41:19 +0300

    Complete task 1060

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11163,3 +11163,5 @@ def numericals(s):
 # Complete the solution so that it returns true if it contains any duplicate argument values.
 # Any number of arguments may be passed into the function.
 # The array values passed in will only be strings or numbers. The only valid return values are true and false.
+def solution(*args):
+    return len(set(args)) != len(args)
