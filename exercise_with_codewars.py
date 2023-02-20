af56ac72a8a577f133b39aa474ac8bab71e69634
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-20 18:50:21 +0300

    Added task 1063

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11186,3 +11186,11 @@ def fibonacci(n):
 import re
 def is_a_valid_message(message):
     return all(k and int(k) == len(v) for k, v in re.findall("(\d*)(\D*)", message)[:-1])
+
+# You will be given an array of objects (associative arrays in PHP) representing data about
+# developers who have signed up to attend the next coding meetup that you are organising.
+# Your task is to return:
+# true if developers from all of the following age groups have signed up:
+# teens, twenties, thirties, forties, fifties, sixties, seventies, eighties, nineties, centenarian
+# (at least 100 years young).
+# false otherwise.
