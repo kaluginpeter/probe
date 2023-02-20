575afcc5d12f63e475b9a501710edb794da24b8d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-20 18:44:15 +0300

    Complete task 1061

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11169,3 +11169,10 @@ def solution(*args):
 # The function 'fibonacci' should return an array of fibonacci numbers. The function takes a
 # number as an argument to decide how many no. of elements to produce. If
 # the argument is less than or equal to 0 then return empty array
+def fibonacci(n):
+    l = []
+    a, b = 0, 1
+    for i in range(n):
+        l.append(a)
+        a, b = b, a + b
+    return l if n > 0 else []
