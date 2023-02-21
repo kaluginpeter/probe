50eb552dc3603347b5d114a820cf0d3aa086ac95
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-21 22:21:28 +0300

    Complete task 1075

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11300,3 +11300,11 @@ def split_integer(num, parts):
 # that are lower than the given number n (n is not the nth element of Fibonacci sequence) without including n.
 # The Fibonacci sequence is a series of numbers where the next value is the addition of
 # the previous two values. The series starts with 0 and 1:
+def even_fib(m):
+    a, b = 0, 1
+    c = 0
+    while b < m:
+        if b % 2 == 0:
+            c += b
+        a, b = b, a + b
+    return c
