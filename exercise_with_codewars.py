834c38a436e21bcac0db2e30e44345651a3e6836
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-22 18:11:52 +0300

    Added task 1077

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11322,3 +11322,9 @@ def mineLocation(field):
     for i in field:
         for j in i:
             if j == 1: return [field.index(i), i.index(j)]
+
+# Your MyRobot-specific (esoteric) scripting language called RoboScript only ever contains the
+# following characters: F, L, R, the digits 0-9 and brackets (( and )). Your goal is
+# to write a function highlight which accepts 1 required argument code which is the RoboScript program passed
+# in as a string and returns the script with syntax highlighting. The following commands/characters
+# should have the following colors:
