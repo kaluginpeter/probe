260fe1a2a80c59c7a6383dfd3330e21591588fa1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-22 18:46:51 +0300

    Added task 1085

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11404,3 +11404,11 @@ from itertools import count
 def next_higher(value):
     c = bin(value).count('1')
     return next(i for i in count(value+1) if bin(i).count('1') == c)
+
+# Consider the numbers 6969 and 9116. When you rotate them 180 degrees (upside down), these numbers
+# remain the same. To clarify, if we write them down on a paper
+# and turn the paper upside down, the numbers will be the same. Try it and
+# see! Some numbers such as 2 or 5 don't yield numbers when rotated.
+# Given a range, return the count of upside down numbers within that range.
+# For example, solve(0,10) = 3, because there are only 3 upside down numbers >= 0 and < 10. They are 0, 1, 8.
+# More examples in the test cases.
