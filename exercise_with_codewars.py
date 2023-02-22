b2050324437ff3e893821fdfa123947a7aba402f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-22 18:12:15 +0300

    Complete task 1077

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11328,3 +11328,10 @@ def mineLocation(field):
 # to write a function highlight which accepts 1 required argument code which is the RoboScript program passed
 # in as a string and returns the script with syntax highlighting. The following commands/characters
 # should have the following colors:
+import re
+def highlight(code):
+    code = re.sub(r"(F+)", '<span style="color: pink">\g<1></span>', code)
+    code = re.sub(r"(L+)", '<span style="color: red">\g<1></span>', code)
+    code = re.sub(r"(R+)", '<span style="color: green">\g<1></span>', code)
+    code = re.sub(r"(\d+)", '<span style="color: orange">\g<1></span>', code)
+    return code
