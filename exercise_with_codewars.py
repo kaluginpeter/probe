3326b3bf329ab2c57820e43d644885a57a85a588
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-23 20:57:46 +0300

    Added task 1091

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11464,3 +11464,10 @@ def encrypt(text, rule):
 # More scientifically: { i : x ≤ i ≤ y, i mod k = 0 }
 def divisible_count(x, y, k):
     return y // k - (x - 1) // k
+
+# Your job is to change the given string s using a non-negative integer n.
+# Each bit in n will specify whether or not to swap the case for each alphabetic
+# character in s: if the bit is 1, swap the case; if its 0, leave it as is.
+# When you finish with the last bit of n, start again with the first bit.
+# You should skip the checking of bits when a non-alphabetic character is encountered, but
+# they should be preserved in their original positions.
