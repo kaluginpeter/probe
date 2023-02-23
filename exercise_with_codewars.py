2f7016ac588af59b0cddc1dd7c5fde7bdf133819
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-23 21:16:34 +0300

    Added task 1094

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11488,3 +11488,12 @@ def expanded_form(num):
 # The resulting carnage looks like this:
 def deadAntCount(ants):
     return max(ants.replace("ant", "").count(i) for i in "ant")
+
+# Here you have to do some mathematical operations on a "dirty string". This kata checks some
+# basics, it's not too difficult.
+# So what to do?
+# Input: String which consists of two positive numbers (doubles) and exactly one operator
+# like +, -, * or / always between these numbers. The string is dirty, which means that
+# there are different characters inside too, not only numbers and the operator. You have to combine
+# all digits left and right, perhaps with "." inside (doubles), and to calculate the
+# result which has to be rounded to an integer and converted to a string at the end.
