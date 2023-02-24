4b580266f3b05897305634453bebff1cfb77d33c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-24 22:28:23 +0300

    Complete task 1096

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11512,3 +11512,7 @@ def linked_list_from_string(s, split=" -> "):
 
 # Write a function that determines whether the passed in sequences are similar. Similar means
 # they contain the same elements, and the same number of occurrences of elements.
+def arrays_similar(seq1, seq2):
+    l1 = ''.join(str(i) for i in seq1)
+    l2 = ''.join(str(i) for i in seq2)
+    return set(seq1) == set(seq2) and sorted(l1) == sorted(l2)
