a3ba60ba138cc33a3113fd5ca095260fa7fdca3a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-24 22:57:34 +0300

    Added task 1099

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11536,3 +11536,11 @@ def reverse(seq):
     l = list()
     for _ in range(len(seq)): l.append(seq.pop())
     seq.extend(l)
+
+# Write a function that takes a string and returns an array of the repeated
+# characters (letters, numbers, whitespace) in the string.
+# If a charater is repeated more than once, only show it once in the result array.
+# Characters should be shown by the order of their first repetition. Note that this may be different
+# from the order of first appearance of the character.
+# Characters are case sensitive.
+# For F# return a "char list"
