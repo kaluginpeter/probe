f9dd12ca9a14ace460a1e8ddef6f79a1414b543b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-24 22:43:26 +0300

    Added task 1098

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11525,3 +11525,10 @@ def next_version(version):
     elif int(version[-1]) < 9:
         return f"{version[0:-1]}{str(int(version[-1]) + 1)}"
     return next_version(version[0:-2]) + '.0'
+
+# Write a function that will take in any array and reverse it.
+# Sounds simple doesn't it?
+# NOTES:
+# Array should be reversed in place! (no need to return it)
+# Usual builtins have been deactivated. Don't count on them.
+# You'll have to do it fast enough, so think about performances
