03be357c95f97a8edb1cb3d0540adc98511bc9e0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-25 22:43:05 +0300

    Complete task 1112

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11702,3 +11702,6 @@ def controller(events):
 # with the elements inside rotated n spaces.
 # If n is greater than 0 it should rotate the array to the right. If n
 # is less than 0 it should rotate the array to the left. If n is 0, then it should return the array unchanged.
+def rotate(arr, n):
+    n = n % len(arr)
+    return arr[-n:] + arr[:-n]
