4fba5745ed1db69cdf1e5a4759422a01f730e15b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-25 22:13:09 +0300

    Added task 1110

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11669,3 +11669,8 @@ class list(list):
             if type(x) == int and r1 <= x <= r2:
                 res.append(x)
         return res
+
+# Implement the method countIf (count_if in PHP and Python), which accepts a linked list
+# (head) and a predicate function, and returns the number of elements which apply to the given predicate.
+# For example: Given the list: 1 -> 2 -> 3, and the predicate x => x >= 2, countIf /
+# count_if should return 2, since x >= 2 applies to both 2 and 3.
