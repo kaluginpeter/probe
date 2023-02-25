419351ddc103e82e023e5c028e4f0be93324fcf2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-25 22:13:31 +0300

    Complete task 1110

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11674,3 +11674,9 @@ class list(list):
 # (head) and a predicate function, and returns the number of elements which apply to the given predicate.
 # For example: Given the list: 1 -> 2 -> 3, and the predicate x => x >= 2, countIf /
 # count_if should return 2, since x >= 2 applies to both 2 and 3.
+def count_if(head, func):
+    c = 0
+    while head:
+        c += func(head.data)
+        head = head.next
+    return c
