44ba030d853bcecc0257c1eb9e4ee8c63ed371d5
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-26 22:44:55 +0300

    Added task 1119

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11755,3 +11755,12 @@ def reduce_by_rules(lst, rules):
 import re
 def split_odd_and_even(n):
     return [int(i) for i in re.findall(r"([2468]+|[13579]+)", str(n))]
+
+# In this task, you need to restore a string from a list of its copies.
+# You will receive an array of strings. All of them are supposed to be
+# the same as the original but, unfortunately, they were corrupted which means some of
+# the characters were replaced with asterisks ("*").
+# You have to restore the original string based on non-corrupted information you have. If
+# in some cases it is not possible to determine what the original
+# character was, use "#" character as a special marker for that.
+# If the array is empty, then return an empty string.
