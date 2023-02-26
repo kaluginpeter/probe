0e8a215dcc4edc41b71e5e9b22ae423910757231
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-26 22:32:20 +0300

    Added task 1116

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11725,3 +11725,10 @@ def sort_array(l):
 from collections import Counter
 def anagram_counter(words):
     return sum(i *(i-1)// 2 for i in Counter(''.join(sorted(j)) for j in words).values())
+
+#  happy number is a number defined by the following process: starting with any positive integer, replace the
+#  number by the sum of the squares of its digits, and repeat the process until the number equals
+#  1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+# Those numbers for which this process ends in 1 are happy numbers, while those that do
+# not end in 1 are unhappy numbers (or sad numbers) (Wikipedia).
+# Write a function that takes n as parameter and return true if and only if n is an happy number, false otherwise.
