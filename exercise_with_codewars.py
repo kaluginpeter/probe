e6055c46cc01b3e547662a2a94c14002e5aacc94
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-27 22:37:49 +0300

    Complete task 1134

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11898,3 +11898,6 @@ def generate_hashtag(s):
 # Write a program that will calculate the number of trailing zeros in a factorial of a given number.
 # N! = 1 * 2 * 3 *  ... * N
 # Be careful 1000! has 2568 digits...
+def zeros(n):
+    integer = n//5
+    return integer + zeros(integer) if integer else 0
