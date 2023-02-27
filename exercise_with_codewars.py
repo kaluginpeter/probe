28b17d1e5c1bc3f8c6915d371da93694696e3548
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-27 22:29:53 +0300

    Added task 1133

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11883,3 +11883,11 @@ def order_weight(_str):
 # can be considered as 0.
 def cakes(recipe, available):
     return min([available[i] // recipe[i] if i in available else 0 for i in recipe])
+
+# The marketing team is spending way too much time typing in hashtags.
+# Let's help them with our own Hashtag Generator!
+# Here's the deal:
+# It must start with a hashtag (#).
+# All words must have their first letter capitalized.
+# If the final result is longer than 140 chars it must return false.
+# If the input or the result is an empty string it must return false.
