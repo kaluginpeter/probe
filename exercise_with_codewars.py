499a23b3527368a16ef9ab956dde0c6e3877c521
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-27 22:10:01 +0300

    Complete task 1128

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11833,3 +11833,5 @@ def pig_it(text):
 
 # Write a function, which takes a non-negative integer (seconds) as input and returns the
 # time in a human-readable format (HH:MM:SS)
+def make_readable(seconds):
+    return "%02d:%02d:%02d" % (seconds / 3600, seconds / 60 % 60, seconds % 60)
