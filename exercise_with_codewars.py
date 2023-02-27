8498f8316570e58e8bd337fb0c3792f3d5c82489
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-27 21:55:39 +0300

    Complete task 1126

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11822,3 +11822,5 @@ def padovan(n):
 
 # Write an algorithm that takes an array and moves all of the zeros to the end, preserving the
 # order of the other elements.
+def move_zeros(lst):
+    return [i for i in lst if i != 0] + [i for i in lst if i == 0]
