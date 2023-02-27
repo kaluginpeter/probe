cafbc2fc28531b12ff2ae61fa0716063c20714c9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-27 22:25:57 +0300

    Added task 1132

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11873,3 +11873,11 @@ def first_non_repeating_letter(string):
 # members in normal order can you give this string ordered by "weights" of these numbers?
 def order_weight(_str):
     return ' '.join(sorted(sorted(_str.split()), key=lambda x: sum(int(i) for i in x)))
+
+# Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately
+# he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
+# Write a function cakes(), which takes the recipe (object) and the available ingredients
+# (also an object) and returns the maximum number of cakes Pete can bake (integer).
+# For simplicity there are no units for the amounts (e.g. 1 lb of flour
+# or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects,
+# can be considered as 0.
