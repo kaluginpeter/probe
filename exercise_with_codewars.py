a15f8ce0f4acc920832b0ede99cb023ca1fb69a9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-27 22:12:55 +0300

    Complete task 1129

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11839,3 +11839,10 @@ def make_readable(seconds):
 # Write a function that takes a string of parentheses, and determines if the
 # order of the parentheses is valid. The function should return true if the
 # string is valid, and false if it's invalid.
+def valid_parentheses(string):
+    c = 0
+    for i in string:
+        if i == "(": c += 1
+        elif i == ")": c -= 1
+        if c < 0: return False
+    return c == 0
