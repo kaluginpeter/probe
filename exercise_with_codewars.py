86bd9bc17e2da51330789023c20e136674efa6ce
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-28 22:37:16 +0300

    Added task 1141

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11957,3 +11957,9 @@ class add(int):
 # contains a digit
 # only contains alphanumeric characters (note that '_' is not alphanumeric)
 regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[^\W_]{6,}$"
+
+# Find all integers between m and n (m and n integers with 1 <= m <= n) such that the sum
+# of their squared divisors is itself a square.
+# We will return an array of subarrays or of tuples (in C an array of Pair) or a string. The
+# subarrays (or tuples or Pairs) will have two elements: first the number the squared divisors
+# of which is a square and then the sum of the squared divisors.
