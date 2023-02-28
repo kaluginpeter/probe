0484f8626df47dcabcad99b0fac008d030ae470c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-28 22:16:17 +0300

    Added task 1138

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11935,3 +11935,10 @@ def perimeter(n):
         a, b = b, a + b
         l.append(a)
     return sum(l) * 4
+
+# In this example you have to validate if a user input string is alphanumeric.
+# The given string is not nil/null/NULL/None, so you don't have to check that.
+# The string has the following conditions to be alphanumeric:
+# At least one character ("" is not valid)
+# Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+# No whitespaces / underscore
