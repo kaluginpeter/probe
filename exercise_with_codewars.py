be3530b3090a747e26ca4f8e89140f3a49d28d60
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-02-28 22:22:59 +0300

    Complete task 1139

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -11946,3 +11946,6 @@ def alphanumeric(p):
     return all(i.isdigit() or i.isalpha() for i in p) and len(p) > 0
 
 # We want to create a function that will add numbers together when called in succession.
+class add(int):
+    def __call__(self, n):
+        return add(self + n)
