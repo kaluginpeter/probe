89565e8064aa5ccdde68c5743f8d1b276c2aaa29
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-01 21:24:13 +0300

    Added task 1147

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12034,3 +12034,9 @@ def josephus(xs, k):
         i = (i + k - 1) % len(xs)
         j.append(xs.pop(i))
     return j
+
+# Complete the function that
+# accepts two integer arrays of equal length
+# compares the value each member in one array to the corresponding member in the other
+# squares the absolute value difference between those two values
+# and returns the average of those squared absolute value difference between each member pair.
