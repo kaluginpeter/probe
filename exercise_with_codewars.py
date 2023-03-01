7ab00152aa0866c6bf684af8135092600fd973ea
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-01 21:56:43 +0300

    Added task 1151

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12078,3 +12078,13 @@ def dec2FactString(nb):
     return "".join(dropwhile(lambda x: x == "0", l))
 def factString2Dec(string):
     return sum(BASIS[k] * DIGITS.index(v) for k, v in enumerate(reversed(string)))
+
+# Carol's boss Bob thinks he is very smart. He says he made an app which renders
+# messages unreadable without changing any letters, only by adding some new ones, while preserving
+# message integrity (i. e. the original message can still be retrieved).
+# He gave some limited access to his app to Carol to challenge her, and hinted
+# that if Carol cannot crack this simple task, she might be fired.
+# Carol was trying to crack this code herself, but got too tired,
+# so she came to you for help. However, she succeeded to hack Bob's app
+# and found a data field called 'marker'. She thinks it can be helpful for cracking Bob's app.
+# Help Carol keep her job!
