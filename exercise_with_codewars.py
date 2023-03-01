dd68080c104d251e89185ebb2e2564b0728feb96
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-01 21:40:57 +0300

    Complete task 1148

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12047,3 +12047,10 @@ def solution(a, b):
 # can be formed from two other strings, part1 and part2.
 # The restriction is that the characters in part1 and part2 should be in the same order as in s.
 # The interviewer gives you the following example and tells you to figure out the rest from the given test cases.
+def is_merge(s, part1, part2):
+    if not part1: return s == part2
+    if not part2: return s == part1
+    if not s: return part1 + part2 == ''
+    if s[0] == part1[0] and is_merge(s[1:], part1[1:], part2): return True
+    if s[0] == part2[0] and is_merge(s[1:], part1, part2[1:]): return True
+    return False
