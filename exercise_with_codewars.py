08ea8b7e8ff0dd087196e87df6d98b16a561958c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-02 22:36:08 +0300

    Complete task 1165

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12204,3 +12204,31 @@ def tower_builder(n_floors, block_size):
 # The code provided has a method hello which is supposed to show only those attributes
 # which have been explicitly set. Furthermore, it is supposed to say them in the same order they were set.
 # But it's not working properly.
+class Dinglemouse(object):
+
+    def __init__(self):
+        self.name = None
+        self.sex = None
+        self.age = None
+        self.hell = 'Hello.'
+
+    def setAge(self, age):
+        if self.age == None:
+            self.hell = self.hell + ' I am {age}.'
+        self.age = age
+        return self
+
+    def setSex(self, sex):
+        if self.sex == None:
+            self.hell = self.hell + ' I am {sex}.'
+        self.sex = "male" if sex == 'M' else "female"
+        return self
+
+    def setName(self, name):
+        if self.name == None:
+            self.hell = self.hell + ' My name is {name}.'
+        self.name = name
+        return self
+
+    def hello(self):
+        return self.hell.format(age=self.age, sex=self.sex, name=self.name)
