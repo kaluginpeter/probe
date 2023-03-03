2da2de803d61cd13193dfe338d6ac05414070902
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-03 22:24:22 +0300

    Complete task 1170

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12278,3 +12278,11 @@ def f(n, m):
 # Always keep a space between the input string and the left and right borders.
 # The biggest string inside the array should always fit in the frame.
 # The input array is never empty.
+def frame(text, char):
+    text_lens = [len(i) for i in text]
+    longest_len = max(text_lens)
+    frame_list = [char*(longest_len + 4)]
+    for i in text:
+         frame_list.append("{} {}{} {}".format(char, i, " " * (longest_len - len(i)), char))
+    frame_list.append(char*(longest_len + 4))
+    return "\n".join(frame_list)
