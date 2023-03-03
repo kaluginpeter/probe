b0529a7394c22d168b86bdf97ee13b613e2c119c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-03 22:13:53 +0300

    Complete task 1168

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12261,3 +12261,10 @@ def is_img(file_name):
 # In this Kata, you have to design a simple routing class for a web framework.
 # The router should accept bindings for a given url, http method and an action.
 # Then, when a request with a bound url and method comes in, it should return the result of the action.
+class Router:
+    def __init__(self):
+        self._routes = {}
+    def bind(self, url, method, a):
+        self._routes[(url, method)] = a
+    def runRequest(self, url, method):
+        return self._routes.get((url, method), lambda: "Error 404: Not Found")()
