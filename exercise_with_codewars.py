efc7ded8b203f7e4e3e8ecd96e5e8b208372f918
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-03 22:27:19 +0300

    Added task 1171

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12286,3 +12286,11 @@ def frame(text, char):
          frame_list.append("{} {}{} {}".format(char, i, " " * (longest_len - len(i)), char))
     frame_list.append(char*(longest_len + 4))
     return "\n".join(frame_list)
+
+# Your goal is to create a function to format a number given a template; if the number is not present,
+# use the digits 1234567890 to fill in the spaces.
+# A few rules:
+# the template might consist of other numbers, special characters or the like: you
+# need to replace only alphabetical characters (both lower- and uppercase);
+# if the given or default string representing the number is shorter than the template, just
+# repeat it to fill all the spaces.
