2d0c2840209c3271cb29844ad831166fae6ed2f6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-03 21:56:36 +0300

    Added task 1167

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12238,3 +12238,18 @@ class Dinglemouse(object):
 # Both integers are equal or greater than 1; no need to check for invalid parameters.
 def dot(n, m):
     return ("+---" * n + "+\n" + "| o " * n + "|\n") * m + ("+---" * n + "+")
+
+# Are you a file extension master? Let's find out by checking if Bill's files are
+# images or audio files. Please use regex if available natively for your language.
+# You will create 2 string methods:
+# isAudio/is_audio, matching 1 or + uppercase/lowercase letter(s) (combination possible), with
+# the extension .mp3, .flac, .alac, or .aac.
+# isImage/is_image, matching 1 or + uppercase/lowercase letter(s) (combination possible), with
+# the extension .jpg, .jpeg, .png, .bmp, or .gif.
+# Note that this is not a generic image/audio files checker. It's
+# meant to be a test for Bill's files only. Bill doesn't like
+# punctuation. He doesn't like numbers, neither. Thus, his filenames are letter-only
+# Rules
+# It should return true or false, simply.
+# File extensions should consist of lowercase letters and numbers only.
+# File names should consist of letters only (uppercase, lowercase, or both)
