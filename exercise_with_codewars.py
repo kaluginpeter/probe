bc23fe67425825b7e369eb68141b3650a36f4549
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-03 22:32:16 +0300

    Added task 1172

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12305,3 +12305,9 @@ def numeric_formatter(template, data='1234567890'):
 # first there are 3 A, then 3 B, then 3 C and last there is 1 D.
 # Your task is to write a RLE encoder and decoder using this technique. The texts
 # to encode will always consist of only uppercase characters, no numbers.
+import re
+def encode(s):
+    return "".join(f"{len(k)}{v}" for k, v in re.findall(r"((.)\2*)", s))
+
+def decode(s):
+    return "".join(int(k) * v for k, v in re.findall(r"(\d+)(\w)", s))
