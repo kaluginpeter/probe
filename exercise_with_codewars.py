8e4bd79306d0f857659caab013096010bc49b748
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-03 22:20:01 +0300

    Complete task 1169

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12270,3 +12270,5 @@ class Router:
         return self._routes.get((url, method), lambda: "Error 404: Not Found")()
 
 # for i from 1 to n, do i % m and return the sum
+def f(n, m):
+    return (n // m) * (m - 1) * m / 2 + (n % m) * (n % m + 1) / 2
