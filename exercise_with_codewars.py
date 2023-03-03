f46322aa90f9332ae881917b3eeff89898324197
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-03 21:37:23 +0300

    Complete task 1166

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12236,3 +12236,5 @@ class Dinglemouse(object):
 # You will get two integers n (width) and m (height) and your task is to
 # draw the following pattern. Each line is seperated with a newline (\n)
 # Both integers are equal or greater than 1; no need to check for invalid parameters.
+def dot(n, m):
+    return ("+---" * n + "+\n" + "| o " * n + "|\n") * m + ("+---" * n + "+")
