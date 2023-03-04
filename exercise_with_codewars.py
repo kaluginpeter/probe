0cbc9d66fe1c2039502809b8acfd01a841501646
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-04 22:13:47 +0300

    Complete task 1178

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12349,3 +12349,6 @@ def is_divisible_by_6(s):
 # I want to know the size of the longest consecutive elements of X in Y.
 # You will receive two arguments: items and key. Return the length of the
 # longest segment of consecutive keys in the given items.
+import re
+def get_consective_items(item, key):
+    return len(max(re.findall(f'{key}+', str(item)) or ['']))
