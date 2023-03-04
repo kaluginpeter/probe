fd912060432540e2ac4d2a2d535b0f9726a45385
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-04 22:27:35 +0300

    Added task 1181

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12371,3 +12371,13 @@ def find_routes(routes):
     r = list(d.keys() - d.values())
     while r[-1] in d: r.append(d[r[-1]])
     return ', '.join(r)
+
+# Complete the method that will determine the minimum number of coins needed to make change
+# for a given amount in American currency.
+# Coins used will be half-dollars, quarters, dimes, nickels, and pennies, worth
+# 50¢, 25¢, 10¢, 5¢ and 1¢, respectively. They'll be represented by the symbols
+# H, Q, D, N and P (symbols in Ruby, strings in in other languages)
+# The argument passed in will be an integer representing the value in cents. The return
+# value should be a hash/dictionary/object with the symbols as keys, and the numbers of
+# coins as values. Coins that are not used should not be included in the hash. If the
+# argument passed in is 0, then the method should return an empty hash.
