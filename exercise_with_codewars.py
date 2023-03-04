fc88e15d1af4be5f4a7f9af7b85c42063fb6698f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-04 22:47:37 +0300

    Added task 1184

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12420,3 +12420,8 @@ def isomorph(s: str, t: str) -> bool:
                a=m[0].index(i)
                ans.append(m[1][a])
         return "".join(ans) == t
+
+# Two integer numbers are added using the column addition method. When using this
+# method, some additions of digits produce non-zero carries to the next positions.
+# Your task is to calculate the number of non-zero carries that will occur while adding the given numbers.
+# The numbers are added in base 10.
