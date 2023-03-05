1e5858e659549f8b387eba159dfce5015b4864ca
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-05 22:08:45 +0300

    Complete task 1191

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12512,3 +12512,11 @@ def last_fib_digit(n):
 # num is a numerical palindrome itself, return it.
 # For this kata, single digit numbers will NOT be considered numerical palindromes.
 # Also, you know the drill - be sure to return "Not valid" if the input is not an integer or is less than 0.
+def palindrome(num):
+    if not isinstance(num, int) or num < 0: return 'Not valid'
+    if num < 10: num = 11
+    if str(num) == str(num)[::-1]: return num
+    r, l, d = 0, 0, num
+    while str(num)!=str(num)[::-1]: num, l = num+1, l+1
+    while str(d)!=str(d)[::-1]: d, r = d-1, r+1
+    return d if r<l else num
