51a11f0869a0f2791a7fc7da95d6842c8130d746
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-05 21:05:36 +0300

    Added task 1186

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12445,3 +12445,13 @@ def f(s):
         w += i
         if w * c == s: return (w, c)
     return (s, 1)
+
+# You need to write a password generator that meets the following criteria:
+# 6 - 20 characters long
+# contains at least one lowercase letter
+# contains at least one uppercase letter
+# contains at least one number
+# contains only alphanumeric characters (no special characters)
+# Return the random password as a string.
+# Note: "randomness" is checked by counting the characters used in the generated passwords - all characters
+# should have less than 50% occurance. Based on extensive tests, the normal rate is around 35%.
