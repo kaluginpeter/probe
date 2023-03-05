d5bd7fb2125c66124ba9fdd099442338c75e44ec
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-05 21:46:05 +0300

    Complete task 1190

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12499,3 +12499,5 @@ def arrange(s):
 # the last digit of the nth element in the Fibonacci sequence (starting with 1,1, to be extra clear,
 # not with 0,1 or other numbers).
 # You will just get much bigger numbers, so good luck bruteforcing your way through it ;)
+def last_fib_digit(n):
+    return int('011235831459437077415617853819099875279651673033695493257291'[n % 60])
