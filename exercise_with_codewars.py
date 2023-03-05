5506beee9b1e35c02b4b2e6c72f527fe0301f8b6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-05 22:24:54 +0300

    Added task 1193

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12525,3 +12525,8 @@ def palindrome(num):
 # depending on whether the variable exists in a multidimentional sequence.
 def locate(seq, v):
     return v in seq or any(locate(i, v) for i in seq if isinstance(i, list))
+
+# In this Kata, you will be given a series of times at which an alarm sounds. Your
+# task will be to determine the maximum time interval between alarms. Each alarm
+# starts ringing at the beginning of the corresponding minute and rings for exactly one
+# minute. The times in the array are not in chronological order. Ignore duplicate times, if any.
