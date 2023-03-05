a92c344002a522927b2aa2e5b313dc96ce719817
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-05 21:14:29 +0300

    Added task 1188

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12470,3 +12470,14 @@ def password_gen():
 def rankings(arr):
     l = sorted(arr, reverse=True)
     return [l.index(i)+1 for i in arr]
+
+# A palindrome is a word, phrase, number, or other sequence of
+# characters which reads the same backward as forward. Examples of numerical palindromes are:
+# 2332
+# 110011
+# 54322345
+# For this kata, single digit numbers will not be considered numerical palindromes.
+# For a given number num, write a function to test if the number contains a
+# numerical palindrome or not and return a boolean (true if it does and false if does not).
+# Return "Not valid" if the input is not an integer or is less than 0.
+# Note: Palindromes should be found without permutating num.
