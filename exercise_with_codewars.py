27fb265b16aec8a14f87441f486f12dfc7321278
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-07 17:21:23 +0300

    Added task 1208

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12665,3 +12665,9 @@ def solve(s):
 # Note: big inputs will be tested.
 def oddity(n):
     return 'odd' if n **.5 == int(n **.5) else 'even'
+
+# Given two array of integers(arr1,arr2). Your task is going to find a
+# pair of numbers(an element in arr1, and another element in arr2), their
+# difference is as big as possible(absolute value); Again, you should to find a
+# pair of numbers, their difference is as small as possible. Return the maximum and
+# minimum difference values by an array: [  max difference,  min difference  ]
