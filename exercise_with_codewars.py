e6f653d744619c3ae78040be8786d135eb49e760
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-07 17:21:47 +0300

    Complete task 1208

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12671,3 +12671,6 @@ def oddity(n):
 # difference is as big as possible(absolute value); Again, you should to find a
 # pair of numbers, their difference is as small as possible. Return the maximum and
 # minimum difference values by an array: [  max difference,  min difference  ]
+def max_and_min(arr1,arr2):
+    l = [abs(i-j) for i in arr1 for j in arr2]
+    return [max(l), min(l)]
