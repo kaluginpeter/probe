b251db770e927671ec51fbf66b0f6640b01d6a11
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-08 12:59:00 +0300

    Added task 1216

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12736,3 +12736,14 @@ def group_in_10s(*args):
         if l[i] is None: l[i] = [j]
         else: l[i].append(j)
     return l
+
+# You're continuing to enjoy your new piano, as described in Piano Kata, Part 1.
+# You're also continuing the exercise where you start on the very first (leftmost, lowest in pitch)
+# key on the 88-key keyboard, which (as shown below) is the note A, with the little finger
+# on your left hand, then the second key, which is the black key A# ("A sharp"), with your
+# left ring finger, then the third key, B, with your left middle finger, then the fourth key, C,
+# with your left index finger, and then the fifth key, C#, with your left thumb. Then
+# you play the sixth key, D, with your right thumb, and continue on playing the seventh, eighth,
+# ninth, and tenth keys with the other four fingers of your right hand. Then for the eleventh
+# key you go back to your left little finger, and so on. Once you get to the rightmost/highest,
+# 88th, key, C, you start all over again with your left little finger on the first key.
