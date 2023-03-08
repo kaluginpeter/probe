88cb84332207124a4b2e6e45e13eb58ecd969114
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-08 13:36:01 +0300

    Complete task 1223

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12801,3 +12801,10 @@ def find_the_key(message, code):
 # in the learning process is to have some fun with her students. At school, she wants
 # to make an arrangement of her class to play a certain game with her pupils.
 # For that, she needs to create the arrangement with the minimum amount of groups that have consecutive sizes.
+def shortest_arrang(n):
+    r = n // 2 + 2
+    l = [i for i in range(r, 0, -1)]
+    for i in range(r):
+        for j in range(r + 1):
+            if sum(l[i:j]) == n: return l[i:j]
+    return [-1]
