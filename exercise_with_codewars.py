8bd55438a0adbaf94323c7d542f8aa6ff9132641
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-08 13:18:42 +0300

    Added task 1219

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12761,3 +12761,12 @@ def solve(n, i=0):
 # b) only capitalise the first letter of each sentence. Make sure the rest of the sentence is in lower case.
 def proofread(s):
     return '. '.join(_.lower().replace('ie', 'ei').capitalize() for _ in s.split('. '))
+
+# A rectangle can be split up into a grid of 1x1 squares, the amount of
+# which being equal to the product of the two dimensions of the rectangle. Depending on the
+# size of the rectangle, that grid of 1x1 squares can also be split up into
+# larger squares, for example a 3x2 rectangle has a total of 8 squares,
+# as there are 6 distinct 1x1 squares, and two possible 2x2 squares. A 4x3 rectangle contains 20 squares.
+# Your task is to write a function `findSquares` that returns the total number of squares
+# for any given rectangle, the dimensions of which being given as two integers with
+# the first always being equal to or greater than the second.
