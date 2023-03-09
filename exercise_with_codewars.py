f2fdefe6246a1edddd7e7fc15af58ce4f9a1d632
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-09 22:36:37 +0300

    Added task 1235

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12898,3 +12898,10 @@ def keyword_cipher(s, keyword, key=""):
     for i in keyword + w:
         if i not in key: key += i
     return s.lower().translate(str.maketrans(w, key))
+
+# In his publication Liber Abaci Leonardo Bonacci, aka Fibonacci, posed a problem involving a population
+# of idealized rabbits. These rabbits bred at a fixed rate, matured over the course of
+# one month, had unlimited resources, and were immortal.
+# Create a function that determines the number of pairs of mature rabbits after n
+# months, beginning with one immature pair of these idealized rabbits that produce b pairs of offspring at
+# the end of each month.
