92834d7feec132713d4d8246c0ac127076dd47c1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-10 21:41:26 +0300

    Added task 1240

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12950,3 +12950,8 @@ from itertools import cycle, chain
 def custom_christmas_tree(chars, n):
     c, l = cycle(chars), 2*n-1
     return '\n'.join(chain((' '.join(next(c) for i in range(j)).center(l).rstrip() for j in range(1,n+1)), ('|'.center(l).rstrip() for k in range(n//3 or 1))))
+
+# No Story
+# No Description
+# Only by Thinking and Testing
+# Look at the result of testcase, guess the code!
