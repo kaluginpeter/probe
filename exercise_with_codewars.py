8356c6a9c9884bbf4cfff4a67987c8e088beabaf
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-10 21:55:29 +0300

    Added task 1245

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13004,3 +13004,12 @@ def pattern(rows, col, s):
         res += '|\n'
     res += seperator
     return res
+
+# Traditionally in FizzBuzz, multiples of 3 are replaced by "Fizz" and multiples of
+# 5 are replaced by "Buzz". But we could also play FizzBuzz with any other integer pair
+# [n, m] whose multiples are replaced with Fizz and Buzz.
+# For a sequence of numbers, Fizzes, Buzzes and FizzBuzzes, find the numbers
+# whose multiples are being replaced by Fizz and Buzz. Return them as an array [n, m]
+# The Fizz and Buzz numbers will always be integers between 1 and 50, and
+# the sequence will have a maximum length of 100. The Fizz and Buzz numbers
+# might be equal, and might be equal to 1.
