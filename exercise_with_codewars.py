6b3136a2cba0f368e86e0cab173702acce5373a0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-10 21:43:40 +0300

    Added task 1241

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -12957,3 +12957,13 @@ def custom_christmas_tree(chars, n):
 # Look at the result of testcase, guess the code!
 def test_it(a, b):
     return sum(map(int, str(a))) * sum(map(int, str(b)))
+
+# Cara is applying for several different jobs. The online
+# application forms ask her to respond within a specific character count. Cara
+# needs to check that her answers fit into the character limit.
+# Annoyingly, some application forms count spaces as a character, and some don't.
+# Your challenge:
+# Write Cara a function charCheck() with the arguments:
+# "text": a string containing Cara's answer for the question
+# "max": a number equal to the maximum number of characters allowed in the answer
+# "spaces": a boolean which is True if spaces are included in the character count and False if they are not
