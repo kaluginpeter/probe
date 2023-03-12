7808750f760c69f0b29992dab9021185bb350444
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-12 19:33:50 +0300

    Added task 1262

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13209,3 +13209,9 @@ def sum_nested_numbers(a, c=0):
 from collections import Counter
 def common(a,b,c):
     return sum((Counter(a) & Counter(b) & Counter(c)).elements())
+
+# You're playing to scrabble. But counting points is hard.
+# You decide to create a little script to calculate the best possible value.
+# The function takes two arguments :
+# `points` : an array of integer representing for each letters from A to Z the points that it pays
+# `words` : an array of strings, uppercase
