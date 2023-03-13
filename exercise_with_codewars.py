2e9a4bfb7bc61741044ba5de33e750ada243d679
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-13 22:34:06 +0300

    Complete task 1271

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13341,3 +13341,25 @@ def reverse(s):
 
 # Following on from Part 1, part 2 looks at some more complicated array contents.
 # So let's try filling an array with...
+def squares(n):
+    return [i**2 for i in range(1, n+1)]
+def num_range(n, start, step):
+    l = []
+    for i in range(n):
+        l.append(start)
+        start += step
+    return l
+def rand_range(n, mn, mx):
+    from random import randint
+    l = []
+    for i in range(n):
+        l.append(randint(mn, mx))
+    return l
+def primes(n):
+    from gmpy2 import is_prime
+    l = []
+    c = 1
+    while len(l) < n:
+        if is_prime(c): l.append(c)
+        c += 1
+    return l
