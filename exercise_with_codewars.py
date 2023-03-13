8ee097fa085f306761b9a3aab65df7ff8a2b8884
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-13 21:52:43 +0300

    Complete task 1267

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13293,3 +13293,18 @@ def insert_at_indexes(phrase, word, indexes):
 # n!! = 2 * 4 * 6 * ... * n if n is even.
 # Hence 8!! = 2 * 4 * 6 * 8 = 384, there is no zero at the end. 30!! has 3 zeros at the end.
 # For a positive integer n, please count how many zeros are there at the end of n!!.
+import math
+def count_zeros_n_double_fact(n):
+    c = 0
+    if n % 2 == 0:
+        n = math.prod([i for i in range(2, n+1, 2)])
+        while str(n).endswith('0'):
+            c += 1
+            n = str(n)[:-1]
+        return c
+    if n % 2 != 0:
+        n = math.prod([i for i in range(1, n+1, 2)])
+        while str(n).endswith('0'):
+            c += 1
+            n = str(n)[:-1]
+        return c
