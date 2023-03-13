fab11c75e11ddc0f9bcc5680ad9251643ca825b9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-13 21:52:21 +0300

    Added task 1267

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13287,3 +13287,9 @@ def insert_at_indexes(phrase, word, indexes):
     for i in indexes[::-1]:
         phrase = phrase[:i] + word + phrase[i:]
     return phrase
+
+# Define n!! as
+# n!! = 1 * 3 * 5 * ... * n if n is odd,
+# n!! = 2 * 4 * 6 * ... * n if n is even.
+# Hence 8!! = 2 * 4 * 6 * 8 = 384, there is no zero at the end. 30!! has 3 zeros at the end.
+# For a positive integer n, please count how many zeros are there at the end of n!!.
