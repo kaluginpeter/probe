907c1f369723ac18d8d19393ed937774898ad312
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-13 22:06:48 +0300

    Added task 1269

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13317,3 +13317,13 @@ def count_zeros_n_double_fact(n):
 def triangle_area(triangle):
     a, b, c = triangle.a, triangle.b, triangle.c
     return round(abs(a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) / 2, 6)
+
+# A twin prime is a prime number that is either 2 less or 2 more than another
+# prime number—for example, either member of the twin prime pair (41, 43). In other
+# words, a twin prime is a prime that has a prime gap of two. Sometimes
+# the term twin prime is used for a pair of twin primes; an alternative name for this
+# is prime twin or prime pair. (from wiki https://en.wikipedia.org/wiki/Twin_prime)
+# Your mission, should you choose to accept it, is to write a function that
+# counts the number of sets of twin primes from 1 to n.
+# If n is wrapped by twin primes (n-1 == prime && n+1 == prime) then that should
+# also count even though n+1 is outside the range.
