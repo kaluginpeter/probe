6c5535a756796727b17f267ce94fdee60eb20b50
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-13 21:56:16 +0300

    Complete task 1268

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13314,3 +13314,6 @@ def count_zeros_n_double_fact(n):
 # their corners, each of them is a Point.
 # Write a function calculating area of a Triangle defined this way.
 # Tests round answers to 6 decimal places.
+def triangle_area(triangle):
+    a, b, c = triangle.a, triangle.b, triangle.c
+    return round(abs(a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y)) / 2, 6)
