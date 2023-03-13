1e97365c659a3ebb1c63798f7371b5dd988d3b42
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-13 21:55:54 +0300

    Added task 1268

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13308,3 +13308,9 @@ def count_zeros_n_double_fact(n):
             c += 1
             n = str(n)[:-1]
         return c
+
+# This series of katas will introduce you to basics of doing geometry with computers.
+# Point objects have x, y attributes. Triangle objects have attributes a, b, c describing
+# their corners, each of them is a Point.
+# Write a function calculating area of a Triangle defined this way.
+# Tests round answers to 6 decimal places.
