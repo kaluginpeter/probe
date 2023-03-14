696066ae07e427d758ea399aff3958f565eb535f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-14 20:31:44 +0300

    Added task 1282

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13473,3 +13473,13 @@ def count_of_heads(initial, n, swings):
 from decimal import Decimal, ROUND_HALF_UP
 def round_by_2_decimal_places(n):
     return n.quantize(Decimal('.01'), rounding = ROUND_HALF_UP)
+
+# In this Kata, you will be given an array and your task will be to determine if an array
+# is in ascending or descending order and if it is rotated or not.
+# Consider the array [1,2,3,4,5,7,12]. This array is sorted in Ascending order.
+# If we rotate this array once to the left, we get [12,1,2,3,4,5,7] and twice-rotated
+# we get [7,12,1,2,3,4,5]. These two rotated arrays are in Rotated Ascending order.
+# Similarly, the array [9,6,5,3,1] is in Descending order, but we can
+# rotate it to get an array in Rotated Descending order: [1,9,6,5,3] or [3,1,9,6,5] etc.
+# Arrays will never be unsorted, except for those that are rotated as shown above. Arrays
+# will always have an answer, as shown in the examples below. Arrays will never contain duplicated elements.
