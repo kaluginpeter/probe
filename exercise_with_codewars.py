4dee06aea8cdc69aa7fdc8a765d39084a5086078
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-14 20:28:11 +0300

    Added task 1281

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13465,3 +13465,8 @@ def count_of_heads(initial, n, swings):
     for i in range(1, swings+1):
         initial = initial - 1 + n * math.factorial(i)
     return initial
+
+# Since there are lots of katas requiring you to round numbers to 2 decimal places, you decided to
+# extract the method to ease out the process.
+# And you can't even get this right!
+# Quick, fix the bug before everyone in CodeWars notices that you can't even round a number correctly!
