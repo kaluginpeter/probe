907a803c61ec93e711ef7719a6d32e11bfada07b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-15 15:23:01 +0300

    Added task 1292

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13567,3 +13567,9 @@ def differentiate(x):
 from gmpy2 import is_prime
 def summationOfPrimes(primes):
     return sum(i for i in range(2, primes+1) if is_prime(i))
+
+# X and Y are playing a game. A list will be provided which contains n pairs of strings
+# and integers. They have to add the integeri to the ASCII values of the stringi characters. Then
+# they have to check if any of the new added numbers is prime or not. If for any character of
+# the word the added number is prime then the word will be considered as prime word.
+# Can you help X and Y to find the prime words?
