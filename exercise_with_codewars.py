05b893b5fed13e33dd01803accdde3964cf5dc6e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-15 15:27:58 +0300

    Added task 1293

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13581,3 +13581,10 @@ def prime_word(array):
         c = [ord(i) + array[2][1] for i in array[2][0]]
     f = [1 if any(is_prime(i) for i in a) else 0, 1 if any(is_prime(i) for i in b) else 0]
     return f if len(array) ==2 else f + [1 if any(is_prime(i) for i in c) else 0]
+
+# Playing ping-pong can be really fun! Unfortunatelly after a long and exciting play you can forget
+# who's service turn it is. Let's do something about that!
+# Write a function that takes the current score as a string separated by : sign as an only
+# parameter and returns "first" or "second" depending on whose service turn it is.
+# We're playing old-school, so the rule is that players take turn after every 5 services. That
+# is until the score is 20:20 - from that moment each player serves 2 times in his turn.
