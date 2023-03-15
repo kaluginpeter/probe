30256d25a96577ccdc73b0f0ac0f6489fc6a5626
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-15 15:15:44 +0300

    Complete task 1291

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13564,3 +13564,6 @@ def differentiate(x):
 
 # The sum of the primes below or equal to 10 is 2 + 3 + 5 + 7 = 17. Find the sum
 # of all the primes below or equal to the number passed in.
+from gmpy2 import is_prime
+def summationOfPrimes(primes):
+    return sum(i for i in range(2, primes+1) if is_prime(i))
