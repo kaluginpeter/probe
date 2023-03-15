24188d467f63631b29326895c57b7b9e1397109f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-15 15:28:17 +0300

    Complete task 1293

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13588,3 +13588,6 @@ def prime_word(array):
 # parameter and returns "first" or "second" depending on whose service turn it is.
 # We're playing old-school, so the rule is that players take turn after every 5 services. That
 # is until the score is 20:20 - from that moment each player serves 2 times in his turn.
+def service(score):
+    c = sum(int(i) for i in score.split(":"))
+    return "first" if ((c%10 < 5) if c < 40 else (c%4 < 2)) else "second"
