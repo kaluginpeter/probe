fc9b750a0c714c21c961bee7eed442cf751d381b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-15 14:57:19 +0300

    Complete task 1288

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13539,3 +13539,8 @@ def sum_of_sums(x):
 # the string can have leading/trailing spaces - you should not count them
 # the strings contain only unique characters from a to z
 # the order of characters in the returned array doesn't matter
+import re
+def loneliest(strng):
+    l = [re.match(r'\s*\w\s*', strng.strip()[i:]) for i in range(len(strng))]
+    le = max(len(i.group(0)) for i in l if i)
+    return [i.group(0).strip() for i in l if i and len(i.group(0)) == le]
