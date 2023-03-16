1e7819af63fba4295f27a9968c27ba58a2abb119
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-16 18:17:27 +0300

    Added task 1296

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13606,3 +13606,10 @@ def get_function(sequence):
     for i in range(1, 5):
         if sequence[i] - sequence[i-1] != s: return "Non-linear sequence"
     return lambda a : s * a + sequence[0]
+
+# In this Kata, you will implement a function count that takes an integer and returns the number of
+# digits in factorial(n).
+# For example, count(5) = 3, because 5! = 120, and 120 has 3 digits.
+# More examples in the test cases.
+# Brute force is not possible. A little research will go a long way, as this is a well known series.
+# Good luck!
