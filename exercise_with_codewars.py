276a3e59426f6071a5f848a432a9db27a5e6b277
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-16 18:17:57 +0300

    Complete task 1296

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13613,3 +13613,9 @@ def get_function(sequence):
 # More examples in the test cases.
 # Brute force is not possible. A little research will go a long way, as this is a well known series.
 # Good luck!
+import math
+def count(n):
+    if (n < 0): return 0
+    if (n <= 1): return 1
+    x = ((n * math.log10(n / math.e) + math.log10(2 * math.pi * n) /2.0))
+    return math.floor(x) + 1
