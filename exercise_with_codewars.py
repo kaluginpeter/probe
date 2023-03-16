662b11a103e223accbe39f6a10724c6b90664a9c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-16 22:43:19 +0300

    Complete task 1301

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13680,3 +13680,13 @@ def replace_zero(arr):
 # Similarly, if we start with prime number 7, the sequence is: 7->49->97->130->10->1.
 # Given a range, how many primes within that range will eventually end up being 1?
 # The upperbound for the range is 50,000. A range of (2,25) means that: 2 <= n < 25.
+from gmpy2 import is_prime
+def check(n):
+    l = []
+    while n not in l:
+        l += [n]
+        n = sum(int(i)**2 for i in str(n))
+        if n==1: return True
+    return False
+def solve(a,b):
+    return len([i for i in range(a,b) if is_prime(i) and check(i)])
