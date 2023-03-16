c17e8fd3864286c2d5651642ba3b7873ee6fe039
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-16 22:49:16 +0300

    Complete task 1303

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13708,3 +13708,6 @@ def fizzbuzz_plusplus(nums, words):
 # For this kata, you are given three points (x1,y1,z1), (x2,y2,z2), and (x3,y3,z3) that lie on
 # a straight line in 3-dimensional space. You have to figure out which point lies in between the other two.
 # Your function should return 1, 2, or 3 to indicate which point is the in-between one.
+def middle_point(x1, y1, z1, x2, y2, z2, x3, y3, z3):
+    d = {(x1, y1, z1): 1, (x2, y2, z2): 2, (x3, y3, z3): 3}
+    return d[sorted(d)[1]]
