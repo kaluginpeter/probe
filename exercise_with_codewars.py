fb3179337ebfb709aabcbed22b02deeadafc55ed
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-16 22:48:54 +0300

    Added task 1303

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13704,3 +13704,7 @@ from functools import reduce
 
 def fizzbuzz_plusplus(nums, words):
     return ["".join(b for a, b in zip(nums, words) if not i % a) or i for i in range(1, reduce(lambda a, b: a * b, nums, 1) + 1)]
+
+# For this kata, you are given three points (x1,y1,z1), (x2,y2,z2), and (x3,y3,z3) that lie on
+# a straight line in 3-dimensional space. You have to figure out which point lies in between the other two.
+# Your function should return 1, 2, or 3 to indicate which point is the in-between one.
