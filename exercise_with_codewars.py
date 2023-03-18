7bc8ce5146c1ad7a3166682caa70347f4f081ced
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-18 22:21:46 +0300

    Complete task 1325

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13980,3 +13980,6 @@ def check_valid_tr_number(n):
 
 # In this kata you're given an n x n array and you're expected to traverse the
 # elements diagonally from the bottom right to the top left.
+def diagonal(arr):
+    l = sorted(((i, j) for j in range(len(arr)) for i in range(len(arr))), key=sum)[::-1]
+    return [arr[i][j] for i, j in l]
