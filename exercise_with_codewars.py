f989c56a22f6f450f8a07ffcfed4db6724dcfaf0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-18 22:15:56 +0300

    Added task 1324

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -13966,3 +13966,10 @@ def jump_to(x, y):
         else: y-=1
         c+=1
     return c
+
+# Every Turkish citizen has an identity number whose validity can be checked by these set of rules:
+# It is an 11 digit number
+# First digit can't be zero
+# Take the sum of 1st, 3rd, 5th, 7th and 9th digit and multiply it by 7. Then subtract the sum of
+# 2nd, 4th, 6th and 8th digits from this value. Modulus 10 of the result should be equal to 10th digit.
+# Sum of first ten digits' modulus 10 should be equal to eleventh digit.
