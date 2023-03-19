7f87efff1f3e7722136c662df32c5ffd5a666c39
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-19 18:55:31 +0300

    Added task 1329

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -14005,3 +14005,14 @@ def mult_triangle(n):
 # Write that given an array of numbers >= 0, will arrange them such that they form the biggest number.
 def biggest(nums):
     return str(int(''.join(sorted(map(str,nums),key=lambda x:3*x)[::-1])))
+
+# The prime number sequence starts with: 2,3,5,7,11,13,17,19.... Notice that 2 is in position one.
+# occupies position two, which is a prime-numbered position. Similarly, 5, 11 and 17 also
+# occupy prime-numbered positions. We shall call primes such as 3,5,11,17 dominant primes
+# because they occupy prime-numbered positions in the prime number sequence. Let's call this listA.
+# As you can see from listA, for the prime range range(0,10), there are
+# only two dominant primes (3 and 5) and the sum of these primes is: 3 + 5 = 8.
+# Similarly, as shown in listA, in the range (6,20), the dominant
+# primes in this range are 11 and 17, with a sum of 28.
+# Given a range (a,b), what is the sum of dominant primes within that range?
+# Note that a <= range <= b and b will not exceed 500000.
