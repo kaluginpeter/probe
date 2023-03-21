f28b8afe4095a1f41c48aac48154262c58f745a4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-21 14:51:09 +0300

    Added task 1355

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -14336,3 +14336,7 @@ def bad_apples(a):
                     l.append(su)
             else : l.append(j)
     return l
+
+# Write a method named getExponent(n,p) that returns the largest integer
+# exponent x such that px evenly divides n. if p<=1 the method should return
+# null/None (throw an ArgumentOutOfRange exception in C#).
