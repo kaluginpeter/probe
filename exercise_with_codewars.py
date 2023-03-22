d0b26f227e465951e618b362d3e1e32667ca3dc7
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-22 17:35:13 +0300

    Complete task 1359

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -14377,3 +14377,5 @@ def uniq_count(string: str) -> int:
 # The integer 64 is the first integer that has all of its digits even and furthermore, is a perfect square.
 # The second one is 400 and the third one 484.
 # Give the numbers of this sequence that are in the range [a,b] (both values inclusive)
+def even_digit_squares(a, b):
+    return [i**2 for i in range(int(a**0.5)+bool(a**0.5 % 1), int(b**0.5)+1) if not any(int(d) % 2 for d in str(i**2))]
