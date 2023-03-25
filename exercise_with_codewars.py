77d5c094ae49246fc8cda97cac85bb5673e22815
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-25 16:13:28 +0300

    Complete task 1387

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -14669,3 +14669,15 @@ def answer(question, information):
 # to Yes and No. The sequence values are the answer to "Do you see your number?" for
 # each one of the above tuples. You are given the sequence and must return
 # the number which originated that sequence of answers.
+sequence = ((1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31),
+            (2, 3, 6, 7, 10, 11, 14, 15, 18, 19, 22, 23, 26, 27, 30, 31),
+            (4, 5, 6, 7, 12, 13, 14, 15, 20, 21, 22, 23, 28, 29, 30, 31),
+            (8, 9, 10, 11, 12, 13, 14, 15, 24, 25, 26, 27, 28, 29, 30, 31),
+            (16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31))
+
+
+def guess_number(answers):
+    return sum(sequence[i][0] for i in range(len(answers)) if answers[i] == 1)
+
+def answers_sequence(n):
+    return [1 if n in i else 0 for i in sequence]
