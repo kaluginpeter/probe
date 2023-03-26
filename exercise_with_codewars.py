e7be591f79e454bb195220b73db7ab0e5c1448b6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-26 20:39:58 +0300

    Complete task 1397

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -14787,3 +14787,10 @@ def prime_primes(N):
 # 263 is a colorful number because [2, 6, 3, 2*6, 6*3, 2*6*3] are all
 # different; whereas 236 is not colorful, because [2, 3, 6, 2*3, 3*6, 2*3*6] have 6 twice.
 # So take all consecutive subsets of digits, take their product and ensure all the products are different.
+def colorful(number):
+    l = []
+    for x in str(number): l.append(int(x))
+    for y in range(len(l) - 1):
+        temp = l[y] * l[y + 1]
+        l.append(temp)
+    return len(set(l)) == len(l)
