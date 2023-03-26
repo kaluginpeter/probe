444bd731003be9156596394af9ebb88d6475eb59
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-26 20:55:58 +0300

    Complete task 1401

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -14823,3 +14823,6 @@ def weigh_the_list(a):
 # Remove odd number continuous exclamation marks and question marks(from the left to the right), until
 # no continuous exclamation marks and question marks exist. Please note: One exclamation mark or
 # question mark is not a continuous exclamation marks or question marks. The string only contains ! and ?.
+def remove(s, last = ''):
+    s = ''.join(i for i in s.replace('?!', '? !').replace('!?', '! ?').split() if len(i) == 1 or len(i) % 2 == 0)
+    return s if last == s else remove(s, s)
