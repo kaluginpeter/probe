32461055d7f8302f644b5a6ca04f3c18cff1963f
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-27 22:16:31 +0300

    Complete task 1406

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -14874,3 +14874,15 @@ def almost_increasing_sequence(sequence):
 # that combines the elements of an array by sorting the elements ascending by their
 # absolute value and outputs negative and non-negative integers alternatingly (starting with the
 # negative value, if any).
+def alternate_sort(l):
+    l1 = sorted(i for i in l if i >= 0)[::-1]
+    l2 = sorted(i for i in l if i < 0)
+    f = []
+    while l1 or l2:
+        if l2:
+            f.append(l2.pop())
+            if l1: f.append(l1.pop())
+        elif l1:
+            f.append(l1.pop())
+            if l2: f.append(l2.pop())
+    return f
