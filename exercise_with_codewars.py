d6560a9b46a01422a3c4073f084c7aa4ad90d1d2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-27 22:28:46 +0300

    Complete task 1408

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -14902,3 +14902,6 @@ def convert(s):
 # Given an array (ints) of n integers, find three integers in
 # arr such that the sum is closest to a given number (num), target.
 # Return the sum of the three integers. You may assume that each input would have exactly one solution.
+from itertools import combinations
+def closest_sum(ints, num):
+    return sum(min(combinations(ints, 3), key=lambda x: abs(num - sum(x))))
