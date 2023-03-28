bb832b79599d1cbef313109903c327b43d3ab463
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-28 21:30:21 +0300

    Added task 1417

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15007,3 +15007,12 @@ def find_number(compare):
         elif compare(count) == 1: b = count
         else: break
     return count
+
+# A step(x) operation works like this: it changes a number x into x - s(x), where s(x)
+# is the sum of x's digits. You like applying functions to numbers, so
+# given the number n, you decide to build a decreasing sequence of numbers: n, step(n),
+# step(step(n)), etc., with 0 as the last element.
+# Building a single sequence isn't enough for you, so you replace all elements
+# of the sequence with the sums of their digits (s(x)). Now you're
+# curious as to which number appears in the new sequence most often. If there are several
+# answers, return the maximal one.
