f45f6acf28bd1b8d278de7094cfe7d766323c744
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-28 21:37:40 +0300

    Complete task 1419

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15038,3 +15038,7 @@ def quidditch_scoreboard(teams, actions):
 # A number n is called prime happy if there is at least one prime less
 # than n and the sum of all primes less than n is evenly divisible by n. Write
 # isPrimeHappy(n) which returns true if n is prime happy else false.
+from gmpy2 import is_prime
+def is_prime_happy(n):
+    if n < 5: return False
+    return sum([i for i in range(2, n) if is_prime(i)]) % n == 0
