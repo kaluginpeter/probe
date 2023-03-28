f48c3d412125524d35e2e612763c5c05baa07993
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-28 18:26:50 +0300

    Added task 1416

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -14990,3 +14990,12 @@ def line_up(c):
         if i=='R': x-=1
         co += (not x%2)
     return co
+
+# You should find a searched number by approximation.
+# The searched number will always be between 0 and 100.
+# You have to write a method, that will get only a function to compare your guess number with the searched number.
+# Your method have to find the number with a precision of 5 fractional digits.
+# The tolerance for the value: The difference from the searched number must be smaller than 0.00002.
+# The compare-function, that your method will get as parameter, takes the guessed number as parameter
+# and returns 0 for the correct number, -1 if your number is smaller than
+# the searched number and 1 if your guessed number is greater than the searched number.
