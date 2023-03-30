ec1b648c0ba62548b7afc5f0ca26b59f72a39d9b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-30 23:02:08 +0300

    Added task 1444

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15276,3 +15276,10 @@ def longest_bouncy_list(arr):
         else: l = l[-1:] + [v] if v != l[-1] else [v]
         if len(l)>len(s): s = l
     return s
+
+# Consider the array [3,6,9,12]. If we generate all the combinations with repetition that
+# sum to 12, we get 5 combinations: [12], [6,6], [3,9], [3,3,6], [3,3,3,3]. The length of
+# the sub-arrays (such as [3,3,3,3]
+# should be less than or equal to the length of the initial array ([3,6,9,12]).
+# Given an array of positive integers and a number n, count all combinations with repetition of
+# integers that sum to n. For example:
