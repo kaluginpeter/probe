ca6393bcc07807927de69f993393644947ba0d80
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-30 22:15:10 +0300

    Complete task 1436

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15203,3 +15203,6 @@ def longest_sequence(n):
 # Consider having a cow that gives a child every year from her fourth
 # year of life on and all her subsequent children do the same.
 # After n years how many cows will you have?
+def count_cows(n):
+    if not isinstance(n, int): return None
+    return count_cows(n-1) + count_cows(n-3) if n > 2 else 1
