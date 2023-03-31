e0727256addd8b7c96c9a81553d5702083812d52
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-03-31 22:52:02 +0300

    Complete task 1450

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15364,3 +15364,11 @@ f=lambda s:s[-1]>"Z"and-~f(s[:-1])
 # The behavior of the decorated function must be the same as before. Your
 # decorator must be well-behaved, i.e. the returned function must have the
 # same name and docstring as the original, and must be able to handle the same arguments.''
+from functools import wraps
+def count_calls(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        wrapper.call_count += 1
+        return func(*args, **kwargs)
+    wrapper.call_count = 0
+    return wrapper
