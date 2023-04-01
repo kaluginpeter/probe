81fc4166f95c418f6f035317816afdb1eaf52a53
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-01 22:13:50 +0300

    Complete task 1462

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15480,3 +15480,10 @@ def how_many_apples(n):
 
 # Well, for my first kata, I did a mess. Would you help me, please, to make my code work ?
 # I'm sure I didn't mix the numbers, but all the rest...
+from math import pi
+def whatpimeans(alpha = 'abcdefghijklmnopqrstuvwxyz'):
+    d = dict(zip([85,24,32,64,11,52,91,79,78,99,62,27,74,35,14,16,66,81,19,39,13,33,45,49,95,10],alpha.upper()))
+    crypt = str(pi).replace(".", "")[::-1]
+    code = [int(crypt[i:i+2]) for i in range(0, len(crypt), 2)]
+    s = ''.join(d[i] for i in code)
+    return s
