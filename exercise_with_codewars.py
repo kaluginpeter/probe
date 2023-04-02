4df05e89560d4c2cc5bd2ca2d5f0c27d85057e61
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-02 22:06:15 +0300

    Added task 1467

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15513,3 +15513,8 @@ def last_digit(n):
 # Given the constraints, there will always be exactly one such number.
 def oddest(a):
     return max(a, key=lambda x: f'{x+2**32:b}'[::-1])
+
+# Write the processArray function, which takes an array and a callback function as parameters.
+# The callback function can be, for example, a mathematical
+# function that will be applied on each element of this array. Optionally, also
+# write tests similar to the examples below.
