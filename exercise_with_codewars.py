76f3c6573fcb52e4b4feb5feaa04bad24150209e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-03 22:28:09 +0300

    Added task 1479

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15597,3 +15597,13 @@ def late_ride(n):
 import math
 def process_data(data):
     return math.prod(i-j for i,j in data)
+
+# Your company, Congo Pizza, is the second-largest online frozen pizza retailer.
+# You own a number of international warehouses that you use to store your frozen pizzas,
+# and you need to figure out how many crates of pizzas you can store at each location.
+# Congo recently standardized its storage containers: all pizzas fit inside a
+# cubic crate, 16-inches on a side. The crates are super tough so you can stack them as high as you want.
+# Write a function box_capacity() that figures out how many crates you can store in a given
+# warehouse. The function should take three arguments: the length, width, and height
+# of your warehouse (in feet) and should return an integer representing the number
+# of boxes you can store in that space.
