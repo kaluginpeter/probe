a737b6d2b6dca640a5c02f3e3781b3d480e6fa49
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-03 22:57:19 +0300

    Complete task 1485

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15654,3 +15654,15 @@ def insurance(age, size, num_of_days):
 # List class he has created, this is used for type strict arrays (which timmy calls Lists).
 # When timmy calls the Count property of the list it still remains at 0 when adding items.
 # Also it fails when timmy trys to chain the adds e.g.
+class List:
+    def __init__(self, list_type):
+        self.type = list_type
+        self.items = []
+        self.count = 0
+
+    def add(self, item):
+        if type(item) != self.type:
+            return "This item is not of type: {}".format(self.type.__name__)
+        self.items.append(item)
+        self.count += 1
+        return self
