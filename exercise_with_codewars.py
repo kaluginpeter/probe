e361df4e88dc73de13448678436ef52817220620
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-04 22:55:46 +0300

    Added task 1494

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15741,3 +15741,8 @@ def guess_hat_color(a,b,c,d):
 # If the string's length is odd drop the central element.
 def char_concat(word):
     return ''.join((word[x] + word[-1-x] + str(x+1)) for x in range(len(word)//2))
+
+# The sum of x consecutive integers is y. What is the consecutive
+# integer at position n? Given x, y, and n, solve for the integer. Assume the starting position is 0.
+# For example, if the sum of 4 consecutive integers is 14, what is the consecutive integer at position 3?
+# We find that the consecutive integers are [2, 3, 4, 5], so the integer at position 3 is 5.
