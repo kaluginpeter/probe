16bf000201ecadb202d7e005acf763f179204c1b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-04 22:56:02 +0300

    Complete task 1494

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15746,3 +15746,5 @@ def char_concat(word):
 # integer at position n? Given x, y, and n, solve for the integer. Assume the starting position is 0.
 # For example, if the sum of 4 consecutive integers is 14, what is the consecutive integer at position 3?
 # We find that the consecutive integers are [2, 3, 4, 5], so the integer at position 3 is 5.
+def position(x, y, n):
+    return y//x - x//2 + (x % 2 == 0) + n
