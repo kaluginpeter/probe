1f03bc28df5bb9ca8db01156786501c2579fdc3b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-04 22:14:55 +0300

    Added task 1488

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15686,3 +15686,11 @@ def quadratic_formula(a, b, c):
     root1 = (-b + d**.5) / (2 * a)
     root2 = (-b - d**.5) / (2 * a)
     return [root1, root2]
+
+# Find the anonymous function in the given array and use the function to filter the array
+# Input
+# Your input. First Parameter will be an array with an anonymous function somewhere in the lot,
+# The second Parameter will be an array which you will filter using the anonymous function you find.
+# Output
+# Your output. Output a filtered version of the second parameter using the function found in the first parameter.
+# Reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
