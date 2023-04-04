ec9438af23fb98964f5df44a6e50465757384e0a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-04 21:55:50 +0300

    Complete task 1486

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15670,3 +15670,5 @@ class List:
 # Create a function that returns the total of a meal including tip and tax. You should not tip on the tax.
 # You will be given the subtotal, the tax as a percentage and the tip as a percentage. Please round your
 # result to two decimal places.
+def calculate_total(subtotal, tax, tip):
+    return round(subtotal * (1 + tax / 100 + tip /100), 2)
