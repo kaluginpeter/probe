baa9aa616fbff8b09635d1e746ef914b249d4d8b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-05 22:41:55 +0300

    Added task 1505

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15821,3 +15821,10 @@ def split_by_value(k, elements):
 # ones. If there is none, return an empty array
 def ones_counter(input):
     return [ch.count('1') for ch in ''.join(map(str, input)).split('0') if ch]
+
+# As you probably know, Fibonacci sequence are the numbers in the following integer
+# sequence: 1, 1, 2, 3, 5, 8, 13... Write a method that takes the index as
+# an argument and returns last digit from fibonacci number. Example: getLastDigit(15) - 610.
+# Your method must return 0 because the last digit of 610 is 0. Fibonacci sequence
+# grows very fast and value can take very big numbers (bigger than integer type can contain), so,
+# please, be careful with overflow.
