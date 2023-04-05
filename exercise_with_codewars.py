c840db6f82aed855a4aeae24d277cbe1e4a45fca
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-05 22:14:04 +0300

    Complete task 1496

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15759,3 +15759,5 @@ def sum_differences_between_products_and_LCMs(pairs):
 # Implement a function to calculate the distance between two points in n-dimensional space.
 # The two points will be passed to your function as arrays of the same length (tuples in Python).
 # Round your answers to two decimal places.
+def euclidean_distance(point1, point2):
+    return round(sum((j - i)**2 for i, j in zip(point1, point2)) ** 0.5, 2)
