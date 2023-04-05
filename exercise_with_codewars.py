e9011b256eac2f2cd4c61f0011a33da5eaf062e6
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-05 22:16:41 +0300

    Complete task 1497

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15765,3 +15765,10 @@ def euclidean_distance(point1, point2):
 # Build a function that will take the length of each side of a
 # triangle and return if it's either an Equilateral, an Isosceles, a Scalene or an invalid triangle.
 # It has to return a string with the type of triangle.
+def type_of_triangle(a, b, c):
+    if any(not isinstance(i, int) for i in (a, b, c)): return "Not a valid triangle"
+    a, b, c = sorted((a, b, c))
+    if a + b <= c: return "Not a valid triangle"
+    if a == b and b == c: return "Equilateral"
+    if a == b or a == c or b == c: return "Isosceles"
+    return "Scalene"
