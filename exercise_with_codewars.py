974432576fba324cc45c014826a1ec39bbd63e3d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-06 22:02:02 +0300

    Added task 1506

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15835,3 +15835,8 @@ def get_last_digit(index):
     for i in range(index):
         a, b = b, a + b
     return int(str(a)[-1])
+
+# You are given a random string of lower-case letters. Your job is to find out how many
+# ordered and consecutive vowels there are in the given string beginning from 'a'. Keep in mind
+# that the consecutive vowel to 'u' is 'a' and the cycle continues.
+# Return an integer with the length of the consecutive vowels found.
