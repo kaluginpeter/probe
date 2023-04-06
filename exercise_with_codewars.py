778d3eade63ae454405b28b40be39294b1bcbfd0
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-06 22:35:10 +0300

    Complete task 1514

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15910,3 +15910,7 @@ def graceful_tipping(bill):
 # You are given a function that should insert an asterisk (*) between every
 # pair of even digits in the given input, and return it as a string.
 # If the input is a sequence, concat the elements first as a string.
+def asterisc_it(n):
+    if type(n) == list: n = ''.join(str(i) for i in n)
+    if type(n) == int : n = str(n)
+    return ''.join([a + '*' if int(a) % 2 == 0 and int(b) % 2 == 0 else a for a,b in zip(n, n[1:])]) + n[-1]
