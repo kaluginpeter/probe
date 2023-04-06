06117c4a02afa2d9210d14c6c1379271b809637d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-06 22:02:22 +0300

    Complete task 1506

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15840,3 +15840,8 @@ def get_last_digit(index):
 # ordered and consecutive vowels there are in the given string beginning from 'a'. Keep in mind
 # that the consecutive vowel to 'u' is 'a' and the cycle continues.
 # Return an integer with the length of the consecutive vowels found.
+def get_the_vowels(word):
+    c = 0
+    for i in word:
+        if i == "aeiou"[c % 5]: c += 1
+    return c
