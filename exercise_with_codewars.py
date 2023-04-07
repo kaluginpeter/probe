adf2ff3ffeed028e86ebc57c1604c327c04f3ff2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-07 22:23:14 +0300

    Complete task 1516

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15927,3 +15927,5 @@ def procedure(n):
 # You are given an array with several "even" words, one "odd" word, and some numbers mixed in.
 # Determine if any of the numbers in the array is the index of the "odd" word. If so, return
 # true, otherwise false.
+def odd_ball(arr):
+    return any(arr[i] == 'odd' for i in [i for i in arr if type(i) == int and i <= len(arr)-1])
