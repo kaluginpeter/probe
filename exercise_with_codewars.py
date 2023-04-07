76f7fe4b8df08fef20998162548c36f789460a50
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-07 22:30:57 +0300

    Complete task 1517

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15939,3 +15939,10 @@ def odd_ball(arr):
 # a collection of speedlimits [90,100,110,120,....] and the speed
 # of the driver will be the same on all roads and can never be negative and
 # the amount of the fine will be accumulated example 95km/h.
+def speed_limit(speed, signals):
+    c = 0
+    for i in signals:
+        if 10 <= speed - i <= 19: c += 100
+        if 20 <= speed - i <= 29: c += 250
+        if 30 <= speed - i: c += 500
+    return c
