2f5f09f1ec595521287eeb2cd045768a847c7c4b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-07 22:49:51 +0300

    Complete task 1521

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -15974,3 +15974,15 @@ def array_packing(arr):
 
 # Given a non-empty array of non-empty integer arrays, multiply the contents of each nested
 # array and return the smallest total.
+import math
+# Однострочное решение
+def smallest_product(a):
+    return min([math.prod(i) for i in a])
+
+# Более подробное решение
+# def smallest_product(список_списков):
+#     список_сумм = []
+#     for список in список_списков:
+#         список_сумм.append(math.prod(список))
+#     минимальное_значение = min(список_сумм)
+#     return минимальное_значение
