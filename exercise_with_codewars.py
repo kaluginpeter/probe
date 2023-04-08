e5c473751e614b439ae9937a164a319f8546599a
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-08 20:10:12 +0300

    Added task 1527

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16029,3 +16029,11 @@ def generator(a):
 # disregarded, and an average is taken of the remaining times).
 def cube_times(times):
     return round(sum(sorted(times)[1:4])/3, 2), min(times)
+
+# Write a function that takes one or more arrays and returns a new array of
+# unique values in the order of the original provided arrays.
+# In other words, all values present from all arrays should be included in their original order,
+# but with no duplicates in the final array.
+# The unique numbers should be sorted by their original order, but the final
+# array should not be sorted in numerical order.
+# Check the assertion tests for examples.
