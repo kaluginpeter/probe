98c5cf7a497e719c9f1d0bcfb50d961a989a2931
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-08 20:23:41 +0300

    Added task 1531

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16069,3 +16069,12 @@ def skiponacci(n):
         a, b = a + b, a
         l.append('skip' if i % 2 else str(a))
     return ' '.join(l)
+
+# Step through my green glass door.
+# You can take the moon, but not the sun.
+# You can take your slippers, but not your sandals.
+# You can go through yelling, but not shouting.
+# You can't run through fast, but you can run with speed.
+# You can take a sheet, but not your blanket.
+# You can wear your glasses, but not your contacts.
+# Have you figured it out? Good! Then write a program that can figure it out as well.
