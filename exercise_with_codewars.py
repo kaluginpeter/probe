499a45a55b8c6a3aa1ec81d98aef1ebca1283be2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-08 20:26:40 +0300

    Added task 1532

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16080,3 +16080,10 @@ def skiponacci(n):
 # Have you figured it out? Good! Then write a program that can figure it out as well.
 def step_through_with(s):
     return any(i == j for i, j in zip(s, s[1:]))
+
+# In a genetic algorithm, a population is a collection of candidates that may evolve toward a better solution.
+# We determine how close a chromosome is to a ideal solution by
+# calculating its fitness. https://www.codewars.com/kata/567b468357ed7411be00004a/train You are
+# given two parameters, the population containing all individuals and a function fitness that determines
+# how close to the solution a chromosome is.
+# Your task is to return a collection containing an object with the chromosome and the calculated fitness.
