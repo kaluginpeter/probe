e6e9185bbd1bbb0e65b01943b590bdcd8693de4b
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-09 19:22:16 +0300

    Complete task 1541

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16163,3 +16163,9 @@ def sum_and_multiply(sum, multiply):
 
 # Given any number of boolean flags function should return true if and only if one
 # of them is true while others are false. If function is called without arguments it should return false.
+def only_one(*args):
+    c = 0
+    if len(args) == 0: return False
+    for i in args:
+        if i == True: c += 1
+    return c == 1
