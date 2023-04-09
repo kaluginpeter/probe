afc58b1ea3db678afd61f57b5330d4dacf6690e4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-09 19:05:10 +0300

    Added task 1537

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16126,3 +16126,12 @@ def remove_nth_element(lst, n):
     lst_copy = lst.copy()
     del lst_copy[n]
     return lst_copy
+
+# KISS stands for Keep It Simple Stupid. It is a design principle for keeping things simple rather than complex.
+# You are the boss of Joe.
+# Joe is submitting words to you to publish to a blog. He likes to complicate things# .
+# Define a function that determines if Joe's work is simple or complex.
+# Input will be non emtpy strings with no punctuation.
+# It is simple if: the length of each word does not exceed the amount of
+# words in the string (See example test cases)
+# Otherwise it is complex.
