8a7dc76759084ad197de9edaa5e449924e9b7e23
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-09 19:25:05 +0300

    Added task 1542

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16169,3 +16169,16 @@ def only_one(*args):
     for i in args:
         if i == True: c += 1
     return c == 1
+
+# This Kata is intended as a small challenge for my students
+# All Star Code Challenge #24
+# Your friend David is an architect who is working on a triangle-focused design. He needs
+# a quick way for knowing the measurement of a right triangle's side, only knowing two of the sides.
+# He knows about the Pythagorean Theorem, but is too lazy to do the math.
+# Help him by making 2 functions to ease his troubles.
+# Create 2 functions:
+# hypotenuse(), which takes 2 integer arguments, the length of two regular sides of a right
+# triangle, and returns the length of the missing side, the hypotenuse, as a number.
+# leg(), which takes 2 integer arguments, the first being the length of the hypotenuse, and
+# the second being the length of a regular side of a right triangle. This function
+# should return the length of the missing regular side, as a number.
