fa41f7a0ba89098834ea19378312407ccde97304
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-09 19:13:24 +0300

    Added task 1539

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16145,3 +16145,10 @@ def is_kiss(words):
 # Given your and your friend's arms' lifting capabilities find out if you two are equally strong.
 def are_equally_strong(your_left, your_right, friends_left, friends_right):
     return sorted([your_left, your_right]) == sorted([friends_left, friends_right])
+
+# Once Mary heard a famous song, and a line from it stuck in her head. That line was "Will you
+# still love me when I'm no longer young and beautiful?". Mary believes
+# that a person is loved if and only if he/she is both young and beautiful, but this
+# is quite a depressing thought, so she wants to put her belief to the test.
+# Knowing whether a person is young, beautiful and loved, find out if they contradict Mary's belief.
+# A person contradicts Mary's belief if one of the following statements is true:
