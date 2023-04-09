9890a951d2cacaef9f5799076be2b8c5f5e212c1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-09 18:46:58 +0300

    Added task 1536

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16116,3 +16116,9 @@ def solve(arr):
 from gmpy2 import is_prime
 def sexy_prime(x, y):
     return abs(x - y) == 6 and is_prime(x) and is_prime(y)
+
+# In this kata the function returns an array/list like the one passed to
+# it but with its nth element removed (with 0 <= n <= array/list.length - 1). The function
+# is already written for you and the basic tests pass, but random tests fail. Your
+# task is to figure out why and fix it.
+# Good luck!
