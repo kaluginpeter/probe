07239e61d398a626ccd29873e9812729d06c2d98
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-10 22:22:42 +0300

    Complete task 1550

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16263,3 +16263,11 @@ def separate_types(seq):
 # Array following the Example-Array.
 # Assume Example Array catalogs all elements possibly seen in the input Array. However, the input
 # Array does not necessarily have to have all elements seen in the Example.
+def example_sort(arr, example_arr):
+    l = []
+    for i in example_arr:
+        if i in arr:
+            while i in arr:
+                l.append(i)
+                arr.remove(i)
+    return l
