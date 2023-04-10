5024389675f04463e10a2bd5d0602fbbb7b7314d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-10 22:32:36 +0300

    Complete task 1552

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16283,3 +16283,6 @@ def remove(text, what):
 # Take a number and check each digit if it is divisible by the digit
 # on its left checked and return an array of booleans.
 # The booleans should always start with false becase there is no digit before the first one.
+def divisible_by_last(n):
+    l = list(map(int, f"0{n}"))
+    return [i and not j%i for i,j in zip(l, l[1:])]
