0e3603769651977cc66930e38c9eed14c3f03500
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-10 21:59:22 +0300

    Added task 1546

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16217,3 +16217,12 @@ def vowel_start(s):
 from math import gcd
 def relatively_prime(n, l):
     return [i for i in l if gcd(n, i) == 1]
+
+# Thanks to the effects of El Nino this year my holiday snorkelling trip was
+# akin to being in a washing machine... Not fun at all.
+# Given a string made up of '~' and '_' representing waves
+# and calm respectively, your job is to check whether a person would become seasick.
+# Remember, only the process of change from wave to calm will add to
+# the effect (really wave peak to trough but this will do). Find out
+# how many changes in level the string has and if that figure is more than 20%
+# of the array, return "Throw Up", if less, return "No Problem".
