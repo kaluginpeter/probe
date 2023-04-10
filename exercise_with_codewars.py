992d87f4bf9c3c9867e5f1f277cbe888f1c067ec
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-10 22:07:17 +0300

    Added task 1548

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16232,3 +16232,15 @@ def sea_sick(s):
 # Write a function that sums squares of numbers in list that may contain more lists
 def sumsquares(l):
     return l**2 if type(l) != list else sum(sumsquares(i) for i in l)
+
+# The Chinese zodiac is a repeating cycle of 12 years, with each year
+# being represented by an animal and its reputed attributes. The lunar calendar is divided into
+# cycles of 60 years each, and each year has a combination of an animal and
+# an element. There are 12 animals and 5 elements; the animals change each year, and the elements change every
+# 2 years. The current cycle was initiated in the year of 1984 which was the year of the Wood Rat.
+# Since the current calendar is Gregorian, I will only be using years from the epoch
+# 1924. For simplicity I am counting the year as a whole year and
+# not from January/February to the end of the year.
+##Task
+# Given a year, return the element and animal that year represents ("Element Animal"). For example
+# I'm born in 1998 so I'm an "Earth Tiger".
