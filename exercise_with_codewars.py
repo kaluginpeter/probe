d13a74e99bf29b75012f998501fc4929d6852c62
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-11 17:56:19 +0300

    Added task 1563

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16368,3 +16368,8 @@ def find_lowest_int(k1):
         return sorted(str(c))
     while digits(c*k1) != digits(c*k2): c += 1
     return c
+
+# In this kata, you need to write a function that takes a string and a letter as
+# input and then returns the index of the second occurrence of that letter in the string. If
+# there is no such letter in the string, then the function should return -1.
+# If the letter occurs only once in the string, then -1 should also be returned.
