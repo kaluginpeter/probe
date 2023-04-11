c563e938f88eae16a85b99f5a22a58548c7da3f8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-11 17:10:41 +0300

    Added task 1560

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16342,3 +16342,10 @@ def char_to_ascii(s):
 # If there is a number, punctuation or underscore following the letter, it should not be returned.
 def comes_after(st, l):
     return ''.join(j for i, j in zip(st, st[1:]) if i.lower() == l.lower() and j.isalpha())
+
+# The best way to have a productive day is to plan out
+# your work schedule. Given the following three inputs, please create an array of time alloted to
+# work, broken up with time alloted with breaks:
+# Input 1: Hours - Number of hours available to you to get your work done!
+# Input 2: Tasks - How many tasks you have to do througout the day
+# Input 3: Duration (minutes)- How long each of your tasks will take to complete
