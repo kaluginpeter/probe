7707c7716cb4730624c4466dc934c68d16e6db47
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-11 16:52:23 +0300

    Complete task 1558

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16330,3 +16330,9 @@ def factors(x):
 # string. Returns nil if the string is empty. The key is the character, and
 # the value is the ascii value of the character. Repeated characters are to be ignored
 # and non-alphebetic characters as well.
+def char_to_ascii(s):
+    if isinstance(s, str) and s != '':
+        d = {}
+        for i in s:
+            if i not in d and i in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ': d[i] = ord(i)
+        return d
