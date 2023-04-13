b23d4831bd178fa59193662e148874bc4124d321
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-13 22:25:29 +0300

    Complete task 1567

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16405,3 +16405,5 @@ def largest_sum(s):
 # Correct this code so that it takes one argument, x, and returns "x is more than zero"
 # if x is positive (and nonzero), and otherwise, returns "x is equal to
 # or less than zero." In both cases, replace x with the actual value of x.
+def corrections(x):
+    return f"{x} is more than zero." if x > 0 else f"{x} is equal to or less than zero."
