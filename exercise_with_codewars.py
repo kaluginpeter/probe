88c6136d4455e607050f1eda4457ecf8bdfa97be
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-15 21:23:50 +0300

    Complete task 1569

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16421,3 +16421,9 @@ def get_new_notes(salary,bills):
 # This kata is about static method that should return different values.
 # On the first call it must be 1, on the second and others - it must be a double from previous value.
 # Look tests for more examples, good luck :)
+class Class:
+    count = 1
+    def get_number():
+        res = Class.count
+        Class.count *= 2
+        return res
