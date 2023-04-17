3dc2078ca5e8e19538c100b33b814de42f45a5f4
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-17 22:17:11 +0300

    Complete task 1573

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16460,3 +16460,8 @@ class Solution:
 # If your score is higher than the user's already, return "Winning!" and if they are
 # equal, return "Only need one!"
 # Note: You are looking to complete as few kata as possible to get to your target.
+def leader_b(u, us, ys):
+    if ys > us: return 'Winning!'
+    if ys == us: return 'Only need one!'
+    s = f"To beat {u}'s score, I must complete {(us-ys)//3} Beta kata and {(us-ys)%3//1} 8kyu kata."
+    return s + ' Dammit!' if ((us-ys)//3) + ((us-ys)%3//1) > 1000 else s
