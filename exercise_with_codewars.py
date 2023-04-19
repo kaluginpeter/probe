30b8269046619877471f49aa97ef221e9fd0f4ec
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-19 21:34:33 +0300

    Complete task 1577

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16492,3 +16492,5 @@ class Solution:
 # element n, there exists another element that is the negative of n. Write a function named ìsZeroBalanced
 # that returns true if its argument is zero-balanced array, else return false. Note that
 # an empty array will not sum to zero.
+def is_zero_balanced(arr):
+    return sum(arr) == 0 and all(abs(i) in arr if i < 0 else -i in arr for i in arr) if len(arr) > 0 else False
