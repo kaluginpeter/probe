631e815913944674397efed62c95abcef1b547ad
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-19 21:34:08 +0300

    Added task 1577

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16487,3 +16487,8 @@ class Solution:
         for i in nums:
             for j in str(i): l.append(int(j))
         return l
+
+# An array is called zero-balanced if its elements sum to 0 and for each positive
+# element n, there exists another element that is the negative of n. Write a function named ìsZeroBalanced
+# that returns true if its argument is zero-balanced array, else return false. Note that
+# an empty array will not sum to zero.
