4442b926a013121b58de2add4ef45d2f25b05c54
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-04-23 16:24:51 +0300

    Added task 1585

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16550,3 +16550,12 @@ def backwards_prime(start, stop):
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
         return all(i in sentence for i in 'abcdefghijklmnopqrstuvwxyz')
+
+# In this kata, you should calculate type of triangle with three given sides a, b and c (given in any order).
+# If all angles are less than 90°, this triangle is acute and function should return 1.
+# If one angle is strictly 90°, this triangle is right and function should return 2.
+# If one angle more than 90°, this triangle is obtuse and function should return 3.
+# If three sides cannot form triangle, or one angle is 180° (which turns triangle into segment) -
+# function should return 0.
+# Input parameters are sides of given triangle. All input values are
+# non-negative floating point or integer numbers (or both).
