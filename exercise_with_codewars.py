d90eeb7458ec04c6c6f6d44b01424b8c968f8481
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-05-02 19:56:22 +0300

    Added task 1603

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16769,3 +16769,19 @@ def survivor(n):
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
         return sorted(nums, key=lambda x: x % 2)
+
+# How many elephants can the spider web hold?
+# Imagine a spider web that is defined by two variables:
+# strength, measured as the weight in kilograms that surely breaks the web
+# length, measured as the number of elephants that fit one after the other on the web :)
+# Paraphrasing the song "One elephant went out to play", how many elephants will the web hold if we put them one
+# after the other, without breaking?
+# You must take into account two things:
+# elephants like to create super high pyramids, so, on each level of the structure fits one
+# elephant less than in the previous one.
+# elephants sitting on the first row weight 1000 kg, the ones sitting on the second row
+# weight 2000 kg, and so on. When rows are full of elephants, next elephants go up
+# one level, and weight 1000 kg more than the previous ones.
+# Have fun!
+# Notes:
+# check all the possible values for the input parameters, even absurd ones :D
