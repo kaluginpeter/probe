7f36df51c8d36fbcc2a6ee706698f320d740f06d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-05-02 18:14:55 +0300

    Complete task 1602 LeetCode

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16766,3 +16766,6 @@ def survivor(n):
     return True
 
 # 905. Sort Array By Parity
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        return sorted(nums, key=lambda x: x % 2)
