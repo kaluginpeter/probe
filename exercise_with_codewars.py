1559e83796999dea9f36cbc6faa1c96636aa8475
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-05-04 22:42:38 +0300

    Complete task 1607

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16843,3 +16843,6 @@ class Solution:
 
 # You are given 2 two-digit numbers. You should check if they are similar
 # by comparing their numbers, and return the result in %.
+def compare(a, b):
+    fir, sec = sorted(str(a)), sorted(str(b))
+    return '100%' if fir == sec else '50%' if fir[0] in sec or fir[1] in sec else '0%'
