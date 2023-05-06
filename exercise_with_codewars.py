a0d1d6254bf40d7295f09d8f8322457433b6eee2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-05-06 16:32:46 +0300

    Added task 1611

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16869,3 +16869,11 @@ class Solution:
         if sp % (l - 1) == 0:
             return back.join(i for i in text.split())
         return back.join(i for i in text.split()) + ' ' * (sp % (l - 1))
+
+# You've intercepted an encrypted message, and you are really curious about its contents.
+# You were able to find out that the message initially
+# contained only lowercase English letters, and was encrypted with the following cipher:
+# Let all letters from 'a' to 'z' correspond to the numbers from 0 to 25, respectively.
+# The number corresponding to the ith letter of the encrypted message is then equal to the sum of
+# numbers corresponding to the first i letters of the initial unencrypted message modulo 26.
+# Now that you know how the message was encrypted, implement the algorithm to decipher it.
