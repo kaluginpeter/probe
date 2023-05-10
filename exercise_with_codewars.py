82c78a9f88fa26aaba241f880da2e4d291a4fbd7
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-05-10 18:02:15 +0300

    Complete task 1619

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -16981,3 +16981,5 @@ class Solution:
 # size is the size of the shoe.
 # Your task is to check whether it is possible to pair the shoes you found in such
 # a way that each pair consists of a right and a left shoe of an equal size.
+def pair_of_shoes(shoes):
+    return sorted(v for k, v in shoes if k == 1) == sorted(v for k, v in shoes if k == 0)
