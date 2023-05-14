5b04609d5ddea0348fc792681ddbb46353e381be
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-05-14 20:27:59 +0300

    Added task 1627

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17062,3 +17062,15 @@ class Solution:
             if words[i] == words[i][::-1]:
                 return words[i]
         return ''
+
+# After yet another dispute on their game the Bingo Association decides to change course and automate the game.
+# Can you help the association by writing a method to create a random Bingo card?
+# Bingo Cards
+# A Bingo card contains 24 unique and random numbers according to this scheme:
+# 5 numbers from the B column in the range 1 to 15
+# 5 numbers from the I column in the range 16 to 30
+# 4 numbers from the N column in the range 31 to 45
+# 5 numbers from the G column in the range 46 to 60
+# 5 numbers from the O column in the range 61 to 75
+# Task
+# Write the function get_card()/getCard(). The card must be returned as an array of Bingo style numbers:
