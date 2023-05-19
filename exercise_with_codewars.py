376538d83a2acca47fad373afd6066f5e8b39e25
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-05-19 10:09:20 +0300

    Complete task 1637

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17218,3 +17218,19 @@ class Solution:
         return count
 
 # Given a number, find the permutation with the smallest absolute value (no leading zeros).
+def min_permutation(n):
+    if n == 0: return n
+    flag, count = True if n < 0 else False, 0
+    out = ''.join(sorted(str(n)))
+    if not flag:
+        while out.startswith('0'):
+            out = out[1:]
+            count += 1
+        out = int(out[0] + '0' * count + out[1:])
+    if flag:
+        out = out[1:]
+        while out.startswith('0'):
+            out = out[1:]
+            count += 1
+        out = int('-' + out[0] + '0' * count + out[1:])
+    return out
