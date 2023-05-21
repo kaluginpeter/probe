a6c0e886e0e08abed0d6c24c52eea4b7e7e271ee
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-05-21 12:35:51 +0300

    Complete task 1640 LeetCode

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17255,3 +17255,10 @@ def solve(a, b):
     return l[a:a+b]
 
 # 2255. Count Prefixes of a Given String
+class Solution:
+    def countPrefixes(self, words: List[str], s: str) -> int:
+        count = 0
+        for i in words:
+            if s.startswith(i):
+                count += 1
+        return count
