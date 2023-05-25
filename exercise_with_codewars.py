c3c3af77f85a1de91a1176ec53e844f64ceafabe
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-05-25 18:53:10 +0300

    Added task 1648 LeetCode

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17384,3 +17384,10 @@ class Solution:
 # Write a function that calculates the number of points in the circle where
 # (x,y) - the cartesian coordinates of the points - are integers.
 # Example: for radius = 2 the result should be 13.
+def points(n):
+    count = 0
+    for i in range(1, n + 1):
+        count += int((n**2 - i**2) ** (1/2))
+    return 1 + int((count + n) * 4)
+
+# 2278. Percentage of Letter in String
