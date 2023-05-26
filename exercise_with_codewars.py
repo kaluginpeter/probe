45a4842d65aa71b8ea0c8864ec99842e447b66d8
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-05-26 11:38:37 +0300

    Complete task 1651

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17427,3 +17427,16 @@ class Solution:
 # You begin a new line with \n
 # Each Line must be 7 chars long
 # Fill the missing pins with a whitespace
+def bowling_pins(arr):
+    pins, word = '   1   \n  2 3  \n 4 5 6 \n7 8 9 I', ''
+    for i in arr:
+        if i == 10:
+            pins = pins[:-1] + ' '
+            continue
+        pins = pins.replace(str(i), ' ')
+    for i in pins.split('\n')[::-1]:
+        print(i)
+        for j in i:
+            word += ' ' if j == ' ' else 'I'
+        word += '\n'
+    return word[:-1]
