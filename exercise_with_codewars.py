b641db973cd67ccfbe95320c6814fe79dbbf5bf1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-05-30 12:53:25 +0300

    Added task 1659

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17531,3 +17531,11 @@ def encryptor(key, message):
 class Solution:
     def findLUSlength(self, a, b):
         return max(len(a), len(b)) if a != b else -1
+
+# You certainly can tell which is the larger number between 210 and 215.
+# But what about, say, 210 and 310? You know this one too.
+# Things tend to get a bit more complicated with both
+# different bases and exponents: which is larger between 39 and 56?
+#  Well, by now you have surely guessed that you have to build
+#  a function to compare powers, returning -1 if the first member is
+#  larger, 0 if they are equal, 1 otherwise; powers to compare will be provided in the [base, exponent] format:
