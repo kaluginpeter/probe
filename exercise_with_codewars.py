847388c434e3c5f0e0861c6c108458bf8f17c51e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-03 13:30:46 +0300

    Complete task 1667

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17653,3 +17653,5 @@ class Solution:
 # But the function return some weird result and skip some of the matching results.
 # Does the function have special movie taste?
 # Let's figure out !
+def search(titles, term):
+    return list(filter(lambda title: term in title.lower(), titles))
