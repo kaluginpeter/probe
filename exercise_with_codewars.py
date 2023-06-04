44c24b2c25d513ab37186a33e1d8f43f2c509d70
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-04 14:12:38 +0300

    Added task 1669

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17671,3 +17671,9 @@ class Solution:
             count += start + inp
             weeks += 1
         return count
+
+# You get a "text" and have to shift the vowels by "n" positions to the right.
+# (Negative value for n should shift to the left.)
+# "Position" means the vowel's position if taken as one item in a list of all vowels within the string.
+# A shift by 1 would mean, that every vowel shifts to the place of the next vowel.
+# Shifting over the edges of the text should continue at the other edge.
