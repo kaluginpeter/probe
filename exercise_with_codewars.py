cc35cb1a0e7b283507ca7e0e0527dd870a728b89
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-05 07:18:09 +0300

    Complete task 1671

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17714,3 +17714,11 @@ class Solution:
 # Give the lamp an instance method called toggle_switch that will switch the value of the on attribute.
 # Define another instance method called state that will return "The lamp is on." if it's on and
 # "The lamp is off." otherwise.
+class Lamp():
+    def __init__(self, color, on=False):
+        self.color = color
+        self.on = on
+    def toggle_switch(self):
+        self.on = not self.on
+    def state(self):
+        return 'The lamp is on.' if self.on else 'The lamp is off.'
