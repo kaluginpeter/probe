964de22233079d7cf914ccd2caf3b6cc81eb4338
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-05 07:17:45 +0300

    Added task 1671

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17707,3 +17707,10 @@ class Solution:
             if dx * (y - y1) != dy * (x - x1):
                 return False
         return True
+
+# Define a class called Lamp. It will have a string attribute for color and
+# boolean attribute, on, that will refer to whether the lamp is on or not.
+# Define your class constructor with a parameter for color and assign on as false on initialize.
+# Give the lamp an instance method called toggle_switch that will switch the value of the on attribute.
+# Define another instance method called state that will return "The lamp is on." if it's on and
+# "The lamp is off." otherwise.
