ced0b330840d465b9315043ad0f5ebeec051671e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-10 12:56:26 +0300

    Added task 1681 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17831,3 +17831,7 @@ def dirReduc(arr):
 class Solution:
     def countSegments(self, s: str) -> int:
         return len(s.split())
+
+# Greed is a dice game played with five six-sided dice. Your mission, should you
+# choose to accept it, is to score a throw according to these rules. You will always be given an array with
+# five six-sided dice values.
