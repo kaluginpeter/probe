afd028d7b3029acbbc40a6d47b22585e7f86d0d1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-11 12:34:24 +0300

    Added task 1684 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17923,3 +17923,10 @@ class Solution:
             if i != min_num and i != max_num:
                 return i
         return -1
+
+# For this exercise you will be strengthening your page-fu mastery. You will complete
+# the PaginationHelper class, which is a utility class helpful for querying paging
+# information related to an array.
+# The class is designed to take in an array of values and an integer indicating how many items
+# will be allowed per each page. The types of values contained within the collection/array are not relevant.
+# The following are some examples of how this class is used:
