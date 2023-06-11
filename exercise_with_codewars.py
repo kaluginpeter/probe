5a8b54c7b97e2519e0a86781a6fa1190d2490a74
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-11 12:35:27 +0300

    Complete task 1684 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -17930,3 +17930,46 @@ class Solution:
 # The class is designed to take in an array of values and an integer indicating how many items
 # will be allowed per each page. The types of values contained within the collection/array are not relevant.
 # The following are some examples of how this class is used:
+
+
+class PaginationHelper:
+    # The constructor takes in an array of items and an integer indicating
+    # how many items fit within a single page
+    def __init__(self, collection, items_per_page):
+        self.collection = collection
+        self.items_per_page = items_per_page
+
+    # returns the number of items within the entire collection
+    def item_count(self):
+        return len(self.collection)
+
+    # returns the number of pages
+    def page_count(self):
+        count = len(self.collection) % self.items_per_page
+        delet = len(self.collection) // self.items_per_page
+        delet = delet if count == 0 else delet + 1
+        l, page = [], 0
+        for i in range(0, len(self.collection), self.items_per_page):
+            l.append([page, self.collection[i:i + self.items_per_page]])
+            page += 1
+        self.page_counts = l
+        return len(self.page_counts)
+
+    # returns the number of items on the given page. page_index is zero based
+    # this method should return -1 for page_index values that are out of range
+    def page_item_count(self, page_index):
+        if (page_index + 1 > len(self.page_counts)) or page_index < 0:
+            return -1
+        return len(self.page_counts[page_index][1])
+
+    # determines what page an item at the given index is on. Zero based indexes.
+    # this method should return -1 for item_index values that are out of range
+    def page_index(self, item_index):
+        if (item_index < 0) or item_index + 1 > len(self.collection):
+            return -1
+        count, page = 0, 0
+        for i in self.page_counts:
+            count += len(i[1])
+            if item_index + 1 <= count:
+                return page
+            page += 1
