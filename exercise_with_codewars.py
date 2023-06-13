22c0049ba160f000a3255759457bf5ab31ff6931
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-13 07:26:09 +0300

    Added task 1688 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18028,3 +18028,13 @@ class Solution:
             l.append(int(operations[i]))
             top += 1
         return sum(l)
+
+# How can you tell an extrovert from an introvert at NSA?
+# Va gur ryringbef, gur rkgebireg ybbxf ng gur BGURE thl'f fubrf.
+#
+# I found this joke on USENET, but the punchline is scrambled. Maybe you can decipher it?
+# According to Wikipedia, ROT13 is frequently used to obfuscate jokes on USENET.
+#
+# For this task you're only supposed to substitute characters. Not spaces, punctuation, numbers, etc.
+#
+# Test examples:
