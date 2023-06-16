0c7ea96e1a7539eda5b3125ef33b5cc9a1e360c9
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 07:00:13 +0300

    Complete task 1694 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18121,3 +18121,6 @@ class Solution:
 # Accepts a case-insensitive hexadecimal color string as its parameter (ex. "#FF9933" or "#ff9933")
 # Returns a Map<String, int> with the structure {r: 255, g: 153, b: 51} where r, g, and b range from 0 through 255
 # Note: your implementation does not need to support the shorthand form of hexadecimal notation (ie "#FFF")
+def hex_string_to_RGB(hex_string):
+    colors = [int(hex_string.lstrip('#')[i:i+2], 16) for i in (0, 2, 4)]
+    return {'r':colors[0], 'g':colors[1], 'b':colors[2]}
