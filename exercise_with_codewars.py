b432c4fec42641339bf8d598ccf2731f03759ae7
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-16 06:59:49 +0300

    Added task 1694 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18114,3 +18114,10 @@ class Solution:
             if b[i] in vowels:
                 top_b += 1
         return top_a == top_b
+
+# When working with color values it can sometimes be useful to extract the individual red,
+# green, and blue (RGB) component values for a color. Implement a function that meets these requirements:
+#
+# Accepts a case-insensitive hexadecimal color string as its parameter (ex. "#FF9933" or "#ff9933")
+# Returns a Map<String, int> with the structure {r: 255, g: 153, b: 51} where r, g, and b range from 0 through 255
+# Note: your implementation does not need to support the shorthand form of hexadecimal notation (ie "#FFF")
