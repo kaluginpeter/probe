649dabd4b7722f57b1019aa49c78897ed26c01f0
kaluginpeter <streetworkout200412@gmail.com>
2023-06-18 13:59:23 +0300

    Added task 1698 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18195,3 +18195,15 @@ def valid_ISBN10(isbn):
         return sum(int(isbn[i]) * (i+1) for i in range(len(isbn))) % 11 == 0
     except:
         return False
+    
+# Write a method that takes a number and returns a string of that number in English.
+#
+# Your method should be able to handle any number between 0 and 99999. If the given number is outside of that range or not an integer, the method should return an empty string.
+#
+# Examples
+# 0      -->  "zero"
+# 27     -->  "twenty seven"
+# 100    -->  "one hundred"
+# 7012   -->  "seven thousand twelve"
+# 99205  -->  "ninety nine thousand two hundred five"
+# STRINGSPARSINGALGORITHMS
