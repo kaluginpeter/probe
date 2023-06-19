38ed1b48da54ee7fccf7034d50472638b02dcaf3
kaluginpeter <streetworkout200412@gmail.com>
2023-06-19 16:57:28 +0300

    Complete task 1701 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18251,3 +18251,14 @@ def log(real, imag):
         pass
 
 # 1688. Count of Matches in Tournament
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        count = 0
+        while n != 1:
+            if n % 2 == 0:
+                n = n / 2
+                count += n
+                continue
+            n = (n - 1) / 2
+            count += n + 1
+        return int(count)
