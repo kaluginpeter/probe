8277ccd5c3fdce3713eb886f8067ab4e8b7926db
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-20 20:09:43 +0300

    Added task 1703 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18270,3 +18270,12 @@ class Solution:
             count += 1
             n -= count
         return count if n == 0 else count - 1
+
+# # You're going to provide a needy programmer a utility method that generates an infinite amount of sequential fibonacci numbers.
+# #
+# # to do this write a 'generator' starting with 1
+# #
+# # A fibonacci sequence starts with two 1s. Every element afterwards is the sum of the two previous elements. See:
+# #
+# # 1, 1, 2, 3, 5, 8, 13, ..., 89, 144, 233, 377, ...
+# # ALGORITHMS
