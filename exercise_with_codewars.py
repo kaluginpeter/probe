777a59904ef4d2b8d5dcb552f4fd3f5823ad07a2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-22 08:12:14 +0300

    Complete task 1706 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18315,3 +18315,11 @@ def digits_product(product):
     return -1
 
 # 342. Power of Four
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        i = 0
+        while 4 ** i <= n:
+            if 4 ** i == n:
+                return True
+            i += 1
+        return False
