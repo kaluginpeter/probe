1649dd53fb150e2b038f34d3d53af245dab6f28e
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-24 14:00:21 +0300

    Complete task 1710 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18411,3 +18411,27 @@ def solve(input_string):
     return '\n'.join(out)
 
 # 225. Implement Stack using Queues
+class MyStack:
+
+    def __init__(self):
+        self.l = []
+
+    def push(self, x: int) -> None:
+        self.l.append(x)
+
+    def pop(self) -> int:
+        return self.l.pop()
+
+    def top(self) -> int:
+        return self.l[-1]
+
+    def empty(self) -> bool:
+        return len(self.l) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
