182aac7c4885d97905095ec385ad746cd5850845
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-27 10:31:57 +0300

    Complete task 1716 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18535,3 +18535,47 @@ def from_base_64(string):
         return b64decode((string + '==').encode('ascii'), '-_').decode('ascii')
 
 # 232. Implement Queue using Stacks
+class MyQueue:
+
+    def __init__(self):
+        self.l = []
+        self.l2 = []
+
+    def push(self, x: int) -> None:
+        self.l.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.l2.pop()
+
+    def peek(self) -> int:
+        if not self.l2:
+            while self.l:
+                self.l2.append(self.l.pop())
+        return self.l2[-1]
+
+    def empty(self) -> bool:
+        return not self.l and not self.l2
+# Solution 2
+# class MyQueue:
+#
+#     def __init__(self):
+#         self.l = []
+#
+#     def push(self, x: int) -> None:
+#         self.l.append(x)
+#
+#     def pop(self) -> int:
+#         return self.l.pop(0)
+#
+#     def peek(self) -> int:
+#         return self.l[0]
+#
+#     def empty(self) -> bool:
+#         return len(self.l) == 0
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
