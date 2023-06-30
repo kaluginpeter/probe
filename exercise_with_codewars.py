abf988a1ef6c3738d1c3241d12f80ae57394a673
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-06-30 12:38:54 +0300

    Complete task 1723 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18646,3 +18646,19 @@ class Solution:
         return l
 
 # Airport Arrivals/Departures - #1
+def flap_display(lines, rotors):
+    word, steps, out = '', [], []
+    al = ALPHABET + ALPHABET
+    for i in rotors:
+        top = []
+        for j in range(len(i) + 1):
+            top.append(sum(i[:j + 1]))
+        steps.append(top)
+    for j in range(len(lines)):
+        for i in range(len(lines[j])):
+            if steps[j][i] > len(ALPHABET):
+                steps[j][i] = steps[j][i] % len(ALPHABET)
+            word += al[al.index(lines[j][i]) + steps[j][i]]
+        out.append(word)
+        word = ''
+    return out
