26248aa49bb40225b31159cdeeb04269c8718911
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-01 14:43:14 +0300

    Complete task 1724 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18664,3 +18664,6 @@ def flap_display(lines, rotors):
     return out
 
 # 1816. Truncate Sentence
+class Solution:
+    def truncateSentence(self, s: str, k: int) -> str:
+        return ' '.join(s.split()[:k])
