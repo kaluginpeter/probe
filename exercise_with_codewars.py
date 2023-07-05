b61f729a17279a8c30c87154700acea9fbf74ff1
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-05 10:14:40 +0300

    Complete task 1732 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18792,3 +18792,7 @@ def separate_liquids(glass):
     return l
 
 # 290. Word Pattern
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split()
+        return (len(set(pattern)) == len(set(s)) == len(set(zip_longest(pattern,s))))
