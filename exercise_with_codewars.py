65de915be58963f1c9487c580692ab87bd6d63de
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-05 10:17:52 +0300

    Added task 1733 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18796,3 +18796,5 @@ class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s = s.split()
         return (len(set(pattern)) == len(set(s)) == len(set(zip_longest(pattern,s))))
+        
+# Diophantine Equation
