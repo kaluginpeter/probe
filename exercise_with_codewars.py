ea4f3c9c21373c42e10f470541709414de633894
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-10 16:36:21 +0300

    Complete task 1742 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -18900,5 +18900,22 @@ class RSA:
     def decrypt(self, c):
         d = pow(self.e, -1, self.phi_n)
         return pow(c, d, self.n)
-
+    
 # 111. Minimum Depth of Binary Tree
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        l = self.minDepth(root.left)
+        r = self.minDepth(root.right)
+        if root.left is None:
+            return 1 + r
+        if root.right is None:
+            return 1 + l
+        return min(l, r) + 1
