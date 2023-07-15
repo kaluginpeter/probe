1f125fbec5542369b6d006231e75dc7d287729d2
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-15 18:25:55 +0300

    Complete task 1753 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -19043,3 +19043,16 @@ def count_languages(lst):
     return Counter(i['language'] for i in lst)
 
 # 278. First Bad Version
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        l, r = 1, n
+        while l < r:
+            mid = (l + r) // 2
+            if isBadVersion(mid):
+                r = mid
+            else:
+                l = mid + 1
+        return l
