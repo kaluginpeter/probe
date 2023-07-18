a47c5358ea5edc4f6f79dbf9afdf52d96753a713
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-18 21:32:04 +0300

    Complete task 1759 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -19145,3 +19145,31 @@ def smallest(n):
     return l
 
 # 33. Search in Rotated Sorted Array
+# My solution with flag
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right, flag = 0, len(nums) - 1, True
+        while left <= right:
+            mid = (left + right) // 2
+            flag = nums[left] <= nums[mid]
+            if flag:
+                if nums[left] <= target <= nums[mid]:
+                    if nums[mid] == target:
+                        return mid
+                    if nums[mid] > target:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    if nums[mid] == target:
+                        return mid
+                    if nums[mid] > target:
+                        right = mid - 1
+                    else:
+                        left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
