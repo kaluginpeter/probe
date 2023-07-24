e8f7cd328f7422e6a3c4fc9be667af255f1f080c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-24 20:29:01 +0300

    Complete task 1771 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -19324,3 +19324,15 @@ def center(strng, width, fill=' '):
     return cop + fill * (width - len(cop))
 
 # 50. Pow(x, n)
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1 / x
+            n *= -1
+        if n % 2 != 0:
+            return x * self.myPow(x, n - 1)
+        else:
+            temp = self.myPow(x, n/2)
+            return temp * temp
