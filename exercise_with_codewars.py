e24442b2dbcf5e6e53d0d1e3d3e829da6da9a168
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-25 21:41:05 +0300

    Complete task 1772 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -19338,3 +19338,13 @@ class Solution:
             return temp * temp
 
 # 852. Peak Index in a Mountain Array
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l, r = 0, len(arr) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if arr[mid] > arr[mid + 1]:
+                r = mid
+            else:
+                l = mid + 1
+        return l
