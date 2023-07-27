b8ac8276eeef3feb160381433c227b2c57a5600d
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-07-27 21:34:30 +0300

    Complete task 1776 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -19396,3 +19396,14 @@ def valid_braces(string):
     return not stack
     
 # 263. Ugly Number
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        while n % 2 == 0:
+            n //= 2
+        while n % 3 == 0:
+            n //= 3
+        while n % 5 == 0:
+            n //= 5
+        return n == 1
