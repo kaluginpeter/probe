4126437ab9c12276c858c541228dd29031ba6b6c
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-13 20:09:37 +0300

    Complete task 1812 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -19788,3 +19788,7 @@ class Solution:
         return res
 
 # The mean of two means
+def get_mean(arr,x,y):
+    if (x <= 1 or y <= 1) or (x > len(arr) or y > len(arr)):
+        return -1
+    return (sum(arr[:x]) / x + sum(arr[-y:]) / y) / 2
