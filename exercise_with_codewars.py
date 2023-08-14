f14ef21593672f4bb9bfa599eb3900b65ff47491
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-14 19:28:00 +0300

    Added task 1813 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -19786,9 +19786,11 @@ class Solution:
             res.append(nums2[right])
             right += 1
         return res
-
+    
 # The mean of two means
 def get_mean(arr,x,y):
     if (x <= 1 or y <= 1) or (x > len(arr) or y > len(arr)):
         return -1
     return (sum(arr[:x]) / x + sum(arr[-y:]) / y) / 2
+
+# 215. Kth Largest Element in an Array
