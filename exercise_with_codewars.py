b6d473db8547ddf66cee3a18dd06763f40a2afad
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-25 21:54:02 +0300

    Complete task 1836 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -20046,3 +20046,13 @@ def solution(args):
     return ','.join(l)
 
 # Roman Numerals Encoder
+def solution(n):
+    integer = ''
+    rom = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
+           (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
+    while n > 0:
+        for k,v in rom:
+            while n >= k:
+                integer += v
+                n -= k
+    return integer
