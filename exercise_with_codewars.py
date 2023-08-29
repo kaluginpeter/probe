3baffad3a9c450901706a6e702794b4496949291
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-08-29 19:51:58 +0300

    Complete task 1847 4kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -20364,3 +20364,15 @@ class Solution(object):
         return time + 1
 
 # So Many Permutations!
+def permutations(s):
+    l = []
+    def permute(data, i, length):
+        if i == length:
+            l.append(''.join(data) )
+        else:
+            for j in range(i, length):
+                data[i], data[j] = data[j], data[i]
+                permute(data, i + 1, length)
+                data[i], data[j] = data[j], data[i]
+    permute(list(s), 0, len(s))
+    return set(l)
