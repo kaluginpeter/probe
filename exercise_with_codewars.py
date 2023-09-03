766470002e9e0ead89ae841e4cb9d584fe0b9494
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-03 14:05:12 +0300

    Added task 1857 4kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -20500,3 +20500,5 @@ class Solution:
     def maxSubsequence(self, nums: List[int], k: int) -> List[int]:
         ans = sorted(enumerate(nums), key=lambda x: x[1])[-k:]
         return [v for k, v in sorted(ans)]
+
+# Roman Numerals Helper
