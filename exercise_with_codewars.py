f76be09edb54dab42cac4dce16aae92128628e08
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-04 22:29:31 +0300

    Complete task 1862 4kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -20594,3 +20594,25 @@ class Solution:
         return False
 
 # Recover a secret string from random triplets
+def recoverSecret(triplets):
+    unique_letters = set()
+    letter_order = {}
+    for triplet in triplets:
+        for letter in triplet:
+            unique_letters.add(letter)
+    for triplet in triplets:
+        for i in range(2):
+            if triplet[i + 1] not in letter_order:
+                letter_order[triplet[i + 1]] = set()
+            letter_order[triplet[i + 1]].add(triplet[i])
+    sorted_letters = []
+    while unique_letters:
+        for letter in unique_letters:
+            if letter not in letter_order or not letter_order[letter]:
+                sorted_letters.append(letter)
+                unique_letters.remove(letter)
+                for key in letter_order:
+                    if letter in letter_order[key]:
+                        letter_order[key].remove(letter)
+                break
+    return ''.join(sorted_letters)
