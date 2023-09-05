1007cda58d8d0d714f42df98d9d07b2ae58ddada
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-05 19:09:22 +0300

    Complete task 1863 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -20616,5 +20616,12 @@ def recoverSecret(triplets):
                         letter_order[key].remove(letter)
                 break
     return ''.join(sorted_letters)
-    
+
 # 1450. Number of Students Doing Homework at a Given Time
+class Solution:
+    def busyStudent(self, startTime: List[int], endTime: List[int], queryTime: int) -> int:
+        count = 0
+        for i in range(len(startTime)):
+            if endTime[i] >= queryTime and startTime[i] <= queryTime:
+                count += 1
+        return count
