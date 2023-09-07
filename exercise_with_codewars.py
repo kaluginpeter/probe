3d93e1749230c1d8fb15a00d7c510385dd57eb50
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-07 21:25:41 +0300

    Complete task 1868 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -20682,3 +20682,9 @@ class Solution:
         return ans
 
 # Bubblesort Once
+def bubblesort_once(l):
+    l = l.copy()
+    for i in range(1, len(l)):
+        if l[i-1] > l[i]:
+            l[i-1], l[i] = l[i], l[i-1]
+    return l
