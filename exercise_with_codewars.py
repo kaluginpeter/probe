fe8f9d16c77b013a254fc311d22699b96b5930bb
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-09 13:32:57 +0300

    Complete task 1872 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -20720,3 +20720,12 @@ class Solution:
         return ans
 
 # Ninja vs Samurai: Strike
+class Warrior:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+
+    def strike(self, enemy, swings):
+        # health cannot go below zero
+        enemy.health = max([0, enemy.health - (swings * 10)])
+        return enemy.health
