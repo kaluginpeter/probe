52c2bf28e121b215d5bd07d82e52d30334af84dd
kaluginpeter <107790992+kaluginpeter@users.noreply.github.com>
2023-09-18 19:24:09 +0300

    Complete task 1890 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -21442,3 +21442,8 @@ def computer_to_phone(numbers):
 # 2 <= n, m <= 100
 # 1 <= k <= m
 # matrix[i][j] is either 0 or 1.
+class Solution:
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        row_strength = [(sum(row), i) for i, row in enumerate(mat)]
+        row_strength.sort(key=lambda x: (x[0], x[1]))
+        return [row[1] for row in row_strength[:k]]
