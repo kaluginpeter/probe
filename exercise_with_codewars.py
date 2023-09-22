4a5573c78b384f753911fbfd8dda8c8873ad3711
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-22 21:48:41 +0300

    Complete task 1900 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -21874,3 +21874,19 @@ def least_larger(a, i):
 # The number of nodes in the list is in the range [0, 104].
 # 1 <= Node.val <= 50
 # 0 <= val <= 50
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        dummy = ListNode(None, next=head)
+        prev, cur = dummy, head
+        while cur:
+            if cur.val == val:
+                prev.next = cur.next
+            else:
+                prev = cur
+            cur = cur.next
+        return dummy.next
