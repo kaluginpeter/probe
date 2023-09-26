c37495bcbd96b9caa3eab9edd7024db14b1e4a7c
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 19:21:06 +0300

    Added task 1910 8kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -22224,3 +22224,27 @@ def flick_switch(lst):
             flag = not flag
         ans.append(flag)
     return ans
+
+# A Letter's Best Friend
+# Task
+# Given a string, return if a given letter always appears immediately before another given letter.
+#
+# Worked Example
+# ("he headed to the store", "h", "e") ➞ True
+#
+# # All occurences of "h": ["he", "headed", "the"]
+# # All occurences of "h" have an "e" after it.
+# # Return True
+#
+# ('abcdee', 'e', 'e') ➞ False
+#
+# # For first "e" we can get "ee"
+# # For second "e" we cannot have "ee"
+# # Return False
+# Examples
+# ("i found an ounce with my hound", "o", "u") ➞ True
+#
+# ("we found your dynamite", "d", "y") ➞ False
+# Notes
+# All sentences will be given in lowercase.
+# FUNDAMENTALSSTRINGS
