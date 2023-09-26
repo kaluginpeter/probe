ff9339f39f0a44128f76bfffa22ca2b182199d5d
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 18:52:31 +0300

    Added task 1909 8kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -22201,3 +22201,19 @@ def replicate(times, number, ans=[]):
     if times == 0:
         return ans
     return replicate(times - 1, number, ans + [number])
+
+# Flick Switch
+# Task
+# Create a function that always returns True for every item in a given list. However, if an element is the word "flick", switch to always returning the opposite boolean value.
+#
+# Examples
+# ["codewars", "flick", "code", "wars"] ➞ [True, False, False, False]
+#
+# ['flick', 'chocolate', 'adventure', 'sunshine'] ➞[False, False, False, False]
+#
+# ['bicycle', 'jarmony', 'flick', 'sheep', 'flick'] ➞ [True, True, False, False, True]
+# Notes
+# "flick" will always be given in lowercase.
+# A list may contain multiple flicks.
+# Switch the boolean value on the same element as the flick itself.
+# FUNDAMENTALSLISTS
