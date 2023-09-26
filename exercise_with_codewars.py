2039f839bfd93d393e45368e6e9eba52a571791a
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-26 18:42:57 +0300

    Added task 1908 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -22185,3 +22185,12 @@ class Solution:
         if word == word.lower():
             return True
         return False
+
+# Recursive Replication
+# You need to design a recursive function called replicate which will receive arguments times and number.
+#
+# The function should return an array containing repetitions of the number argument. For instance, replicate(3, 5) should return [5,5,5]. If the times argument is negative, return an empty array.
+#
+# As tempting as it may seem, do not use loops to solve this problem.
+#
+# RECURSIONALGORITHMS
