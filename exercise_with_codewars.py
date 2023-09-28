a12e5961cb1ee7e09273d36beb7f7e4141227762
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 19:13:45 +0300

    Added task 1913 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -22333,3 +22333,26 @@ class Solution:
 # BITSBINARYALGORITHMS
 def range_bit_count(a, b):
     return sum(bin(i).count('1') for i in range(a, b + 1))
+
+# 905. Sort Array By Parity
+# Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+#
+# Return any array that satisfies this condition.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [3,1,2,4]
+# Output: [2,4,3,1]
+# Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+# Example 2:
+#
+# Input: nums = [0]
+# Output: [0]
+#
+#
+# Constraints:
+#
+# 1 <= nums.length <= 5000
+# 0 <= nums[i] <= 5000
