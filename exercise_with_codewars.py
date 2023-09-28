0aceb7be3aa29624cd240923292867b63fd770d5
Peter Kalugin <streetworkout200412@gmail.com>
2023-09-28 19:21:48 +0300

    Complete task 1914 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -22401,3 +22401,11 @@ class Solution:
 # I have also created other katas. Take a look if you enjoyed this kata!
 #
 # ALGORITHMSFUNDAMENTALSLOGICMATHEMATICSRESTRICTEDPUZZLES
+def mod256_without_mod(number):
+    if number < 0:
+        while number < 0:
+            number += 256
+        return number
+    while number >= 256:
+        number -= 256
+    return number
