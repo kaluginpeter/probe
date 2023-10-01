a265f7187e8e70892fb86e712f882cfe8e3705e8
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-01 10:39:37 +0300

    Added task 1920 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -22622,3 +22622,16 @@ class Solution:
 class Solution:
     def reverseWords(self, s: str) -> str:
         return ' '.join(i[::-1] for i in s.split(' '))
+
+# Penultimate
+# Find the second-to-last element of a list.
+#
+# The input list will always contain at least two elements.
+#
+# Example:
+#
+# penultimate([1,2,3,4])            # => 3
+# penultimate("Python is dynamic") # => 'i'
+# (courtesy of haskell.org)
+#
+# LISTSFUNDAMENTALS
