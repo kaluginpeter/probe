94dec1bf72f84dae67578c61ef720349c7cc2066
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-04 19:53:31 +0300

    Added task 1927 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -22954,3 +22954,16 @@ class MyHashMap:
 # obj.put(key,value)
 # param_2 = obj.get(key)
 # obj.remove(key)
+
+# International Morse Code Encryption
+# Write a function that will encrypt a given sentence into International Morse Code, both the input and out puts will be strings.
+#
+# Characters should be separated by a single space. Words should be separated by a triple space.
+#
+# For example, "HELLO WORLD" should return -> ".... . .-.. .-.. --- .-- --- .-. .-.. -.."
+#
+# To find out more about Morse Code follow this link: https://en.wikipedia.org/wiki/Morse_code
+#
+# A preloaded object/dictionary/hash called CHAR_TO_MORSE will be provided to help convert characters to Morse Code.
+#
+# FUNDAMENTALS
