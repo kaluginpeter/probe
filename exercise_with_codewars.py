3f6f85a8a143922eeb733e22c01b51cd2915e131
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-06 19:41:23 +0300

    Added task 1931 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -23088,3 +23088,15 @@ class Solution:
             if top > 0:
                 ans.append(top)
         return ans
+
+# Product of the main diagonal of a square matrix.
+# Given a list of rows of a square matrix, find the product of the main diagonal.
+#
+# Examples:
+#
+# main_diagonal_product([[1,0],[0,1]]) => 1
+#
+# main_diagonal_product([[1,2,3],[4,5,6],[7,8,9]]) => 45
+# http://en.wikipedia.org/wiki/Main_diagonal
+#
+# MATRIXLINEAR ALGEBRAALGORITHMS
