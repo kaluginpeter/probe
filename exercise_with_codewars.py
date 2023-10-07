8282b0441a84d2f51f2259fd5aca1fc70c133be8
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-07 14:44:32 +0300

    Complete task 1933 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -23175,3 +23175,9 @@ class Solution:
 # when the length of "title" is growing.
 # Write your own function "exp [x n]".
 # FUNDAMENTALSALGORITHMS
+def title_to_number(title):
+    count = 0
+    for i in range(len(title)):
+        x = ord(title[i]) - 64
+        count += x * max(26 ** (len(title) - i - 1), 1)
+    return count
