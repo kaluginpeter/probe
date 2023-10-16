60d27839f56a6d51c750e136276927f46f572f08
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-16 16:36:28 +0300

    Complete task 1950 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -23853,3 +23853,28 @@ class HTMLGen:
 # 0 <= rowIndex <= 33
 #
 # Follow up: Could you optimize your algorithm to use only O(rowIndex) extra space?
+# Solution
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        for i in range(rowIndex):
+            row = [sum(x) for x in zip([0] + row, row + [0])]
+        return row
+# Solution 2
+class Solution(object):
+    def getRow(self, r):
+        ans = [1]*(r+1);
+        up = r
+        down = 1
+        for i in range(1, r):
+            ans[i] = int(ans[i-1]*up/down);
+            up = up - 1
+            down = down + 1
+        return ans;
+# Solution 3 Speed O(N), Memory O(1)
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        row = [1]
+        for i in range(rowIndex):
+            row = [x + y for x,y in zip([0] + row, row + [0])]
+        return row
