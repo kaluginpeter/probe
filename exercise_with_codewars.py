9865824cb31846d2c1ad72ca1210268b1d782649
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-18 21:41:16 +0300

    Complete task 1955 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -24068,3 +24068,5 @@ class Solution:
 # -----
 #   122  // 122 in binary is 1111010
 # MATHEMATICSFUNDAMENTALS
+def binary_pyramid(m,n):
+    return bin(sum(int(bin(i)[2:]) for i in range(m, n + 1)))[2:]
