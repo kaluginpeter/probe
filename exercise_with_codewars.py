810a590c21da7eca8f3b00faed92d3a253198a33
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-22 11:17:47 +0300

    Complete task 1964 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -24413,3 +24413,9 @@ SELECT name FROM Customer WHERE referee_id != 2 or referee_id IS NULL
 # Example: input: [5, 0, 3], "I Love You" output: "ivy" (0 = "i", 3 = "v", 5 = "y")
 #
 # STRINGSARRAYSALGORITHMSFUNDAMENTALS
+def missing(nums, s):
+    nums.sort()
+    s = s.replace(' ', '').lower()
+    if max(nums) >= len(s):
+        return 'No mission today'
+    return ''.join(s[i] for i in nums)
