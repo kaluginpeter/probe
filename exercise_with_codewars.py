d1e83728516769b55ba65e566c4c2e6916570313
Peter Kalugin <streetworkout200412@gmail.com>
2023-10-30 19:08:10 +0300

    Added task 1984 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -25265,3 +25265,18 @@ class Solution(object):
         for i in range(len(arr)):
             arr[i] = arr[i] % 10001
         return arr
+
+# Suitcase packing
+# Mr. Square is going on a holiday. He wants to bring 2 of his favorite squares with him, so he put them in his rectangle suitcase.
+#
+# Write a function that, given the size of the squares and the suitcase, return whether the squares can fit inside the suitcase.
+#
+# fit_in(a,b,m,n)
+# a,b are the sizes of the 2 squares
+# m,n are the sizes of the suitcase
+# Example
+# fit_in(1,2,3,2) should return True
+# fit_in(1,2,2,1) should return False
+# fit_in(3,2,3,2) should return False
+# fit_in(1,2,1,2) should return False
+# PUZZLES
