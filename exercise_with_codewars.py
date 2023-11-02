6920139ebf4696d3feefa63cb8fa7c8ce40aecb2
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-02 19:02:46 +0300

    Added task 1989 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -25434,7 +25434,7 @@ class Solution(object):
             elif j == 0 or typed[j] != typed[j - 1]:
                 return False
         return i == len(name)
-
+    
 # Simple Fun #8: Kill K-th Bit
 # Task
 # In order to stop the Mad Coder evil genius you need to decipher the encrypted message he sent to his minions. The message contains several numbers that, when typed into a supercomputer, will launch a missile into the sky blocking out the sun, and making all the people on Earth grumpy and sad.
@@ -25465,3 +25465,33 @@ class Solution(object):
 # PUZZLESBITSBINARY
 def kill_kth_bit(n, k):
     return n & ~(1 << k - 1)
+
+# 2578. Split With Minimum Sum
+# Given a positive integer num, split it into two non-negative integers num1 and num2 such that:
+#
+# The concatenation of num1 and num2 is a permutation of num.
+# In other words, the sum of the number of occurrences of each digit in num1 and num2 is equal to the number of occurrences of that digit in num.
+# num1 and num2 can contain leading zeros.
+# Return the minimum possible sum of num1 and num2.
+#
+# Notes:
+#
+# It is guaranteed that num does not contain any leading zeros.
+# The order of occurrence of the digits in num1 and num2 may differ from the order of occurrence of num.
+#
+#
+# Example 1:
+#
+# Input: num = 4325
+# Output: 59
+# Explanation: We can split 4325 so that num1 is 24 and num2 is 35, giving a sum of 59. We can prove that 59 is indeed the minimal possible sum.
+# Example 2:
+#
+# Input: num = 687
+# Output: 75
+# Explanation: We can split 687 so that num1 is 68 and num2 is 7, which would give an optimal sum of 75.
+#
+#
+# Constraints:
+#
+# 10 <= num <= 109
