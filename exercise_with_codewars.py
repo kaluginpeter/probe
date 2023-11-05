ccfc894947570ac481b19646f4ca6641c635886d
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-05 16:04:14 +0300

    Added task 1896 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -25780,3 +25780,17 @@ class Solution(object):
                 arr.append(arr.pop(0))
             if d[arr[0]] == k:
                 return arr[0]
+
+# ⚠️Fusion Chamber Shutdown⚠️
+# A laboratory is testing how atoms react in ionic state during nuclear fusion. They introduce different elements with Hydrogen in high temperature and pressurized chamber. Due to unknown reason the chamber lost its power and the elements in it started precipitating
+# Given the number of atoms of Carbon [C],Hydrogen[H] and Oxygen[O] in the chamber. Calculate how many molecules of Water [H2O], Carbon Dioxide [CO2] and Methane [CH4] will be produced following the order of reaction affinity below
+#
+# 1. Hydrogen reacts with Oxygen   = H2O
+# 2. Carbon   reacts with Oxygen   = CO2
+# 3. Carbon   reacts with Hydrogen = CH4
+# FOR EXAMPLE:
+# (C,H,O) = (45,11,100)
+# return no. of water, carbon dioxide and methane molecules
+# Output should be like:
+# (5,45,0)
+# FUNDAMENTALS
