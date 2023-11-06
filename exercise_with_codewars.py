a3ceaaccbfa94445cb8b67fc2d8c5c983c34612b
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-06 11:05:00 +0300

    Added task 1898 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -25805,7 +25805,7 @@ def burner(c,h,o):
     c = max(c - carbon, 0)
     methane = min(c, h // 4)
     return (water, carbon, methane)
-    
+
 # Wealth equality, finally!
 # The year is 2088 and the Radical Marxist Socialist People's Party (RMSPP) has just seized power in Brazil.
 #
@@ -25838,3 +25838,36 @@ def redistribute_wealth(wealth):
         mean = int(mean)
     for i in range(len(wealth)):
         wealth[i] = mean
+
+# 1005. Maximize Sum Of Array After K Negations
+# Given an integer array nums and an integer k, modify the array in the following way:
+#
+# choose an index i and replace nums[i] with -nums[i].
+# You should apply this process exactly k times. You may choose the same index i multiple times.
+#
+# Return the largest possible sum of the array after modifying it in this way.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [4,2,3], k = 1
+# Output: 5
+# Explanation: Choose index 1 and nums becomes [4,-2,3].
+# Example 2:
+#
+# Input: nums = [3,-1,0,2], k = 3
+# Output: 6
+# Explanation: Choose indices (1, 2, 2) and nums becomes [3,1,0,2].
+# Example 3:
+#
+# Input: nums = [2,-3,-1,5,-4], k = 2
+# Output: 13
+# Explanation: Choose indices (1, 4) and nums becomes [2,3,-1,5,4].
+#
+#
+# Constraints:
+#
+# 1 <= nums.length <= 104
+# -100 <= nums[i] <= 100
+# 1 <= k <= 104
