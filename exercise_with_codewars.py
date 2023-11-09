d5b525ecc04daf736106cdf1b41d7d433e77ad71
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-09 19:02:38 +0300

    Added task 1906 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -26173,3 +26173,25 @@ class Solution(object):
                 count += (top * (top + 1) // 2) % (10 ** 9 + 7)
                 top = 1
         return count
+
+# Thinkful - Object Drills: Vectors
+# Create a Vector class with x and a y attributes that represent component magnitudes in the x and y directions.
+#
+# Your vectors should handle vector additon with an .add() method that takes a second vector as an argument and returns a new vector equal to the sum of the vector you call .add() on and the vector you pass in.
+#
+# For example:
+#
+# >>> a = Vector(3, 4)
+# >>> a.x
+# 3
+# >>> a.y
+# 4
+# >>> b = Vector(1, 2)
+# >>> c = a.add(b)
+# >>> c.x
+# 4
+# >>> c.y
+# 6
+# Adding vectors when you have their components is easy: just add the two x components together and the two y components together to get the x and y components for the vector sum.
+#
+# FUNDAMENTALS
