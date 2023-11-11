3ce7af5e6cfb15eed57998e37205fa0332e538c0
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-11 12:24:59 +0300

    Added task 1910 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -26341,3 +26341,26 @@ WHERE LENGTH(content) > 15;
 SELECT tweet_id
 FROM Tweets
 WHERE LENGTH(content) > 15;
+
+# Complete The Pattern #5 - Even Ladder
+# Task:
+# You have to write a function pattern which creates the following pattern up to n/2 number of lines.
+#
+# If n <= 1 then it should return "" (i.e. empty string).
+# If any odd number is passed as argument then the pattern should last up to the largest even number which is smaller than the passed odd number.
+# Examples:
+# pattern(8):
+#
+# 22
+# 4444
+# 666666
+# 88888888
+# pattern(5):
+#
+# 22
+# 4444
+# Note: There are no spaces in the pattern
+#
+# Hint: Use \n in string to jump to next line
+#
+# ASCII ARTFUNDAMENTALS
