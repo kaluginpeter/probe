6df9029def85bff2c0274ea6bcb8d60304b4488c
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-15 21:43:06 +0300

    Complete task 1917 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -26691,3 +26691,11 @@ class Solution(object):
 # "1124" --> "12th"
 # "2000" --> "20th"
 # STRINGSALGORITHMSDATE TIME
+def what_century(year):
+    x = (int(year) - 1) // 100 + 1
+    SUFFIXES = {1: 'st', 2: 'nd', 3: 'rd'}
+    if 10 <= x % 100 <= 20:
+        suffix = 'th'
+    else:
+        suffix = SUFFIXES.get(x % 10, 'th')
+    return str(x) + suffix
