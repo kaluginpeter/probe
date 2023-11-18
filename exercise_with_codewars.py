8d8f8d0ba1e8d525b1b44390fc6e5e6cfad6b4ac
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-18 11:43:16 +0300

    Added task 1923 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -26886,3 +26886,21 @@ class Solution(object):
                 if abs(nums[i] - nums[j]) <= min(nums[i], nums[j]):
                     top = max(top, nums[i] ^ nums[j])
         return top
+        
+# Can you get the loop ?
+# You are given a node that is the beginning of a linked list. This list contains a dangling piece and a loop. Your objective is to determine the length of the loop.
+#
+# For example in the following picture the size of the dangling piece is 3 and the loop size is 12:
+#
+#
+# # Use the `next' attribute to get the following node
+# node.next
+# Notes:
+#
+# do NOT mutate the nodes!
+# in some cases there may be only a loop, with no dangling piece
+# Thanks to shadchnev, I broke all of the methods from the Hash class.
+#
+# Don't miss dmitry's article in the discussion after you pass the Kata !!
+#
+# ALGORITHMSLINKED LISTSPERFORMANCE
