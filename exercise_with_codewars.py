246bedc4a46263f219f1e4791c01dfff27538e48
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-24 18:05:09 +0300

    Complete task 1935 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -27301,7 +27301,7 @@ class Solution(object):
             if check(i):
                 l.append(i)
         return l
-
+    
 # From A to Z
 # Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter. Note that if the range is given in capital letters, return the string in capitals also!
 #
@@ -27314,3 +27314,7 @@ class Solution(object):
 # A hyphen will separate the two letters in the string.
 # You don't need to worry about error handling in this one (i.e. both letters will be the same case and the second letter will always be after the first alphabetically).
 # ALGORITHMSSTRINGS
+def gimme_the_letters(sp):
+    al = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    a, b = sp.split('-')
+    return al[al.index(a): al.index(b) + 1]
