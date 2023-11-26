d73a80ff183ceb2bad7c424ddb2afee5f1b19cd1
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-26 09:30:30 +0300

    Added task 1938 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -27394,3 +27394,26 @@ def calculator(distance, bus_drive, bus_walk):
     elif by_walk <= 2:
         return 'Bus' if by_bus < by_walk else 'Walk'
     return 'Bus'
+
+# 1037. Valid Boomerang
+# Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
+#
+# A boomerang is a set of three points that are all distinct and not in a straight line.
+#
+#
+#
+# Example 1:
+#
+# Input: points = [[1,1],[2,3],[3,2]]
+# Output: true
+# Example 2:
+#
+# Input: points = [[1,1],[2,2],[3,3]]
+# Output: false
+#
+#
+# Constraints:
+#
+# points.length == 3
+# points[i].length == 2
+# 0 <= xi, yi <= 100
