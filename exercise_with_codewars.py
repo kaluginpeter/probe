14bd7df68c24a44b4524b1ca58d9719e81aac8bd
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-27 18:17:02 +0300

    Added task 1940 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -27459,3 +27459,22 @@ def grid(N):
         top = [' '.join(al[i: i + N])]
         word.append(' '.join(top))
     return '\n'.join(word)
+
+# 1556. Thousand Separator
+# Given an integer n, add a dot (".") as the thousands separator and return it in string format.
+#
+#
+#
+# Example 1:
+#
+# Input: n = 987
+# Output: "987"
+# Example 2:
+#
+# Input: n = 1234
+# Output: "1.234"
+#
+#
+# Constraints:
+#
+# 0 <= n <= 231 - 1
