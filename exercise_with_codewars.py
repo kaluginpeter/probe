71235db5fc26f5fe2852f82ae62d20f8ee7e3d8f
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-27 18:17:51 +0300

    Complete task 1940 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -27478,3 +27478,17 @@ def grid(N):
 # Constraints:
 #
 # 0 <= n <= 231 - 1
+class Solution(object):
+    def thousandSeparator(self, n):
+        if n == 0:
+            return '0'
+        top, count = '', 0
+        while n:
+            top = str(n % 10) + top
+            count += 1
+            n //= 10
+            if n:
+                if count == 3:
+                    top = '.' + top
+                    count = 0
+        return top
