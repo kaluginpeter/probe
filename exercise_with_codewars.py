729a59c00bf98d2396257ef254b4e70218a8a3c2
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-28 20:01:42 +0300

    Complete task 1942 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -27555,3 +27555,9 @@ def to_day_of_year(date):
 #
 #
 # Note: This question is the same as 1991: https://leetcode.com/problems/find-the-middle-index-in-array/
+class Solution(object):
+    def pivotIndex(self, nums):
+        for i in range(len(nums)):
+            if sum(nums[:i]) == sum(nums[i+1:]):
+                return i
+        return -1
