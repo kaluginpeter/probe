6941e4e0ce097f9f91b473c322d037b8a2a8fa21
Peter Kalugin <streetworkout200412@gmail.com>
2023-11-29 06:40:35 +0300

    Added task 1944 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -27616,3 +27616,14 @@ def solution(mtrx):
         if '>' in i and 'x' in i:
             return i.index('>') < i.index('x')
     return False
+    
+# noobCode 03: CHECK THESE LETTERS... see if letters in "String 2" are present in "String 1"
+# Write a function that checks if all the letters in the second string are present in the first one at least once, regardless of how many times they appear:
+#
+# ["ab", "aaa"]    =>  true
+# ["trances", "nectar"]    =>  true
+# ["compadres", "DRAPES"]  =>  true
+# ["parses", "parsecs"]    =>  false
+# Function should not be case sensitive, as indicated in example #2. Note: both strings are presented as a single argument in the form of an array.
+#
+# STRINGSALGORITHMS
