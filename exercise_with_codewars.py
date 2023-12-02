ebd824871f8f6a7509ebe601d5cffc157a0bcc7c
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-02 13:01:01 +0300

    Added task 1950 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -27779,7 +27779,7 @@ class Solution(object):
             for j in i:
                 yield j
         yield None
-
+        
 # Stacked Balls - 2D
 # Background
 # I have stacked some pool balls in a triangle.
@@ -27808,3 +27808,30 @@ def stack_height_2d(layers):
     if layers in {0, 1}:
         return layers
     return 1 + (layers - 1) * 0.8660254
+
+# 1160. Find Words That Can Be Formed by Characters
+# You are given an array of strings words and a string chars.
+#
+# A string is good if it can be formed by characters from chars (each character can only be used once).
+#
+# Return the sum of lengths of all good strings in words.
+#
+#
+#
+# Example 1:
+#
+# Input: words = ["cat","bt","hat","tree"], chars = "atach"
+# Output: 6
+# Explanation: The strings that can be formed are "cat" and "hat" so the answer is 3 + 3 = 6.
+# Example 2:
+#
+# Input: words = ["hello","world","leetcode"], chars = "welldonehoneyr"
+# Output: 10
+# Explanation: The strings that can be formed are "hello" and "world" so the answer is 5 + 5 = 10.
+#
+#
+# Constraints:
+#
+# 1 <= words.length <= 1000
+# 1 <= words[i].length, chars.length <= 100
+# words[i] and chars consist of lowercase English letters.
