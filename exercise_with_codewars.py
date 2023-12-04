5ae4b65377798d030eb82dc2dcb480b27ac4780b
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-04 17:45:45 +0300

    Added task 1954 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -27945,7 +27945,7 @@ class Solution(object):
         for i in range(len(points) - 1):
             count += max(abs(points[i][0] - points[i + 1][0]), abs(points[i][1] - points[i + 1][1]))
         return count
-
+    
 # Cartesian coordinates from degree angle
 # Write a simple function that takes polar coordinates (an angle in degrees and a radius) and returns the equivalent cartesian coordinates (rounded to 10 places).
 #
@@ -27962,3 +27962,39 @@ import math
 def coordinates(degrees, radius):
     grad = degrees * (math.pi / 180)
     return (round(radius * math.cos(grad), 10), round(radius * math.sin(grad), 10))
+
+# 2264. Largest 3-Same-Digit Number in String
+# You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+#
+# It is a substring of num with length 3.
+# It consists of only one unique digit.
+# Return the maximum good integer as a string or an empty string "" if no such integer exists.
+#
+# Note:
+#
+# A substring is a contiguous sequence of characters within a string.
+# There may be leading zeroes in num or a good integer.
+#
+#
+# Example 1:
+#
+# Input: num = "6777133339"
+# Output: "777"
+# Explanation: There are two distinct good integers: "777" and "333".
+# "777" is the largest, so we return "777".
+# Example 2:
+#
+# Input: num = "2300019"
+# Output: "000"
+# Explanation: "000" is the only good integer.
+# Example 3:
+#
+# Input: num = "42352338"
+# Output: ""
+# Explanation: No substring of length 3 consists of only one unique digit. Therefore, there are no good integers.
+#
+#
+# Constraints:
+#
+# 3 <= num.length <= 1000
+# num only consists of digits.
