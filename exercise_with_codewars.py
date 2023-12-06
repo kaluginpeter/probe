bf40c28055ca17007bd40b32995448acc169ba0e
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-06 17:36:22 +0300

    Added task 1959 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -28166,3 +28166,12 @@ class Solution(object):
             return sum((1 + 7 + i * 2) * 7 / 2 for i in range(n // 7))
         x = sum((1 + 7 + i * 2) * 7 / 2 for i in range(n // 7))
         return x + sum(i + n // 7 for i in range(1, n % 7 + 1))
+
+# Lottery machine
+# Your task is to write an update for a lottery machine. Its current version produces a sequence of random letters and integers (passed as a string to the function). Your code must filter out all letters and return unique integers as a string, in their order of first appearance. If there are no integers in the string return "One more run!"
+#
+# Examples
+# "hPrBKWDH8yc6Lt5NQZWQ"  -->  "865"
+# "ynMAisVpHEqpqHBqTrwH"  -->  "One more run!"
+# "555"                   -->  "5"
+# STRINGSFUNDAMENTALS
