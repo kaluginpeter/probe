84e4d11ee9a658badf2560c59f5c781b9cf31961
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-09 10:36:57 +0300

    Complete task 1964 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -28352,3 +28352,12 @@ def seven_wonders_science(compasses, gears, tablets):
 #
 #
 # Follow up: Recursive solution is trivial, could you do it iteratively?
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def inorderTraversal(self, root):
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right) if root else []
