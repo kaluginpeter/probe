0dc3163e85bb27b8cc0240595f22e85a722bbdf9
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-09 10:36:35 +0300

    Added task 1964 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -28324,3 +28324,31 @@ def seven_wonders_science(compasses, gears, tablets):
     s1 = min(compasses, gears, tablets) * 7
     s2 = sum(i * i for i in (compasses, gears, tablets))
     return s1 + s2
+
+# 94. Binary Tree Inorder Traversal
+# Given the root of a binary tree, return the inorder traversal of its nodes' values.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: root = [1,null,2,3]
+# Output: [1,3,2]
+# Example 2:
+#
+# Input: root = []
+# Output: []
+# Example 3:
+#
+# Input: root = [1]
+# Output: [1]
+#
+#
+# Constraints:
+#
+# The number of nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+#
+#
+# Follow up: Recursive solution is trivial, could you do it iteratively?
