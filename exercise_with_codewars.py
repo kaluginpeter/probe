20af02c1cdfae9bdfbf2cd6e2a609e7b7076932d
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-11 17:48:35 +0300

    Added task 1968 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -28473,3 +28473,23 @@ def testit(s):
             n += j == w[-1]
             i = (i + 1) % len(w)
     return n
+
+# 1287. Element Appearing More Than 25% In Sorted Array
+# Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs more than 25% of the time, return that integer.
+#
+#
+#
+# Example 1:
+#
+# Input: arr = [1,2,2,6,6,6,6,7,10]
+# Output: 6
+# Example 2:
+#
+# Input: arr = [1,1]
+# Output: 1
+#
+#
+# Constraints:
+#
+# 1 <= arr.length <= 104
+# 0 <= arr[i] <= 105
