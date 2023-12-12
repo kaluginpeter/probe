30acd272539ed325eebc13131986b6f5ad4c85e4
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-12 17:38:43 +0300

    Added task 1970 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -28519,3 +28519,29 @@ def noonerize(numbers):
     if any(not isinstance(i, int) for i in numbers):
         return 'invalid array'
     return abs(int(str(numbers[1])[0] + str(numbers[0])[1:]) - int(str(numbers[0])[0] + str(numbers[1])[1:]))
+
+
+# 1464. Maximum Product of Two Elements in an Array
+# Given the array of integers nums, you will choose two different indices i and j of that array. Return the maximum value of (nums[i]-1)*(nums[j]-1).
+#
+#
+# Example 1:
+#
+# Input: nums = [3,4,5,2]
+# Output: 12
+# Explanation: If you choose the indices i=1 and j=2 (indexed from 0), you will get the maximum value, that is, (nums[1]-1)*(nums[2]-1) = (4-1)*(5-1) = 3*4 = 12.
+# Example 2:
+#
+# Input: nums = [1,5,4,5]
+# Output: 16
+# Explanation: Choosing the indices i=1 and j=3 (indexed from 0), you will get the maximum value of (5-1)*(5-1) = 16.
+# Example 3:
+#
+# Input: nums = [3,7]
+# Output: 12
+#
+#
+# Constraints:
+#
+# 2 <= nums.length <= 500
+# 1 <= nums[i] <= 10^3
