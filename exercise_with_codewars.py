8c6b210f6251efa9b9f1676799ef856dd22e1551
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-13 18:24:42 +0300

    Added task 1973 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -28668,3 +28668,14 @@ class Solution(object):
                     count += 1
             m += 1
         return count
+        
+# Negative Connotation
+# You will be given a string with sets of characters, (i.e. words), seperated by between one and four spaces (inclusive).
+#
+# Looking at the first letter of each word (case insensitive-"A" and "a" should be treated the same), you need to determine whether it falls into the positive/first half of the alphabet ("a"-"m") or the negative/second half ("n"-"z").
+#
+# Return True/true if there are more (or equal) positive words than negative words, False/false otherwise.
+#
+# "A big brown fox caught a bad rabbit" => True/true
+# "Xylophones can obtain Xenon." => False/false
+# FUNDAMENTALS
