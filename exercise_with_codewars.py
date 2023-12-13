1cc6f1ac7d3e842d54d0b61509d9333a8d38cf87
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-13 18:26:04 +0300

    Complete task 1973 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -28668,7 +28668,7 @@ class Solution(object):
                     count += 1
             m += 1
         return count
-        
+    
 # Negative Connotation
 # You will be given a string with sets of characters, (i.e. words), seperated by between one and four spaces (inclusive).
 #
@@ -28679,3 +28679,7 @@ class Solution(object):
 # "A big brown fox caught a bad rabbit" => True/true
 # "Xylophones can obtain Xenon." => False/false
 # FUNDAMENTALS
+def connotation(strng):
+    en_al_l = 'abcdefghijklmnopqrstuvwxyz'
+    n = [1 if en_al_l.index(i[0].lower()) <= en_al_l.index('m') else 0 for i in strng.split()]
+    return n.count(1) >= n.count(0)
