b97d645e2807096a21f960181183d17d22822e63
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-16 13:54:26 +0300

    Added task 1981 4 kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -29027,7 +29027,7 @@ class Solution(object):
             else:
                 d2[i] += 1
         return d1 == d2
-# Solution 3 - Similar Sorting O(N log(N)) O(N)
+ # Solution 3 - Similar Sorting O(N log(N)) O(N)
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
@@ -29037,3 +29037,31 @@ class Solution(object):
         if len(s) != len(t):
             return False
         return all(s.count(i) == t.count(i) for i in t)
+
+# Explosive Sum
+# How many ways can you make the sum of a number?
+# From wikipedia: https://en.wikipedia.org/wiki/Partition_(number_theory)
+#
+# In number theory and combinatorics, a partition of a positive integer n, also called an integer partition, is a way of writing n as a sum of positive integers. Two sums that differ only in the order of their summands are considered the same partition. If order matters, the sum becomes a composition. For example, 4 can be partitioned in five distinct ways:
+#
+# 4
+# 3 + 1
+# 2 + 2
+# 2 + 1 + 1
+# 1 + 1 + 1 + 1
+# Examples
+# Basic
+# exp_sum(1) # 1
+# exp_sum(2) # 2  -> 1+1 , 2
+# exp_sum(3) # 3 -> 1+1+1, 1+2, 3
+# exp_sum(4) # 5 -> 1+1+1+1, 1+1+2, 1+3, 2+2, 4
+# exp_sum(5) # 7 -> 1+1+1+1+1, 1+1+1+2, 1+1+3, 1+2+2, 1+4, 5, 2+3
+#
+# exp_sum(10) # 42
+# Explosive
+# exp_sum(50) # 204226
+# exp_sum(80) # 15796476
+# exp_sum(100) # 190569292
+# See here for more examples.
+#
+# ALGORITHMSMATHEMATICSFUNDAMENTALS
