3f77112d31692855443d5de0060d5944c95fac0e
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-18 19:15:06 +0300

    Complete task 1985 4kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -29244,7 +29244,7 @@ class Solution(object):
             elif z >= nums[i]:
                 z = nums[i]
         return (w * x) - (y * z)
-
+    
 # Hamming Numbers
 # A Hamming number is a positive integer of the form 2i3j5k, for some non-negative integers i, j, and k.
 #
@@ -29262,3 +29262,16 @@ class Solution(object):
 # Your code should be able to compute the first 5 000 ( LC: 400, Clojure: 2 000, Haskell: 12 691, NASM, C, D, C++, Go and Rust: 13 282 ) Hamming numbers without timing out.
 #
 # NUMBER THEORYALGORITHMS
+def hamming(n):
+    l = [1]
+    n2, n3, n5 = 0, 0, 0
+    for i in range(1, n):
+        mn = min(l[n2] * 2, l[n3] * 3, l[n5] * 5)
+        if 2 * l[n2] == mn:
+            n2 += 1
+        if 3 * l[n3] == mn:
+            n3 += 1
+        if 5 * l[n5] == mn:
+            n5 += 1
+        l.append(mn)
+    return l[-1]
