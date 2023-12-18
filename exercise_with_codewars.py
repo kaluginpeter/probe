634ce891498089bbd4ea821934608e094b1e89ba
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-18 18:31:48 +0300

    Added task 1984 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -29158,7 +29158,7 @@ class FoodRatings(object):
 
     def highestRated(self, cuisine):
         return self.csn_dct[cuisine][0][1]
-
+    
 # Common Denominators
 # Common denominators
 #
@@ -29200,3 +29200,32 @@ def convertFracts(lst):
     for _, den in lst:
         D *= den // math.gcd(den, D)
     return [[D*n//d, D] for n, d in lst]
+
+# 1913. Maximum Product Difference Between Two Pairs
+# The product difference between two pairs (a, b) and (c, d) is defined as (a * b) - (c * d).
+#
+# For example, the product difference between (5, 6) and (2, 7) is (5 * 6) - (2 * 7) = 16.
+# Given an integer array nums, choose four distinct indices w, x, y, and z such that the product difference between pairs (nums[w], nums[x]) and (nums[y], nums[z]) is maximized.
+#
+# Return the maximum such product difference.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [5,6,2,7,4]
+# Output: 34
+# Explanation: We can choose indices 1 and 3 for the first pair (6, 7) and indices 2 and 4 for the second pair (2, 4).
+# The product difference is (6 * 7) - (2 * 4) = 34.
+# Example 2:
+#
+# Input: nums = [4,2,5,9,7,4,8]
+# Output: 64
+# Explanation: We can choose indices 3 and 6 for the first pair (9, 8) and indices 1 and 5 for the second pair (2, 4).
+# The product difference is (9 * 8) - (2 * 4) = 64.
+#
+#
+# Constraints:
+#
+# 4 <= nums.length <= 104
+# 1 <= nums[i] <= 104
