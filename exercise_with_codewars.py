5f4bb2fc0b6bab5eebd7c1a77621465a17f912ca
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-22 19:09:02 +0300

    Added task 1993 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -29629,3 +29629,8 @@ class Solution:
                 count_s -= 1
             total = max(total, count_s)
         return total
+
+# Least Common Multiple
+# Write a function that calculates the least common multiple of its arguments; each argument is assumed to be a non-negative integer. In the case that there are no arguments (or the provided array in compiled languages is empty), return 1. If any argument is 0, return 0.
+#
+# MATHEMATICSALGORITHMS
