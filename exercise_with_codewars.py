572fa2e9cdc0f6c28a45d24c4c19eaf92f45bc4e
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-25 21:58:44 +0300

    Complete task 2004 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -30161,13 +30161,13 @@ class Solution:
 # Space complexity: O(1)
 #
 # Code
-# class Solution:
-#     def largestPerimeter(self, nums: List[int]) -> int:
-#         nums.sort()
-#         l: int = -1
-#         s: int = sum(nums[:2])
-#         for i in range(2, len(nums)):
-#             if nums[i] < s:
-#                 l = nums[i] + s
-#             s += nums[i]
-#         return l
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort()
+        l: int = -1
+        s: int = sum(nums[:2])
+        for i in range(2, len(nums)):
+            if nums[i] < s:
+                l = nums[i] + s
+            s += nums[i]
+        return l
