65008c695833acb223aeab1a38784e9b3d7e80e6
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-29 17:21:01 +0300

    Added task 2011 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -30409,7 +30409,7 @@ class Solution:
                 keep = dp(i + 1, s[i], 1, k) + 1
             return min(delete, keep)
         return dp(0, '', 0, k)
-
+    
 # Square Matrix Multiplication
 # Write a function that accepts two square (NxN) matrices (two dimensional arrays), and returns the product of the two. Only square matrices will be given.
 #
@@ -30443,3 +30443,42 @@ def matrix_mult(a, b):
             rows.append(x)
         mt.append(rows)
     return mt
+
+# 2006. Count Number of Pairs With Absolute Difference K
+# Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+# 
+# The value of |x| is defined as:
+# 
+# x if x >= 0.
+# -x if x < 0.
+#  
+# 
+# Example 1:
+# 
+# Input: nums = [1,2,2,1], k = 1
+# Output: 4
+# Explanation: The pairs with an absolute difference of 1 are:
+# - [1,2,2,1]
+# - [1,2,2,1]
+# - [1,2,2,1]
+# - [1,2,2,1]
+# Example 2:
+# 
+# Input: nums = [1,3], k = 3
+# Output: 0
+# Explanation: There are no pairs with an absolute difference of 3.
+# Example 3:
+# 
+# Input: nums = [3,2,1,5,4], k = 2
+# Output: 3
+# Explanation: The pairs with an absolute difference of 2 are:
+# - [3,2,1,5,4]
+# - [3,2,1,5,4]
+# - [3,2,1,5,4]
+#  
+# 
+# Constraints:
+# 
+# 1 <= nums.length <= 200
+# 1 <= nums[i] <= 100
+# 1 <= k <= 99
