e73266766cf1538c1844afd1d35985152df4b650
Peter Kalugin <streetworkout200412@gmail.com>
2023-12-30 13:22:45 +0300

    Added task 2013 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -30548,3 +30548,32 @@ def lcs(s1, s2):
             else:
                 matrix[i][j] = max(matrix[i-1][j], matrix[i][j-1], key=len)
     return matrix[-1][-1]
+
+# 1897. Redistribute Characters to Make All Strings Equal
+# You are given an array of strings words (0-indexed).
+#
+# In one operation, pick two distinct indices i and j, where words[i] is a non-empty string, and move any character from words[i] to any position in words[j].
+#
+# Return true if you can make every string in words equal using any number of operations, and false otherwise.
+#
+#
+#
+# Example 1:
+#
+# Input: words = ["abc","aabc","bc"]
+# Output: true
+# Explanation: Move the first 'a' in words[1] to the front of words[2],
+# to make words[1] = "abc" and words[2] = "abc".
+# All the strings are now equal to "abc", so return true.
+# Example 2:
+#
+# Input: words = ["ab","a"]
+# Output: false
+# Explanation: It is impossible to make all the strings equal using the operation.
+#
+#
+# Constraints:
+#
+# 1 <= words.length <= 100
+# 1 <= words[i].length <= 100
+# words[i] consists of lowercase English letters.
