398895a88e5ad2b2810c1852cd35b3192f043d30
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-02 19:04:39 +0300

    Added task 2025 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -31028,3 +31028,22 @@ def quicksum(packet):
         if packet[i].isalpha():
             s += (ord(packet[i]) - 64) * (i + 1)
     return s
+
+# Case-sensitive!
+# Your task is very simple. Given an input string s, case_sensitive(s), check whether all letters are lowercase or not. Return True/False and a list of all the entries that are not lowercase in order of their appearance in s.
+#
+# For example, case_sensitive('codewars') returns [True, []], but case_sensitive('codeWaRs') returns [False, ['W', 'R']].
+#
+# Goodluck :)
+#
+# Have a look at my other katas!
+#
+# Alphabetically ordered
+#
+# Find Nearest square number
+#
+# Not prime numbers
+#
+# Find your caterer
+#
+# STRINGSFUNDAMENTALS
