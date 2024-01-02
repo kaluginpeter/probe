7a4c167adf6381798d58fd2fec0d66faf0a173f9
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-02 19:07:23 +0300

    Added task 2026 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -31055,3 +31055,12 @@ def case_sensitive(s):
             flag = not flag
             ans += i
     return [flag, ans]
+
+# Acrostic reader
+# An acrostic is a text in which the first letter of each line spells out a word. It is also a quick and cheap way of writing a poem for somebody, as exemplified below :
+#
+#
+#
+# Write a program that reads an acrostic to identify the "hidden" word. Specifically, your program will receive a list of words (reprensenting an acrostic) and will need to return a string corresponding to the word that is spelled out by taking the first letter of each word in the acrostic.
+#
+# STRINGSFUNDAMENTALS
