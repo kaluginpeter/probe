4ff683e6b5d5c1683253760c10319e62d58ccabb
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-04 17:17:51 +0300

    Complete task 2038 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -31471,7 +31471,7 @@ class Solution:
             if nums[i] + nums[i-1] > nums[i-2]:
                 return nums[i-2] + nums[i-1] + nums[i]
         return 0
-
+    
 # 43. Multiply Strings
 # Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
 #
@@ -31494,3 +31494,22 @@ class Solution:
 # 1 <= num1.length, num2.length <= 200
 # num1 and num2 consist of digits only.
 # Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1 == '0' or num2 == '0':
+            return '0'
+        d: dict = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
+        '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
+        r_d: dict = {0: '0', 1: '1', 2: '2', 3: '3', 4: '4',
+        5: '5', 6: '6', 7: '7', 8: '8', 9: '9'}
+        x1, x2 = 0, 0
+        for i in num1:
+            x1 = x1 * 10 + d[i]
+        for i in num2:
+            x2 = x2 * 10 + d[i]
+        x3 = x1 * x2
+        ans: str = ''
+        while x3:
+            ans += r_d[x3 % 10]
+            x3 //= 10
+        return ans[::-1]
