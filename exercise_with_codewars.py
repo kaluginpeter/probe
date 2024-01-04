1754bbbd3de79f4aa4cd09ba2e5a3eee8f5ccb32
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-04 14:40:49 +0300

    Added task 2036 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -31421,3 +31421,13 @@ class Solution:
                 if d[i] % 2 == 0:
                     count += d[i] // 2
         return count
+
+# Schrödinger's Boolean
+# Can a value be both True and False?
+#
+# Define omnibool so that it returns True for the following:
+#
+# omnibool == True and omnibool == False
+# If you enjoyed this kata, be sure to check out my other katas.
+#
+# LANGUAGE FEATURESMETAPROGRAMMINGPUZZLES
