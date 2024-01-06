4c5599f31fdb9ca00e4806c35bde5777d2a23f15
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-06 11:47:42 +0300

    Complete task 2043 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -31664,7 +31664,7 @@ def order_type(arr):
 class Solution:
     def minPartitions(self, n: str) -> int:
         return int(max(n))
-        
+    
 # Mix Fruit Juice
 # Story
 # Jumbo Juice makes a fresh juice out of fruits of your choice.Jumbo Juice charges $5 for regular fruits and $7 for special ones. Regular fruits are Banana, Orange, Apple, Lemon and Grapes. Special ones are Avocado, Strawberry and Mango. Others fruits that are not listed are also available upon request. Those extra special fruits cost $9 per each. There is no limit on how many fruits she/he picks.The price of a cup of juice is the mean of price of chosen fruits. In case of decimal number (ex. $5.99), output should be the nearest integer (use the standard rounding function of your language of choice).
@@ -31675,3 +31675,16 @@ class Solution:
 # Example
 # ['Mango', 'Banana', 'Avocado'] //the price of this juice bottle is (7+5+7)/3 = $6($6.333333...)
 # FUNDAMENTALSARRAYS
+def mix_fruit(arr):
+    basic: set = {'banana', 'orange', 'apple', 'lemon', 'grapes'}
+    special: set = {'avocado', 'strawberry', 'mango'}
+    count: int = 0
+    for i in arr:
+        i = i.lower()
+        if i in basic:
+            count += 5
+        elif i in special:
+            count += 7
+        else:
+            count += 9
+    return round(count / len(arr))
