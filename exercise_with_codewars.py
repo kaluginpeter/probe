004b942356fff73dd40ba4a29f51d982f5ea8bb3
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-06 11:37:29 +0300

    Added task 2042 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -31600,7 +31600,7 @@ class Solution:
                 if nums[i] > nums[j] and dp[i] < dp[j] + 1:
                     dp[i] = dp[j] + 1
         return max(dp)
-
+    
 # Identify the array's ordering
 # Write a function that takes a single array as an argument (containing multiple strings and/or positive numbers and/or arrays), and returns one of four possible string values, depending on the ordering of the lengths of the elements in the input array:
 #
@@ -31633,3 +31633,31 @@ def order_type(arr):
     elif all(x >= y for x, y in zip(l, l[1:])):
         return 'Decreasing'
     return 'Unsorted'
+
+# 1689. Partitioning Into Minimum Number Of Deci-Binary Numbers
+# A decimal number is called deci-binary if each of its digits is either 0 or 1 without any leading zeros. For example, 101 and 1100 are deci-binary, while 112 and 3001 are not.
+#
+# Given a string n that represents a positive decimal integer, return the minimum number of positive deci-binary numbers needed so that they sum up to n.
+#
+#
+#
+# Example 1:
+#
+# Input: n = "32"
+# Output: 3
+# Explanation: 10 + 11 + 11 = 32
+# Example 2:
+#
+# Input: n = "82734"
+# Output: 8
+# Example 3:
+#
+# Input: n = "27346209830709182346"
+# Output: 9
+#
+#
+# Constraints:
+#
+# 1 <= n.length <= 105
+# n consists of only digits.
+# n does not contain any leading zeros and represents a positive integer.
