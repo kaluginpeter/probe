f8f407f72aea90ae5fb6bdd180039973aee665b9
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-08 05:43:49 +0300

    Added task 2050 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -31928,7 +31928,7 @@ class Solution:
     def differenceOfSums(self, n: int, m: int) -> int:
         k = n // m
         return n * (n + 1) // 2 - (k + 1) * k * m
-
+    
 # Arrays and hex color codes
 # Given an array with 3 subarrays, each containing hexadecimal color codes loosely defining red, green and blue colors based on their predominant byte value, return a string description of which of the three colors each array contains.
 #
@@ -31965,3 +31965,35 @@ def get_colors(col_arr):
                 d['Blue'] = d.get('Blue', 0) + 1
         l.append(f"{max(d, key=d.get)}+{min(d, key=d.get)}")
     return ','.join(l)
+
+# 2574. Left and Right Sum Differences
+# Given a 0-indexed integer array nums, find a 0-indexed integer array answer where:
+#
+# answer.length == nums.length.
+# answer[i] = |leftSum[i] - rightSum[i]|.
+# Where:
+#
+# leftSum[i] is the sum of elements to the left of the index i in the array nums. If there is no such element, leftSum[i] = 0.
+# rightSum[i] is the sum of elements to the right of the index i in the array nums. If there is no such element, rightSum[i] = 0.
+# Return the array answer.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [10,4,8,3]
+# Output: [15,1,11,22]
+# Explanation: The array leftSum is [0,10,14,22] and the array rightSum is [15,11,3,0].
+# The array answer is [|0 - 15|,|10 - 11|,|14 - 3|,|22 - 0|] = [15,1,11,22].
+# Example 2:
+#
+# Input: nums = [1]
+# Output: [0]
+# Explanation: The array leftSum is [0] and the array rightSum is [0].
+# The array answer is [|0 - 0|] = [0].
+#
+#
+# Constraints:
+#
+# 1 <= nums.length <= 1000
+# 1 <= nums[i] <= 105
