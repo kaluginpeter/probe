b8e48492db86e61cec980c829dc55aac14e97388
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-12 06:29:12 +0300

    Added task 2060 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -32373,3 +32373,21 @@ class Solution:
             if s[i + n] in vowels:
                 count -= 1
         return count == 0
+
+# Are all elements equal? (Infinity version)
+# Task
+# Create a function eq_all that determines if all elements of any iterable are equal; the iterable may be infinite. Return value is a bool.
+#
+# Examples
+# eq_all('aaa')   : True
+# eq_all('abc')   : False
+# eq_all('')      : True
+#
+# eq_all([0,0,0]) : True
+# eq_all([0,1,2]) : False
+# eq_all([])      : True
+# Notes
+# For the function result to be True, the iterable must be finite; False, however, can result from an element finitely far from the left end. There will be no tests with infinite series of equal elements.
+# Elements will be primitive values.
+#
+# FUNDAMENTALS
