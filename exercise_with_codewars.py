573da5ea0ae855c172029d9fb59a850b65159695
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-12 06:20:49 +0300

    Added task 2059 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -32310,7 +32310,7 @@ class Solution:
             if d[i] > top:
                 v_top, top = i, d[i]
         return v_top
-
+    
 # Fixed xor
 # Fixed xor
 # Write a function that takes two hex strings as input and XORs them against each other. If the strings are different lengths the output should be the length of the shortest string.
@@ -32333,3 +32333,31 @@ class Solution:
 # ALGORITHMSMATHEMATICSLOGICALGEBRABINARYCRYPTOGRAPHY
 def fixed_xor(a, b):
     return ''.join(f'{int(x, 16) ^ int(y, 16):x}' for x, y in zip(a, b))
+
+# 1704. Determine if String Halves Are Alike
+# You are given a string s of even length. Split this string into two halves of equal lengths, and let a be the first half and b be the second half.
+#
+# Two strings are alike if they have the same number of vowels ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'). Notice that s contains uppercase and lowercase letters.
+#
+# Return true if a and b are alike. Otherwise, return false.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "book"
+# Output: true
+# Explanation: a = "bo" and b = "ok". a has 1 vowel and b has 1 vowel. Therefore, they are alike.
+# Example 2:
+#
+# Input: s = "textbook"
+# Output: false
+# Explanation: a = "text" and b = "book". a has 1 vowel whereas b has 2. Therefore, they are not alike.
+# Notice that the vowel o is counted twice.
+#
+#
+# Constraints:
+#
+# 2 <= s.length <= 1000
+# s.length is even.
+# s consists of uppercase and lowercase letters.
