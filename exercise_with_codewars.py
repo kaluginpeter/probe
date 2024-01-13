c683fdce1092bcd2c35c241e4fb114aaa1eee4b0
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-13 08:39:49 +0300

    Added task 2061 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -32373,7 +32373,7 @@ class Solution:
             if s[i + n] in vowels:
                 count -= 1
         return count == 0
-
+    
 # Are all elements equal? (Infinity version)
 # Task
 # Create a function eq_all that determines if all elements of any iterable are equal; the iterable may be infinite. Return value is a bool.
@@ -32401,3 +32401,35 @@ def eq_all(iterable):
         if len(d) > 1:
             return False
     return True
+
+# 1347. Minimum Number of Steps to Make Two Strings Anagram
+# You are given two strings of the same length s and t. In one step you can choose any character of t and replace it with another character.
+#
+# Return the minimum number of steps to make t an anagram of s.
+#
+# An Anagram of a string is a string that contains the same characters with a different (or the same) ordering.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "bab", t = "aba"
+# Output: 1
+# Explanation: Replace the first 'a' in t with b, t = "bba" which is anagram of s.
+# Example 2:
+#
+# Input: s = "leetcode", t = "practice"
+# Output: 5
+# Explanation: Replace 'p', 'r', 'a', 'i' and 'c' from t with proper characters to make t anagram of s.
+# Example 3:
+#
+# Input: s = "anagram", t = "mangaar"
+# Output: 0
+# Explanation: "anagram" and "mangaar" are anagrams.
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 5 * 104
+# s.length == t.length
+# s and t consist of lowercase English letters only.
