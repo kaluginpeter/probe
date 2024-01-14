1fd624e256023b63125ff7507545e2e0045fdd4e
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-14 07:42:21 +0300

    Added task 2066 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -32625,3 +32625,17 @@ class Solution:
                 mx = (i[0]**2 + i[1]**2)**.5
                 area = max(i[0] * i[1], area)
         return area
+        
+# Guess and convert
+# In this kata, you have to define a function named func that will take a list as input.
+#
+# You must try and guess the pattern how we get the output number and return list - [output number,binary representation,octal representation,hexadecimal representation], but you must convert that specific number without built-in : bin,oct and hex functions.
+#
+# Examples :
+#
+# func([12,13,6,3,6,45,123]) returns - [29,'11101','35','1d']
+#
+# func([1,9,23,43,65,31,63,99]) returns - [41,'101001','51','29']
+#
+# func([2,4,6,8,10,12,14,16,18,19]) returns - [10,'1010','12','a']
+# PUZZLES
