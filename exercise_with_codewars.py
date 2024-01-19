bd6f5f4f7a31d3c4330881f309677ac9b2a1b2a4
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-19 06:38:17 +0300

    Complete task 2075 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -33088,7 +33088,7 @@ class Solution:
         else:
             seq = 0
     return words[top]
-    
+ 
 # 7. Reverse Integer
 # Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 #
@@ -33113,3 +33113,15 @@ class Solution:
 # Constraints:
 #
 # -231 <= x <= 231 - 1
+class Solution:
+    def reverse(self, x: int) -> int:
+        flag: bool = x < 0
+        if flag:
+            x *= -1
+        ans: int = 0
+        while x:
+            ans = ans * 10 + (x % 10)
+            x //= 10
+        if ans < -2**31 or ans > 2**31-1:
+            return 0
+        return -ans if flag else ans
