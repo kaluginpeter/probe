a3959f7cdb8bb17f1a848108257ea319cdfd173a
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-19 17:43:25 +0300

    Complete task 2076 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -33125,7 +33125,7 @@ class Solution:
         if ans < -2**31 or ans > 2**31-1:
             return 0
         return -ans if flag else ans
-
+    
 # LOTTO 6 aus 49 - 6 of 49
 # In Germany we have "LOTTO 6 aus 49". That means that 6 of 49 numbers are drawn as winning combination.
 # There is also a "Superzahl", an additional number, which can increase your winning category.
@@ -33165,3 +33165,17 @@ class Solution:
 # I have created other katas. Have a look if you like coding and challenges.
 #
 # MATHEMATICSALGORITHMS
+import random
+def number_generator():
+    ans: list = [random.randint(1, 49) for i in range(6)]
+    return sorted(ans) + [random.randint(0, 9)]
+
+def check_for_winning_category(your_numbers, winning_numbers):
+    count: int = sum(i in winning_numbers[:6] for i in your_numbers[:6])
+    top: int = your_numbers[-1] == winning_numbers[-1]
+    if count == 6: return 1 if top else 2
+    elif count == 5: return 3 if top else 4
+    elif count == 4: return 5 if top else 6
+    elif count == 3: return 7 if top else 8
+    elif count == 2: return 9 if top  else -1
+    return -1
