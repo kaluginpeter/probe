dea6118db29c09874ea2d6287a604442d7f76843
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-22 06:30:34 +0300

    Complete task 2081 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -33417,7 +33417,7 @@ def self_converge(n):
         x = convert(x, flag=True, zeroes=zeroes) - convert(x)
 
     return count
-    
+
 # 645. Set Mismatch
 # You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
 #
@@ -33441,3 +33441,30 @@ def self_converge(n):
 #
 # 2 <= nums.length <= 104
 # 1 <= nums[i] <= 104
+# Solution
+class Solution(object):
+    def findErrorNums(self, nums):
+        return [sum(nums) - sum(set(nums)), list(set(range(1, len(nums) + 1)) - set(nums))[0]]
+
+
+# Solution 2 Math O(N) O(1)
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        l, x, y = len(nums), sum(nums), sum(set(nums))
+        k = (l * (l + 1)) // 2
+        return [x-y, k-y]
+# Solution 3 HashTable O(N) O(N)
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        ht: dict = {}
+        for i in range(1, len(nums) + 1):
+            ht[i] = 0
+        for i in nums:
+            ht[i] += 1
+        missing, dub = 0, 0
+        for i in ht:
+            if ht[i] == 0:
+                missing = i
+            elif ht[i] > 1:
+                dub = i
+        return [dub, missing]
