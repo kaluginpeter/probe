7a53631f2b1de7525005a0e283e154ab0b7cc835
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-25 06:23:52 +0300

    Complete task 2087 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -33655,3 +33655,13 @@ def telephone_words(digit_string):
 #
 # 1 <= text1.length, text2.length <= 1000
 # text1 and text2 consist of only lowercase English characters.
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        F: list = [[0] * (len(text2) + 1) for i in range((len(text1) + 1))]
+        for i in range(1, len(text1) + 1):
+            for j in range(1, len(text2) + 1):
+                if text1[i-1] == text2[j-1]:
+                    F[i][j] = 1 + F[i-1][j-1]
+                else:
+                    F[i][j] = max(F[i-1][j], F[i][j-1])
+        return F[-1][-1]
