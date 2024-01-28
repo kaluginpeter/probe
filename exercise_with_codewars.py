0ee09375ff8f05372317f1f60c20ee5b06ce3f07
Peter Kalugin <streetworkout200412@gmail.com>
2024-01-28 07:57:35 +0300

    Complete task 2096 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -34053,3 +34053,14 @@ class Solution:
 # 1 <= word.length <= 26
 # word consists of lowercase English letters.
 # All letters in word are distinct.
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        count: int = 0
+        top: int = 1
+        n: int = 0
+        for i in range(len(word)):
+            count += top
+            n += 1
+            if n == 8:
+                n, top = 0, top + 1
+        return count
