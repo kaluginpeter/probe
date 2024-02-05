25f8e017db1cf05a1d65e98be93783613dd96f3f
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-05 06:23:40 +0300

    Added task 2028 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -34972,3 +34972,72 @@ class NestedIterator:
 # Your NestedIterator object will be instantiated and called as such:
 # i, v = NestedIterator(nestedList), []
 # while i.hasNext(): v.append(i.next())
+
+# By the Power Set of Castle Grayskull
+# Write a function that returns all of the sublists of a list/array.
+#
+# Example:
+#
+# �
+# �
+# �
+# �
+# �
+# (
+# [
+# 1
+# ,
+# 2
+# ,
+# 3
+# ]
+# )
+# ;
+# =
+# >
+# [
+# [
+# ]
+# ,
+# [
+# 1
+# ]
+# ,
+# [
+# 2
+# ]
+# ,
+# [
+# 1
+# ,
+# 2
+# ]
+# ,
+# [
+# 3
+# ]
+# ,
+# [
+# 1
+# ,
+# 3
+# ]
+# ,
+# [
+# 2
+# ,
+# 3
+# ]
+# ,
+# [
+# 1
+# ,
+# 2
+# ,
+# 3
+# ]
+# ]
+# power([1,2,3]);=>[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+# For more details regarding this, see the power set entry in wikipedia.
+#
+# MATHEMATICSALGORITHMS
