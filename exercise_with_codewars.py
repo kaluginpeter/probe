d46db182c2c8bc7c8760169e6ac8339fe0354b92
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-11 10:59:05 +0300

    Complete task 2040 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -35616,3 +35616,19 @@ class Solution:
 #   [1, 4, 7]
 # ]
 # MATRIXARRAYSALGORITHMS
+def rotate(matrix, direction):
+    mtrx: list = []
+    if direction == 'clockwise':
+        for column in range(len(matrix[0])):
+            top: list = []
+            for row in range(len(matrix) - 1, -1, -1):
+                top.append(matrix[row][column])
+            mtrx.append(top)
+        return mtrx
+    else:
+        for column in range(len(matrix[0]) - 1, -1, -1):
+            top: list = []
+            for row in range(len(matrix)):
+                top.append(matrix[row][column])
+            mtrx.append(top)
+        return mtrx
