1be63b451b43d7e7e04d1399a17d0e507f8af414
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-14 20:51:55 +0300

    Complete task 2047 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -35894,3 +35894,13 @@ class Solution:
 #
 # Hope you have fun! :-)
 # STRINGSARRAYSFUNDAMENTALS
+def reverse_and_combine_text(text):
+    ans: list = text.split()
+    while len(ans) > 1:
+        top: list = []
+        for i in range(1, len(ans), 2):
+            top.append(ans[i-1][::-1] + ans[i][::-1])
+        if len(ans) % 2 != 0:
+            top.append(ans[-1][::-1])
+        ans = top
+    return ''.join(ans)
