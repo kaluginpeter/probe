e28d4c65291ee2a3ca67bc85d7b131efe64c0a1b
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-16 22:54:48 +0300

    Added task 2053 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -36113,7 +36113,7 @@ class Solution:
             if k == 0:
                 break
         return len(ht)
-
+    
 # Versions manager
 # In this kata we are going to mimic a software versioning system.
 #
@@ -36166,3 +36166,38 @@ class VersionManager:
         return self
     def release(self):
         return f"{self.major_}.{self.minor_}.{self.patch_}"
+
+# 705. Design HashSet
+# Design a HashSet without using any built-in hash table libraries.
+#
+# Implement MyHashSet class:
+#
+# void add(key) Inserts the value key into the HashSet.
+# bool contains(key) Returns whether the value key exists in the HashSet or not.
+# void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+#
+#
+# Example 1:
+#
+# Input
+# ["MyHashSet", "add", "add", "contains", "contains", "add", "contains", "remove", "contains"]
+# [[], [1], [2], [1], [3], [2], [2], [2], [2]]
+# Output
+# [null, null, null, true, false, null, true, null, false]
+#
+# Explanation
+# MyHashSet myHashSet = new MyHashSet();
+# myHashSet.add(1);      // set = [1]
+# myHashSet.add(2);      // set = [1, 2]
+# myHashSet.contains(1); // return True
+# myHashSet.contains(3); // return False, (not found)
+# myHashSet.add(2);      // set = [1, 2]
+# myHashSet.contains(2); // return True
+# myHashSet.remove(2);   // set = [1]
+# myHashSet.contains(2); // return False, (already removed)
+#
+#
+# Constraints:
+#
+# 0 <= key <= 106
+# At most 104 calls will be made to add, remove, and contains.
