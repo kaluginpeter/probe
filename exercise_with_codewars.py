1c80836d92097b5ba004471057fde8c226ab0987
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-19 06:24:41 +0300

    Added task 2065 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -36776,7 +36776,7 @@ class Solution:
                 if words[j].startswith(words[i]) and words[j].endswith(words[i]):
                     count += 1
         return count
-        
+
 # High score table
 # You just got done writing a function that calculates the player's final score for your new game, "Flight of the cockatoo".
 #
@@ -36819,3 +36819,33 @@ class HighScoreTable:
     def reset(self):
         self.scores.clear()
         self.size = 0
+
+# 231. Power of Two
+# Given an integer n, return true if it is a power of two. Otherwise, return false.
+#
+# An integer n is a power of two, if there exists an integer x such that n == 2x.
+#
+#
+#
+# Example 1:
+#
+# Input: n = 1
+# Output: true
+# Explanation: 20 = 1
+# Example 2:
+#
+# Input: n = 16
+# Output: true
+# Explanation: 24 = 16
+# Example 3:
+#
+# Input: n = 3
+# Output: false
+#
+#
+# Constraints:
+#
+# -231 <= n <= 231 - 1
+#
+#
+# Follow up: Could you solve it without loops/recursion?
