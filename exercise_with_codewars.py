deb72d8fc1b1036fa0b86f811c600d7f6c686887
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-19 18:16:26 +0300

    Added task 2067 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -36887,3 +36887,26 @@ def last_non_empty_string(strng: str) -> str:
             ans.append(strng[i])
             ht[strng[i]] = -1
     return ''.join(ans[::-1])
+    
+# Elimination Tournament
+# In this Kata you will be given an array (or another language-appropriate collection) representing contestant ranks. You must eliminate contestant in series of rounds comparing consecutive pairs of ranks and store all initial and intermediate results in an array.
+#
+# During one round, the lowest rank of a pair is eliminated while the highest proceeds to the next round. This goes on until one contestant only is left. If the number of contestants is odd, the last one of the current array becomes the first of the next round.
+#
+# At the end of the competition, return the results of all the rounds in the form of array of arrays.
+#
+# Example:
+# input = [9, 5, 4, 7, 6, 3, 8, 2];
+#
+# output = [
+#   [9, 5, 4, 7, 6, 3, 8, 2],  // first round is initial input
+#   [9, 7, 6, 8],              // results of 9 vs 5, 4 vs 7, 6 vs 3, and 8 vs 2
+#   [9, 8],                    // results of 9 vs 7 and 6 vs 8
+#   [9]                        // results of 9 vs 8
+# ];
+# Notes:
+#
+# Array length will alway be >= 2 and <= 100
+# Elements of the array will always be >=1 and <= 100
+# Input must not be altered.
+# ARRAYSFUNDAMENTALSALGORITHMS
