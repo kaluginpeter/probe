48c6ea9915d051af7c501720037675d20ada6a52
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-20 06:28:17 +0300

    Added task 2069 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -36961,3 +36961,21 @@ class Solution:
         for i in nums:
             total -= i
         return total
+
+# X marks the spot!
+# Write a function x(n) that takes in a number n and returns an nxn array with an X in the middle. The X will be represented by 1's and the rest will be 0's.
+# E.g.
+#
+# x(5) ==  [[1, 0, 0, 0, 1],
+#           [0, 1, 0, 1, 0],
+#           [0, 0, 1, 0, 0],
+#           [0, 1, 0, 1, 0],
+#           [1, 0, 0, 0, 1]];
+#
+# x(6) ==  [[1, 0, 0, 0, 0, 1],
+#           [0, 1, 0, 0, 1, 0],
+#           [0, 0, 1, 1, 0, 0],
+#           [0, 0, 1, 1, 0, 0],
+#           [0, 1, 0, 0, 1, 0],
+#           [1, 0, 0, 0, 0, 1]];
+# ARRAYSALGORITHMS
