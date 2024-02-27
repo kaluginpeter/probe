c50b7860bc6d82b19823d55a5cccbdcf3fdd612c
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-27 06:41:51 +0300

    Added task 2084 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -37627,3 +37627,19 @@ class Solution:
             for j in range(i, len(nums)):
                 count += len(set(nums[i:j+1]))**2
         return count
+        
+# Name to Matrix
+# Given a name, turn that name into a perfect square matrix (nested array with the amount of arrays equivalent to the length of each array).
+#
+# You will need to add periods (.) to the end of the name if necessary, to turn it into a matrix.
+#
+# If the name has a length of 0, return "name must be at least one letter"
+#
+# Examples
+# "Bill" ==> [ ["B", "i"],
+#              ["l", "l"] ]
+#
+# "Frank" ==> [ ["F", "r", "a"],
+#               ["n", "k", "."],
+#               [".", ".", "."] ]
+# STRINGSARRAYSALGORITHMSMATRIX
