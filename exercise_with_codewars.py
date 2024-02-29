2b776032f5c2080fec70c8216de5415f2aacf56f
Peter Kalugin <streetworkout200412@gmail.com>
2024-02-29 06:25:39 +0300

    Added task 2086 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -37711,7 +37711,7 @@ class Solution:
             elif i == '*' and boundary % 2 == 0:
                 count += 1
         return count
-
+    
 # Find the Word Pair!
 # Given an array of words and a target compound word, your objective is to find the two words which combine into the target word, returning both words in the order they appear in the array, and their respective indices in the order they combine to form the target word. Words in the array you are given may repeat, but there will only be one unique pair that makes the target compound word. If there is no match found, return null/nil/None.
 #
@@ -37740,3 +37740,53 @@ def compound_match(words, target):
                     x, y = ht[i], ht[j]
                     return [i if x < y else j, i if x > y else j, [ht[i], ht[j]]]
     return
+
+# 2037. Minimum Number of Moves to Seat Everyone
+# There are n seats and n students in a room. You are given an array seats of length n, where seats[i] is the position of the ith seat. You are also given the array students of length n, where students[j] is the position of the jth student.
+#
+# You may perform the following move any number of times:
+#
+# Increase or decrease the position of the ith student by 1 (i.e., moving the ith student from position x to x + 1 or x - 1)
+# Return the minimum number of moves required to move each student to a seat such that no two students are in the same seat.
+#
+# Note that there may be multiple seats or students in the same position at the beginning.
+#
+#
+#
+# Example 1:
+#
+# Input: seats = [3,1,5], students = [2,7,4]
+# Output: 4
+# Explanation: The students are moved as follows:
+# - The first student is moved from from position 2 to position 1 using 1 move.
+# - The second student is moved from from position 7 to position 5 using 2 moves.
+# - The third student is moved from from position 4 to position 3 using 1 move.
+# In total, 1 + 2 + 1 = 4 moves were used.
+# Example 2:
+#
+# Input: seats = [4,1,5,9], students = [1,3,2,6]
+# Output: 7
+# Explanation: The students are moved as follows:
+# - The first student is not moved.
+# - The second student is moved from from position 3 to position 4 using 1 move.
+# - The third student is moved from from position 2 to position 5 using 3 moves.
+# - The fourth student is moved from from position 6 to position 9 using 3 moves.
+# In total, 0 + 1 + 3 + 3 = 7 moves were used.
+# Example 3:
+#
+# Input: seats = [2,2,6,6], students = [1,3,2,6]
+# Output: 4
+# Explanation: Note that there are two seats at position 2 and two seats at position 6.
+# The students are moved as follows:
+# - The first student is moved from from position 1 to position 2 using 1 move.
+# - The second student is moved from from position 3 to position 6 using 3 moves.
+# - The third student is not moved.
+# - The fourth student is not moved.
+# In total, 1 + 3 + 0 + 0 = 4 moves were used.
+#
+#
+# Constraints:
+#
+# n == seats.length == students.length
+# 1 <= n <= 100
+# 1 <= seats[i], students[j] <= 100
