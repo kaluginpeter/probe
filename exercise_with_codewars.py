c10cb526b390baa761117d03e111aba0cc89935c
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-07 06:27:58 +0300

    Complete task 2099 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -38442,3 +38442,15 @@ greet = lambda style, msg: style(msg)
 #
 # The number of nodes in the list is in the range [1, 100].
 # 1 <= Node.val <= 100
+# Solution
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow, fast = slow.next, fast.next.next
+        return slow
