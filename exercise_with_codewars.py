2fd9e51fe4ed568829c5f03dc333ce7855fd8f54
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-09 09:37:34 +0300

    Complete task 2103 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -38662,3 +38662,14 @@ def damaged_or_sunk(board, attacks):
 # 1 <= nums1.length, nums2.length <= 105
 # 1 <= nums1[i], nums2[j] <= 109
 # Both nums1 and nums2 are sorted in non-decreasing order.
+class Solution:
+    def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
+        left, right = 0, 0
+        while left < len(nums1) and right < len(nums2):
+            if nums1[left] == nums2[right]:
+                return nums1[left]
+            elif nums1[left] > nums2[right]:
+                right += 1
+            else:
+                left += 1
+        return -1
