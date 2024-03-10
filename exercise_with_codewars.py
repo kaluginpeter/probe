ec09b905f02ab0e81c80223d9102e75627d95e3a
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-10 06:39:53 +0300

    Added task 2105 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -38673,7 +38673,7 @@ class Solution:
             else:
                 left += 1
         return -1
-
+    
 # Design a Simple Automaton (Finite State Machine)
 # Create a finite automaton that has three states. Finite automatons are the same as finite state machines for our purposes.
 #
@@ -38734,3 +38734,24 @@ my_automaton = Automaton()
 my_automaton.states.append('q1')
 my_automaton.states.append('q2')
 my_automaton.states.append('q3')
+
+# 349. Intersection of Two Arrays
+# Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+#
+#
+#
+# Example 1:
+#
+# Input: nums1 = [1,2,2,1], nums2 = [2,2]
+# Output: [2]
+# Example 2:
+#
+# Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+# Output: [9,4]
+# Explanation: [4,9] is also accepted.
+#
+#
+# Constraints:
+#
+# 1 <= nums1.length, nums2.length <= 1000
+# 0 <= nums1[i], nums2[i] <= 1000
