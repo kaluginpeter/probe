ee8d19ee5a5781bdf56adc5902ae0ba89dd595b6
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-14 06:42:54 +0300

    Added task 2120 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -39414,7 +39414,7 @@ def sort_sequence(sequence):
         i += [0]
         ans.extend(i)
     return ans
-    
+
 # 930. Binary Subarrays With Sum
 # Given a binary array nums and an integer goal, return the number of non-empty subarrays with a sum goal.
 #
@@ -39452,3 +39452,22 @@ class Solution:
             count += ht.get(prefix_sum - goal, 0)
             ht[prefix_sum] = ht.get(prefix_sum, 0) + 1
         return count
+        
+# packing rectangles
+# Problem statement
+# Packing multiple rectangles of varying widths and heights in an enclosing rectangle of minimum area. Given 3 rectangular boxes, find minimal area that they can be placed. Boxes can not overlap, these can only be placed on the floor.
+#
+# Input
+# 3 pairs of numbers come to the input - the lengths(a1, b1, a2, b2, a3, b3) of the sides of the boxes. (1 ⩽ ai, bi ⩽ 10^4)
+#
+# Output
+# Return a number equal to the minimum occupied area.
+#
+# Example
+# Input example:
+#
+# packing_rectangles(4,10,5,11,12,3)
+# Output example:
+#
+# 144
+# ALGORITHMS
