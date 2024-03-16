88201d4cec179874bfee263e10b124a858dfacb7
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-16 09:47:54 +0300

    Added task 2123 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -39584,3 +39584,25 @@ def triangular_sum(n):
             return False
         k += 1
         T_k = T_next
+        
+# 525. Contiguous Array
+# Given a binary array nums, return the maximum length of a contiguous subarray with an equal number of 0 and 1.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [0,1]
+# Output: 2
+# Explanation: [0, 1] is the longest contiguous subarray with an equal number of 0 and 1.
+# Example 2:
+#
+# Input: nums = [0,1,0]
+# Output: 2
+# Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+#
+#
+# Constraints:
+#
+# 1 <= nums.length <= 105
+# nums[i] is either 0 or 1.
