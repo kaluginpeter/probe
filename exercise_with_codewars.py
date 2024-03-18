393be23ea8afb7ddc57480348e28bf3edd6ed5ed
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-18 21:39:16 +0300

    Complete task 2132 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -40014,3 +40014,12 @@ def group_anagrams(words):
 # Follow up:
 #
 # Could you solve this problem in less than O(n) complexity?
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        count: int = 0
+        for i in range(1, n + 1):
+            if n % i == 0:
+                count += 1
+                if count == k:
+                    return i
+        return -1
