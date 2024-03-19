8d2d1926a20cad8bdb7a0fb7d7b99c696e62a400
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-19 06:34:57 +0300

    Added task 2133 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -39981,7 +39981,7 @@ def group_anagrams(words):
         x: str = ''.join(sorted(i))
         ht[x] = ht.get(x, []) + [i]
     return ht.values()
-    
+
 # 1492. The kth Factor of n
 # You are given two positive integers n and k. A factor of an integer n is defined as an integer i where n % i == 0.
 #
@@ -40023,3 +40023,31 @@ class Solution:
                 if count == k:
                     return i
         return -1
+        
+# 1309. Decrypt String from Alphabet to Integer Mapping
+# You are given a string s formed by digits and '#'. We want to map s to English lowercase characters as follows:
+#
+# Characters ('a' to 'i') are represented by ('1' to '9') respectively.
+# Characters ('j' to 'z') are represented by ('10#' to '26#') respectively.
+# Return the string formed after mapping.
+#
+# The test cases are generated so that a unique mapping will always exist.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "10#11#12"
+# Output: "jkab"
+# Explanation: "j" -> "10#" , "k" -> "11#" , "a" -> "1" , "b" -> "2".
+# Example 2:
+#
+# Input: s = "1326#"
+# Output: "acz"
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 1000
+# s consists of digits and the '#' letter.
+# s will be a valid string such that mapping is always possible.
