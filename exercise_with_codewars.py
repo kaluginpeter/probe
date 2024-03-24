56838aacc53e000c0fb404502f412e9efa3783e5
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-24 07:26:49 +0300

    Added task 2143 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -40479,3 +40479,39 @@ def calc_rot_time(box):
     if all(mtrx[i][j] in {0, 2} for j in range(1, len(mtrx[0]) - 1) for i in range(1, len(mtrx) - 1)):
         return count
     return -1
+
+# 287. Find the Duplicate Number
+# Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+#
+# There is only one repeated number in nums, return this repeated number.
+#
+# You must solve the problem without modifying the array nums and uses only constant extra space.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [1,3,4,2,2]
+# Output: 2
+# Example 2:
+#
+# Input: nums = [3,1,3,4,2]
+# Output: 3
+# Example 3:
+#
+# Input: nums = [3,3,3,3,3]
+# Output: 3
+#
+#
+# Constraints:
+#
+# 1 <= n <= 105
+# nums.length == n + 1
+# 1 <= nums[i] <= n
+# All the integers in nums appear only once except for precisely one integer which appears two or more times.
+#
+#
+# Follow up:
+#
+# How can we prove that at least one duplicate number must exist in nums?
+# Can you solve the problem in linear runtime complexity?
