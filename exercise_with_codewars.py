1af7862af9e009e79c492960c6ffe92c70d2249b
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-27 17:36:44 +0300

    Added task 2150 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -40727,7 +40727,7 @@ def pick_peaks(arr):
         ans['pos'] = ans.get('pos', []) + [stack[0]]
         ans['peaks'] = ans.get('peaks', []) + [arr[stack[0]]]
     return ans
-    
+
 # 713. Subarray Product Less Than K
 # Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
 #
@@ -40764,3 +40764,32 @@ class Solution:
             ans += right + 1 - left
             right += 1
         return ans
+
+# PatternCraft - Strategy
+# The Strategy Design Pattern can be used, for example, to determine how a unit moves in the StarCraft game.
+#
+# The pattern consists in having a different strategy to one functionality. A unit, for example, could move by walking or flying.
+#
+# Your Task
+# Complete the code so that when a Viking is flying its position increases by 10 each time it moves. If it is walking then the position is increased by 1.
+#
+# In this Kata, Viking starts as a ground unit when it is created.
+#
+# You have 3 classes:
+#
+# Viking: has a position, moveBehavior and move method.
+# Fly and Walk: the move behaviors with the move(unit) method. Fly has to move 10 positions at a time and Walk has to move 1.
+# Resouces
+# PatternCraft > Strategy
+# SourceMaking > Strategy
+# Wikipedia > Strategy
+# PatternCraft series
+# State Pattern
+# Strategy Pattern
+# Visitor Pattern
+# Decorator Pattern
+# Adapter Pattern
+# Command Pattern
+# The original PatternCraft series (by John Lindquist) is a collection of Youtube videos that explains some of the design patterns and how they are used (or could be) on StarCraft.
+#
+# DESIGN PATTERNSFUNDAMENTALS
