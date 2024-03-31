b57cd3b25b0f9de308579ca7e7864f233488dfc7
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-31 07:33:40 +0300

    Added task 2163 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -41381,7 +41381,7 @@ class Solution:
         return -1
 
 # Simple fraction to mixed number converter
-# Task
+    # Task
 # Given a string representing a simple fraction x/y, your function must return a string representing the corresponding mixed fraction in the following format:
 #
 # [sign]a b/c
@@ -41433,3 +41433,34 @@ def mixed_fraction(s):
             full = abs(full) * -1
         return f'{full} {comprare(x, y, True)}'
     return comprare(x, y, positive)
+    
+# 3099. Harshad Number
+# An integer divisible by the sum of its digits is said to be a Harshad number. You are given an integer x. Return the sum of the digits of x if x is a Harshad number, otherwise, return -1.
+#
+#
+#
+# Example 1:
+#
+# Input: x = 18
+#
+# Output: 9
+#
+# Explanation:
+#
+# The sum of digits of x is 9. 18 is divisible by 9. So 18 is a Harshad number and the answer is 9.
+#
+# Example 2:
+#
+# Input: x = 23
+#
+# Output: -1
+#
+# Explanation:
+#
+# The sum of digits of x is 5. 23 is not divisible by 5. So 23 is not a Harshad number and the answer is -1.
+#
+#
+#
+# Constraints:
+#
+# 1 <= x <= 100
