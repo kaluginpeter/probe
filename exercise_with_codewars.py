86ea88a3b1a83dba5956d2b9ae82bd0721efdbe0
Peter Kalugin <streetworkout200412@gmail.com>
2024-03-31 07:39:37 +0300

    Added task 2164 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -41433,7 +41433,7 @@ def mixed_fraction(s):
             full = abs(full) * -1
         return f'{full} {comprare(x, y, True)}'
     return comprare(x, y, positive)
-    
+
 # 3099. Harshad Number
 # An integer divisible by the sum of its digits is said to be a Harshad number. You are given an integer x. Return the sum of the digits of x if x is a Harshad number, otherwise, return -1.
 #
@@ -41468,3 +41468,35 @@ class Solution:
     def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
         sm: int = sum(int(i) for i in str(x))
         return sm if x % sm == 0 else -1
+        
+# 3100. Water Bottles II
+# You are given two integers numBottles and numExchange.
+#
+# numBottles represents the number of full water bottles that you initially have. In one operation, you can perform one of the following operations:
+#
+# Drink any number of full water bottles turning them into empty bottles.
+# Exchange numExchange empty bottles with one full water bottle. Then, increase numExchange by one.
+# Note that you cannot exchange multiple batches of empty bottles for the same value of numExchange. For example, if numBottles == 3 and numExchange == 1, you cannot exchange 3 empty water bottles for 3 full bottles.
+#
+# Return the maximum number of water bottles you can drink.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: numBottles = 13, numExchange = 6
+# Output: 15
+# Explanation: The table above shows the number of full water bottles, empty water bottles, the value of numExchange, and the number of bottles drunk.
+# Example 2:
+#
+#
+# Input: numBottles = 10, numExchange = 3
+# Output: 13
+# Explanation: The table above shows the number of full water bottles, empty water bottles, the value of numExchange, and the number of bottles drunk.
+#
+#
+# Constraints:
+#
+# 1 <= numBottles <= 100
+# 1 <= numExchange <= 100
