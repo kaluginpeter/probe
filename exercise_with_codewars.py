0f34392d46112ee9c1137c4831c4fe99a4c94495
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-01 06:41:45 +0300

    Added task 2167 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -41573,7 +41573,7 @@ class Solution:
                 ln = 1
             ans += ln * (ln + 1) // 2
         return ans
-        
+
 # 58. Length of Last Word
 # Given a string s consisting of words and spaces, return the length of the last word in the string.
 #
@@ -41614,3 +41614,32 @@ class Solution:
         while left >= 0 and s[left] != ' ':
             left -= 1
         return right - left
+
+# PatternCraft - State
+# The State Design Pattern can be used, for example, to manage the state of a tank in the StarCraft game.
+#
+# The pattern consists in isolating the state logic in different classes rather than having multiple ifs to determine what should happen.
+#
+# Your Task
+# Complete the code so that when a Tank goes into SiegeMode it cannot move and its damage goes to 20. When it goes to TankMode it should be able to move and the damage should be set to 5.
+#
+# You have 3 classes:
+#
+# Tank: has a state, canMove and damage properties
+# SiegeState and TankState: has canMove and damage properties
+# Note: The tank initial state should be TankState.
+#
+# Resources
+# PatternCraft > State
+# SourceMaking > State
+# Wikipedia > State
+# PatternCraft series
+# State Pattern
+# Strategy Pattern
+# Visitor Pattern
+# Decorator Pattern
+# Adapter Pattern
+# Command Pattern
+# The original PatternCraft series (by John Lindquist) is a collection of Youtube videos that explains some of the design patterns and how they are used (or could be) on StarCraft.
+#
+# DESIGN PATTERNSFUNDAMENTALS
