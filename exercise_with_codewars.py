f71051a23bab66b1b3ffc5e4788bdde20bea1f1c
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-03 06:34:47 +0300

    Complete task 2170 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -41798,3 +41798,12 @@ def count_presents(prod: dict, pres: list[str]) -> int:
 # rectangles[i].length == 2
 # 1 <= li, wi <= 109
 # li != wi
+class Solution:
+    def countGoodRectangles(self, rectangles: List[List[int]]) -> int:
+        mx, count = 0, 0
+        for i in rectangles:
+            if mx == min(i):
+                count += 1
+            elif mx < min(i):
+                mx, count = min(i), 1
+        return count
