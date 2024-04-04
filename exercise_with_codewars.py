568ffdcb43ed99571916106ab2f549f77565680f
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-04 06:39:20 +0300

    Complete task 2172 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -41807,41 +41807,6 @@ class Solution:
             elif mx < min(i):
                 mx, count = min(i), 1
         return count
-
-# 1614. Maximum Nesting Depth of the Parentheses
-# A string is a valid parentheses string (denoted VPS) if it meets one of the following:
-#
-# It is an empty string "", or a single character not equal to "(" or ")",
-# It can be written as AB (A concatenated with B), where A and B are VPS's, or
-# It can be written as (A), where A is a VPS.
-# We can similarly define the nesting depth depth(S) of any VPS S as follows:
-#
-# depth("") = 0
-# depth(C) = 0, where C is a string with a single character not equal to "(" or ")".
-# depth(A + B) = max(depth(A), depth(B)), where A and B are VPS's.
-# depth("(" + A + ")") = 1 + depth(A), where A is a VPS.
-# For example, "", "()()", and "()(()())" are VPS's (with nesting depths 0, 1, and 2), and ")(" and "(()" are not VPS's.
-#
-# Given a VPS represented as string s, return the nesting depth of s.
-#
-#
-#
-# Example 1:
-#
-# Input: s = "(1+(2*3)+((8)/4))+1"
-# Output: 3
-# Explanation: Digit 8 is inside of 3 nested parentheses in the string.
-# Example 2:
-#
-# Input: s = "(1)+((2))+(((3)))"
-# Output: 3
-#
-#
-# Constraints:
-#
-# 1 <= s.length <= 100
-# s consists of digits 0-9 and characters '+', '-', '*', '/', '(', and ')'.
-# It is guaranteed that parentheses expression s is a VPS.
     
 # Game Hit the target - 2nd part
 # This is the second part of the kata :3 🎈🎆🎇🎆🎈
@@ -41923,3 +41888,55 @@ def solution(mtrx):
                         return True
                 return False
     return False
+
+
+# 1614. Maximum Nesting Depth of the Parentheses
+# A string is a valid parentheses string (denoted VPS) if it meets one of the following:
+#
+# It is an empty string "", or a single character not equal to "(" or ")",
+# It can be written as AB (A concatenated with B), where A and B are VPS's, or
+# It can be written as (A), where A is a VPS.
+# We can similarly define the nesting depth depth(S) of any VPS S as follows:
+#
+# depth("") = 0
+# depth(C) = 0, where C is a string with a single character not equal to "(" or ")".
+# depth(A + B) = max(depth(A), depth(B)), where A and B are VPS's.
+# depth("(" + A + ")") = 1 + depth(A), where A is a VPS.
+# For example, "", "()()", and "()(()())" are VPS's (with nesting depths 0, 1, and 2), and ")(" and "(()" are not VPS's.
+#
+# Given a VPS represented as string s, return the nesting depth of s.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "(1+(2*3)+((8)/4))+1"
+# Output: 3
+# Explanation: Digit 8 is inside of 3 nested parentheses in the string.
+# Example 2:
+#
+# Input: s = "(1)+((2))+(((3)))"
+# Output: 3
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 100
+# s consists of digits 0-9 and characters '+', '-', '*', '/', '(', and ')'.
+# It is guaranteed that parentheses expression s is a VPS.
+# Solution One Pass Stack O(N) O(N)
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        stack: list[str] = list()
+        depth: int = 0
+        ans: int = 0
+        for i in s:
+            if i == '(':
+                depth += 1
+                stack.append(i)
+            elif i == ')':
+                ans, depth = max(ans, depth), depth - 1
+                if not stack: # if stack is empty and we meet )
+                    continue
+                stack.pop()
+        return ans
