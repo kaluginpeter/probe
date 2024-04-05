d312bb558a73627d2a93cfb5b76e053cc0d9866b
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-05 06:28:15 +0300

    Added task 2174 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -41940,7 +41940,7 @@ class Solution:
                     continue
                 stack.pop()
         return ans
-
+    
 # Statistics for an Athletic Association
 # You are the "computer expert" of a local Athletic Association (C.A.A.). Many teams of runners come to compete. Each time you get a string of all race results of every team who has run. For example here is a string showing the individual results of a team of 5 runners:
 #
@@ -41991,3 +41991,41 @@ def stat(strg):
     median: int = stack[len(stack) // 2] if len(stack) % 2 != 0 else (stack[len(stack) // 2] + stack[len(stack) // 2 - 1]) // 2
     median: str = f'Median: {median // 3600:02d}|{median % 3600 // 60:02d}|{median % 3600 % 60:02d}'
     return f'{diff} {average} {median}'
+    
+# 1544. Make The String Great
+# Given a string s of lower and upper case English letters.
+#
+# A good string is a string which doesn't have two adjacent characters s[i] and s[i + 1] where:
+#
+# 0 <= i <= s.length - 2
+# s[i] is a lower-case letter and s[i + 1] is the same letter but in upper-case or vice-versa.
+# To make the string good, you can choose two adjacent characters that make the string bad and remove them. You can keep doing this until the string becomes good.
+#
+# Return the string after making it good. The answer is guaranteed to be unique under the given constraints.
+#
+# Notice that an empty string is also good.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "leEeetcode"
+# Output: "leetcode"
+# Explanation: In the first step, either you choose i = 1 or i = 2, both will result "leEeetcode" to be reduced to "leetcode".
+# Example 2:
+#
+# Input: s = "abBAcC"
+# Output: ""
+# Explanation: We have many possible scenarios, and all lead to the same answer. For example:
+# "abBAcC" --> "aAcC" --> "cC" --> ""
+# "abBAcC" --> "abBA" --> "aA" --> ""
+# Example 3:
+#
+# Input: s = "s"
+# Output: "s"
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 100
+# s contains only lower and upper case English letters.
