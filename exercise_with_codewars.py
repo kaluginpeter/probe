bc2a902dfb9ec80c98654915876cfda99a951572
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-14 18:18:03 +0300

    Added task 2197 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -43138,3 +43138,14 @@ class Solution:
             return n * (n + 1)
         n = n // 2
         return n**2
+
+# Sum it continuously
+# Make a function "add" that will be able to sum elements of list continuously and return a new list of sums.
+#
+# For example:
+#
+# add [1,2,3,4,5] == [1, 3, 6, 10, 15], because it's calculated like
+# this : [1, 1 + 2, 1 + 2 + 3, 1 + 2 + 3 + 4, 1 + 2 + 3 + 4 + 5]
+# If you want to learn more see https://en.wikipedia.org/wiki/Prefix_sum
+#
+# FUNDAMENTALSLISTS
