fea09f7ef5017a514cb7564517643b8ca8eea887
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-18 06:35:44 +0300

    Added task 2205 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -43424,7 +43424,7 @@ class Solution:
 # FUNDAMENTALSSTRINGSCIPHERSCRYPTOGRAPHY
 def cypher(s):
     return s.translate(str.maketrans('IREASGTBOlzeasbtgo', '123456780123456790'))
-    
+
 # 463. Island Perimeter
 # You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
 #
@@ -43472,3 +43472,19 @@ class Solution:
                             continue
                         ans += grid[rows + x][cols + y] == 0
         return ans
+        
+# Cantor's Diagonals
+# Given a list of lists containing only 1s and 0s, return a new list that differs from list 1 in its first element, list 2 in its second element, list 3 in its 3rd element, and so on.
+#
+# cantor([[0,0,0],
+#         [1,1,1],
+#         [0,1,0]]) = [1,0,1]
+#
+# cantor([[1,0,0],
+#         [0,1,0],
+#         [0,0,1]]) = [0,0,0]
+# The nested list will always be perfectly square. Your solution should be a list containing only 1s and 0s.
+#
+# See Wikipedia for background (if you're interested; it won't help you solve the kata). Obviously this kata is not the same because the lists are not infinite so it doesn't really prove anything -- consider it a tribute...
+#
+# FUNDAMENTALS
