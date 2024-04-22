650c08768cf1a682b2ddd6fd15a0bb5db566da4d
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-22 19:59:33 +0300

    Added task 2013 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -43790,7 +43790,7 @@ def convergence(n):
             ans_ln += 1
             n = generate(n)
     return ans_ln
-    
+
 # 1051. Height Checker
 # A school is trying to take an annual photo of all the students. The students are asked to stand in a single file line in non-decreasing order by height. Let this ordering be represented by the integer array expected where expected[i] is the expected height of the ith student in line.
 #
@@ -43844,3 +43844,30 @@ class Solution:
                 ans += 1
             arr[idx] -= 1
         return ans
+
+# Memesorting
+# Roma is programmer and he likes memes about IT,
+# Maxim is chemist and he likes memes about chemistry,
+# Danik is designer and he likes memes about design,
+# and Vlad likes all other memes.
+#
+# You will be given a meme (string), and your task is to identify its category, and send it to the right receiver: IT - 'Roma', chemistry - 'Maxim', design - 'Danik', or other - 'Vlad'.
+#
+# IT meme has letters b, u, g.
+# Chemistry meme has letters b, o, o, m.
+# Design meme has letters e, d, i, t, s.
+# If there is more than 1 possible answer, the earliest match should be chosen.
+#
+# Note: letters are case-insensetive and should come in the order specified above.
+#
+# Examples:
+# (Matching letters are surrounded by curly braces for readability.)
+#
+# this is programmer meme {b}ecause it has b{ug}
+# this is also program{bu}r meme {g}ecause it has needed key word
+# this is {ed}s{i}gner meme cause i{t} ha{s} key word
+#
+# this could {b}e chemistry meme b{u}t our{g}Gey word 'boom' is too late
+#     instead of
+# this could {b}e chemistry meme but {o}ur gey w{o}rd 'boo{m}' is too late
+# STRINGSREGULAR EXPRESSIONSFUNDAMENTALSSORTING
