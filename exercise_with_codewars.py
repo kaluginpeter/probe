a5e72f15e7eaa7b631deda52b9e13453f29c1735
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-23 17:49:56 +0300

    Complete task 2015 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -43964,3 +43964,13 @@ class Solution:
 # Please also note that the equality m == rotateClockwise(rotateClockwise(rotateClockwise(rotateClockwise(m)))); (360° clockwise rotation), is not always true because rotateClockwise(['']) => [] and rotateClockwise(['','','']) => [] (empty lines information is lost)
 #
 # MATRIXFUNDAMENTALS
+def rotate_clockwise(matrix):
+    ans: list = list()
+    if not matrix:
+        return ans
+    for col in range(len(matrix[0])):
+        top: list = list()
+        for row in range(len(matrix)):
+            top.append(matrix[row][col])
+        ans.append(''.join(top)[::-1])
+    return ans
