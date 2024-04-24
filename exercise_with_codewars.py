c97aef9c0cfc2a86e78dd7dd8e5cdc34b9bd10ba
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-24 06:37:07 +0300

    Added task 2016 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -43933,7 +43933,7 @@ class Solution:
             for j in range(n - 2):
                 ans[i][j] = max(grid[row][col] for row in range(i, i + 3) for col in range(j, j + 3))
         return ans
-        
+    
 # Matrix Rotation
 # Given a matrix represented as a list of string, such as
 #
@@ -43964,7 +43964,7 @@ class Solution:
 # Please also note that the equality m == rotateClockwise(rotateClockwise(rotateClockwise(rotateClockwise(m)))); (360° clockwise rotation), is not always true because rotateClockwise(['']) => [] and rotateClockwise(['','','']) => [] (empty lines information is lost)
 #
 # MATRIXFUNDAMENTALS
-def rotate_clockwise(matrix):
+ def rotate_clockwise(matrix):
     ans: list = list()
     if not matrix:
         return ans
@@ -43974,3 +43974,34 @@ def rotate_clockwise(matrix):
             top.append(matrix[row][col])
         ans.append(''.join(top)[::-1])
     return ans
+    
+# 1769. Minimum Number of Operations to Move All Balls to Each Box
+# You have n boxes. You are given a binary string boxes of length n, where boxes[i] is '0' if the ith box is empty, and '1' if it contains one ball.
+#
+# In one operation, you can move one ball from a box to an adjacent box. Box i is adjacent to box j if abs(i - j) == 1. Note that after doing so, there may be more than one ball in some boxes.
+#
+# Return an array answer of size n, where answer[i] is the minimum number of operations needed to move all the balls to the ith box.
+#
+# Each answer[i] is calculated considering the initial state of the boxes.
+#
+#
+#
+# Example 1:
+#
+# Input: boxes = "110"
+# Output: [1,1,3]
+# Explanation: The answer for each box is as follows:
+# 1) First box: you will have to move one ball from the second box to the first box in one operation.
+# 2) Second box: you will have to move one ball from the first box to the second box in one operation.
+# 3) Third box: you will have to move one ball from the first box to the third box in two operations, and move one ball from the second box to the third box in one operation.
+# Example 2:
+#
+# Input: boxes = "001011"
+# Output: [11,8,5,4,3,4]
+#
+#
+# Constraints:
+#
+# n == boxes.length
+# 1 <= n <= 2000
+# boxes[i] is either '0' or '1'.
