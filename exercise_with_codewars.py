1f0b607db14aa945aed5a299b35a801d7588cbc8
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-28 08:50:58 +0300

    Added task 2232 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -44787,7 +44787,7 @@ class Solution:
                 else:
                     y = i
         return 0 if y is None else y - x
-        
+
 # 3121. Count the Number of Special Characters II
 # You are given a string word. A letter c is called special if it appears both in lowercase and uppercase in word, and every lowercase occurrence of c appears before the first uppercase occurrence of c.
 #
@@ -44844,3 +44844,66 @@ class Solution:
             if (i.lower() in ht and i.upper() in ht) and ht[i.lower()] < ht[i.upper()]:
                 ans += 1
         return ans // 2
+
+# 3128. Right Triangles
+# You are given a 2D boolean matrix grid.
+#
+# Return an integer that is the number of right triangles that can be made with the 3 elements of grid such that all of them have a value of 1.
+#
+# Note:
+#
+# A collection of 3 elements of grid is a right triangle if one of its elements is in the same row with another element and in the same column with the third element. The 3 elements do not have to be next to each other.
+#
+#
+# Example 1:
+#
+# 0	1	0
+# 0	1	1
+# 0	1	0
+# 0	1	0
+# 0	1	1
+# 0	1	0
+# Input: grid = [[0,1,0],[0,1,1],[0,1,0]]
+#
+# Output: 2
+#
+# Explanation:
+#
+# There are two right triangles.
+#
+# Example 2:
+#
+# 1	0	0	0
+# 0	1	0	1
+# 1	0	0	0
+# Input: grid = [[1,0,0,0],[0,1,0,1],[1,0,0,0]]
+#
+# Output: 0
+#
+# Explanation:
+#
+# There are no right triangles.
+#
+# Example 3:
+#
+# 1	0	1
+# 1	0	0
+# 1	0	0
+# 1	0	1
+# 1	0	0
+# 1	0	0
+# Input: grid = [[1,0,1],[1,0,0],[1,0,0]]
+#
+# Output: 2
+#
+# Explanation:
+#
+# There are two right triangles.
+#
+#
+#
+# Constraints:
+#
+# 1 <= grid.length <= 1000
+# 1 <= grid[i].length <= 1000
+# 0 <= grid[i][j] <= 1
