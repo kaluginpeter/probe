2fe8b8acc28f29b23d148db84166bb0c9ba2fdc9
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-28 08:10:20 +0300

    Added task 2230 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -44655,7 +44655,7 @@ class Solution:
                 if len(set(union_lst)) == 1 and union_lst[0] < ans:
                     ans = union_lst[0]
         return ans
-        
+
 # 3111. Minimum Rectangles to Cover Points
 # You are given a 2D integer array points, where points[i] = [xi, yi]. You are also given an integer w. Your task is to cover all the given points with rectangles.
 #
@@ -44732,3 +44732,34 @@ class Solution:
                 start = i[0]
                 ans += 1
         return ans
+        
+# 3115. Maximum Prime Difference
+# You are given an integer array nums.
+#
+# Return an integer that is the maximum distance between the indices of two (not necessarily different) prime numbers in nums.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [4,2,9,5,3]
+#
+# Output: 3
+#
+# Explanation: nums[1], nums[3], and nums[4] are prime. So the answer is |4 - 1| = 3.
+#
+# Example 2:
+#
+# Input: nums = [4,8,2,8]
+#
+# Output: 0
+#
+# Explanation: nums[2] is prime. Because there is just one prime number, the answer is |2 - 2| = 0.
+#
+#
+#
+# Constraints:
+#
+# 1 <= nums.length <= 3 * 105
+# 1 <= nums[i] <= 100
+# The input is generated such that the number of prime numbers in the nums is at least one.
