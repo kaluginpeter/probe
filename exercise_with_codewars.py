9405ec8873bc86b96088e4f028763148f475d533
Peter Kalugin <streetworkout200412@gmail.com>
2024-04-29 10:16:01 +0300

    Added task 2235 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -44970,7 +44970,7 @@ def get_neighbourhood(n_type, mat, cor):
         if cor[1] + mov[1] >= len(mat[0]) or cor[1] + mov[1] < 0: continue
         ans.append(mat[cor[0] + mov[0]][cor[1] + mov[1]])
     return ans
-    
+
 # 2997. Minimum Number of Operations to Make Array XOR Equal to K
 # You are given a 0-indexed integer array nums and a positive integer k.
 #
@@ -45010,3 +45010,46 @@ class Solution:
         for i in range(len(nums)):
             y ^= nums[i]
         return bin(y ^ k).count('1')
+        
+# 2D Cellular Neighbourhood - Part 2
+# This kata is the second part of a series: Neighbourhood kata collection. You may want to do the first one before trying this version.
+#
+# Now we will implement the distance
+# The distance extends the reach of the neighbourhood - instead of returning only the closest neighbours, you'll be searching for all neighbours in a given maximum distance.
+#
+# There are two popular types of cellular neighbourhoods:
+#
+# Moore neighborhood - the eight cells that surround the given cell
+# Von Neumann neighborhood - the four cells that share a border with the given cell
+# Task
+# Given a neighbourhood type ("moore" or "von_neumann"), a 2D matrix (a list of lists), a pair of coordinates and the distance, return the list of neighbours of the given cell.
+#
+# Order of the indices: The first index should be applied for the outer/first matrix layer. The last index for the most inner/last layer. coordinates = (m, n) should be applied like mat[m][n]
+#
+# Note: you should return an empty array if any of these conditions are true:
+#
+# Matrix is empty
+# Coordinates are outside the matrix
+# Distance is equal to 0
+# Example:
+# \ n  0    1    2    3    4
+# m  --------------------------
+# 0  | 0  | 1  | 2  | 3  | 4  |
+# 1  | 5  | 6  | 7  | 8  | 9  |
+# 2  | 10 | 11 | 12 | 13 | 14 |
+# 3  | 15 | 16 | 17 | 18 | 19 |
+# 4  | 20 | 21 | 22 | 23 | 24 |
+#    --------------------------
+#
+# get_neighborhood("moore", arr, (2,2), 1) == [6,7,8,11,13,16,17,18]
+# get_neighborhood("moore", arr, (2,2), 2) == [0,1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23,24]
+#
+# get_neighborhood("von_neumann", arr, (2,2), 1) == [7,11,13,17]
+# get_neighborhood("von_neumann", arr, (2,2), 2) == [2,6,7,8,10,11,13,14,16,17,18,22]
+# Translations are appreciated ^^
+#
+# If you like chess take a look at Chess Aesthetics
+#
+# If you like puzzles take a look at Rubik's cube
+#
+# ALGORITHMSDATA STRUCTURESARRAYSMATRIX
