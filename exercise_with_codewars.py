7e44a314ca633105acfddec207908f7db55b02b0
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-05 07:55:23 +0300

    Added task 2254 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -45867,7 +45867,7 @@ class Solution:
         if s[4] == '?':
             s[4] = '9'
         return ''.join(s)
-        
+
 # 3120. Count the Number of Special Characters I
 # You are given a string word. A letter is called special if it appears both in lowercase and uppercase in word.
 #
@@ -45921,3 +45921,77 @@ class Solution:
             if i.lower() in ht and i.upper() in ht:
                 ans += 1
         return ans // 2
+        
+# 3127. Make a Square with the Same Color
+# You are given a 2D matrix grid of size 3 x 3 consisting only of characters 'B' and 'W'. Character 'W' represents the white color, and character 'B' represents the black color.
+#
+# Your task is to change the color of at most one cell so that the matrix has a 2 x 2 square where all cells are of the same color.
+#
+# Return true if it is possible to create a 2 x 2 square of the same color, otherwise, return false.
+#
+#
+#
+# Example 1:
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# Input: grid = [["B","W","B"],["B","W","W"],["B","W","B"]]
+#
+# Output: true
+#
+# Explanation:
+#
+# It can be done by changing the color of the grid[0][2].
+#
+# Example 2:
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# Input: grid = [["B","W","B"],["W","B","W"],["B","W","B"]]
+#
+# Output: false
+#
+# Explanation:
+#
+# It cannot be done by changing at most one cell.
+#
+# Example 3:
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# Input: grid = [["B","W","B"],["B","W","W"],["B","W","W"]]
+#
+# Output: true
+#
+# Explanation:
+#
+# The grid already contains a 2 x 2 square of the same color.
+#
+#
+#
+# Constraints:
+#
+# grid.length == 3
+# grid[i].length == 3
+# grid[i][j] is either 'W' or 'B'.
