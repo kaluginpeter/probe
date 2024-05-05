e2174dcd076a1859eff74ff5288c787fbf9486d3
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-05 07:39:28 +0300

    Added task 2251 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -45728,7 +45728,7 @@ class Solution:
             if len(x) == k:
                 ht[x] = ht.get(x, 0) + 1
         return len(word) // k - max(ht.values())
-        
+
 # 3138. Minimum Length of Anagram Concatenation
 # You are given a string s, which is known to be a concatenation of anagrams of some string t.
 #
@@ -45776,3 +45776,37 @@ class Solution:
             if n % i == 0:
                 if all(v % i == 0 for v in ht.values()):
                     return sum(v // i for v in ht.values())
+
+# 3110. Score of a String
+# You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+#
+# Return the score of s.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "hello"
+#
+# Output: 13
+#
+# Explanation:
+#
+# The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+#
+# Example 2:
+#
+# Input: s = "zaz"
+#
+# Output: 50
+#
+# Explanation:
+#
+# The ASCII values of the characters in s are: 'z' = 122, 'a' = 97. So, the score of s would be |122 - 97| + |97 - 122| = 25 + 25 = 50.
+#
+#
+#
+# Constraints:
+#
+# 2 <= s.length <= 100
+# s consists only of lowercase English letters.
