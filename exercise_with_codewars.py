589d507063873158f512762bf8321fdeed88921e
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-05 07:19:30 +0300

    Complete task 2247 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -45574,3 +45574,22 @@ def calc_distance(path):
 # -1000 <= Node.val <= 1000
 # The value of each node in the list is unique.
 # The node to be deleted is in the list and is not a tail node.
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        tmp = node
+        while tmp:
+            if tmp is node:
+                tmp.val = tmp.next.val
+                tmp.next = tmp.next.next
+                return
+            tmp = tmp.next
