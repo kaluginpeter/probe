e3c01862831bf74ae5a9e485cf2dfd139468e03b
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-10 11:13:40 +0300

    Added task 2265 4kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -46413,7 +46413,7 @@ def same_structure_as(original,other):
                 return False
             return same_structure_as(original[i], other[i])
     return True
-    
+
 # 786. K-th Smallest Prime Fraction
 # You are given a sorted integer array arr containing 1 and prime numbers, where all the integers of arr are unique. You are also given an integer k.
 #
@@ -46480,3 +46480,36 @@ class Solution:
             else:
                 left = middle
         return [arr[num], arr[den]]
+
+# Matrix Determinant
+# Write a function that accepts a square matrix (N x N 2D array) and returns the determinant of the matrix.
+#
+# How to take the determinant of a matrix -- it is simplest to start with the smallest cases:
+#
+# A 1x1 matrix |a| has determinant a.
+#
+# A 2x2 matrix [ [a, b], [c, d] ] or
+#
+# |a  b|
+# |c  d|
+# has determinant: a*d - b*c.
+#
+# The determinant of an n x n sized matrix is calculated by reducing the problem to the calculation of the determinants of n matrices ofn-1 x n-1 size.
+#
+# For the 3x3 case, [ [a, b, c], [d, e, f], [g, h, i] ] or
+#
+# |a b c|
+# |d e f|
+# |g h i|
+# the determinant is: a * det(a_minor) - b * det(b_minor) + c * det(c_minor) where det(a_minor) refers to taking the determinant of the 2x2 matrix created by crossing out the row and column in which the element a occurs:
+#
+# |- - -|
+# |- e f|
+# |- h i|
+# Note the alternation of signs.
+#
+# The determinant of larger matrices are calculated analogously, e.g. if M is a 4x4 matrix with first row [a, b, c, d], then:
+#
+# det(M) = a * det(a_minor) - b * det(b_minor) + c * det(c_minor) - d * det(d_minor)
+#
+# MATRIXLINEAR ALGEBRAMATHEMATICSRECURSIONALGORITHMS
