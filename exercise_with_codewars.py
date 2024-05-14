31e2beaec8eaef26c5d05924dc8024d4008e32c2
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-14 16:21:54 +0300

    Added task 2276 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -46945,7 +46945,7 @@ def make_me_slow():
     time.sleep(7)
     for i in range(1000000):
         pass
-        
+
 # 2181. Merge Nodes in Between Zeros
 # You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
 #
@@ -47002,7 +47002,7 @@ class Solution:
         inner_tmp.val = top
         inner_tmp.next = None
         return head
-        
+
 # Twice linear
 # Consider a sequence u where u is defined as follows:
 #
@@ -47038,3 +47038,36 @@ def dbl_linear(n):
             u_set.add(z)
             heapq.heappush(u_heap, z)
     return heapq.heappop(u_heap)
+    
+# 495. Teemo Attacking
+# Our hero Teemo is attacking an enemy Ashe with poison attacks! When Teemo attacks Ashe, Ashe gets poisoned for a exactly duration seconds. More formally, an attack at second t will mean Ashe is poisoned during the inclusive time interval [t, t + duration - 1]. If Teemo attacks again before the poison effect ends, the timer for it is reset, and the poison effect will end duration seconds after the new attack.
+#
+# You are given a non-decreasing integer array timeSeries, where timeSeries[i] denotes that Teemo attacks Ashe at second timeSeries[i], and an integer duration.
+#
+# Return the total number of seconds that Ashe is poisoned.
+#
+#
+#
+# Example 1:
+#
+# Input: timeSeries = [1,4], duration = 2
+# Output: 4
+# Explanation: Teemo's attacks on Ashe go as follows:
+# - At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+# - At second 4, Teemo attacks, and Ashe is poisoned for seconds 4 and 5.
+# Ashe is poisoned for seconds 1, 2, 4, and 5, which is 4 seconds in total.
+# Example 2:
+#
+# Input: timeSeries = [1,2], duration = 2
+# Output: 3
+# Explanation: Teemo's attacks on Ashe go as follows:
+# - At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+# - At second 2 however, Teemo attacks again and resets the poison timer. Ashe is poisoned for seconds 2 and 3.
+# Ashe is poisoned for seconds 1, 2, and 3, which is 3 seconds in total.
+#
+#
+# Constraints:
+#
+# 1 <= timeSeries.length <= 104
+# 0 <= timeSeries[i], duration <= 107
+# timeSeries is sorted in non-decreasing order.
