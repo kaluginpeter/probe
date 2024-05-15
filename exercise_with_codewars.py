da0002b6730d325a0d20a8999fafdcff6891fa88
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-15 19:19:11 +0300

    Complete task 2279 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -47211,3 +47211,8 @@ class Solution:
 #
 # 1 <= pref.length <= 105
 # 0 <= pref[i] <= 106
+class Solution:
+    def findArray(self, pref: List[int]) -> List[int]:
+        for i in range(len(pref) - 1, 0, -1):
+            pref[i] ^= pref[i - 1]
+        return pref
