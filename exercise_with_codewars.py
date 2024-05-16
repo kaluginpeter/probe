23ad93c4e52ee6064d8e1cb87db7184634b6e726
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-16 20:13:16 +0300

    Added task 2282 3kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -47288,7 +47288,7 @@ def parse_int(string):
         idx += 1
     number += inner_num
     return number
-    
+
 # 1068. Product Sales Analysis I
 # Table: Sales
 #
@@ -47361,3 +47361,34 @@ def parse_int(string):
 SELECT product_name, year, price
 FROM Sales
 JOIN Product ON Sales.product_id = Product.product_id
+
+# Make a spiral
+# Your task, is to create a NxN spiral with a given size.
+#
+# For example, spiral with size 5 should look like this:
+#
+# 00000
+# ....0
+# 000.0
+# 0...0
+# 00000
+# and with the size 10:
+#
+# 0000000000
+# .........0
+# 00000000.0
+# 0......0.0
+# 0.0000.0.0
+# 0.0..0.0.0
+# 0.0....0.0
+# 0.000000.0
+# 0........0
+# 0000000000
+# Return value should contain array of arrays, of 0 and 1, with the first row being composed of 1s. For example for given size 5 result should be:
+#
+# [[1,1,1,1,1],[0,0,0,0,1],[1,1,1,0,1],[1,0,0,0,1],[1,1,1,1,1]]
+# Because of the edge-cases for tiny spirals, the size will be at least 5.
+#
+# General rule-of-a-thumb is, that the snake made with '1' cannot touch to itself.
+#
+# ALGORITHMSARRAYSLOGIC
