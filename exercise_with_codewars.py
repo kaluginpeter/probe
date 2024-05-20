3cd3debcaf814ca072a6c37f9a114110f592d8ac
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-20 07:05:48 +0300

    Complete task 2291 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -47965,3 +47965,6 @@ def compare(a, b):
 # Constraints:
 #
 # 0 <= start, goal <= 109
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        return bin(start ^ goal).count('1')
