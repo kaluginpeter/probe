07152bba88b6625f0ac1e223a4b26b62a3521b9c
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-22 07:00:58 +0300

    Added task 2299 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -48294,3 +48294,30 @@ for idx in range(n):
     initial[idx] = sum(map(int, sys.stdin.readline().rstrip().split()))
 answer: int = sum(1 for item in initial if item > 1)
 sys.stdout.write(str(answer))
+
+# Find Max Tree Node
+# You are given a binary tree. Implement the method findMax which returns the maximal node value in the tree.
+#
+# For example, maximum in the following Tree is 11.
+#
+#               7
+#             /   \
+#            /     \
+#           5       2
+#            \       \
+#            6        11
+#            /\      /
+#           1  9   4
+# Note:
+#
+# Tree node values any integer value.
+# Tree can unbalanced and unsorted.
+# The root argument is never an empty tree.
+# You are given a tree node class as follows:
+#
+# class TreeNode:
+#     def __init__(self, value, left = None, right = None):
+#         self.left = left
+#         self.right = right
+#         self.value = value
+# ALGORITHMSRECURSIONBINARY SEARCH TREESBINARY
