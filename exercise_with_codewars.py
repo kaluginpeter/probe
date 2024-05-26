f73904aa53b6c06961a09de5a77e9f2f310ea232
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-26 07:45:42 +0300

    Added task 2315 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -49072,7 +49072,7 @@ class Solution:
                 if colors[y] == 1: count += 1 # if value is 1(it means that we got unique color) so just increment count
             output.append(count) # append to the answer list given count of unique colors
         return output
-        
+
 # 3162. Find the Number of Good Pairs I
 # You are given 2 integer arrays nums1 and nums2 of lengths n and m respectively. You are also given a positive integer k.
 #
@@ -49115,7 +49115,7 @@ class Solution:
             for j in range(len(nums2)):
                 if nums1[i] % (nums2[j] * k) == 0: answer += 1
         return answer
-        
+
 # 3163. String Compression III
 # Given a string word, compress it using the following algorithm:
 #
@@ -49176,7 +49176,7 @@ class Solution:
                     count += 1
         comp += str(count) + seq[-1]
         return comp
-        
+
 # 1393. Capital Gain/Loss
 # Table: Stocks
 #
@@ -49242,3 +49242,35 @@ SUM(CASE
 ) AS capital_gain_loss
 FROM Stocks
 GROUP BY stock_name
+
+# A. Domino piling
+# A. Domino piling
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# inputstandard input
+# outputstandard output
+# You are given a rectangular board of M × N squares. Also you are given an unlimited number of standard domino pieces of 2 × 1 squares. You are allowed to rotate the pieces. You are asked to place as many dominoes as possible on the board so as to meet the following conditions:
+#
+# 1. Each domino completely covers two squares.
+#
+# 2. No two dominoes overlap.
+#
+# 3. Each domino lies entirely inside the board. It is allowed to touch the edges of the board.
+#
+# Find the maximum number of dominoes, which can be placed under these restrictions.
+#
+# Input
+# In a single line you are given two integers M and N — board sizes in squares (1 ≤ M ≤ N ≤ 16).
+#
+# Output
+# Output one number — the maximal number of dominoes, which can be placed.
+#
+# Examples
+# inputCopy
+# 2 4
+# outputCopy
+# 4
+# inputCopy
+# 3 3
+# outputCopy
+# 4
