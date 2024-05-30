361a3b7f76bca515c7dc072fa2d6a9fd080283dd
Peter Kalugin <streetworkout200412@gmail.com>
2024-05-30 18:28:56 +0300

    Added task 2328 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -49766,7 +49766,7 @@ def test_it(arr):
         if shape_time > time:
             shape, shape_time = name, time
     return color + ' ' + shape
-    
+
 # 1442. Count Triplets That Can Form Two Arrays of Equal XOR
 # Given an array of integers arr.
 #
@@ -49807,3 +49807,33 @@ class Solution:
                 outer_xor ^= arr[end]
                 if outer_xor == 0: output += end - start
         return output
+        
+# Simple Fun #147: Find The Missing Tree
+# Task
+# After a long night (work, play, study) you find yourself sleeping on a bench in a park. As you wake up and try to figure out what happened you start counting trees.
+#
+# You notice there are different tree sizes but there's always one size which is unbalanced. For example there are 2 size 2, 2 size 1 and 1 size 3. (then the size 3 is unbalanced)
+#
+# Given an array representing different tree sizes. Which one is the unbalanced size.
+#
+# Notes
+# There can be any number of sizes but one is always unbalanced
+# The unbalanced size is always one less than the other sizes
+# The array is not ordered (nor the trees)
+# Examples
+# For trees = [1,1,2,2,3], the result should be 3.
+#
+# For trees = [2,2,2,56,56,56,8,8], the result should be 8.
+#
+# For trees = [34,76,12,99,64,99,76,12,34], the result should be 64.
+#
+# Input/Output
+# [input] integer array trees
+#
+# Array representing different tree sizes
+#
+# [output] an integer
+#
+# The size of the missing tree.
+#
+# ALGORITHMS
