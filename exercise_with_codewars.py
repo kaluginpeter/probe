60537566cdf4e6be9b3c0bfe766defbfa970b2c6
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-01 09:25:21 +0300

    Added task 2335 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -50118,3 +50118,30 @@ for i in range(len(sentence) - 1, -1, -1):
     else:
         sentence[i] = sentence[i].lower()
 print(''.join('.' + i for i in sentence))
+
+# 17. Letter Combinations of a Phone Number
+# Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+#
+# A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+#
+#
+#
+#
+# Example 1:
+#
+# Input: digits = "23"
+# Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+# Example 2:
+#
+# Input: digits = ""
+# Output: []
+# Example 3:
+#
+# Input: digits = "2"
+# Output: ["a","b","c"]
+#
+#
+# Constraints:
+#
+# 0 <= digits.length <= 4
+# digits[i] is a digit in the range ['2', '9'].
