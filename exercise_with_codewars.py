7b42d47ebd165a4aff1f0fc9038b2fe9dbecf3fc
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-02 07:46:02 +0300

    Complete task 2337 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -50272,3 +50272,12 @@ class LCG(object):
 #
 # 1 <= s.length <= 105
 # s[i] is a printable ascii character.
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left, right = 0, len(s) - 1
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left, right = left + 1, right - 1
