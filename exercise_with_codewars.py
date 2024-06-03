67b7d9521126344a239b1788e3fb12e6373f7cd0
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-03 07:11:40 +0300

    Added task 2344 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -50594,7 +50594,7 @@ class Solution:
                 heapq.heappush(heap, (s[i], -i))
         heap.sort(key=lambda x: -x[1])
         return ''.join(i[0] for i in heap)
-        
+
 # A. Stones on the Table
 # A. Stones on the Table
 # time limit per test2 seconds
@@ -50638,3 +50638,38 @@ for idx in range(1, n):
     else:
         current_index = idx
 sys.stdout.write(str(swaped))
+
+# 2486. Append Characters to String to Make Subsequence
+# You are given two strings s and t consisting of only lowercase English letters.
+#
+# Return the minimum number of characters that need to be appended to the end of s so that t becomes a subsequence of s.
+#
+# A subsequence is a string that can be derived from another string by deleting some or no characters without changing the order of the remaining characters.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "coaching", t = "coding"
+# Output: 4
+# Explanation: Append the characters "ding" to the end of s so that s = "coachingding".
+# Now, t is a subsequence of s ("coachingding").
+# It can be shown that appending any 3 characters to the end of s will never make t a subsequence.
+# Example 2:
+#
+# Input: s = "abcde", t = "a"
+# Output: 0
+# Explanation: t is already a subsequence of s ("abcde").
+# Example 3:
+#
+# Input: s = "z", t = "abcde"
+# Output: 5
+# Explanation: Append the characters "abcde" to the end of s so that s = "zabcde".
+# Now, t is a subsequence of s ("zabcde").
+# It can be shown that appending any 4 characters to the end of s will never make t a subsequence.
+#
+#
+# Constraints:
+#
+# 1 <= s.length, t.length <= 105
+# s and t consist only of lowercase English letters.
