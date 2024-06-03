1c01f9483cea8d49db8ddd6fb5e3d0d22f0a1821
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-03 06:53:53 +0300

    Added task 2343 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -50543,7 +50543,7 @@ class Solution:
                 start, end = meetings[i]
         days -= end - start + 1
         return days
-        
+    
 # 3170. Lexicographically Minimum String After Removing Stars
 # You are given a string s. It may contain any number of '*' characters. Your task is to remove all '*' characters.
 #
@@ -50594,3 +50594,36 @@ class Solution:
                 heapq.heappush(heap, (s[i], -i))
         heap.sort(key=lambda x: -x[1])
         return ''.join(i[0] for i in heap)
+        
+# A. Stones on the Table
+# A. Stones on the Table
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# inputstandard input
+# outputstandard output
+# There are n stones on the table in a row, each of them can be red, green or blue. Count the minimum number of stones to take from the table so that any two neighboring stones had different colors. Stones in a row are considered neighboring if there are no other stones between them.
+# 
+# Input
+# The first line contains integer n (1 ≤ n ≤ 50) — the number of stones on the table.
+# 
+# The next line contains string s, which represents the colors of the stones. We'll consider the stones in the row numbered from 1 to n from left to right. Then the i-th character s equals "R", if the i-th stone is red, "G", if it's green and "B", if it's blue.
+# 
+# Output
+# Print a single integer — the answer to the problem.
+# 
+# Examples
+# inputCopy
+# 3
+# RRG
+# outputCopy
+# 1
+# inputCopy
+# 5
+# RRRRR
+# outputCopy
+# 4
+# inputCopy
+# 4
+# BRBG
+# outputCopy
+# 0
