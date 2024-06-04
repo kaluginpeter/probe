1cf1a5ecd4f5d80c6a061f7306010ee058f086d8
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-04 07:51:52 +0300

    Added task 2348 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -50830,3 +50830,17 @@ while n > 0:
     n %= divider
     divider -= 1
 sys.stdout.write(str(answer))
+
+# Tree to list
+# Given the root of a tree with an arbitrary number of child nodes, return a list containing the nodes' data in breadth-first order (left to right, top to bottom).
+#
+# Child nodes are stored in a list. An empty tree is represented by an empty list.
+#
+# Example:
+#
+#            1
+#           / \
+#          2   3  -> [1,2,3,4,5,6,7]
+#         /|\   \
+#        4 5 6   7
+# TREESFUNDAMENTALS
