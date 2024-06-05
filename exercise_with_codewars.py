aca912c6f154c13d42536f2fbc784c4ca4c3a0f2
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-05 07:05:13 +0300

    Added task 2349 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -50861,3 +50861,24 @@ def tree_to_list(tree_root):
         current = nxt
         nxt = []
     return answer
+    
+# 1002. Find Common Characters
+# Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
+#
+#
+#
+# Example 1:
+#
+# Input: words = ["bella","label","roller"]
+# Output: ["e","l","l"]
+# Example 2:
+#
+# Input: words = ["cool","lock","cook"]
+# Output: ["c","o"]
+#
+#
+# Constraints:
+#
+# 1 <= words.length <= 100
+# 1 <= words[i].length <= 100
+# words[i] consists of lowercase English letters.
