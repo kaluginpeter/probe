58437ce4b02711640dc9f421b91ebc91f16c3766
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-05 07:05:44 +0300

    Complete task 2349 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -50882,3 +50882,20 @@ def tree_to_list(tree_root):
 # 1 <= words.length <= 100
 # 1 <= words[i].length <= 100
 # words[i] consists of lowercase English letters.
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        letter_storage: list[list[int]] = [[] for _ in range(27)] # total storage
+        n: int = 0 # length of words
+        for word in words:
+            n += 1
+            # local storage to each word
+            local_letter_storage: list[int] = [0 for _ in range(27)]
+            for letter in word: # count all frequences of each charackter in one word
+                local_letter_storage[ord(letter) - 96] += 1
+            for char in range(27): # Adding result of local storage to total storage
+                letter_storage[char] += [local_letter_storage[char]]
+        common_characters: list[int] = [] # Answer array
+        for char in range(27):
+            if len(letter_storage[char]) == n: # if character contained in all words
+                common_characters.extend([chr(char + 96)] * min(letter_storage[char])) # addind minimum frequence
+        return common_characters
