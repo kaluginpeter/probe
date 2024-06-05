44f4fef70bc9f2248cf2096fc6be51b63ffaa47d
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-05 17:32:34 +0300

    Added task 2351 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -50861,7 +50861,7 @@ def tree_to_list(tree_root):
         current = nxt
         nxt = []
     return answer
-    
+
 # 1002. Find Common Characters
 # Given a string array words, return an array of all characters that show up in all strings within the words (including duplicates). You may return the answer in any order.
 #
@@ -50899,7 +50899,7 @@ class Solution:
             if len(letter_storage[char]) == n: # if character contained in all words
                 common_characters.extend([chr(char + 96)] * min(letter_storage[char])) # addind minimum frequence
         return common_characters
-        
+
 # A. Soldier and Bananas
 # A. Soldier and Bananas
 # time limit per test1 second
@@ -50927,3 +50927,47 @@ total: int = 0
 for i in range(1, w + 1):
     total += i * k
 sys.stdout.write(str(max(0, total - n)))
+
+# Simple Fun #150: Robot Transfer
+# Task
+# We have a N×N matrix (N<10) and a robot.
+#
+# We wrote in each point of matrix x and y coordinates of a point of matrix.
+#
+# When robot goes to a point of matrix, reads x and y and transfer to point with x and y coordinates.
+#
+# For each point in the matrix we want to know if robot returns back to it after EXACTLY k moves. So your task is to count points to which Robot returns in EXACTLY k moves.
+#
+# You should stop counting moves as soon as the robot returns to the starting point. That is, if the robot returns to the starting point in fewer than k moves, that point should not count as a valid point.
+#
+# example
+# For:
+#
+# matrix=[
+# ["0,1","0,0","1,2"],
+# ["1,1","1,0","0,2"],
+# ["2,1","2,0","0,0"]]
+# k= 2
+# The result should be 8
+#
+# Robot start at (0,0) --> (0,1) --> (0,0), total 2 moves
+# Robot start at (0,1) --> (0,0) --> (0,1), total 2 moves
+# Robot start at (0,2) --> (1,2) --> (0,2), total 2 moves
+# Robot start at (1,2) --> (0,2) --> (1,2), total 2 moves
+# Robot start at (1,0) --> (1,1) --> (1,0), total 2 moves
+# Robot start at (1,1) --> (1,0) --> (1,1), total 2 moves
+# Robot start at (2,0) --> (2,1) --> (2,0), total 2 moves
+# Robot start at (2,1) --> (2,0) --> (2,1), total 2 moves
+# Robot start at (2,2) --> (0,0) --> (0,1) --> (0,0) --> (0,1) ....
+# (Robot can not transfer back to 2,2)
+# So the result is 8.
+#
+# Input/Output
+# [input] 2D integer array matrix
+# n x n matrix. 3 <= n <=9
+#
+# [input] integer k
+# 2 <= k <= 5
+#
+# [output] an integer
+# PUZZLES
