6063f8845ebb0fd69e89391c480be667572c78bb
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-09 07:18:27 +0300

    Complete task 2366 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -51724,3 +51724,14 @@ sys.stdout.write(['NO', 'YES']['1111111' in n or '0000000' in n])
 #
 # 2 <= n <= 50
 # 1 <= k <= 50
+class Solution:
+    def numberOfChild(self, n: int, k: int) -> int:
+        reverse: bool = False
+        cur: int = 0
+        for _ in range(k):
+            cur += 1 if not reverse else -1
+            if cur == n - 1:
+                reverse = True
+            elif cur == 0:
+                reverse = False
+        return cur
