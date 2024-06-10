6a56a0765d3b25703b936efc8dad92ff2b468a38
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-10 06:42:00 +0300

    Complete task 2369 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -51865,3 +51865,13 @@ def weight_table(n):return[[min(i,n-1-i,j,n-1-j)for j in range(n)]for i in range
 #
 # 1 <= heights.length <= 100
 # 1 <= heights[i] <= 100
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        correct_height: list[int] = []
+        total_students: list[int] = [0 for _ in range(max(heights) + 1)]
+        for student in heights:
+            total_students[student] += 1
+        for student in range(len(total_students)):
+            if total_students[student] > 0:
+                correct_height += [student] * total_students[student]
+        return sum(correct_height[idx] != heights[idx] for idx in range(len(heights)))
