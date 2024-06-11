0a8278e4ba25576992819f011b602328d4434c0b
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-11 19:49:09 +0300

    Added task 2376 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -52133,7 +52133,7 @@ def minimum_bonus(scores):
                 swaped = True
         if not swaped:
             return sum(ht.values())
-            
+
 # 63. Unique Paths II
 # You are given an m x n integer array grid. There is a robot initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
 #
@@ -52181,3 +52181,29 @@ class Solution:
                 current_row[col] = current_row[col + 1] + prev_row[col]
             prev_row = current_row
         return prev_row[0]
+
+# 198. House Robber
+# You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+#
+# Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [1,2,3,1]
+# Output: 4
+# Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+# Total amount you can rob = 1 + 3 = 4.
+# Example 2:
+#
+# Input: nums = [2,7,9,3,1]
+# Output: 12
+# Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+# Total amount you can rob = 2 + 9 + 1 = 12.
+#
+#
+# Constraints:
+#
+# 1 <= nums.length <= 100
+# 0 <= nums[i] <= 400
