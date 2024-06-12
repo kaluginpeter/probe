02380abb391152ac8d27065cf6f60dbdd72a9096
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-12 10:54:49 +0300

    Added task 2378 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -52233,7 +52233,7 @@ class Solution:
         for idx in range(2, len(nums)):
             nums[idx] = max(nums[idx - 1], nums[idx] + nums[idx - 2])
         return nums[-1]
-        
+
 # 75. Sort Colors
 # Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
 #
@@ -52278,3 +52278,34 @@ class Solution:
                 twos -= 1
             if current_idx < n and nums[current_idx] == 1:
                 current_idx += 1
+                
+# A. Translation
+# A. Translation
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# inputstandard input
+# outputstandard output
+# The translation from the Berland language into the Birland language is not an easy task. Those languages are very similar: a berlandish word differs from a birlandish word with the same meaning a little: it is spelled (and pronounced) reversely. For example, a Berlandish word code corresponds to a Birlandish word edoc. However, it's easy to make a mistake during the «translation». Vasya translated word s from Berlandish into Birlandish as t. Help him: find out if he translated the word correctly.
+#
+# Input
+# The first line contains word s, the second line contains word t. The words consist of lowercase Latin letters. The input data do not consist unnecessary spaces. The words are not empty and their lengths do not exceed 100 symbols.
+#
+# Output
+# If the word t is a word s, written reversely, print YES, otherwise print NO.
+#
+# Examples
+# inputCopy
+# code
+# edoc
+# outputCopy
+# YES
+# inputCopy
+# abb
+# aba
+# outputCopy
+# NO
+# inputCopy
+# code
+# code
+# outputCopy
+# NO
