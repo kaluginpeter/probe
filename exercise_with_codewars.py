d8fe73aac209a190082265ce5c9f6cf33b58ed42
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-14 17:23:27 +0300

    Added task 2383 4kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -52494,3 +52494,17 @@ for fr in friends:
     else:
         total += 1
 sys.stdout.write(str(total))
+
+# Path Finder #2: shortest path
+# Task
+# You are at position [0, 0] in maze NxN and you can only move in one of the four cardinal directions (i.e. North, East, South, West). Return the minimal number of steps to exit position [N-1, N-1] if it is possible to reach the exit from the starting position. Otherwise, return false.
+#
+# Empty positions are marked .. Walls are marked W. Start and exit positions are guaranteed to be empty in all test cases.
+#
+# Path Finder Series:
+# #1: can you reach the exit?
+# #2: shortest path
+# #3: the Alpinist
+# #4: where are you?
+# #5: there's someone here
+# ALGORITHMS
