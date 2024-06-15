c8407a802034aa293614e7a5a81782fee1e09e3b
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-15 12:14:26 +0300

    Added task 2387 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -52610,7 +52610,7 @@ class Solution:
                 total += nxt_uniq_number - nums[idx]
                 nums[idx] = nxt_uniq_number
         return total
-        
+
 # 502. IPO
 # Suppose LeetCode will start its IPO soon. In order to sell a good price of its shares to Venture Capital, LeetCode would like to work on some projects to increase its capital before the IPO. Since it has limited resources, it can only finish at most k distinct projects before the IPO. Help LeetCode design the best way to maximize its total capital after finishing at most k distinct projects.
 # 
@@ -52711,3 +52711,29 @@ class Solution:
             w += -heapq.heappop(max_profit_projects)
             k -= 1
         return w
+        
+# A. Beautiful Year
+# A. Beautiful Year
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# inputstandard input
+# outputstandard output
+# It seems like the year of 2013 came only yesterday. Do you know a curious fact? The year of 2013 is the first year after the old 1987 with only distinct digits.
+#
+# Now you are suggested to solve the following problem: given a year number, find the minimum year number which is strictly larger than the given one and has only distinct digits.
+#
+# Input
+# The single line contains integer y (1000 ≤ y ≤ 9000) — the year number.
+#
+# Output
+# Print a single integer — the minimum year number that is strictly larger than y and all it's digits are distinct. It is guaranteed that the answer exists.
+#
+# Examples
+# inputCopy
+# 1987
+# outputCopy
+# 2013
+# inputCopy
+# 2013
+# outputCopy
+# 2014
