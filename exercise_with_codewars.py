630322428c0a62292a54d398c6f15786a457dd04
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-15 14:09:48 +0300

    Added task 2388 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -52711,7 +52711,7 @@ class Solution:
             w += -heapq.heappop(max_profit_projects)
             k -= 1
         return w
-        
+
 # A. Beautiful Year
 # A. Beautiful Year
 # time limit per test2 seconds
@@ -52759,3 +52759,22 @@ def solution(current_year: int) -> str:
 if __name__ == '__main__':
     current_year: int = int(sys.stdin.readline().rstrip())
     sys.stdout.write(solution(current_year))
+
+# Is there enough ink?
+# Is there enough ink for printing the image?
+# 
+# You will get an image and the level of ink for each primary color in the tank of the printer. The image is a matrix where each cell is the color of a pixel. The color is a string of a RGB hexadecimal notation (e.g white is 'ffffff' and black is '000000'). Each primary color is a integer.
+# 
+# A pixel with the color code 'fefdfc' need 1 unit of Red, 2 units of Green and 3 units of Blue.
+# 
+# A pixel with the color code '00ff01' need 255 units of Red, 0 units of Green and 254 units of Blue.
+# 
+# The image is two dimensional. E.g: image = [["ffffff", "ffffff"], ["ffffff", "ffffff"]]
+# 
+# Your task is to determine if they are enough ink in the tank to print the image.
+# 
+# If the ink is enough, the "enough_ink()" function should return True. False otherwise.
+# 
+# Have fun!
+# 
+# FUNDAMENTALS
