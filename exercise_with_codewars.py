60b1afdcc4e8b8a48728d7747f241ec847e453c9
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-18 18:35:24 +0300

    Added task 2404 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -53504,7 +53504,7 @@ class Solution:
             if work >= most_payment_job[0]:
                 total_profit += most_payment_job[1]
         return total_profit
-        
+
 # A. In Search of an Easy Problem
 # A. In Search of an Easy Problem
 # time limit per test1 second
@@ -53560,3 +53560,28 @@ def solution(n: str) -> bool:
 if __name__ == '__main__':
     _ = sys.stdin.readline()
     sys.stdout.write(solution(sys.stdin.readline().rstrip()))
+
+# Temperature converter
+# Write a function convert_temp(temp, from_scale, to_scale) converting temperature from one scale to another. Return converted temp value.
+#
+# Round converted temp value to an integer(!).
+#
+# Reading: http://en.wikipedia.org/wiki/Conversion_of_units_of_temperature
+#
+# possible scale inputs:
+#     "C"  for Celsius
+#     "F"  for Fahrenheit
+#     "K"  for Kelvin
+#     "R"  for Rankine
+#     "De" for Delisle
+#     "N"  for Newton
+#     "Re" for Réaumur
+#     "Ro" for Rømer
+# temp is a number, from_scale and to_scale are strings.
+#
+# convert_temp(   100, "C",  "F") # => 212
+# convert_temp(    40, "Re", "C") # => 50
+# convert_temp(    60, "De", "F") # => 140
+# convert_temp(373.15, "K",  "N") # => 33
+# convert_temp(   666, "K",  "K") # => 666
+# FUNCTIONAL PROGRAMMINGDATA STRUCTURESSTRINGSMATHEMATICSFUNDAMENTALS
