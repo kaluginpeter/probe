8d3e4dca96622ad51d5001e7733fcff6f5de213f
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-19 18:37:05 +0300

    Added task 2406 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -53609,7 +53609,7 @@ def convert_temp(temp, from_scale, to_scale):
     }
     celsius = scale.get(from_scale).get('C')(temp)
     return int(scale.get('C').get(to_scale)(celsius))
-    
+
 # 1482. Minimum Number of Days to Make m Bouquets
 # You are given an integer array bloomDay, an integer m and an integer k.
 # 
@@ -53688,3 +53688,21 @@ class Solution:
             else:
                 left = middle + 1
         return left
+
+# Calculate Parity bit!
+# A parity bit, or check bit, is a bit added to a string of bits to ensure that the total number of 1-bits in the string is even or odd. Parity bits are used as the simplest form of error detecting code.
+#
+# You have two parameters, one being the wanted parity (always 'even' or 'odd'), and the other being the binary representation of the number you want to check.
+#
+# Your task is to return an integer (0 or 1), whose parity bit you need to add to the binary representation so that the parity of the resulting string is as expected.
+#
+# Example:
+#
+#   Parity: 'even'
+#   Bin: '0101010'
+#   Result: 1
+# Because there is an odd number of 1-bits (3) you need to put another 1 to it to get an even number of 1-bits.
+#
+# For more information: https://en.wikipedia.org/wiki/Parity_bit
+#
+# FUNDAMENTALS
