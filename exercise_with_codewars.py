44fea068be5dadcddc1b5fcf8d805a3a70750054
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-22 12:13:01 +0300

    Added task 2415 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -54116,3 +54116,34 @@ def solution(n: int) -> str:
 if __name__ == '__main__':
     n: int = int(sys.stdin.readline().rstrip())
     sys.stdout.write(solution(n))
+
+# Simple Fun #6: Is Infinite Process?
+# Task
+# Given integers a and b, determine whether the following pseudocode results in an infinite loop
+# 
+#  while (a !== b){
+#      a++
+#      b--
+#  }
+# Assume that the program is executed on a virtual machine which can store arbitrary long numbers and execute forever.
+# 
+# Example
+# For a = 2 and b = 6, the output should be
+# 
+# isInfiniteProcess(a, b) = false (or equivalent in PHP)
+# 
+# For a = 2 and b = 3, the output should be
+# 
+# isInfiniteProcess(a, b) = true (or equivalent in PHP)
+# 
+# Input/Output
+# [input] integer a
+# Constraints: 0 ≤ a ≤ 100.
+# 
+# [input] integer b
+# Constraints: 0 ≤ b ≤ 100.
+# 
+# [output] a boolean value
+# true if the pseudocode will never stop, false otherwise.
+# 
+# PUZZLES
