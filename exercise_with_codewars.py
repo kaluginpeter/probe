09ab725383e16f4fc7a96c78079f12c2ca976439
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-23 08:07:32 +0300

    Added task 2423 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -54517,7 +54517,7 @@ class Solution:
             min_average = min(min_average, (nums[left] + nums[right]) / 2)
             left, right = left + 1, right - 1
         return min_average
-        
+
 # 3195. Find the Minimum Area to Cover All Ones I
 # You are given a 2D binary array grid. Find a rectangle with horizontal and vertical sides with the smallest area, such that all the 1's in grid lie inside this rectangle.
 # 
@@ -54569,3 +54569,35 @@ class Solution:
                     left = min(left, col)
         area: int = (right - left + 1) * (down - up + 1)
         return area
+
+# A. Calculating Function
+# A. Calculating Function
+# time limit per test1 second
+# memory limit per test256 megabytes
+# inputstandard input
+# outputstandard output
+# For a positive integer n let's define a function f:
+#
+# f(n) =  - 1 + 2 - 3 + .. + ( - 1)nn
+#
+# Your task is to calculate f(n) for a given integer n.
+#
+# Input
+# The single line contains the positive integer n (1 ≤ n ≤ 1015).
+#
+# Output
+# Print f(n) in a single line.
+#
+# Examples
+# inputCopy
+# 4
+# outputCopy
+# 2
+# inputCopy
+# 5
+# outputCopy
+# -3
+# Note
+# f(4) =  - 1 + 2 - 3 + 4 = 2
+#
+# f(5) =  - 1 + 2 - 3 + 4 - 5 =  - 3
