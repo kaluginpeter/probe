3d2929081338f86b25039085d5f812a83b3b911a
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-23 07:49:40 +0300

    Added task 2421 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -54364,7 +54364,7 @@ class Solution:
                 count += 1
             idx += 1
         return count
-        
+
 # 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
 # Given an array of integers nums and an integer limit, return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
 #
@@ -54448,3 +54448,63 @@ class Solution:
                 left_pointer += 1
             max_length_of_subarray = max(max_length_of_subarray, right_pointer - left_pointer + 1)
         return max_length_of_subarray
+        
+# 3194. Minimum Average of Smallest and Largest Elements
+# You have an array of floating point numbers averages which is initially empty. You are given an array nums of n integers where n is even.
+# 
+# You repeat the following procedure n / 2 times:
+# 
+# Remove the smallest element, minElement, and the largest element maxElement, from nums.
+# Add (minElement + maxElement) / 2 to averages.
+# Return the minimum element in averages.
+# 
+#  
+# 
+# Example 1:
+# 
+# Input: nums = [7,8,3,4,15,13,4,1]
+# 
+# Output: 5.5
+# 
+# Explanation:
+# 
+# step	nums	averages
+# 0	[7,8,3,4,15,13,4,1]	[]
+# 1	[7,8,3,4,13,4]	[8]
+# 2	[7,8,4,4]	[8,8]
+# 3	[7,4]	[8,8,6]
+# 4	[]	[8,8,6,5.5]
+# The smallest element of averages, 5.5, is returned.
+# Example 2:
+# 
+# Input: nums = [1,9,8,3,10,5]
+# 
+# Output: 5.5
+# 
+# Explanation:
+# 
+# step	nums	averages
+# 0	[1,9,8,3,10,5]	[]
+# 1	[9,8,3,5]	[5.5]
+# 2	[8,5]	[5.5,6]
+# 3	[]	[5.5,6,6.5]
+# Example 3:
+# 
+# Input: nums = [1,2,3,7,8,9]
+# 
+# Output: 5.0
+# 
+# Explanation:
+# 
+# step	nums	averages
+# 0	[1,2,3,7,8,9]	[]
+# 1	[2,3,7,8]	[5]
+# 2	[3,7]	[5,5]
+# 3	[]	[5,5,5]
+#  
+# 
+# Constraints:
+# 
+# 2 <= n == nums.length <= 50
+# n is even.
+# 1 <= nums[i] <= 50
