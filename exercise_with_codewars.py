b1d3e59321e651e29b3135dda5529377898282a4
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-24 18:11:11 +0300

    Added task 2427 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -54705,7 +54705,7 @@ class Solution:
             elif nums[idx] == 0:
                 return -1
         return total_flips
-        
+
 # A. Even Odds
 # A. Even Odds
 # time limit per test1 second
@@ -54746,3 +54746,23 @@ def solution(n: int, k: int) -> str:
 if __name__ == '__main__':
     n, k = map(int, sys.stdin.readline().rstrip().split())
     sys.stdout.write(solution(n, k))
+
+# Simple Fun #182: Happy "g"
+# Task
+# Let's say that "g" is happy in the given string, if there is another "g" immediately to the right or to the left of it.
+#
+# Find out if all "g"s in the given string are happy.
+#
+# Example
+# For str = "gg0gg3gg0gg", the output should be true.
+#
+# For str = "gog", the output should be false.
+#
+# Input/Output
+# [input] string str
+# A random string of lower case letters, numbers and spaces.
+#
+# [output] a boolean value
+# true if all "g"s are happy, false otherwise.
+#
+# PUZZLESSTRINGSREGULAR EXPRESSIONS
