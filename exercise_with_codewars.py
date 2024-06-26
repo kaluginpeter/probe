075e2ecda1985bbf7c96208863ff0c7f95e80c87
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-26 06:58:13 +0300

    Added task 2434 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -54948,7 +54948,7 @@ class Solution:
             dfs(root.left)
         dfs(root)
         return root
-        
+
 # A. Presents
 # A. Presents
 # time limit per test2 seconds
@@ -55118,3 +55118,28 @@ def solution(t: int) -> None:
 if __name__ == '__main__':
     t: int = int(sys.stdin.readline().rstrip())
     solution(t)
+    
+# 1382. Balance a Binary Search Tree
+# Given the root of a binary search tree, return a balanced binary search tree with the same node values. If there is more than one answer, return any of them.
+#
+# A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: root = [1,null,2,null,3,null,4,null,null]
+# Output: [2,1,3,null,null,null,4]
+# Explanation: This is not the only correct answer, [3,1,4,null,2] is also correct.
+# Example 2:
+#
+#
+# Input: root = [2,1,3]
+# Output: [2,1,3]
+#
+#
+# Constraints:
+#
+# The number of nodes in the tree is in the range [1, 104].
+# 1 <= Node.val <= 105
