52a9a54af7adcbc55358c84815caea0dd0057dd8
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-28 17:01:32 +0300

    Added task 2441 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -55427,7 +55427,7 @@ class Solution:
             total += frequences[vertex] * accumulate
             accumulate -= 1
         return total
-        
+
 # A. Is your horseshoe on the other hoof?
 # A. Is your horseshoe on the other hoof?
 # time limit per test2 seconds
@@ -55463,3 +55463,31 @@ def solution(shoes: list) -> str:
 if __name__ == '__main__':
     shoes: list = list(map(int, sys.stdin.readline().rstrip().split()))
     sys.stdout.write(solution(shoes))
+
+# Thue-Morse Sequence
+# Given a positive integer n, return first n dgits of Thue-Morse sequence, as a string (see examples).
+# 
+# Thue-Morse sequence is a binary sequence with 0 as the first element. The rest of the sequence is obtained by adding the boolean (binary) complement of a group obtained so far.
+# 
+# For example:
+# 
+# 0
+# 01
+# 0110
+# 01101001
+# and so on...
+# alt
+# 
+# Ex.:
+# 
+#  1 --> "0"
+#  2 --> "01"
+#  5 --> "01101"
+# 10 --> "0110100110"
+# You don't need to test if n is valid - it will always be a positive integer.
+# n will be between 1 and 10000
+# Thue-Morse on Wikipedia
+# 
+# Another kata on Thue-Morse by @myjinxin2015
+# 
+# STRINGSBINARYALGORITHMS
