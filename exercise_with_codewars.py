97587da5fa49fee0cf2bf4f73824162813a5f29b
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-30 09:41:23 +0300

    Added task 2445 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -55708,3 +55708,34 @@ def solve(time):
             else:
                 second_part = f'{tens[second_part // 10]} {string_representation[second_part % 10]}'
         return f"{second_part} minutes {'to' if more_than_half else 'past'} {string_representation[first_part]}"
+        
+# 684. Redundant Connection
+# In this problem, a tree is an undirected graph that is connected and has no cycles.
+#
+# You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
+#
+# Return an edge that can be removed so that the resulting graph is a tree of n nodes. If there are multiple answers, return the answer that occurs last in the input.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: edges = [[1,2],[1,3],[2,3]]
+# Output: [2,3]
+# Example 2:
+#
+#
+# Input: edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
+# Output: [1,4]
+#
+#
+# Constraints:
+#
+# n == edges.length
+# 3 <= n <= 1000
+# edges[i].length == 2
+# 1 <= ai < bi <= edges.length
+# ai != bi
+# There are no repeated edges.
+# The given graph is connected.
