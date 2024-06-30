bd542236fa52b43029459eb09103ecc43c562a84
Peter Kalugin <streetworkout200412@gmail.com>
2024-06-30 10:33:01 +0300

    Complete task 2448 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -55977,3 +55977,12 @@ class Solution:
 # 3
 # outputCopy
 # I hate that I love that I hate it
+import sys
+
+def solution(n: int) -> str:
+    words: list = [['I love', 'I hate'][i & 1] for i in range(1, n + 1)]
+    return ' that '.join(words) + ' it'
+
+if __name__ == '__main__':
+    n: int = int(sys.stdin.readline().rstrip())
+    sys.stdout.write(solution(n))
