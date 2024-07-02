1e0c0e7ac3d4cc57013ff1252d00b6765e47a63f
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-02 18:14:20 +0300

    Added task 2454 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -56278,3 +56278,28 @@ class Solution:
             if frequences_in_second_array > 0:
                 duplicate.extend([target] * min(frequences_in_first_array, frequences_in_second_array))
         return duplicate
+
+# Asyncio - The Beginning
+# Story
+# As you walk the streets with your crush beside you, you are thinking about the world and how everything works... Wait!! Your crush? Shit, you are dreaming again.
+# 
+# Task
+# Now implement a coroutine dreaming which sleeps for n seconds and then returns m ** n without entirely blocking the execution of other coroutines that might be running.
+# 
+# Note
+# 0 < n < 7
+# 
+# 0 < m < 100
+# 
+# ASYNCHRONOUSStory
+# As you walk the streets with your crush beside you, you are thinking about the world and how everything works... Wait!! Your crush? Shit, you are dreaming again.
+# 
+# Task
+# Now implement a coroutine dreaming which sleeps for n seconds and then returns m ** n without entirely blocking the execution of other coroutines that might be running.
+# 
+# Note
+# 0 < n < 7
+# 
+# 0 < m < 100
+# 
+# ASYNCHRONOUS
