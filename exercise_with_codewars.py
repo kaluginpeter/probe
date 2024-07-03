860e4e877de869ca3bfeab1080d5f0704a87ccd3
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-03 18:29:58 +0300

    Added task 2458 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -56428,7 +56428,7 @@ class Solution:
         c: int = nums[-2] - nums[2] # Third last with thrid first
         d: int = nums[-1] - nums[3] # Last with fourth first
         return min(a, b, c, d)
-        
+
 # A. I Wanna Be the Guy
 # A. I Wanna Be the Guy
 # time limit per test1 second
@@ -56476,3 +56476,20 @@ if __name__ == '__main__':
     y: list = list(map(int, sys.stdin.readline().rstrip().split()))
     y_n: int = y[0]
     sys.stdout.write(solution(n, x_n, x[1:], y_n, y[1:]))
+    
+# Tree-D Printer
+# Your task is simple enough. You will write a function which takes a tree as its input. Your function should output one string with the values of the nodes in an order corresponding to a breadth first search. This string also should be broken into levels corresponding to the depth of the nodes tree, and there should be a space between each value. For example if the following tree were input to our function :
+# 
+# alt text
+# 
+# the output would be a string and look something like this:
+# 
+# 2 
+# 7 5 
+# 2 6 9 
+# 5 11 4 
+# All nodes in the tree will have two properties, .value and .children. You may assume the .value property will always be either a string or a number. The .children property will be an array containing all of the node's children. The tree is NOT necessarily a binary tree, each node may have many children nodes
+# 
+# You may implement whatever method you see fit to search the tree. There are both breadth first and depth first approaches.
+# 
+# ALGORITHMS
