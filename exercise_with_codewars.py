303d912e139f67adb89ab9b16ed93c5e262c74ae
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-04 17:12:58 +0300

    Added task 2460 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -56519,7 +56519,7 @@ def tree_printer(tree : TreeNode) -> str:
         current_nodes = next_nodes
         next_nodes = []
     return '\n'.join(output_array)
-    
+
 # 2181. Merge Nodes in Between Zeros
 # You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
 #
@@ -56577,3 +56577,23 @@ class Solution:
                 tmp = tmp.next
             local_tmp = local_tmp.next
         return head
+
+# Lojban Numbers
+# Lojban Numbers
+# Counting in Lojban, an artificial language developed over the last forty years, is easier than in most languages. The numbers from zero to nine are:
+# 
+# 1 pa 4 vo 7 ze
+# 2 re 5 mu 8 bi 0 no
+# 3 ci 6 xa 9 so
+# Larger numbers are created by gluing the digits together. For example, 123 is pareci.
+# 
+# Write a program that reads in a Lojban string (representing a number less than or equal to 1,000,000) and outputs it in numbers.
+# 
+# Example:
+# renonore  # Lojban string
+# 2002  # Number
+# Input/Output
+# [input] string representing the number in Lojban pareci
+# Constraints: Lojban number ≤ 1,000,000
+# [output] integer representing the Lojban number 123
+# Source: 2002 British Informatics Olympiad
