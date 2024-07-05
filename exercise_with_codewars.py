2c9a82002b75c7a11abf9d40691a6d9a43337f2d
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-05 17:37:04 +0300

    Added task 2464 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -56719,7 +56719,7 @@ class Solution:
         max_distance: int = distances[-1] - distances[0]
         min_distance: int = min(y - x for x, y in zip(distances, distances[1::]))
         return [min_distance, max_distance]
-        
+
 # A. Arrival of the General
 # A. Arrival of the General
 # time limit per test2 seconds
@@ -56787,3 +56787,21 @@ if __name__ == '__main__':
     n: int = int(sys.stdin.readline().rstrip())
     soldiers: list = list(map(int, sys.stdin.readline().rstrip().split()))
     sys.stdout.write(solution(n, soldiers))
+
+# [Code Golf] Knight's Next Position
+# The knight is a piece in the game of chess. It may move two squares vertically and one square horizontally or two squares horizontally and one square vertically. For a visual representation of the possible moves, take a look here (second image).
+# 
+# Given the current position of the knight on the chessboard (e.g."b8") return all its possible next positions, alphabetically sorted (e.g. ["a6", "c6", "d7"])
+# 
+# Your code can be maximum 170 150 bytes long (I underestimated your skills ;-)
+# 
+# 
+# Note: this is the code golf version of this kata
+# 
+# My other katas
+# If you enjoyed this kata then please try my other katas! :-)
+# 
+# Translations are welcome!
+# ...except for Ruby, which will arrive soon
+# 
+# RESTRICTEDPUZZLES
