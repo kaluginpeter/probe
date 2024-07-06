d13d5663c0851de90f5d67f6c009692607996a91
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-06 10:00:32 +0300

    Added task 2467 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -56862,7 +56862,7 @@ class Solution:
         whole: int = time // (n - 1)
         remainder: int = time % (n - 1)
         return 1 + remainder if whole & 1 == 0 else n - remainder
-        
+
 # A. Expression
 # A. Expression
 # time limit per test1 second
@@ -56915,3 +56915,19 @@ if __name__ == '__main__':
     b: int = int(sys.stdin.readline().rstrip())
     c: int = int(sys.stdin.readline().rstrip())
     sys.stdout.write(solution(a, b, c))
+
+# Fun with trees: is perfect
+# A perfect binary tree is a binary tree in which all interior nodes have two children and all leaves have the same depth or same level.
+# 
+# You are given a class called TreeNode. Implement the method isPerfect which determines if a given tree denoted by its root node is perfect.
+# 
+# Note: TreeNode class contains helper methods for tree creation, which might be handy for your solution. Feel free to update those methods, but make sure you keep their signatures intact (since they are used from within test cases).
+# 
+# You can (and should) add more tests to validate your solution, since not all cases are covered in the example test cases.
+# 
+# This kata is part of fun with trees series:
+# 
+# Fun with trees: max sum
+# Fun with trees: array to tree
+# Fun with trees: is perfect
+# TREESRECURSIONBINARY TREESBINARY SEARCH TREESDATA STRUCTURESALGORITHMS
