90481ae129cf0e6e37de627a4839896cac3695cc
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-07 10:12:18 +0300

    Added task 2479 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -57677,3 +57677,27 @@ class Solution:
                 count_y = prefix_y[i][j] - prefix_y[0][j] - prefix_y[i][0] + prefix_y[0][0]
                 if count_x == count_y and count_x > 0: valid_submatrices += 1
         return valid_submatrices
+
+# Binary Tree Serpentine Traversal
+# Given the root node of a binary tree, write a function that will traverse the tree in a serpentine fashion. You could also think of this as a zig-zag.
+# 
+# We want to visit the first level from left to right, the second level from right to left, third level from left to right, and so on...
+# 
+# The function will return a list of the visited nodes in serpentine order.
+# 
+# The tree is not necessarily balanced. It is very important that you start the first level from left to right!
+# 
+# A Node has 3 properties: data (a number or string) left (a reference to the left child) right (a refencence to the right child)
+# 
+# The empty reference if there is no child is undefined (JS) / None (Python)
+# 
+# Example:
+# 
+#     A
+#    /\
+#   B  C
+#  /\  /\
+# D E F G
+# Should result in ["A", "C", "B", "D", "E", "F", "G"]
+# 
+# BINARY TREESTREESRECURSIONALGORITHMS
