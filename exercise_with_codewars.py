1a7b3701ef4d68a51c64fd88e1ae66b5c2eb5a03
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-07 07:27:34 +0300

    Added task 2478 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -57560,7 +57560,7 @@ class Solution:
     def getEncryptedString(self, s: str, k: int) -> str:
         n: int = len(s)
         return ''.join(s[(idx + k) % n] for idx in range(n))
-        
+
 # 3211. Generate Binary Strings Without Adjacent Zeros
 # You are given a positive integer n.
 # 
@@ -57613,3 +57613,50 @@ class Solution:
             elif s[-1] == '0': valid_strings.append(s + '1')
         self.memo[n] = valid_strings
         return valid_strings
+        
+# 3212. Count Submatrices With Equal Frequency of X and Y
+# Given a 2D character matrix grid, where grid[i][j] is either 'X', 'Y', or '.', return the number of 
+# submatrices
+#  that contains:
+# 
+# grid[0][0]
+# an equal frequency of 'X' and 'Y'.
+# at least one 'X'.
+#  
+# 
+# Example 1:
+# 
+# Input: grid = [["X","Y","."],["Y",".","."]]
+# 
+# Output: 3
+# 
+# Explanation:
+# 
+# 
+# 
+# Example 2:
+# 
+# Input: grid = [["X","X"],["X","Y"]]
+# 
+# Output: 0
+# 
+# Explanation:
+# 
+# No submatrix has an equal frequency of 'X' and 'Y'.
+# 
+# Example 3:
+# 
+# Input: grid = [[".","."],[".","."]]
+# 
+# Output: 0
+# 
+# Explanation:
+# 
+# No submatrix has at least one 'X'.
+# 
+#  
+# 
+# Constraints:
+# 
+# 1 <= grid.length, grid[i].length <= 1000
+# grid[i][j] is either 'X', 'Y', or '.'.
