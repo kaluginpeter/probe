7b88cfd110c38ca27bfe5e372176fe32feba8485
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-07 07:08:42 +0300

    Added task 2474 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -57363,7 +57363,7 @@ class Solution:
             if right - left_pointer + 1 >= k:
                 groups += 1
         return groups
-        
+
 # 3209. Number of Subarrays With AND Value of K
 # Given an array of integers nums and an integer k, return the number of 
 # subarrays
@@ -57407,7 +57407,7 @@ class Solution:
 # 
 # 1 <= nums.length <= 105
 # 0 <= nums[i], k <= 109
-from collections import defaultdict
+ from collections import defaultdict
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
         n: int = len(nums)
@@ -57422,3 +57422,43 @@ class Solution:
             current_subs = new_subs
             count += current_subs[k]
         return count
+        
+# A. Insomnia cure
+# A. Insomnia cure
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# inputstandard input
+# outputstandard output
+# «One dragon. Two dragon. Three dragon», — the princess was counting. She had trouble falling asleep, and she got bored of counting lambs when she was nine.
+# 
+# However, just counting dragons was boring as well, so she entertained herself at best she could. Tonight she imagined that all dragons were here to steal her, and she was fighting them off. Every k-th dragon got punched in the face with a frying pan. Every l-th dragon got his tail shut into the balcony door. Every m-th dragon got his paws trampled with sharp heels. Finally, she threatened every n-th dragon to call her mom, and he withdrew in panic.
+# 
+# How many imaginary dragons suffered moral or physical damage tonight, if the princess counted a total of d dragons?
+# 
+# Input
+# Input data contains integer numbers k, l, m, n and d, each number in a separate line (1 ≤ k, l, m, n ≤ 10, 1 ≤ d ≤ 105).
+# 
+# Output
+# Output the number of damaged dragons.
+# 
+# Examples
+# inputCopy
+# 1
+# 2
+# 3
+# 4
+# 12
+# outputCopy
+# 12
+# inputCopy
+# 2
+# 3
+# 4
+# 5
+# 24
+# outputCopy
+# 17
+# Note
+# In the first case every first dragon got punched with a frying pan. Some of the dragons suffered from other reasons as well, but the pan alone would be enough.
+# 
+# In the second case dragons 1, 7, 11, 13, 17, 19 and 23 escaped unharmed.
