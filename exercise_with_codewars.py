b9fda5b96c2e91abef484f75dcb9bf3b6605138e
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-08 18:23:16 +0300

    Added task 2481 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -57722,7 +57722,7 @@ def serpentine_traversal(root_node : Node) -> list:
         current_nodes = next_nodes
         next_nodes = []
     return output
-    
+
 # 1823. Find the Winner of the Circular Game
 # There are n friends that are playing a game. The friends are sitting in a circle and are numbered from 1 to n in clockwise order. More formally, moving clockwise from the ith friend brings you to the (i+1)th friend for 1 <= i < n, and moving clockwise from the nth friend brings you to the 1st friend.
 # 
@@ -57792,3 +57792,34 @@ class Solution:
             start = (start + k - 1) % len(survivors)
             survivors.pop(start)
         return survivors[0]
+
+# Clothes size number converter
+# Description
+# You have clothes international size as text (xs, s, xxl).
+# Your goal is to return European number size as an number from that size
+#
+# Notice that there is arbitrary amount of modifiers (x), excluding m size and input can be any string
+#
+# Linearity
+# Base size for medium (m) is 38
+# (then, small (s), is 36 and large (l) is 40)
+#
+# As you can see scale is linear, modifier x continues that by adding or subtracting 2 from resulting size
+#
+# Invalid cases
+# Function should handle wrong/invalid sizes
+#
+# Valid input has any base size (s/m/l) and any amount of modifiers (x) before base size (like xxl).
+# Notice that you cannot apply modifier for m size, consider that case as invalid!
+# Anything else is disallowed and should be considered as invalid (None for Python).
+#
+# Invalid examples: xxx, sss, xm, other string
+#
+# Valid Examples
+# xs: 34
+# s: 36
+# m: 38
+# l: 40
+# xl: 42
+#
+# STRINGSFUNDAMENTALS
