76cc729b07ac1b06f7694edbd43a3f6dec20aa4a
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-09 18:43:25 +0300

    Added task 2486 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -58045,3 +58045,16 @@ if __name__ == '__main__':
     n: int = int(sys.stdin.readline().rstrip())
     arr: list = list(map(int, sys.stdin.readline().rstrip().split()))
     sys.stdout.write(solution(n, arr))
+
+# Personalized brand list
+# Assume you are creating a webshop and you would like to help the user in the search. You have products with brands, prices and name. You have the history of opened products (the most recently opened being the first item).
+#
+# Your task is to create a list of brands ordered by popularity, if two brands have the same popularity level then choose the one which was opened last from the two and second the other one.
+#
+# Product popularity is calculated from the history. If a product is more times in the history than it is more popular.
+#
+# Your function will have one parameter which will be always an array/list of object.
+#
+# example product: { name: "Phone", price: 25, brand: "Fake brand" }
+#
+# ALGORITHMS
