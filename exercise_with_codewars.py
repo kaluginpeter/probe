470245d2e835c0d84e418c8c6338f322d906a332
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-09 21:24:15 +0300

    Added task 2487 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -58045,7 +58045,7 @@ if __name__ == '__main__':
     n: int = int(sys.stdin.readline().rstrip())
     arr: list = list(map(int, sys.stdin.readline().rstrip().split()))
     sys.stdout.write(solution(n, arr))
-
+    
 # Personalized brand list
 # Assume you are creating a webshop and you would like to help the user in the search. You have products with brands, prices and name. You have the history of opened products (the most recently opened being the first item).
 #
@@ -58064,3 +58064,36 @@ def sorted_brands(history):
     store = list(storage.keys())
     store.sort(key=lambda x: storage[x], reverse=True)
     return store
+    
+# 2265. Count Nodes Equal to Average of Subtree
+# Given the root of a binary tree, return the number of nodes where the value of the node is equal to the average of the values in its subtree.
+#
+# Note:
+#
+# The average of n elements is the sum of the n elements divided by n and rounded down to the nearest integer.
+# A subtree of root is a tree consisting of root and all of its descendants.
+#
+#
+# Example 1:
+#
+#
+# Input: root = [4,8,5,0,1,null,6]
+# Output: 5
+# Explanation:
+# For the node with value 4: The average of its subtree is (4 + 8 + 5 + 0 + 1 + 6) / 6 = 24 / 6 = 4.
+# For the node with value 5: The average of its subtree is (5 + 6) / 2 = 11 / 2 = 5.
+# For the node with value 0: The average of its subtree is 0 / 1 = 0.
+# For the node with value 1: The average of its subtree is 1 / 1 = 1.
+# For the node with value 6: The average of its subtree is 6 / 1 = 6.
+# Example 2:
+#
+#
+# Input: root = [1]
+# Output: 1
+# Explanation: For the node with value 1: The average of its subtree is 1 / 1 = 1.
+#
+#
+# Constraints:
+#
+# The number of nodes in the tree is in the range [1, 1000].
+# 0 <= Node.val <= 1000
