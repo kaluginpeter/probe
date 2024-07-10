fdbfd2cf0692ad4e10cc4e9a61a73162e6714795
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-10 18:34:32 +0300

    Added task 2491 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -58261,7 +58261,7 @@ def solution(n: str) -> str:
 if __name__ == '__main__':
     n: str = sys.stdin.readline().rstrip()
     sys.stdout.write(solution(n))
-
+    
 # Best Stock Profit in Single Sale
 # You're a buyer/seller and your buisness is at stake... You need to make profit... Or at least, you need to lose the least amount of money!
 # Knowing a list of prices for buy/sell operations, you need to pick two of them. Buy/sell market is evolving across time and the list represent this evolution. First, you need to buy one item, then sell it later. Find the best profit you can do.
@@ -58290,3 +58290,28 @@ def max_profit(prices):
         else:
             profit = max(profit, price - local_min)
     return profit if profit != float('-inf') else -1
+    
+# 1315. Sum of Nodes with Even-Valued Grandparent
+# Given the root of a binary tree, return the sum of values of nodes with an even-valued grandparent. If there are no nodes with an even-valued grandparent, return 0.
+#
+# A grandparent of a node is the parent of its parent if it exists.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+# Output: 18
+# Explanation: The red nodes are the nodes with even-value grandparent while the blue nodes are the even-value grandparents.
+# Example 2:
+#
+#
+# Input: root = [1]
+# Output: 0
+#
+#
+# Constraints:
+#
+# The number of nodes in the tree is in the range [1, 104].
+# 1 <= Node.val <= 100
