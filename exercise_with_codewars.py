b0b74b74e8fc6a96e5001f3cf854f74c642609d6
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-10 17:47:48 +0300

    Added task 2490 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -58206,7 +58206,7 @@ class Solution:
             if log == '../': depth = max(depth - 1, 0)
             elif log != './': depth += 1
         return depth
-        
+
 # A. Dubstep
 # A. Dubstep
 # time limit per test2 seconds
@@ -58261,3 +58261,21 @@ def solution(n: str) -> str:
 if __name__ == '__main__':
     n: str = sys.stdin.readline().rstrip()
     sys.stdout.write(solution(n))
+
+# Best Stock Profit in Single Sale
+# You're a buyer/seller and your buisness is at stake... You need to make profit... Or at least, you need to lose the least amount of money!
+# Knowing a list of prices for buy/sell operations, you need to pick two of them. Buy/sell market is evolving across time and the list represent this evolution. First, you need to buy one item, then sell it later. Find the best profit you can do.
+#
+# Example:
+# Given an array of prices [3, 10, 8, 4], the best profit you could make would be 7 because you buy at 3 first, then sell at 10.
+#
+# Input:
+# A list of prices (integers), of length 2 or more.
+#
+# Output:
+# The result of the best buy/sell operation, as an integer.
+#
+# Note:
+# Be aware you'll face lists with several thousands of elements, so think about performance.
+#
+# ALGORITHMS
