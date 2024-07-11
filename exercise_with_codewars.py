53f94bbf781cfed8a8536f4ae7228b430eb1f72e
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-11 06:39:32 +0300

    Added task 2492 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -58290,7 +58290,7 @@ def max_profit(prices):
         else:
             profit = max(profit, price - local_min)
     return profit if profit != float('-inf') else -1
-    
+
 # 1315. Sum of Nodes with Even-Valued Grandparent
 # Given the root of a binary tree, return the sum of values of nodes with an even-valued grandparent. If there are no nodes with an even-valued grandparent, return 0.
 #
@@ -58372,3 +58372,34 @@ class Solution:
                     grandparents.append(current_node.val)
                     current_nodes.append(current_node.right)
         return sum_even_nodes
+        
+# 1190. Reverse Substrings Between Each Pair of Parentheses
+# You are given a string s that consists of lower case English letters and brackets.
+# 
+# Reverse the strings in each pair of matching parentheses, starting from the innermost one.
+# 
+# Your result should not contain any brackets.
+# 
+#  
+# 
+# Example 1:
+# 
+# Input: s = "(abcd)"
+# Output: "dcba"
+# Example 2:
+# 
+# Input: s = "(u(love)i)"
+# Output: "iloveu"
+# Explanation: The substring "love" is reversed first, then the whole string is reversed.
+# Example 3:
+# 
+# Input: s = "(ed(et(oc))el)"
+# Output: "leetcode"
+# Explanation: First, we reverse the substring "oc", then "etco", and finally, the whole string.
+#  
+# 
+# Constraints:
+# 
+# 1 <= s.length <= 2000
+# s only contains lower case English characters and parentheses.
+# It is guaranteed that all parentheses are balanced.
