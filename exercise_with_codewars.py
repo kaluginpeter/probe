a126f526205663c4b9f1e55f82ec46ee6ca7004b
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-11 17:43:36 +0300

    Added task 2494 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -58424,7 +58424,7 @@ class Solution:
             else:
                 current_substring.append(char)
         return ''.join(current_substring)
-        
+
 # A. Anton and Polyhedrons
 # A. Anton and Polyhedrons
 # time limit per test2 seconds
@@ -58493,3 +58493,29 @@ def solution(n: int) -> str:
 if __name__ == '__main__':
     n: int = int(sys.stdin.readline().rstrip())
     sys.stdout.write(solution(n))
+    
+# Decode Diagonal
+# Decode the diagonal.
+# Given a grid of characters. Output a decoded message as a string.
+# 
+# Input
+# 
+#   H Z R R Q
+#   D I F C A E A !
+#   G H T E L A E 
+#   L M N H P R F 
+#   X Z R P E
+# Output
+# 
+# HITHERE! (diagonally down right ↘ and diagonally up right ↗ if you can't go further).
+# 
+# The message ends when there is no space at the right up or down diagonal.
+# 
+# To make things even clearer: the same example, but in a simplified view
+# 
+#   H _ _ _ _
+#   _ I _ _ _ _ _ !
+#   _ _ T _ _ _ E 
+#   _ _ _ H _ R _
+#   _ _ _ _ E
+# ALGORITHMS
