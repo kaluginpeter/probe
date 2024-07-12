03349e1823012e68f1b4aa72c11416d665484833
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-12 18:12:59 +0300

    Added task 2498 4kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -58632,7 +58632,7 @@ class Solution:
     def constructMaximumBinaryTree(self, nums: List[int]) -> Optional[TreeNode]:
         tree: TreeNode = self.dfs(nums, 0, len(nums))
         return tree
-        
+
 # B. Taxi
 # B. Taxi
 # time limit per test3 seconds
@@ -58756,3 +58756,34 @@ class Solution:
                 second_stack.pop()
 
         return total_points
+
+# Sort binary tree by levels
+# You are given a binary tree:
+# 
+# class Node:
+#     def __init__(self, L, R, n):
+#         self.left = L
+#         self.right = R
+#         self.value = n
+# Your task is to return the list with elements from tree sorted by levels, which means the root element goes first, then root children (from left to right) are second and third, and so on.
+# 
+# Return empty list if root is None.
+# 
+# Example 1 - following tree:
+# 
+#                  2
+#             8        9
+#           1  3     4   5
+# Should return following list:
+# 
+# [2,8,9,1,3,4,5]
+# Example 2 - following tree:
+# 
+#                  1
+#             8        4
+#               3        5
+#                          7
+# Should return following list:
+# 
+# [1,8,4,3,5,7]
+# TREESBINARY TREESPERFORMANCEALGORITHMSSORTING
