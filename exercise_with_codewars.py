dfc3501ca2149a96490e0d3a1bb0a90ce8364704
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-13 10:23:52 +0300

    Added task 2503 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -59011,7 +59011,7 @@ class Solution:
                     break
             if not smashed_current: stack.append(asteroid)
         return stack
-        
+
 # A. Games
 # A. Games
 # time limit per test1 second
@@ -59073,3 +59073,30 @@ if __name__ == '__main__':
     n: int = int(sys.stdin.readline().rstrip())
     colors: list = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
     sys.stdout.write(solution(n, colors))
+
+
+# Fun with trees: array to tree
+# You are given an array of integers. Implement a function which creates a complete binary tree from the array (complete meaning that every level of the tree, except possibly the last, is completely filled).
+#
+# The elements of the array are to be taken left-to-right, and put into the tree top-to-bottom, left-to-right.
+#
+# For example, given the array [17, 0, -4, 3, 15] you should create the following tree:
+#
+#     17
+#    /  \
+#   0   -4
+#  / \
+# 3   15
+# A tree node type is preloaded for you:
+#
+# class Node:
+#     def __init__(self, value, left=None, right=None):
+#         self.value = value
+#         self.left = left
+#         self.right = right
+# This kata is part of fun with trees series:
+#
+# Fun with trees: max sum
+# Fun with trees: array to tree
+# Fun with trees: is perfect
+# TREESARRAYSBINARY TREESDATA STRUCTURESALGORITHMS
