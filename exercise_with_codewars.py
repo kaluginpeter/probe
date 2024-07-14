e6dcddbb9f14d95c4789a3609148d2a6b41c254a
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-14 08:40:21 +0300

    Added task 2511 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -59417,7 +59417,7 @@ class Solution:
                 v_pieces += 1
 
         return total_cost
-        
+
 # 3219. Minimum Cost for Cutting Cake II
 # There is an m x n cake that needs to be cut into 1 x 1 pieces.
 # 
@@ -59607,3 +59607,29 @@ class Solution:
             self.extend_groups(atoms, stack.pop())
 
         return ''.join(f'{atom}{atoms[atom] if atoms[atom] > 1 else ""}' for atom in sorted(atoms))
+
+# A. IQ test
+# A. IQ test
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# inputstandard input
+# outputstandard output
+# Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one of the given n numbers differs from the others. Bob observed that one number usually differs from the others in evenness. Help Bob — to check his answers, he needs a program that among the given n numbers finds one that is different in evenness.
+# 
+# Input
+# The first line contains integer n (3 ≤ n ≤ 100) — amount of numbers in the task. The second line contains n space-separated natural numbers, not exceeding 100. It is guaranteed, that exactly one of these numbers differs from the others in evenness.
+# 
+# Output
+# Output index of number that differs from the others in evenness. Numbers are numbered from 1 in the input order.
+# 
+# Examples
+# inputCopy
+# 5
+# 2 4 7 8 10
+# outputCopy
+# 3
+# inputCopy
+# 4
+# 1 2 1 1
+# outputCopy
+# 2
