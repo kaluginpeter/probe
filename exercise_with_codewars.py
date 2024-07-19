958eb952d8ce12325ecfc32b26441417d5ad40c2
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-19 07:10:29 +0300

    Added task 2529 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -60649,7 +60649,7 @@ def radii(a, b, c):
     s: float = (a + b + c) / 2
     A: float = (s*(s - a)*(s - b)*(s - c))**.5
     return (A / s, (a * b * c) / (4 * A))
-    
+
 # 1380. Lucky Numbers in a Matrix
 # Given an m x n matrix of distinct numbers, return all lucky numbers in the matrix in any order.
 #
@@ -60687,3 +60687,59 @@ class Solution:
         min_in_a_row: set[int] = set(min(row) for row in matrix)
         max_in_a_col: set[int] = set(max(matrix[row][col] for row in range(len(matrix))) for col in range(len(matrix[0])))
         return [luck_num for luck_num in min_in_a_row if luck_num in max_in_a_col]
+        
+# A. Fox And Snake
+# A. Fox And Snake
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# inputstdin
+# outputstdout
+# Fox Ciel starts to learn programming. The first task is drawing a fox! However, that turns out to be too hard for a beginner, so she decides to draw a snake instead.
+# 
+# A snake is a pattern on a n by m table. Denote c-th cell of r-th row as (r, c). The tail of the snake is located at (1, 1), then it's body extends to (1, m), then goes down 2 rows to (3, m), then goes left to (3, 1) and so on.
+# 
+# Your task is to draw this snake for Fox Ciel: the empty cells should be represented as dot characters ('.') and the snake cells should be filled with number signs ('#').
+# 
+# Consider sample tests in order to understand the snake pattern.
+# 
+# Input
+# The only line contains two integers: n and m (3 ≤ n, m ≤ 50).
+# 
+# n is an odd number.
+# 
+# Output
+# Output n lines. Each line should contain a string consisting of m characters. Do not output spaces.
+# 
+# Examples
+# inputCopy
+# 3 3
+# outputCopy
+# ###
+# ..#
+# ###
+# inputCopy
+# 3 4
+# outputCopy
+# ####
+# ...#
+# ####
+# inputCopy
+# 5 3
+# outputCopy
+# ###
+# ..#
+# ###
+# #..
+# ###
+# inputCopy
+# 9 9
+# outputCopy
+# #########
+# ........#
+# #########
+# #........
+# #########
+# ........#
+# #########
+# #........
+# #########
