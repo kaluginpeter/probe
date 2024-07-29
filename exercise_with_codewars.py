35c9b902c93d6a4210797accc178edf6516c2972
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-29 07:15:32 +0300

    Added task 2572 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -63334,7 +63334,7 @@ class Solution:
                 semi_primes += 1
             if semi_prime > r: break
         return (r - l + 1) - semi_primes
-        
+
 # 1395. Count Number of Teams
 # There are n soldiers standing in a line. Each soldier is assigned a unique rating value.
 # 
@@ -63381,3 +63381,35 @@ class Solution:
                 elif rating[k] > rating[j]: right_greater += 1
             teams += left_less * right_greater + left_greater * right_less
         return teams
+        
+# A. Soft Drinking
+# A. Soft Drinking
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# This winter is so cold in Nvodsk! A group of n friends decided to buy k bottles of a soft drink called "Take-It-Light" to warm up a bit. Each bottle has l milliliters of the drink. Also they bought c limes and cut each of them into d slices. After that they found p grams of salt.
+#
+# To make a toast, each friend needs nl milliliters of the drink, a slice of lime and np grams of salt. The friends want to make as many toasts as they can, provided they all drink the same amount. How many toasts can each friend make?
+#
+# Input
+# The first and only line contains positive integers n, k, l, c, d, p, nl, np, not exceeding 1000 and no less than 1. The numbers are separated by exactly one space.
+#
+# Output
+# Print a single integer — the number of toasts each friend can make.
+#
+# Examples
+# inputCopy
+# 3 4 5 10 8 100 3 1
+# outputCopy
+# 2
+# inputCopy
+# 5 100 10 1 19 90 4 3
+# outputCopy
+# 3
+# inputCopy
+# 10 1000 1000 25 23 1 50 1
+# outputCopy
+# 0
+# Note
+# A comment to the first sample:
+#
+# Overall the friends have 4 * 5 = 20 milliliters of the drink, it is enough to make 20 / 3 = 6 toasts. The limes are enough for 10 * 8 = 80 toasts and the salt is enough for 100 / 1 = 100 toasts. However, there are 3 friends in the group, so the answer is min(6, 80, 100) / 3 = 2.
