319850991b5ce2b58f797bd90ec545626df42414
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-31 17:50:24 +0300

    Added task 2583 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -63828,7 +63828,7 @@ def find_glasses(lst):
     for idx in range(len(lst)):
         if re.search(r'O-+O', lst[idx]):
             return idx
-            
+
 # 144. Binary Tree Preorder Traversal
 # Given the root of a binary tree, return the preorder traversal of its nodes' values.
 # 
@@ -63899,3 +63899,31 @@ class Solution:
             queue.append(cur_root.right)
             queue.append(cur_root.left)
         return order
+        
+# 145. Binary Tree Postorder Traversal
+# Given the root of a binary tree, return the postorder traversal of its nodes' values.
+# 
+#  
+# 
+# Example 1:
+# 
+# 
+# Input: root = [1,null,2,3]
+# Output: [3,2,1]
+# Example 2:
+# 
+# Input: root = []
+# Output: []
+# Example 3:
+# 
+# Input: root = [1]
+# Output: [1]
+#  
+# 
+# Constraints:
+# 
+# The number of the nodes in the tree is in the range [0, 100].
+# -100 <= Node.val <= 100
+#  
+# 
+# Follow up: Recursive solution is trivial, could you do it iteratively?
