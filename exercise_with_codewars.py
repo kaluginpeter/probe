6d209f5370e576036a05c68de53fabbe33db8189
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-31 17:08:04 +0300

    Added task 2581 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -63805,3 +63805,21 @@ def solution(nums: list) -> str:
 if __name__ == '__main__':
     nums: list = list(map(int, sys.stdin.readline().rstrip().split()))
     sys.stdout.write(solution(nums))
+
+# Where Are My Glasses?
+# Oh no! I've lost my glasses, but paradoxically, I need glasses to find my glasses!
+# 
+# Please help me out by showing me the index in the list which contains my glasses. They look like two capital Os, with at least one dash in between!
+# 
+# This means that both O--O and O------------O are valid glasses, but not O----#--O for example!
+# Search thoroughly, you might find my glasses in places such as 'dustO-Odust'
+# Examples
+# ["phone", "O-O", "coins", "keys"] ➞ 1
+# 
+# ["OO", "wallet", "O##O", "O----O"] ➞ 3
+# 
+# ["O--#--O", "dustO---Odust", "more dust"] ➞ 1
+# Notes
+# All lists will include one valid pair of glasses because I swear I dropped them around here somewhere ...
+# All elements in the list are strings.
+# REGULAR EXPRESSIONS
