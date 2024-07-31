a11afcbb7cafea3e2b98c69902813c2af13a1df2
Peter Kalugin <streetworkout200412@gmail.com>
2024-07-31 07:20:51 +0300

    Added task 2580 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -63676,7 +63676,7 @@ class Solution:
 
     def isBalanced(self, root: Optional[TreeNode], current_depth: int = 0) -> bool:
         return self.dfs(root) >= 0
-        
+
 # 1105. Filling Bookcase Shelves
 # You are given an array books where books[i] = [thicknessi, heighti] indicates the thickness and height of the ith book. You are also given an integer shelfWidth.
 #
@@ -63733,3 +63733,60 @@ class Solution:
                 min_heights[book_idx] = min(min_heights[book_idx], min_heights[prev_book] + current_height)
 
         return min_heights[len(books)]
+        
+# A. Restoring Three Numbers
+# A. Restoring Three Numbers
+# time limit per test1 second
+# memory limit per test256 megabytes
+# Polycarp has guessed three positive integers a
+# , b
+#  and c
+# . He keeps these numbers in secret, but he writes down four numbers on a board in arbitrary order — their pairwise sums (three numbers) and sum of all three numbers (one number). So, there are four numbers on a board in random order: a+b
+# , a+c
+# , b+c
+#  and a+b+c
+# .
+# 
+# You have to guess three numbers a
+# , b
+#  and c
+#  using given numbers. Print three guessed integers in any order.
+# 
+# Pay attention that some given numbers a
+# , b
+#  and c
+#  can be equal (it is also possible that a=b=c
+# ).
+# 
+# Input
+# The only line of the input contains four positive integers x1,x2,x3,x4
+#  (2≤xi≤109
+# ) — numbers written on a board in random order. It is guaranteed that the answer exists for the given number x1,x2,x3,x4
+# .
+# 
+# Output
+# Print such positive integers a
+# , b
+#  and c
+#  that four numbers written on a board are values a+b
+# , a+c
+# , b+c
+#  and a+b+c
+#  written in some order. Print a
+# , b
+#  and c
+#  in any order. If there are several answers, you can print any. It is guaranteed that the answer exists.
+# 
+# Examples
+# inputCopy
+# 3 6 5 4
+# outputCopy
+# 2 1 3
+# inputCopy
+# 40 40 40 60
+# outputCopy
+# 20 20 20
+# inputCopy
+# 201 101 101 200
+# outputCopy
+# 1 100 100
