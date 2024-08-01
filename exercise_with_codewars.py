b628f29969eb021b6fa4c7ac3c514056775375a2
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-01 06:51:24 +0300

    Added task 2585 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -63970,7 +63970,7 @@ class Solution:
             elif cur_root:
                 order.append(cur_root.val)
         return order
-        
+
 # 2678. Number of Senior Citizens
 # You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
 # 
@@ -64014,3 +64014,30 @@ class Solution:
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
        return sum(detail[-4:-2] > '60' for detail in details)
+       
+# B. T-primes
+# B. T-primes
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# We know that prime numbers are positive integers that have exactly two distinct positive divisors. Similarly, we'll call a positive integer t Т-prime, if t has exactly three distinct positive divisors.
+#
+# You are given an array of n positive integers. For each of them determine whether it is Т-prime or not.
+#
+# Input
+# The first line contains a single positive integer, n (1 ≤ n ≤ 105), showing how many numbers are in the array. The next line contains n space-separated integers xi (1 ≤ xi ≤ 1012).
+#
+# Please, do not use the %lld specifier to read or write 64-bit integers in С++. It is advised to use the cin, cout streams or the %I64d specifier.
+#
+# Output
+# Print n lines: the i-th line should contain "YES" (without the quotes), if number xi is Т-prime, and "NO" (without the quotes), if it isn't.
+#
+# Examples
+# inputCopy
+# 3
+# 4 5 6
+# outputCopy
+# YES
+# NO
+# NO
+# Note
+# The given test has three numbers. The first number 4 has exactly three divisors — 1, 2 and 4, thus the answer for this number is "YES". The second number 5 has two divisors (1 and 5), and the third number 6 has four divisors (1, 2, 3, 6), hence the answer for them is "NO".
