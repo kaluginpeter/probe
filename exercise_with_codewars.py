f7e7b934a68ab98e314df72cfed29c4025501004
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-03 10:59:54 +0300

    Added task 2591 5kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -64280,7 +64280,7 @@ class Solution:
         for num in storage:
             if storage[num] != 0: return False
         return True
-        
+
 # B. Xenia and Ringroad
 # B. Xenia and Ringroad
 # time limit per test2 seconds
@@ -64328,3 +64328,37 @@ if __name__ == '__main__':
     n, m = map(int, sys.stdin.readline().rstrip().split())
     houses: list = list(map(int, sys.stdin.readline().rstrip().split()))
     sys.stdout.write(solution(n, m, houses))
+
+# Digitwise addition
+# Digitwise addition is a special kind of addition where instead of adding 1 normally to the number, it adds 1 to every digit of that number. If the digit is a 9, we replace it with a "10" without carrying over to the next digit.
+#
+# Examples:
+#
+# 123 -> 234
+#
+# 910 -> 1021
+#
+# 789 -> 8910
+# Program a function that takes two numbers, N and K, and outputs the number of digits in N after applying Digitwise addition K times. Since the answer can be very large, return the answer modulo 10 ** 9 + 7
+#
+# Specifications: 1 <= N <= 10^9, 1 <= K <= 10^5
+#
+# Example 1:
+#
+# digitwise_addition(9812, 2) -> 6
+#
+# # Explanation:
+# # 9812 -> 10923 -> 211034 -> 6 digits
+# Example 2:
+#
+# digitwise_addition(9899, 3) -> 8
+#
+# # Explanation:
+# # 9899 -> 1091010 -> 21102121 -> 32213232 -> 8 digits
+# Subtask 1: K <= 20, 30 tests
+#
+# Subtask 2: K <= 50, 20 tests
+#
+# Subtask 3: K <= 10^5, 50 tests
+#
+# ALGORITHMSMATHEMATICS
