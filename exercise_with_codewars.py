a708603b19e0787bd44757fa769bd7717a81309e
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-04 21:56:42 +0300

    Added task 2601 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -64933,7 +64933,7 @@ def num_blocks(w, l, h):
     term3 = (n1 * n * (2 * n - 1)) // 6
 
     return term1 + term2 + term3
-    
+
 # 443. String Compression
 # Given an array of characters chars, compress it using the following algorithm:
 # 
@@ -65026,3 +65026,26 @@ class Solution:
                     main_index += 1
             left = right
         return main_index
+        
+# 560. Subarray Sum Equals K
+# Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+# 
+# A subarray is a contiguous non-empty sequence of elements within an array.
+# 
+#  
+# 
+# Example 1:
+# 
+# Input: nums = [1,1,1], k = 2
+# Output: 2
+# Example 2:
+# 
+# Input: nums = [1,2,3], k = 3
+# Output: 2
+#  
+# 
+# Constraints:
+# 
+# 1 <= nums.length <= 2 * 104
+# -1000 <= nums[i] <= 1000
+# -107 <= k <= 107
