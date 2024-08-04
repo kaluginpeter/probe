d6e1324a151d3c5df4acc1935927e938c1bb37e3
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-04 18:31:21 +0300

    Added task 2600 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -64903,7 +64903,7 @@ class Solution:
             paths[source].append(destination)
             answer.append(self.dijkstra(0, n - 1, paths))
         return answer
-        
+
 # Blocks in an Irregular Pyramid
 # Consider a pyramid made up of blocks. Each layer of the pyramid is a rectangle of blocks, and the dimensions of these rectangles increment as you descend the pyramid. So, if a layer is a 3x6 rectangle of blocks, then the next layer will be a 4x7 rectangle of blocks. A 1x10 layer will be on top of a 2x11 layer on top of a 3x12 layer, and so on.
 #
@@ -64933,3 +64933,40 @@ def num_blocks(w, l, h):
     term3 = (n1 * n * (2 * n - 1)) // 6
 
     return term1 + term2 + term3
+    
+# 443. String Compression
+# Given an array of characters chars, compress it using the following algorithm:
+# 
+# Begin with an empty string s. For each group of consecutive repeating characters in chars:
+# 
+# If the group's length is 1, append the character to s.
+# Otherwise, append the character followed by the group's length.
+# The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+# 
+# After you are done modifying the input array, return the new length of the array.
+# 
+# You must write an algorithm that uses only constant extra space.
+# 
+#  
+# 
+# Example 1:
+# 
+# Input: chars = ["a","a","b","b","c","c","c"]
+# Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+# Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+# Example 2:
+# 
+# Input: chars = ["a"]
+# Output: Return 1, and the first character of the input array should be: ["a"]
+# Explanation: The only group is "a", which remains uncompressed since it's a single character.
+# Example 3:
+# 
+# Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+# Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+# Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+#  
+# 
+# Constraints:
+# 
+# 1 <= chars.length <= 2000
+# chars[i] is a lowercase English letter, uppercase English letter, digit, or symbol.
