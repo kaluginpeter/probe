7d2f0a3ddc3feea70e2b8e1298418e6fbf53f533
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-05 06:34:32 +0300

    Complete task 2602 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -65098,3 +65098,14 @@ class Solution(object):
 # 1 <= k <= arr.length <= 1000
 # 1 <= arr[i].length <= 5
 # arr[i] consists of lowercase English letters.
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        storage: dict[str, int] = dict()
+        for char in arr:
+            storage[char] = storage.get(char, 0) + 1
+        idx: int = 0
+        for char in storage:
+            if storage[char] == 1:
+                idx += 1
+            if k == idx: return char
+        return ''
