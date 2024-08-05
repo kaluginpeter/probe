c267986604dffbd51448f304828da851d3a884c5
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-05 06:59:21 +0300

    Added task 2604 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -65109,7 +65109,7 @@ class Solution:
                 idx += 1
             if k == idx: return char
         return ''
-        
+
 # A. Remove Smallest
 # A. Remove Smallest
 # time limit per test1 second
@@ -65205,3 +65205,24 @@ def solution(t: int) -> None:
 if __name__ == '__main__':
     t: int = int(sys.stdin.readline().rstrip())
     solution(t)
+
+# Node Mania
+# This kata is about singly-linked lists. A linked list is an ordered set of data elements, each containing a link to its successor (and sometimes its predecessor, known as a double linked list). You are you to implement an algorithm to find the kth to last element.
+#
+# k will be an integer greater than or equal to 1.
+#
+# Example
+# For example given the following linked list: a -> b -> c -> d
+#
+# if k = 1 then d should be returned
+# if k = 2 then c should be returned
+# if k = 3 then b should be returned
+# if k = 4 then a should be returned
+# if k exceeds the length of the list then None(Python) or null(Java, JavaScript) or false (C) should be returned
+# Each item in the linked list is a Node containing two fields:
+#
+# data - the value of the node
+# next - pointing to the next node in the list, or to a null reference (null/NULL/None, depending on your language) for the last Node.
+# An empty list is represented as a null reference.
+#
+# LINKED LISTSALGORITHMSDATA STRUCTURES
