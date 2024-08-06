62bed36a6a6322861ac992551b1d853631d14e34
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-06 17:15:19 +0300

    Added task 2608 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -65428,3 +65428,12 @@ def solution(n: int) -> str:
 if __name__ == '__main__':
     n: int = int(sys.stdin.readline().rstrip())
     sys.stdout.write(solution(n))
+
+# Levenshtein Distance
+# In information theory and computer science, the Levenshtein distance is a string metric for measuring the difference between two sequences. Informally, the Levenshtein distance between two words is the minimum number of single-character edits (i.e. insertions, deletions or substitutions) required to change one word into the other.
+# 
+# (http://en.wikipedia.org/wiki/Levenshtein_distance)
+# 
+# Your task is to implement a function which calculates the Levenshtein distance for two arbitrary strings.
+# 
+# STRINGSALGORITHMS
