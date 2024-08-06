3c76e6e02d5275a767c65669d357f0620cce438f
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-06 20:22:07 +0300

    Added task 2610 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -65448,7 +65448,7 @@ def levenshtein(a,b):
         if up - i < do: break
         cost = min(cost, sum(x != y for x, y in zip(big[i:], small)))
     return cost + (up - do)
-    
+
 # 22. Generate Parentheses
 # Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 #
@@ -65486,3 +65486,26 @@ class Solution:
         output: list[str] = []
         self.backtrack(n, 0, 0, output, result)
         return result
+        
+# 567. Permutation in String
+# Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
+# 
+# In other words, return true if one of s1's permutations is the substring of s2.
+# 
+#  
+# 
+# Example 1:
+# 
+# Input: s1 = "ab", s2 = "eidbaooo"
+# Output: true
+# Explanation: s2 contains one permutation of s1 ("ba").
+# Example 2:
+# 
+# Input: s1 = "ab", s2 = "eidboaoo"
+# Output: false
+#  
+# 
+# Constraints:
+# 
+# 1 <= s1.length, s2.length <= 104
+# s1 and s2 consist of lowercase English letters.
