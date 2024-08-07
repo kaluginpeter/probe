63535818c1d937270d3e66f996792d5a2e2aca96
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-07 17:03:42 +0300

    Added task 2614 LeetCode Hard

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -65733,3 +65733,26 @@ def frame(score):
         elif y > x:
             second += 1
     return [first, second]
+    
+# 273. Integer to English Words
+# Convert a non-negative integer num to its English words representation.
+# 
+#  
+# 
+# Example 1:
+# 
+# Input: num = 123
+# Output: "One Hundred Twenty Three"
+# Example 2:
+# 
+# Input: num = 12345
+# Output: "Twelve Thousand Three Hundred Forty Five"
+# Example 3:
+# 
+# Input: num = 1234567
+# Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+#  
+# 
+# Constraints:
+# 
+# 0 <= num <= 231 - 1
