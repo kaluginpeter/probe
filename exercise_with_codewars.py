a43e0b5780affdab1d69ccf61165904b9219d6b4
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-08 17:25:54 +0300

    Added task 2618 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -65914,7 +65914,7 @@ class Solution:
             boundary += 1
 
         return output
-        
+
 # A. Sereja and Dima
 # A. Sereja and Dima
 # time limit per test1 second
@@ -65972,3 +65972,14 @@ if __name__ == '__main__':
     n: int = int(sys.stdin.readline().rstrip())
     cards: list = list(map(int, sys.stdin.readline().rstrip().split()))
     sys.stdout.write(solution(n, cards))
+
+# Vector Affinity
+# Calculate the number of items in a vector that appear at the same index in each vector, with the same value.
+#
+#    vector_affinity([1, 2, 3, 4, 5], [1, 2, 2, 4, 3]) # => 0.6
+#    vector_affinity([1, 2, 3], [1, 2, 3]) # => 1.0
+# Affinity value should be realized on a scale of 0.0 to 1.0, with 1.0 being absolutely identical. Two identical sets should always be evaulated as having an affinity or 1.0.
+#
+# Hint: The last example test case holds a significant clue to calculating the affinity correctly.
+#
+# DATA STRUCTURESALGORITHMS
