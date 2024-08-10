e2b79b6dee78fbd56454ff5f73b7b11e55aa6737
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-10 09:53:00 +0300

    Added task 2623 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -66182,7 +66182,7 @@ class Solution:
                 col_rep += 1
             row_rep += 1
         return magic_squares
-        
+
 # 56. Merge Intervals
 # Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
 #
@@ -66248,3 +66248,36 @@ class Solution:
         intervals[main_idx] = [start, end]
         main_idx += 1
         return intervals[:main_idx]
+        
+# 959. Regions Cut By Slashes
+# An n x n grid is composed of 1 x 1 squares where each 1 x 1 square consists of a '/', '\', or blank space ' '. These characters divide the square into contiguous regions.
+#
+# Given the grid grid represented as a string array, return the number of regions.
+#
+# Note that backslash characters are escaped, so a '\' is represented as '\\'.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: grid = [" /","/ "]
+# Output: 2
+# Example 2:
+#
+#
+# Input: grid = [" /","  "]
+# Output: 1
+# Example 3:
+#
+#
+# Input: grid = ["/\\","\\/"]
+# Output: 5
+# Explanation: Recall that because \ characters are escaped, "\\/" refers to \/, and "/\\" refers to /\.
+#
+#
+# Constraints:
+#
+# n == grid.length == grid[i].length
+# 1 <= n <= 30
+# grid[i][j] is either '/', '\', or ' '.
