37d916ea77b9f00282cc9c978283081e84e07126
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-13 18:09:21 +0300

    Added task 2636 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -67095,3 +67095,28 @@ class Solution:
         candidates.sort()
         self.find_comb(candidates, target, 0, [], 0)
         return self.global_combs
+
+# Some (but not all)
+# Some but not all
+# Description
+# Your task is to create a function that given a sequence and a predicate, returns True if only some (but not all) the elements in the sequence are True after applying the predicate
+#
+# Examples
+# some_but_not_all('abcdefg&%$', str.isalpha)
+# >>> True
+#
+# some_but_not_all('&%$=', str.isalpha)
+# >>> False
+#
+# some_but_not_all('abcdefg', str.isalpha)
+# >>> False
+#
+# some_but_not_all([4, 1], lambda x: x>3)
+# >>> True
+#
+# some_but_not_all([1, 1], lambda x: x>3)
+# >>> False
+#
+# some_but_not_all([4, 4], lambda x: x>3)
+# >>> False
+# LISTSSTRINGSFUNDAMENTALS
