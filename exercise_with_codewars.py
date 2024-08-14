ffa1eb8c58358334d092d167c0e8a787db23dbbf
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-14 17:17:26 +0300

    Complete task 2640 LeetCode Hard

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -67267,3 +67267,25 @@ def balanced_parens(n):
 # 2 <= n <= 104
 # 0 <= nums[i] <= 106
 # 1 <= k <= n * (n - 1) / 2
+class Solution:
+    def smallestDistancePair(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        low, high = 0, nums[-1] - nums[0]
+        
+        while low < high:
+            mid: int = low + (high - low) // 2
+            # Case if pairs less that k on middle difference
+            if self.count_pairs(nums, mid) < k:
+                low = mid + 1
+            else:
+                high = mid
+        
+        return low
+
+    def count_pairs(self, nums: List[int], distance: int) -> int:
+        count = left = 0
+        for right in range(1, len(nums)):
+            while nums[right] - nums[left] > distance:
+                left += 1
+            count += right - left
+        return count
