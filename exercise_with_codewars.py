8834758c549f412b8bf5100eac98443250688f87
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-14 06:37:44 +0300

    Added task 2638 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -67162,3 +67162,29 @@ def solution(n: int, m: int, a: int, b: int) -> str:
 if __name__ == '__main__':
     n, m, a, b = map(int, sys.stdin.readline().rstrip().split())
     sys.stdout.write(solution(n, m, a, b))
+
+# B. Borze
+# # B. Borze
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# Ternary numeric notation is quite popular in Berland. To telegraph the ternary number the Borze alphabet is used. Digit 0 is transmitted as «.», 1 as «-.» and 2 as «--». You are to decode the Borze code, i.e. to find out the ternary number given its representation in Borze alphabet.
+#
+# Input
+# The first line contains a number in Borze code. The length of the string is between 1 and 200 characters. It's guaranteed that the given string is a valid Borze code of some ternary number (this number can have leading zeroes).
+#
+# Output
+# Output the decoded ternary number. It can have leading zeroes.
+#
+# Examples
+# inputCopy
+# .-.--
+# outputCopy
+# 012
+# inputCopy
+# --.
+# outputCopy
+# 20
+# inputCopy
+# -..-.--
+# outputCopy
+# 1012
