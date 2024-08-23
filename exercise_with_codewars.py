4f9b823a0b4b973400525ee4eef0ddf960bf8486
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-23 17:35:21 +0300

    Added task 2675 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -69282,3 +69282,12 @@ class Solution:
         current_fraction = self.simplify_fraction(*current_fraction)
 
         return f'{current_fraction[0]}/{current_fraction[1]}'
+
+# Multiples of 3 or 5
+// Multiples of 3 or 5
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+// Additionally, if the number is negative, return 0.
+// Note: If the number is a multiple of both 3 and 5, only count it once.
+// Courtesy of projecteuler.net (Problem 1)
+// MATHEMATICSALGORITHMS
