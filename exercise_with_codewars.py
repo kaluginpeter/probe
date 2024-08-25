005262a58553abc18daa8a6d66d102c1b1d05401
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-25 14:50:30 +0300

    Complete task 2683 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -69844,3 +69844,11 @@ class Solution:
 #
 #
 # Follow up: If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        max_sum: int = nums[0]
+        cur_sum: int = nums[0]
+        for right in range(1, len(nums)):
+            cur_sum = max(cur_sum + nums[right], nums[right])
+            max_sum = max(max_sum, cur_sum)
+        return max_sum
