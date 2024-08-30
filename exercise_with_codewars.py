1ff9a2fd2ac5ad082a29d1ca400dd3f8aa7d4603
Peter Kalugin <streetworkout200412@gmail.com>
2024-08-30 17:38:45 +0300

    Added task 2699 8kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -70939,3 +70939,15 @@ def solution(t: int) -> None:
 if __name__ == '__main__':
     t: int = int(sys.stdin.readline().rstrip())
     solution(t)
+
+
+Find the smallest integer in the array
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+FUNDAMENTALS
