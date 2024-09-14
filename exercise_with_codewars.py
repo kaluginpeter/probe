094999460b7f076e801f1264e99c55476f62f2ac
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-14 12:53:53 +0300

    Complete task 2746 8kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -74325,3 +74325,6 @@ int main() {
 # Volume of a Cuboid
 # Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
 # GEOMETRYFUNDAMENTALSMATHEMATICS
+double getVolumeOfCuboid(double length, double width, double height) {
+  return length * width * height;
+}
