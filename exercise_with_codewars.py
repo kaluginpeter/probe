bd2a3362be008c71a0d620da4d98cf639d46980d
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-16 21:07:16 +0300

    Added task 2756 8kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -74981,3 +74981,18 @@ if __name__ == '__main__':
     t, n = map(int, sys.stdin.readline().rstrip().split())
     books: list = list(map(int, sys.stdin.readline().rstrip().split()))
     sys.stdout.write(solution(t, n, books))
+
+#  Task
+# Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+#
+# The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+#
+# Mind the input validation.
+#
+# Example
+# { 6, 2, 1, 8, 10 } => 16
+# { 1, 1, 11, 2, 3 } => 6
+# Input validation
+# If an empty value ( null, None, Nothing, nil etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+#
+# FUNDAMENTALS
