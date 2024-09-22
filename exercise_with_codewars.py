0bffec25d0b15f48a809b2a96dda23249a84a186
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-22 09:01:11 +0300

    Added task 2779 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -76448,3 +76448,14 @@ int main() {
         std::cout << (n.substr(0, n.size() / 2) == n.substr(n.size() / 2, n.size())? "YES" : "NO") << std::endl;
     }
 }
+
+# Split Strings
+/*
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+Examples:
+
+* 'abc' =>  ['ab', 'c_']
+* 'abcdef' => ['ab', 'cd', 'ef']
+REGULAR EXPRESSIONSSTRINGSALGORITHMS
+*/
