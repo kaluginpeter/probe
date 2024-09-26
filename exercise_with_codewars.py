64514ba05b377dd2075a48035054c80ab87f0ec2
Peter Kalugin <streetworkout200412@gmail.com>
2024-09-26 18:18:46 +0300

    Complete task 2793 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -77396,3 +77396,14 @@ if __name__ == '__main__':
 # Each block falls individually, meaning there are no rigid objects. Think about it like falling sand in Minecraft as opposed to the rigid blocks in Tetris.
 #
 # ALGORITHMSMATRIXARRAYS
+def switch_gravity(lst):
+    for cur_col in range(len(lst[0])):
+        cur_row: int = len(lst) - 1
+        while cur_row >= 0 and lst[cur_row][cur_col] == '#':
+            cur_row -= 1
+        if cur_row < 0: continue
+        for row in range(cur_row, -1, -1):
+            if lst[row][cur_col] == '#':
+                lst[cur_row][cur_col], lst[row][cur_col] = lst[row][cur_col], lst[cur_row][cur_col]
+                cur_row -= 1
+    return lst
