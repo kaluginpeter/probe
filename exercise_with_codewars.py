b9a7fde6c972bbc920eafe56223ba7acb8951922
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-08 18:00:11 +0300

    Added task 2833 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -80750,3 +80750,16 @@ class Solution:
                 right -= 1
             left += 1
         return swaps
+
+# Matrix Exponentiation
+# Your task in this kata is to write a function which will take a square matrix and a non-negative integer n as inputs and return a matrix raised to the power n.
+#
+# Be ready to handle big ns.
+#
+# Example:
+#
+# A = [[1, 2], [1, 0]]
+# calc(A, 2) = [[3, 2], [1, 2]]
+# External aiding modules disabled
+#
+# ARRAYSMATRIXALGORITHMSLOGIC
