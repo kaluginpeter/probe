c99cf709f1b623012f8afead67fafc732cb652e7
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-09 17:52:54 +0300

    Added task 2836 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -80971,3 +80971,24 @@ class Solution:
                 count += 1
                 close_br -= 1
         return count + max(open_br - close_br, 0)
+
+# Find the smallest power higher than a given a value
+/*
+We have the number 12385 . We want to know the value of the closest cube but higher than 12385. The answer will be 13824 .
+
+Now, another case. We have the number 1245678 . We want to know the 5th power, closest and higher than that number. The value will be 1419857.
+
+We need a function find_next_power or findNextPower that receives two arguments, a value val , and the exponent of the power,  pow_ , and outputs the value that we want to find.
+
+Let'see some cases:
+
+(12385, 3) ==> 13824
+(1245678, 5) ==> 1419857
+The value, val will be always a positive integer.
+
+The power, pow_ , always higher than 1 .
+
+Happy coding!!
+
+FUNDAMENTALSMATHEMATICSLOGIC
+*/
