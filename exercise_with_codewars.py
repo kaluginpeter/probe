a26e105a0a9e219edc1d3021c162b15b6a339510
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-13 14:09:02 +0300

    Complete task 2860 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -82776,3 +82776,69 @@ public:
 #
 # How can we prove that at least one duplicate number must exist in nums?
 # Can you solve the problem in linear runtime complexity?
+# Solution 1 - HashTable
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        d = {}
+        for i in nums:
+            if i in d:
+                return i
+            else:
+                d[i] = 1
+
+# Solution 2 - HashSet
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        s = set()
+        for i in nums:
+            if i in s:
+                return i
+            else:
+                s.add(i)
+# Solution 3 - Floyd Circle Algorithm
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = nums[0], nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+        slow = nums[0]
+        while slow != fast:
+            slow, fast = nums[slow], nums[fast]
+        return slow
+
+
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        array: list[bool] = [True] * len(nums)
+        for i in nums:
+            if array[i-1]:
+                array[i-1] = False
+            else:
+                return i
+
+# Python O(N) O(1) In place
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        num: int = nums[0]
+        while nums[num]:
+            next_num: int = nums[num]
+            nums[num] = 0
+            num = next_num
+        return num
+
+# C++ O(N) O(1) In place
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int num = nums[0];
+        while (nums[num]) {
+            int next_num = nums[num];
+            nums[num] = 0;
+            num = next_num;
+        }
+        return num;
+    }
+};
