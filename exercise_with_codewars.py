ac7842339def3b269ac7202650d1100ccb6a9dda
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-13 10:55:02 +0300

    Complete task 2855 8kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -82442,3 +82442,11 @@ Given a non-empty array of integers, return the result of multiplying the values
 [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
 FUNDAMENTALSARRAYS
 */
+#include <vector>
+int grow(std::vector<int> nums) {
+  int accumulate = 1;
+  for (int num : nums) {
+    accumulate *= num;
+  }
+  return accumulate;
+}
