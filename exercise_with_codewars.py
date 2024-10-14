4bd2fc770019f11becd4c0f657e36429a0ba18f7
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-14 07:27:13 +0300

    Complete task 2863 8kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -83046,3 +83046,14 @@ Quadrants (this kata)
 Quadrants 2: Segments
 FUNDAMENTALSMATHEMATICSGEOMETRY
 */
+int quadrant(int x, int y) {
+  if (x < 0 && y < 0) {
+    return 3;
+  } else if (x < 0 && y > 0) {
+    return 2;
+  } else if (x > 0 && y < 0) {
+    return 4;
+  } else {
+    return 1;
+  }
+}
