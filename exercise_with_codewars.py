935e0d9067e008192d52691470245030c24da1fa
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-23 15:37:51 +0300

    Complete task 2913 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -87948,3 +87948,60 @@ public:
 # The number of nodes in the subRoot tree is in the range [1, 1000].
 # -104 <= root.val <= 104
 # -104 <= subRoot.val <= 104
+# C++ O(NM) O(N) Depth-First-Search
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool checkMatch(TreeNode* x, TreeNode* y) {
+        if (!x || !y) {
+            return !x && !y;
+        }
+        if (x->val != y->val) {
+            return false;
+        }
+        return checkMatch(x->left, y->left) && checkMatch(x->right, y->right);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (!root) {
+            return false;
+        }
+        bool isEqual = false;
+        if (root->val == subRoot->val) {
+            isEqual = checkMatch(root, subRoot);
+        }
+        return isEqual || isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+};
+
+# Python O(NM) O(N) Depth-First-Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def check_match(self, x: Optional[TreeNode], y: Optional[TreeNode]) -> bool:
+        if not x or not y:
+            return not x and not y
+        if x.val != y.val:
+            return False
+        return self.check_match(x.left, y.left) and self.check_match(x.right, y.right)
+
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not root:
+            return False
+        is_equal: bool = False
+        if root.val == subRoot.val:
+            is_equal = self.check_match(root, subRoot)
+        return is_equal or self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
