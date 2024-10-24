d93e60a402b087954e650ae6f2919316adba95c6
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-24 07:35:40 +0300

    Added task 2917 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -88425,3 +88425,19 @@ def solution(t: int) -> None:
 if __name__ == '__main__':
     t: int = int(sys.stdin.readline().rstrip())
     solution(t)
+
+# Make the Deadfish Swim
+/*
+Write a simple parser that will parse and run Deadfish.
+
+Deadfish has 4 commands, each 1 character long:
+
+i increments the value (initially 0)
+d decrements the value
+s squares the value
+o outputs the value into the return array
+Invalid characters should be ignored.
+
+parse("iiisdoso") => [ 8, 64 ]
+PARSINGALGORITHMS
+*/
