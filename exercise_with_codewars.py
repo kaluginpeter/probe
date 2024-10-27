ee58b67cd36931ebd054ebadd98809fba1bfb70f
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-27 13:46:10 +0300

    Added task 2931 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -89950,3 +89950,15 @@ def solution(t: int) -> None:
 if __name__ == '__main__':
     t: int = int(sys.stdin.readline().rstrip())
     solution(t)
+
+# Sort the odd
+/*
+Task
+You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even numbers at their original positions.
+
+Examples
+[7, 1]  =>  [1, 7]
+[5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+FundamentalsArraysSorting
+*/
