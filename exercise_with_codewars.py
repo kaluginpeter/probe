634c04078d6ed8442a188df88d2527070089a738
Peter Kalugin <streetworkout200412@gmail.com>
2024-10-31 19:26:33 +0300

    Added task 2943 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -90889,3 +90889,21 @@ def solution(t: int) -> None:
 if __name__ == '__main__':
     t: int = int(sys.stdin.readline().rstrip())
     solution(t)
+
+
+# Sum of odd numbers
+/*
+Given the triangle of consecutive odd numbers:
+
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+1 -->  1
+2 --> 3 + 5 = 8
+ArraysListsMathematicsFundamentals
+*/
