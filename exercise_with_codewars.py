e90c81d52e7a06e946422e4d68eb0213c4d985d1
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-05 18:09:41 +0300

    Complete task 2958 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -91883,3 +91883,16 @@ Input -> Output
 1,2,9 -> false
 MathematicsFundamentals
 */
+namespace Triangle
+{
+  bool isTriangle(int a, int b, int c)
+  {
+    if (a == 0 || b == 0 || c == 0) {
+      return false;
+    }
+    if (a + b > c && a + c > b && b + c > a) {
+      return true;
+    }
+    return false;
+  }
+};
