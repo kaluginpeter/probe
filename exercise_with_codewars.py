6126f78b874d225c2ed449981430427c249291d0
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-10 12:00:01 +0300

    Added task 2979 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -93395,3 +93395,28 @@ if __name__ == '__main__':
 def adjacent_double_double_letters(word):
     if len(word) < 4: return False
     return any(word[idx - 3] == word[idx - 2] and word[idx - 1] == word[idx] for idx in range(3, len(word)))
+
+# 46. Permutations
+# Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [1,2,3]
+# Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+# Example 2:
+#
+# Input: nums = [0,1]
+# Output: [[0,1],[1,0]]
+# Example 3:
+#
+# Input: nums = [1]
+# Output: [[1]]
+#
+#
+# Constraints:
+#
+# 1 <= nums.length <= 6
+# -10 <= nums[i] <= 10
+# All the integers of nums are unique.
