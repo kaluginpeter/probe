c8285f2ed66ce06fc1e15ffc306d4dfed0f9d114
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-13 17:55:58 +0300

    Added task 2988 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -94184,3 +94184,10 @@ public:
         return output;
     }
 };
+
+# Sum of angles
+/*
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
+
+GeometryFundamentals
+*/
