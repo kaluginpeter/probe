adee17def54ccbc46b8c3ca64d6395a80feb03c4
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-20 17:36:58 +0300

    Added task 3020 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -96997,3 +96997,52 @@ def make_chocolates(small, big, goal):
     if goal // 2 > small:
         return -1
     return goal // 2
+
+# B. Ordinary Numbers
+# B. Ordinary Numbers
+# time limit per test2 seconds
+# memory limit per test256 megabytes
+# Let's call a positive integer n
+#  ordinary if in the decimal notation all its digits are the same. For example, 1
+# , 2
+#  and 99
+#  are ordinary numbers, but 719
+#  and 2021
+#  are not ordinary numbers.
+#
+# For a given number n
+# , find the number of ordinary numbers among the numbers from 1
+#  to n
+# .
+#
+# Input
+# The first line contains one integer t
+#  (1≤t≤104
+# ). Then t
+#  test cases follow.
+#
+# Each test case is characterized by one integer n
+#  (1≤n≤109
+# ).
+#
+# Output
+# For each test case output the number of ordinary numbers among numbers from 1
+#  to n
+# .
+#
+# Example
+# InputCopy
+# 6
+# 1
+# 2
+# 3
+# 4
+# 5
+# 100
+# OutputCopy
+# 1
+# 2
+# 3
+# 4
+# 5
+# 18
