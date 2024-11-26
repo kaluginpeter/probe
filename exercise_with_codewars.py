461b85c35dd763b0c4b38d54e14594ba1cf4a118
Peter Kalugin <streetworkout200412@gmail.com>
2024-11-26 17:33:55 +0300

    Complete task 3041 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -98893,3 +98893,8 @@ public:
 # alt text
 #
 # Fundamentals
+import math
+def collision(x1, y1, radius1, x2, y2, radius2):
+    distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    radius_sum = radius1 + radius2
+    return distance <= radius_sum
