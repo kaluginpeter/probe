780df657891a5c5f5fdcc1824662bc44ce35a466
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-02 17:17:41 +0300

    Added task 3063 Codeforces

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -100629,3 +100629,34 @@ public:
 def build_or_buy(hand):
     stack: dict[str, str] = {'road': 'bw','settlement': 'bgws','city': 'ggooo','development': 'gos'}
     return [name for name, quant in stack.items() if all(it in hand and quant.count(it) <= hand.count(it) for it in quant)]
+
+# A. Arpa’s hard exam and Mehrdad’s naive cheat
+# A. Arpa’s hard exam and Mehrdad’s naive cheat
+# time limit per test1 second
+# memory limit per test256 megabytes
+# There exists an island called Arpa’s land, some beautiful girls live there, as ugly ones do.
+#
+# Mehrdad wants to become minister of Arpa’s land. Arpa has prepared an exam. Exam has only one question, given n, print the last digit of 1378n.
+#
+#
+# Mehrdad has become quite confused and wants you to help him. Please help, although it's a naive cheat.
+#
+# Input
+# The single line of input contains one integer n (0  ≤  n  ≤  109).
+#
+# Output
+# Print single integer — the last digit of 1378n.
+#
+# Examples
+# InputCopy
+# 1
+# OutputCopy
+# 8
+# InputCopy
+# 2
+# OutputCopy
+# 4
+# Note
+# In the first example, last digit of 13781 = 1378 is 8.
+#
+# In the second example, last digit of 13782 = 1378·1378 = 1898884 is 4.
