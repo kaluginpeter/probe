b47a5b8ac52e714c4bd4ad1e28a3e2a620bd59ce
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-11 17:18:33 +0300

    Added task 3095 8kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -103256,3 +103256,14 @@ int main() {
     std::cin >> t;
     solution(t);
 }
+
+# Playing with cubes I
+/*
+Create a public class called Cube without a constructor which gets one single private integer variable Side, a getter GetSide() and a setter SetSide(int num) method for this property. Actually, getter and setter methods are not the common way to write this code in C#. In the next kata of this series, we gonna refactor the code and make it a bit more professional...
+
+Notes:
+
+There's no need to check for negative values!
+initialise the side to 0.
+MathematicsFundamentals
+*/
