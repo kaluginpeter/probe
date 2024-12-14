5cbdfef393a0815716d83fd912a40eec2087b752
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-14 11:05:31 +0300

    Added task 3104 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -104106,3 +104106,16 @@ def solution(t: int) -> None:
 if __name__ == '__main__':
     t: int = int(sys.stdin.readline().rstrip())
     solution(t)
+
+# Find the index of the second occurrence of a letter in a string
+/*
+In this kata, you need to write a function that takes a string and a letter as input and then returns the index of the second occurrence of that letter in the string. If there is no such letter in the string, or if the letter occurs only once in the string, -1 should be returned.
+
+Examples:
+
+for inputs  "Hello world!!!", 'l'  ->  return 3
+for inputs  "Hello world!!!", 'A'  ->  return -1
+Good luck ;) And don't forget to rate this kata if you liked it.
+
+FundamentalsStrings
+*/
