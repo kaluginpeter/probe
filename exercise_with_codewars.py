ff6d30b354ff35e35f01276d89bb11b9eba4feff
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-18 07:12:44 +0300

    Added task 3123 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -105740,3 +105740,23 @@ def solution(t: int) -> None:
 if __name__ == '__main__':
     t: int = int(sys.stdin.readline().rstrip())
     solution(t)
+
+# GPS coordinate conversions - DD to DMS
+# Convert DD (decimal degrees) position to DMS (degrees, minutes, seconds).
+#
+# Inputs:
+# dd_lat and dd_lon 2 floats representing the latitude and the longitude in degree -i.e. 2 floats included in [-90, 90] and [-180, 180]. Note that latitude 0 is north, longitude 0 is east.
+#
+# Outputs:
+# A tuple of DMS latitudes formated as follows: DDD*mm'ss.sss"C
+#
+# With:
+#
+# DDD: degrees
+# mm: minutes
+# ss.sss: seconds rounded to 3 decimals
+# C: first letter uppercase of the cardinal direction
+# ressources
+# about WGS 84 on Wikipedia
+#
+# StringsFundamentals
