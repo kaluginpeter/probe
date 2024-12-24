0723c06deecb4ef2b8a7c011f65c3dc6b88fbd76
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-24 17:53:41 +0300

    Added task 3149 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -107840,3 +107840,22 @@ public:
         return std::max(diameter1, std::max(diameter2, combinedDiameter));
     }
 };
+
+# Group prime numbers
+# Write a function generator that will generate the first n primes grouped in tuples of size m. If there are not enough primes for the last tuple it will have the remaining values as None.
+#
+# Examples
+# For n = 11 and m = 2:
+# (2, 3), (5, 7), (11, 13), (17, 19), (23, 29), (31, None)
+#
+# For n = 11 and m = 3:
+# (2, 3, 5), (7, 11, 13), (17, 19, 23), (29, 31, None)
+#
+# For n = 11 and m = 5:
+# (2, 3, 5, 7, 11), (13, 17, 19, 23, 29), (31, None, None, None, None)
+#
+# For n = 3 and m = 1:
+# (2,), (3,), (5,)
+# Note: large numbers of n will be tested, up to 50000
+#
+# AlgorithmsFundamentals
