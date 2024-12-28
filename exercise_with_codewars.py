2a1bd90c6ab3217a79889fbefbe5988cbe305d47
Peter Kalugin <streetworkout200412@gmail.com>
2024-12-28 14:19:49 +0300

    Added task 3162 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -108905,3 +108905,17 @@ def solution() -> None:
 
 if __name__ == '__main__':
     solution()
+
+# Adding Binary Numbers
+# ##Task: You have to write a function add which takes two binary numbers as strings and returns their sum as a string.
+#
+# ##Note:
+#
+# You are not allowed to convert binary to decimal & vice versa.
+# The sum should contain No leading zeroes.
+# ##Examples:
+#
+# add('111','10'); => '1001'
+# add('1101','101'); => '10010'
+# add('1101','10111') => '100100'
+# BinaryBitsFundamentals
