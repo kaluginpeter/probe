9b8325f322cf9775d42d0cb0ac0f6b26592b6b30
kaluginpeter <streetworkout200412@gmail.com>
2025-01-08 15:04:19 +0300

    Added task 3200 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -111976,3 +111976,53 @@ def egged(year, span):
                 coop_idx += 1
         coops.extend([[300, span], [300, span], [300, span]])
     return total_eggs
+
+# 3407. Substring Matching Pattern
+# You are given a string s and a pattern string p, where p contains exactly one '*' character.
+#
+# The '*' in p can be replaced with any sequence of zero or more characters.
+#
+# Return true if p can be made a
+# substring
+#  of s, and false otherwise.
+#
+#
+#
+# Example 1:
+#
+# Input: s = "leetcode", p = "ee*e"
+#
+# Output: true
+#
+# Explanation:
+#
+# By replacing the '*' with "tcod", the substring "eetcode" matches the pattern.
+#
+# Example 2:
+#
+# Input: s = "car", p = "c*v"
+#
+# Output: false
+#
+# Explanation:
+#
+# There is no substring matching the pattern.
+#
+# Example 3:
+#
+# Input: s = "luck", p = "u*"
+#
+# Output: true
+#
+# Explanation:
+#
+# The substrings "u", "uc", and "uck" match the pattern.
+#
+#
+#
+# Constraints:
+#
+# 1 <= s.length <= 50
+# 1 <= p.length <= 50
+# s contains only lowercase English letters.
+# p contains only lowercase English letters and exactly one '*'
