ba34decf1a1c40ed0d2a11786b41d9cad44e14b1
kaluginpeter <streetworkout200412@gmail.com>
2025-01-29 17:41:05 +0300

    Added task 3265 LeetCode Medium

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -117179,3 +117179,34 @@ int main() {
 def sort_grades(lst):
     pos: dict[str, int] = {'VB': -1, 'V0': 1, 'V0+': 2,}
     return sorted(lst, key=lambda x: (pos.get(x, 2), (int(x[1:]) if x not in {'VB', 'V0+'} else 0)))
+
+# 684. Redundant Connection
+# In this problem, a tree is an undirected graph that is connected and has no cycles.
+#
+# You are given a graph that started as a tree with n nodes labeled from 1 to n, with one additional edge added. The added edge has two different vertices chosen from 1 to n, and was not an edge that already existed. The graph is represented as an array edges of length n where edges[i] = [ai, bi] indicates that there is an edge between nodes ai and bi in the graph.
+#
+# Return an edge that can be removed so that the resulting graph is a tree of n nodes. If there are multiple answers, return the answer that occurs last in the input.
+#
+#
+#
+# Example 1:
+#
+#
+# Input: edges = [[1,2],[1,3],[2,3]]
+# Output: [2,3]
+# Example 2:
+#
+#
+# Input: edges = [[1,2],[2,3],[3,4],[1,4],[1,5]]
+# Output: [1,4]
+#
+#
+# Constraints:
+#
+# n == edges.length
+# 3 <= n <= 1000
+# edges[i].length == 2
+# 1 <= ai < bi <= edges.length
+# ai != bi
+# There are no repeated edges.
+# The given graph is connected.
