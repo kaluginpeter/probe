e4911ab37e26125f4cc61fad80f6655f165dc585
kaluginpeter <streetworkout200412@gmail.com>
2026-07-26 12:35:35 +0300

    Complete task 4659 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -248941,3 +248941,19 @@ public:
 #
 # 3 <= nums.length <= 104
 # -1000 <= nums[i] <= 1000
+
+# Python O(NlogN) O(1) Sorting Math
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        return max(nums[0] * nums[1] * nums[-1], nums[-1] * nums[-2] * nums[-3])
+
+# C++ O(NlogN) O(1) Math Sorting
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        size_t n = nums.size();
+        return std::max(nums[0] * nums[1] * nums[n - 1], nums[n - 1] * nums[n - 2] * nums[n - 3]);
+    }
+};
