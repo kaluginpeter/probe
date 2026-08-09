59ed76996026294bb6fea81a1e2900a9219f9d61
kaluginpeter <streetworkout200412@gmail.com>
2026-08-09 21:26:41 +0300

    Complete task 4687 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -250753,3 +250753,14 @@ Courtesy of projecteuler.net (Problem 1)
 
 MathematicsAlgorithms
 */
+package kata
+
+func Multiple3And5(number int) int {
+  var output int = 0
+  for i := 3; i < number; i++ {
+    if (i % 3 == 0 || i % 5 == 0) {
+      output += i
+    }
+  }
+  return output
+}
