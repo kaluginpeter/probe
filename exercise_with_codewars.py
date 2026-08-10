55d46b88f5b769b42f70e08364a4590abdfac263
kaluginpeter <streetworkout200412@gmail.com>
2026-08-10 19:29:31 +0300

    Complete task 4689 LeetCode Hard

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -250862,3 +250862,19 @@ Constraints:
 
 1 <= n <= 105
 
+static constexpr int MAX = 100000;
+class Solution {
+public:
+    inline static std::bitset<MAX + 1> dp;
+    inline static bool init = []() {
+        for (int i = 0; i <= MAX; ++i) {
+            if (dp.test(i)) continue;
+            for (int j = 1; j * j <= MAX - i; ++j) dp.set(i + j * j);
+        }
+        return false;
+    }();
+
+    bool winnerSquareGame(int n) {
+        return dp.test(n);
+    }
+};
