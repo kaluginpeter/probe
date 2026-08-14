256929d592186e10244e668b08c81a085f66aa6e
kaluginpeter <streetworkout200412@gmail.com>
2026-08-14 18:27:30 +0300

    Complete task 4698 LeetCode Easy

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -251420,3 +251420,54 @@ func Merge(a <-chan string, b <-chan string) <-chan string {
 #
 # 2 <= s.length <= 100
 # s consists only of lowercase English letters.
+
+# Python O(N) O(D) SlidingWindow
+from array import array
+class Solution:
+    def maximumLengthSubstring(self, s: str) -> int:
+        n: int = len(s)
+        left: int = 0
+        output: int = 0
+        hashmap: list[int] = array('H', [0]) * 26
+        for right in range(n):
+            hashmap[ord(s[right]) - 97] += 1
+            while hashmap[ord(s[right]) - 97] > 2:
+                hashmap[ord(s[left]) - 97] -= 1
+                left += 1
+            output = max(output, right - left + 1)
+        return output
+
+# C++ O(N) O(D) SlidingWindow
+class Solution {
+public:
+    int maximumLengthSubstring(string s) {
+        size_t n = s.size(), left = 0, output = 0;
+        std::array<int, 26> hashmap{};
+        for (size_t right = 0; right < n; ++right) {
+            ++hashmap[s[right] - 'a'];
+            while (hashmap[s[right] - 'a'] > 2) {
+                --hashmap[s[left] - 'a'];
+                ++left;
+            }
+            output = std::max(output, right - left + 1);
+        }
+        return output;
+    }
+};
+
+# Go O(N) O(D) SlidingWindow
+func maximumLengthSubstring(s string) int {
+    var n int = len(s)
+    var left int = 0
+    var output int = 0
+    var hashmap [26]int
+    for right := 0; right < n; right++ {
+        hashmap[s[right] - 'a']++
+        for hashmap[s[right] - 'a'] > 2 {
+            hashmap[s[left] - 'a']--
+            left++
+        }
+        output = max(output, right - left + 1)
+    }
+    return output
+}
