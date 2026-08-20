62d9209ed65ce8a2e45ef95cc5235db020305f44
kaluginpeter <streetworkout200412@gmail.com>
2026-08-20 23:08:03 +0300

    Added task 4709 7kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -252360,3 +252360,20 @@ std::vector<int> mountain(const std::vector<int>& arr_in) {
 
     return arr;
 }
+
+
+//
+Find the next perfect square!
+/*
+You might know some pretty large perfect squares. But what about the NEXT one?
+
+Complete the findNextSquare method that finds the next integral perfect square after the one passed as a parameter. Recall that an integral perfect square is an integer n such that sqrt(n) is also an integer.
+
+If the argument is itself not a perfect square then return either -1 or an empty value like None or null, depending on your language. You may assume the argument is non-negative.
+
+Examples ( Input --> Output )
+121 --> 144
+625 --> 676
+114 --> -1  #  because 114 is not a perfect square
+AlgebraFundamentals
+*/
