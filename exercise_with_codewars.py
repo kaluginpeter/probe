4eb1298015bef9bb4996cb07388f02140ad2cc9b
Peter Kalugin <streetworkout200412@gmail.com>
2026-09-07 19:22:54 +0300

    Added task 4744 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -254883,3 +254883,15 @@ func Solution(ar []int) int {
 	}
 	return g * len(ar)
 }
+
+// Stop gninnipS My sdroW!
+/*
+Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (just like the name of this kata). Strings passed in will consist of only letters and spaces. Words will be separated by exactly one space. There will be no leading or trailing spaces.
+
+Examples:
+
+"Hey fellow warriors"  --> "Hey wollef sroirraw" 
+"This is a test        --> "This is a test" 
+"This is another test" --> "This is rehtona test"
+StringsAlgorithms
+*/
