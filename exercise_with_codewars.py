8253152814638886969591f100440252b875102f
Peter Kalugin <streetworkout200412@gmail.com>
2026-09-09 17:15:26 +0300

    Complete taks 4749 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -255175,3 +255175,17 @@ Examples
 
 Fundamentals
 */
+package kata
+
+func FindOdd(seq []int) int {
+    hashmap := map[int]int{}
+    for _, num := range seq {
+        hashmap[num]++
+    }
+    for num, freq := range hashmap {
+        if freq & 1 == 1 {
+            return num
+        }
+    }
+    return -1
+}
