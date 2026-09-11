38c36b2e444a98a9165649e840ecc503cf3daade
Peter Kalugin <streetworkout200412@gmail.com>
2026-09-11 15:32:12 +0300

    Added task 4752 6kyu

--- exercise_with_codewars.py
+++ exercise_with_codewars.py
@@ -255333,3 +255333,12 @@ func FindEvenIndex(arr []int) int {
   }
   return -1
 }
+
+// Bit Counting
+/*
+Write a function that takes an integer as input, and returns the number of bits that are equal to one in the binary representation of that number. You can guarantee that input is non-negative.
+
+Example: The binary representation of 1234 is 10011010010, so the function should return 5 in this case
+
+BitsAlgorithms
+*/
